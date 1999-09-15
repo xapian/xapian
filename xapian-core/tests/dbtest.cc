e97e09b3d640375aa95ea8e7cb40c4debdf5b657
Richard Boulton <richard@tartarus.org>
1999-09-15 11:59:55 +0000

    ProtoDatabase now has a complete, but dummy, implementation. (Doesn't yet read data from any files)
    git-svn-id: file:///home/xapian-svn/xapian/trunk@21 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/dbtest.cc
+++ xapian-core/tests/dbtest.cc
@@ -3,10 +3,19 @@
 
 int main(int argc, char *argv[]) {
     ProtoDatabase database;
+    ProtoPostListIterator * postlist;
 
     try {
 	database.open("testdir", 0);
-	database.open_post_list(0);
+	postlist = database.open_post_list(0);
+	while(!postlist->at_end()) {
+	    docid id;
+	    id = postlist->get_docid();
+	    printf("DocId: %d\n", id);
+
+	    postlist->next();
+	}
+	postlist->close();
 	database.close();
     }
     catch (OmError e) {
