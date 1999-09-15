51b578a9e4db6e2c43243ba3db567f8f2394bddc
Richard Boulton <richard@tartarus.org>
1999-09-15 18:18:27 +0000

    Code to insert an item into a postlist is there.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@30 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/dbtest.cc
+++ xapian-core/tests/dbtest.cc
@@ -6,15 +6,15 @@
 int main(int argc, char *argv[]) {
     ProtoDatabase database;
     PostListIterator * postlist;
+    termid tid;
 
     try {
 	database.open("testdir", 0);
-	postlist = database.open_post_list(1);
+	tid = database.term_name_to_id("2");
+	postlist = database.open_post_list(tid);
 	while(!postlist->at_end()) {
 	    docid did;
-	    termid tid;
 
-	    tid = database.term_name_to_id("1");
 	    did = postlist->get_docid();
 	    printf("TermId: %d  DocId: %d\n", tid, did);
 	    if(did == 2) postlist->skip_to(5);
