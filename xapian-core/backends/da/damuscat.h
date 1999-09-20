6cf475e1f7a31dc230cafc4f5faa0627421a4b65
Richard Boulton <richard@tartarus.org>
1999-09-20 11:02:22 +0000

    Changed name to keep olly happy(ier).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@69 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/backends/da/damuscat.h
@@ -0,0 +1,31 @@
+#define until(a)    while(!(a))
+#define unless(a)   if(!(a))
+#define repeat      for(;;)
+#define eq          ==
+#define ne          !=
+#define and         &&
+#define or          ||
+#define not         !
+#define true        1
+#define false       0
+
+typedef  unsigned char  byte;
+typedef  int  filehandle;     /* would be 'FILE *' in ANSI C */
+
+#define BYTERANGE   256
+
+/* next 2 defs are different in HDM (Heavy Duty Muscat) */
+
+#define LOF(p,c)    ((p)[(c)+1] << 8 | (p)[(c)])
+#define LWIDTH      2         /* bytes in a Muscat length (3 in HDM) */
+
+#define MAXINT      0X7FFFFFFF
+#define DATERMS     10101     /* word used to identify DA index files */
+#define DARECS      23232     /* word used to identify DA record files */
+#define TVSTART     (LWIDTH+1)
+#define TVSIZE(p,c) (LOF(p,c)+1)
+#define ILEN 4
+#define L2(p,c) ((p)[(c)+1] << 8 | (p)[(c)])
+
+
+
