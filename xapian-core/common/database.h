33b34e41dbbd960677f189c1f6d0d91d5d7ce488
Richard Boulton <richard@tartarus.org>
1999-09-20 10:44:48 +0000

    Moved exception definitions into a separate file.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@68 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/database.h
+++ xapian-core/common/database.h
@@ -10,30 +10,7 @@
 #include <vector>
 
 #include "omtypes.h"
-
-class OmError {
-    private:
-        string msg;
-    public:
-        OmError(string error_msg)
-        {
-            msg = error_msg;
-        }
-        string get_msg()
-        {
-            return msg;
-        }
-};
-
-class RangeError : public OmError {
-    public:
-	RangeError(string msg) : OmError(msg) {};
-};
-
-class OpeningError : public OmError {
-    public:
-        OpeningError(string msg) : OmError(msg) {};
-};
+#include "error.h"
 
 class PostList {
     private:
---
+++ xapian-core/common/omerror.h
@@ -0,0 +1,35 @@
+/* error.h
+ */
+
+#ifndef _error_h_
+#define _error_h_
+
+#include <string>
+
+#include "omtypes.h"
+
+class OmError {
+    private:
+        string msg;
+    public:
+        OmError(string error_msg)
+        {
+            msg = error_msg;
+        }
+        string get_msg()
+        {
+            return msg;
+        }
+};
+
+class RangeError : public OmError {
+    public:
+	RangeError(string msg) : OmError(msg) {};
+};
+
+class OpeningError : public OmError {
+    public:
+        OpeningError(string msg) : OmError(msg) {};
+};
+
+#endif /* _error_h_ */
