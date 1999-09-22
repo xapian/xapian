b2e57ddfde1a03fdf9cdba6e6746de6999ea4595
Olly Betts <olly@survex.com>
1999-09-22 06:34:30 +0000

    made parameter const
    git-svn-id: file:///home/xapian-svn/xapian/trunk@85 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/stem.h
+++ xapian-core/common/stem.h
@@ -3,12 +3,12 @@
 class Stem {
     private:
     public:
-        virtual string stem_word(string &) = 0;
+        virtual string stem_word(const string &) = 0;
         virtual ~Stem() = 0;
 };
 
 class StemEn {
     private:
     public:
-        string stem_word(string &);
+        string stem_word(const string &);
 };
