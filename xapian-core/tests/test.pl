9c91773ec0a887a7651f223741625b2ca1bc4d3a
Olly Betts <olly@survex.com>
1999-10-05 09:54:18 +0000

    Added --verbose option
    git-svn-id: file:///home/xapian-svn/xapian/trunk@164 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/test.pl
+++ xapian-core/tests/test.pl
@@ -2,6 +2,11 @@
 require 5.003;
 use strict;
 
+my $verbose = 0;
+if ($ARGV[0] =~ /^--?v(?:erbose)$/) {
+   $verbose = 1;
+}
+
 my %tests = (
 'B financial company AND report OR dolly OR' => 5772,
 'finance company report dolly' => 26427,
@@ -13,12 +18,15 @@ my $fail = 0;
 for my $args (sort keys %tests) {
    open PIPE, "./matchtest $args|" or die $!;
    my $mtotal;
+   print "=" x 75, "\n" if $verbose;
    while (<PIPE>) {
+      print if $verbose;
       if (/msize = \d+, mtotal = (\d+)/) {
          $mtotal = $1;
       }
    }
    close PIPE;
+   print "=" x 75, "\n" if $verbose;
    if ($mtotal != $tests{$args}) {
       print "Got $mtotal, expected $tests{$args} - $args\n";
       $fail++;
