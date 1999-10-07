02295b125ccd7c38cba855e16e05cc83a69c9b86
Richard Boulton <richard@tartarus.org>
1999-10-07 09:02:57 +0000

    Don't need to explicitly set termweight any more (or at least, soon won't need to)
    git-svn-id: file:///home/xapian-svn/xapian/trunk@182 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/dbtest.cc
+++ xapian-core/tests/dbtest.cc
@@ -26,8 +26,6 @@ int main(int argc, char *argv[]) {
 	    printf("tname is `%s'\n", tname.c_str());
 	    // posting list 122 141 142 174 ...
 	    postlist = database.open_post_list(tid);
-	    IRWeight wt(&database, postlist);
-	    postlist->set_termweight(wt.get_weight());
 	    printf("Termfreq: %d\n", postlist->get_termfreq());
 	    postlist->next(0.0);
 	    while(!postlist->at_end()) {
