1db2e24a1983ae6d2e33445acacd2a6c0236e92c
Olly Betts <olly@survex.com>
1999-10-08 09:10:55 +0000

    Changed to use macros not templates since most compilers will include the assert strings if templates are used
    git-svn-id: file:///home/xapian-svn/xapian/trunk@203 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/omassert.h
+++ xapian-core/common/omassert.h
@@ -5,20 +5,8 @@
 
 #include "error.h"
 
-#ifdef NDEBUG
-const bool CHECK_PARANOID = false;
-const bool CHECK_ASSERT = false;
-#else
-const bool CHECK_PARANOID = true;
-const bool CHECK_ASSERT = true;
-#endif
-
-// FIXME: the where strings typically don't get thrown away by the compiler
-template<class X, class A> inline void _Assert(A assertion, const char *where)
-{
-    if (!assertion) throw X(where);
-}
-
+// 2nd level not need here (since we're using this in a macro) but
+// is required in general
 #define STRINGIZE(N) _STRINGIZE(N)
 #define _STRINGIZE(N) #N
 
@@ -26,12 +14,22 @@ template<class X, class A> inline void _Assert(A assertion, const char *where)
 // all cases easily
 #define ASSERT_LOCN(a) __FILE__":"STRINGIZE(__LINE__)": "#a
 
-// paranoid checks, typically too expensive to include in debug versions
-#define AssertParanoid(a) \
- _Assert<AssertionFailed>(!CHECK_PARANOID || (a), ASSERT_LOCN(a))
+// use macros - with a template approach the where strings typically don't get
+// thrown away by the compiler
 
-// assertions to put in debug builds
-#define Assert(a) \
- _Assert<AssertionFailed>(!CHECK_ASSERT || (a), ASSERT_LOCN(a))
+#ifdef NDEBUG
+#define AssertParanoid(a)
+#define Assert(a)
+#else
+// NB use an else clause to avoid dangling else damage
+
+// Paranoid checks, typically too expensive to include in debug versions
+// for use by developers but useful to turn on when debugging OM itself
+#define AssertParanoid(a) if (a) { } else throw AssertionFailed(ASSERT_LOCN(a))
+
+// Assertions to put in debug builds
+#define Assert(a) if (a) { } else throw AssertionFailed(ASSERT_LOCN(a))
+#endif
+
 
 #endif /* _omassert_h_ */
