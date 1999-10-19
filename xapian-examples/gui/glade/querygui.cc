8d1910972baf2a2de09eff2902542a085fa93d52
Richard Boulton <richard@tartarus.org>
1999-10-19 17:45:12 +0000

    Run a search, of sorts.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@324 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/gui/glade/querygui.cc
+++ xapian-examples/gui/glade/querygui.cc
@@ -6,11 +6,60 @@
 #include <string>
 #include "querygui.h"
 
+#include "database.h"
+#include "postlist.h"
+#include "termlist.h"
+#include "irdocument.h"
+#include "match.h"
+#include "stem.h"
+#include "textfile_database.h"
+
+#include "config.h"
+
+IRDatabase *database;
+
 void on_query_changed(GtkWidget *widget, gpointer user_data) {
-    
+    GtkEditable *textbox = GTK_EDITABLE(widget);
+    char *tmp = gtk_editable_get_chars( textbox, 0, -1);
+    string query(tmp);
+    g_free(tmp);
+
+    try {
+	Match matcher(database); 
+
+	// FIXME - split into terms
+	matcher.add_term(query);
+
+	matcher.set_max_msize(10);
+
+	matcher.match();
+
+	weight maxweight = matcher.get_max_weight();
+	doccount mtotal = matcher.mtotal;
+	doccount msize = matcher.msize;
+
+	cout << "MTotal: " << mtotal << " Maxweight: " << maxweight << endl;
+	for (docid i = 0; i < msize; i++) {
+	    docid q0 = matcher.mset[i].id;
+	    IRDocument *doc = database->open_document(q0);
+	    IRData data = doc->get_data();
+	    string p = data.value;
+	    cout << q0 << ":[" << p << "] " << matcher.mset[i].w << "\n\n";
+	}
+
+    } catch (OmError e) {
+	cout << e.get_msg() << endl;
+    }
 }
 
 int main(int argc, char *argv[]) {
+    try {
+	database = new TextfileDatabase();
+	database->open("/mnt/ivory/disk1/home/richard/textfile", true);
+    } catch (OmError e) {
+	cout << e.get_msg() << endl;
+    }
+
     GladeXML *xml;
     string gladefile = "querygui.glade";
 
@@ -21,7 +70,7 @@ int main(int argc, char *argv[]) {
     xml = glade_xml_new(gladefile.c_str(), NULL);
     if(xml == NULL) {
 	cerr << "Unable to open " << gladefile << endl;
-	return 0;
+	return 1;
     }
 
     /* connect the signals in the interface */
