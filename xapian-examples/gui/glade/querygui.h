1ff5d1595597d301991c9dd559b16aa29e065a6c
Richard Boulton <richard@tartarus.org>
1999-10-19 17:08:15 +0000

    Added querygui.h
    git-svn-id: file:///home/xapian-svn/xapian/trunk@323 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-examples/gui/glade/querygui.h
@@ -0,0 +1,19 @@
+/* querygui.h */
+
+#ifndef _querygui_h_
+#define _querygui_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+void on_query_changed(GtkWidget *widget, gpointer user_data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _querygui_h_ */
