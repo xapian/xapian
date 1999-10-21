749fd04abc7b63130486d83cee1143cc61d7ce1d
Richard Boulton <richard@tartarus.org>
1999-10-21 17:15:30 +0000

    RSet now calculates reltermfreqs and things like that itself.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@372 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/rset.h
+++ xapian-core/common/rset.h
@@ -8,12 +8,42 @@
 
 class RSetItem {
     public:
+	RSetItem(docid did_new) : did(did_new) { return; }
 	docid did;
 };
 
 class RSet {
-    public:
+    private:
 	vector<RSetItem> documents;
+	IRDatabase *root;
+    public:
+	RSet(IRDatabase *root_new) : root(root_new) { return; }
+	void add_document(docid did);
+	doccount get_rsize() const;
+	doccount get_reltermfreq(termid tid) const;
 };
+
+///////////////////////////////
+// Inline method definitions //
+///////////////////////////////
+
+inline void
+RSet::add_document(docid did)
+{
+    documents.push_back(RSetItem(did));
+}
+
+inline doccount
+RSet::get_rsize() const
+{
+    return documents.size();
+}
+
+// FIXME - dummy implementation for now
+inline doccount
+RSet::get_reltermfreq(termid tid) const
+{
+    return 1;
+}
 
 #endif /* _rset_h_ */
