27da3a178a6685f2d1a22eff9899864b668b95a6
Richard Boulton <richard@tartarus.org>
1999-11-19 16:42:07 +0000

    Use MUS_DEBUG and MUS_DEBUG_PARANOID to decide what assertion checking to do.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@462 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/omassert.h
+++ xapian-core/common/omassert.h
@@ -1,12 +1,22 @@
-/* omassert.h : Assert templates */
+/* omassert.h : Assert templates
+ *
+ * ----START-LICENCE----
+ * ----END-LICENCE----
+ */
+
+// use macros - with a template approach the location strings typically
+// don't get thrown away by the compiler
 
 #ifndef _omassert_h_
 #define _omassert_h_
 
+// Include the definitions of the exceptions we're going to throw
 #include "omerror.h"
 
-// 2nd level not need here (since we're using this in a macro) but
-// is required in general
+
+// 2nd level of stringize definition not needed for the use we put this
+// to in this file (since we always use it within a macro here) but
+// is required in general  (#N doesn't work outside a macro definition)
 #define STRINGIZE(N) _STRINGIZE(N)
 #define _STRINGIZE(N) #N
 
@@ -14,21 +24,27 @@
 // all cases easily
 #define ASSERT_LOCN(a) __FILE__":"STRINGIZE(__LINE__)": "#a
 
-// use macros - with a template approach the where strings typically don't get
-// thrown away by the compiler
-
-#ifdef NDEBUG
-#define AssertParanoid(a)
-#define Assert(a)
-#else
-// NB use an else clause to avoid dangling else damage
-
+#ifdef MUS_DEBUG_PARANOID
 // Paranoid checks, typically too expensive to include in debug versions
 // for use by developers but useful to turn on when debugging OM itself
-#define AssertParanoid(a) if (a) { } else throw AssertionFailed(ASSERT_LOCN(a))
 
+// If we want the paranoid checks, want other checks too
+#ifndef MUS_DEBUG
+#define MUS_DEBUG
+#endif /* !MUS_DEBUG */
+
+// NB use an else clause to avoid dangling else damage
+#define AssertParanoid(a) if (a) { } else throw AssertionFailed(ASSERT_LOCN(a))
+#else /* MUS_DEBUG_PARANOID */
+#define AssertParanoid(a)
+#endif /* MUS_DEBUG_PARANOID */
+
+#ifdef MUS_DEBUG
 // Assertions to put in debug builds
+// NB use an else clause to avoid dangling else damage
 #define Assert(a) if (a) { } else throw AssertionFailed(ASSERT_LOCN(a))
+#else
+#define Assert(a)
 #endif
 
 
