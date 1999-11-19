5d1b75b8506fd9dccf38185b1b07e53df36aad25
Richard Boulton <richard@tartarus.org>
1999-11-19 17:07:25 +0000

    Only include content of config.h once.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@463 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/acconfig.h
@@ -0,0 +1,11 @@
+/* acconfig.h : config.h template
+ *
+ * ----START-LICENCE----
+ * ----END-LICENCE----
+ */
+
+#ifndef _om_config_h_
+#define _om_config_h_
+@TOP@
+@BOTTOM@
+#endif /* _om_config_h_ */
