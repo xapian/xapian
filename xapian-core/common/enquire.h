330fc29d00d93dc2e8b4d7396cb087f77da552fa
Richard Boulton <richard@tartarus.org>
1999-12-10 19:39:45 +0000

    Enquire modules.  To be implemented.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@574 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/common/enquire.h
@@ -0,0 +1,17 @@
+#ifndef _enquire_h_
+#define _enquire_h_
+
+// This class provides an interface to the information retrieval
+// system for the purpose of searching.
+
+class Enquire {
+    private:
+    public:
+        Enquire();
+        ~Enquire();
+
+	// Methods to:
+	// Open a database
+};
+
+#endif /* _enquire_h_ */
---
+++ xapian-core/matcher/enquire.cc
@@ -0,0 +1,2 @@
+/* enquire.cc
+ */
