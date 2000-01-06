466efdf7869e27a305f8f055b07ad411e7921b52
Richard Boulton <richard@tartarus.org>
2000-01-06 11:49:52 +0000

    Utility functions and moved DbPostlist into a separate file.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@642 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/common/dbpostlist.h
@@ -0,0 +1,68 @@
+/* dbpostlist.h
+ *
+ * ----START-LICENCE----
+ * Copyright 1999 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef _dbpostlist_h_
+#define _dbpostlist_h_
+
+#include "postlist.h"
+#include "irweight.h"
+
+// Postlist which generates termweights (rather than merely modifying them
+// and passing them on)
+// FIXME - choose a better name for this class
+class DBPostList : public virtual PostList
+{
+    protected:
+	const IRWeight * ir_wt;
+    public:
+	DBPostList() : ir_wt(NULL) { return; }
+
+	~DBPostList() { return; }
+
+	// Sets term weighting formula, and needed information
+	virtual void set_termweight(const IRWeight *);
+
+        weight get_maxweight() const;    // Gets max weight
+        weight recalc_maxweight();       // recalculate weights
+};
+
+inline void
+DBPostList::set_termweight(const IRWeight * wt)
+{
+    ir_wt = wt;
+}
+
+// return an upper bound on the termweight
+inline weight
+DBPostList::get_maxweight() const
+{
+    Assert(ir_wt != NULL);
+    return ir_wt->get_maxweight();
+}
+
+inline weight
+DBPostList::recalc_maxweight()
+{
+    return DBPostList::get_maxweight();
+}
+
+#endif /* _dbpostlist_h_ */
---
+++ xapian-core/common/utils.h
@@ -0,0 +1,31 @@
+/* utils.h: Various useful utilities
+ *
+ * ----START-LICENCE----
+ * Copyright 1999 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef _utils_h_
+#define _utils_h_
+
+#include <string>
+
+// Convert an integer to a string
+string inttostring(int a);
+
+#endif /* _utils_h_ */
