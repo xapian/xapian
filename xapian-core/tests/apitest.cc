41cb0c2754a0907dec8e4a8d2639280f2259377e
Richard Boulton <richard@tartarus.org>
2000-02-18 13:58:44 +0000

    Added test for reversed boolean queries.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@921 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/apitest.cc
+++ xapian-core/tests/apitest.cc
@@ -75,6 +75,8 @@ bool test_allowqterms1();
 bool test_maxattain1();
 // tests the collapse-on-key
 bool test_collapsekey1();
+// tests a reversed boolean query
+bool test_reversebool1();
 
 om_test tests[] = {
     {"trivial",            test_trivial},
@@ -97,6 +99,7 @@ om_test tests[] = {
     {"allowqterms1",       test_allowqterms1},
     {"maxattain1",         test_maxattain1},
     {"collapsekey1",	   test_collapsekey1},
+    {"reversebool1",	   test_reversebool1},
     {0, 0}
 };
 
@@ -785,4 +788,60 @@ bool test_collapsekey1()
     }
 
     return success;
+}
+
+bool test_reversebool1()
+{
+    OmEnquire enquire;
+    init_simple_enquire(enquire);
+
+    OmMatchOptions mymopt;
+    OmMSet mymset1 = enquire.get_mset(0, 100, 0, &mymopt);
+    mymopt.set_sort_forward();
+    OmMSet mymset2 = enquire.get_mset(0, 100, 0, &mymopt);
+    mymopt.set_sort_forward(false);
+    OmMSet mymset3 = enquire.get_mset(0, 100, 0, &mymopt);
+
+    // mymset1 and mymset2 should be identical
+    if(mymset1.items.size() != mymset2.items.size()) return false;
+    {
+	vector<OmMSetItem>::const_iterator i;
+	vector<OmMSetItem>::const_iterator j;
+	for (i = mymset1.items.begin(), j = mymset2.items.begin();
+	     i != mymset1.items.end(), j != mymset2.items.end();
+	     ++i, j++) {
+	    if(i->did != j->did) {
+		if (verbose) {
+		    cout << "Calling OmMatchOptions::set_sort_forward() was not"
+			    "same as default." << endl;
+		    cout << "docids " << i->did << " and " << j->did <<
+			    " should have been the same" << endl;
+		}
+		return false;
+	    }
+	}
+    }
+
+    // mymset1 and mymset3 should be same but reversed
+    if(mymset1.items.size() != mymset3.items.size()) return false;
+    {
+	vector<OmMSetItem>::const_iterator i;
+	vector<OmMSetItem>::reverse_iterator j;
+	for (i = mymset1.items.begin(),
+	     j = mymset3.items.rbegin();
+	     i != mymset1.items.end();
+	     ++i, j++) {
+	    if(i->did != j->did) {
+		if (verbose) {
+		    cout << "Calling OmMatchOptions::set_sort_forward(false) "
+			    "did not reverse results." << endl;
+		    cout << "docids " << i->did << " and " << j->did <<
+			    " should have been the same" << endl;
+		}
+		return false;
+	    }
+	}
+    }
+
+    return true;
 }
