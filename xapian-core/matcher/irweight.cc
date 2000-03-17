a7c8aa8090dd15c42a00b1e74da7df834ba8e0c8
Richard Boulton <richard@tartarus.org>
2000-03-17 18:59:53 +0000

    Added new file, contains create method for all types of irweight.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1099 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/matcher/irweight.cc
@@ -0,0 +1,37 @@
+/* irweight.cc: How to create irweight objects */
+
+#include "omassert.h"
+#include "irweight.h"
+
+// Include headers for all the weight types
+#include "tradweight.h"
+#include "bm25weight.h"
+#include "boolweight.h"
+
+IRWeight * 
+IRWeight::create(IRWeight::weight_type wt_type)
+{
+    IRWeight * weight = NULL;
+
+    // Create weight of correct type
+    switch(wt_type) {
+	case WTTYPE_BOOL:
+	    weight = new BoolWeight();
+	    break;
+	case WTTYPE_TRAD:
+	    weight = new TradWeight();
+	    break;
+	case WTTYPE_BM25:
+	    weight = new BM25Weight();
+	    break;
+	default:
+	    throw OmInvalidArgumentError("Unknown weighting scheme");
+    }
+
+    // Check that we have a weighting object
+    if(weight == NULL) {
+	throw OmOpeningError("Couldn't create weighting object");
+    }
+
+    return weight;
+}
