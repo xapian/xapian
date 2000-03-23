d66e4559321586ba7b30106f9ba41fa288b81403
Chris Emerson <cemerson>
2000-03-23 16:53:38 +0000

    Added (currently unused and mostly empty) files.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1141 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/common/networkmatch.h
@@ -0,0 +1,74 @@
+/* networkmatch.h: class for communicating with remote match processes
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef OM_HGUARD_NETWORKMATCH_H
+#define OM_HGUARD_NETWORKMATCH_H
+
+#include "match.h"
+#include "stats.h"
+
+/** Class for performing match calculations over a network interface.
+ */
+class NetworkMatch : public SingleMatch
+{
+    private:
+	// disallow copies
+	NetworkMatch(const NetworkMatch &);
+	void operator=(const NetworkMatch &);
+    public:
+        NetworkMatch(IRDatabase * database_);
+        ~NetworkMatch();
+
+	///////////////////////////////////////////////////////////////////
+	// Implement these virtual methods 
+	void link_to_multi(StatsGatherer *gatherer);
+
+	void set_query(const OmQueryInternal * query_);
+
+        void set_rset(RSet * rset_);
+	void set_weighting(IRWeight::weight_type wt_type_);
+        void set_min_weight_percent(int pcent);
+	void set_collapse_key(om_keyno key);
+	void set_no_collapse();
+
+        om_weight get_max_weight();
+
+	void match(om_doccount first,
+		   om_doccount maxitems,
+		   vector<OmMSetItem> & mset,
+		   mset_cmp cmp,
+		   om_doccount * mbound,
+		   om_weight * greatest_wt,
+		   const OmMatchDecider *mdecider
+		   );
+
+	///////////////////////////////////////////////////////////////////
+	// Miscellaneous
+	// =============
+
+	/** Called by postlists to indicate that they've rearranged themselves
+	 *  and the maxweight now possible is smaller.
+	 */
+        void recalc_maxweight();
+};
+
+#endif /* OM_HGUARD_NETWORKMATCH_H */
