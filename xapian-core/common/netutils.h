2436b323319b4526cfe93b1a2da29bf68d209b29
Chris Emerson <cemerson>
2000-03-29 16:38:18 +0000

    Added file for useful net-related utility functions.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1195 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/common/netutils.h
@@ -0,0 +1,51 @@
+/* netutils.h: Various useful network-related utilities
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef OM_HGUARD_NETUTILS_H
+#define OM_HGUARD_NETUTILS_H
+
+#include <string>
+
+inline string
+tohex(char c)
+{
+    static char hexdigits[] = "0123456789ABCDEF";
+    int high = (c & 0xf0) >> 4;
+    int low = (c & 0x0f);
+    return string() + hexdigits[high] + hexdigits[low];
+}
+
+inline string
+encode_tname(string tname)
+{
+    string result;
+
+    string::const_iterator i;
+    for (i = tname.begin();
+	 i != tname.end();
+	 ++i) {
+	result += tohex(*i);
+    }
+    return result;
+}
+
+#endif /* OM_HGUARD_NETUTILS_H */
