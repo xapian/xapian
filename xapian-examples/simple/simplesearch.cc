0177e9a4ac356f5a3b4e0d1f7cd2b9a09626ca9c
Richard Boulton <richard@tartarus.org>
2000-04-11 09:52:03 +0000

    Have a simple makefile, and more content in simplesearch.cc
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1251 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/simple/simplesearch.cc
+++ xapian-examples/simple/simplesearch.cc
@@ -1,9 +1,31 @@
 // simplesearch.cc
 
-
-#include "om.h"
+#include <stdio.h>
+#include <om/om.h>
 
 int main(int argc, char *argv[])
 {
-    OmDatabase();
+    // Simplest possible options parsing: we just require two or more
+    // parameters.
+    if(argc < 3) {
+	printf("usage: %s <path to database> <search terms>\n", argv[0]);
+    }
+    
+    // Make the database group
+    OmDatabase databases;
+    vector<string> parameters;
+    parameters.push_back(argv[1]);
+    databases.add_database("sleepycat", parameters);
+
+    // Start an enquire session
+    OmEnquire enquire(databases);
+
+    // Prepare the query terms
+    vector<om_termname> queryterms;
+    for(int optpos = 2; optpos < argc; optpos++) {
+	queryterms.push_back(argv[optpos]);
+    }
+
+    // 
+    OmQuery query(OM_MOP_OR, queryterms.begin(), queryterms.end());
 }
