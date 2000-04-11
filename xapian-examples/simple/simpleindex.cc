1a04fe1034db3e233857a36dcbf561ce2260e41a
Richard Boulton <richard@tartarus.org>
2000-04-11 20:30:08 +0000

    Added the simplest code possible to make an index entry.  Should work once the appropriate API methods are implemented.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1268 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/simple/simpleindex.cc
+++ xapian-examples/simple/simpleindex.cc
@@ -4,43 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-    // Simplest possible options parsing: we just require two or more
+    // Simplest possible options parsing: we just require three or more
     // parameters.
-    if(argc < 3) {
+    if(argc < 4) {
 	cout << "usage: " << argv[0] <<
-		" <path to database> <search terms>" << endl;
+		" <path to database> <document data> <document terms>" << endl;
 	exit(1);
     }
     
     // Catch any OmError exceptions thrown
     try {
-	// Make the database group
-	OmDatabase databases;
+	// Make the database
 	vector<string> parameters;
 	parameters.push_back(argv[1]);
-	databases.add_database("sleepycat", parameters);
+	OmWritableDatabase database("sleepycat", parameters);
 
-	// Start an enquire session
-	OmEnquire enquire(databases);
+	OmDocumentContents newdocument;
 
-	// Prepare the query terms
-	vector<om_termname> queryterms;
-	for(int optpos = 2; optpos < argc; optpos++) {
-	    queryterms.push_back(argv[optpos]);
+	newdocument.data = string(argv[2]);
+
+	for(int i = 3; i < argc; i++) {
+	    newdocument.add_posting(argv[i], i - 2);
 	}
-
-	// Build the query object
-	OmQuery query(OM_MOP_OR, queryterms.begin(), queryterms.end());
-	cout << "Performing query `" << query.get_description() << "'" << endl;
-
-	// Give the query object to the enquire session
-	enquire.set_query(query);
-
-	// Get the top 10 results of the query
-	OmMSet matches = enquire.get_mset(0, 10);
-
-	// Display the results
-	cout << matches.items.size() << " results found" << endl;
+	
+	database.add_document(newdocument);
     }
     catch(OmError &error) {
 	cout << "Exception: "  << error.get_msg() << endl;
