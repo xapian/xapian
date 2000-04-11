2548f28346dda37f08adfa29d4e9055098776d6b
Richard Boulton <richard@tartarus.org>
2000-04-11 17:27:40 +0000

    Now performs a query on the database.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1263 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/simple/simplesearch.cc
+++ xapian-examples/simple/simplesearch.cc
@@ -1,6 +1,5 @@
 // simplesearch.cc
 
-#include <stdio.h>
 #include <om/om.h>
 
 int main(int argc, char *argv[])
@@ -8,24 +7,42 @@ int main(int argc, char *argv[])
     // Simplest possible options parsing: we just require two or more
     // parameters.
     if(argc < 3) {
-	printf("usage: %s <path to database> <search terms>\n", argv[0]);
+	cout << "usage: " << argv[0] <<
+		" <path to database> <search terms>" << endl;
+	exit(1);
     }
     
-    // Make the database group
-    OmDatabase databases;
-    vector<string> parameters;
-    parameters.push_back(argv[1]);
-    databases.add_database("sleepycat", parameters);
+    // Catch any OmError exceptions thrown
+    try {
+	// Make the database group
+	OmDatabaseGroup databases;
+	vector<string> parameters;
+	parameters.push_back(argv[1]);
+	databases.add_database("sleepycat", parameters);
 
-    // Start an enquire session
-    OmEnquire enquire(databases);
+	// Start an enquire session
+	OmEnquire enquire(databases);
 
-    // Prepare the query terms
-    vector<om_termname> queryterms;
-    for(int optpos = 2; optpos < argc; optpos++) {
-	queryterms.push_back(argv[optpos]);
+	// Prepare the query terms
+	vector<om_termname> queryterms;
+	for(int optpos = 2; optpos < argc; optpos++) {
+	    queryterms.push_back(argv[optpos]);
+	}
+
+	// Build the query object
+	OmQuery query(OM_MOP_OR, queryterms.begin(), queryterms.end());
+	cout << "Performing query `" << query.get_description() << "'" << endl;
+
+	// Give the query object to the enquire session
+	enquire.set_query(query);
+
+	// Get the top 10 results of the query
+	OmMSet matches = enquire.get_mset(0, 10);
+
+	// Display the results
+	cout << matches.items.size() << " results found" << endl;
     }
-
-    // 
-    OmQuery query(OM_MOP_OR, queryterms.begin(), queryterms.end());
+    catch(OmError &error) {
+	cout << "Exception: "  << error.get_msg() << endl;
+    }
 }
