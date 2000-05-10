60bb0ccf78a0e2891f4cb809faa0d40eb78e9872
Chris Emerson <cemerson>
2000-05-10 16:34:23 +0000

    Added TcpClient
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1430 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/common/tcpclient.h
@@ -0,0 +1,55 @@
+/* tcpclient.h: implementation of NetClient which opens a TCP/IP socket
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef OM_HGUARD_TCPCLIENT_H
+#define OM_HGUARD_TCPCLIENT_H
+
+#include "socketclient.h"
+#include "socketcommon.h"
+
+/** An implementation of the NetClient interface using a program.
+ *  TcpClient opens a TCP/IP socket to a remote machine.
+ */
+class TcpClient : public SocketClient {
+    private:
+	// disallow copies
+	TcpClient(const TcpClient &);
+	void operator=(const TcpClient &);
+
+	/** Spawn a program and return a filedescriptor of
+	 *  the local end of a socket to it.
+	 */
+	int get_remote_socket(string hostname, int port);
+
+    public:
+	/** Constructor.
+	 *
+	 *  @param hostname The name of the remote host
+	 *  @param port	    The TCP port to connect to.
+	 */
+	TcpClient(string hostname, int port);
+
+	/** Destructor. */
+	~TcpClient();
+};
+
+#endif  /* OM_HGUARD_TCPCLIENT_H */
---
+++ xapian-core/net/tcpclient.cc
@@ -0,0 +1,74 @@
+/* tcpclient.cc: implementation of NetClient which connects to a remote server.
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 Dialog Corporation
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "config.h"
+#include "tcpclient.h"
+#include "om/omerror.h"
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+TcpClient::TcpClient(string hostname, int port)
+	: SocketClient(get_remote_socket(hostname, port))
+{
+
+}
+
+int
+TcpClient::get_remote_socket(string hostname, int port)
+{
+    struct hostent *host = gethostbyname(hostname.c_str());
+
+    if (host == 0) {
+	throw OmNetworkError(string("Couldn't resolve host ") +
+			     hostname);
+    }
+
+    int socketfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    if (socketfd < 0) {
+	throw OmNetworkError(string("socket: ") + strerror(errno));
+    }
+
+    struct sockaddr_in remaddr;
+    remaddr.sin_family = AF_INET;
+    remaddr.sin_port = htons(port);
+    remaddr.sin_addr.s_addr = *(host->h_addr);
+
+    int retval = connect(socketfd,
+			 reinterpret_cast<sockaddr *>(&remaddr),
+			 sizeof(remaddr));
+
+    if (retval < 0) {
+	close(socketfd);
+	throw OmNetworkError(string("connect: ") + strerror(errno));
+    }
+
+    return socketfd;
+}
+
+TcpClient::~TcpClient()
+{
+}
