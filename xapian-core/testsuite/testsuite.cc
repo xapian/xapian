78046604627d019449e49168aabcef410ba5dd74
Richard Boulton <richard@tartarus.org>
2000-06-29 00:48:46 +0000

    Set number of allocations to an absolutely _silly_ number.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1802 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/testsuite/testsuite.cc
+++ xapian-core/testsuite/testsuite.cc
@@ -72,7 +72,7 @@ test_driver::set_quiet(bool quiet_)
  */
 
 // The maximum number of allocations which can be tracked
-static const int max_allocations = 100000;
+static const int max_allocations = 1000000;
 
 // The number of currently unfreed allocations
 static long num_new_allocations = 0;
