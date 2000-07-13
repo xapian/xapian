8b2bd60be2131bc02c8092cba4c1ea38f0347e43
Martin Porter <martin>
2000-07-13 12:29:42 +0000

    one extra word 'fue' added as form of 'ser'
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1923 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/languages/spanish/stem_spanish.c
+++ xapian-core/languages/spanish/stem_spanish.c
@@ -2,17 +2,17 @@
  *
  * ----START-LICENCE----
  * Copyright 1999,2000 BrightStation PLC
- * 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License as 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
@@ -651,7 +651,7 @@ static const char * irregular_forms[] = {
     "ser" , "ser/"
     "soy/eres/es/somos/sois/son/sea/seas/seamos/seAis/sean/serE/serAs/"
     "serA/seremos/serEis/serAn/serIa/serIas/serIamos/serIais/serIan/"
-    "era/eras/Eramos/erais/eran/fui/fuiste/fuimos/fuisteis/"
+    "era/eras/Eramos/erais/eran/fui/fuiste/fue/fuimos/fuisteis/"
     "fueron/fuera/fueras/fuEramos/fuerais/fueran/fuese/fueses/"
     "fuEsemos/fueseis/fuesen/sintiendo/sentido/sentida/sentidos/"
     "sentidas/siente/sentid/"
