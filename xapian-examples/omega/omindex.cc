00de13f93856edadd201537ebd51f9b52fda3f01
Olly Betts <olly@survex.com>
2000-07-15 18:15:13 +0000

    omindex: added parsers for text/plain and text/pdf
    git-svn-id: file:///home/xapian-svn/xapian/trunk@1949 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/omega/omindex.cc
+++ xapian-examples/omega/omindex.cc
@@ -96,50 +96,91 @@ static om_termpos index_text(const string &s, OmDocumentContents &doc,
 static string root = "/home/httpd/html/open.muscat.com";
 
 static void
-index_file(const string &url)
+index_file(const string &url, const string &mimetype)
 {
     string file = root + url;
-    std::ifstream in(file.c_str());
-    if (!in) {
-	cout << "Can't open \"" << file << "\" - skipping\n";
-	return;
-    }
+    string title;
 
     cout << "Indexing \"" << url << "\"\n";
 
-    string text;   
-    while (!in.eof()) {
-	string line;
-	getline(in, line);
-	text += line;
-    }
-    in.close();
+    string dump;
 
-    MyHtmlParser p;
+    if (mimetype == "text/html") {
+	std::ifstream in(file.c_str());
+	if (!in) {
+	    cout << "Can't open \"" << file << "\" - skipping\n";
+	    return;
+	}
+	string text;
+	while (!in.eof()) {
+	    string line;
+	    getline(in, line);
+	    text += line;
+	}
+	in.close();
+	MyHtmlParser p;
+	p.parse_html(text);
+	dump = p.dump;
+	title = p.title;
+    } else if (mimetype == "text/plain") {
+	std::ifstream in(file.c_str());
+	if (!in) {
+	    cout << "Can't open \"" << file << "\" - skipping\n";
+	    return;
+	}
+	while (!in.eof()) {
+	    string line;
+	    getline(in, line);
+	    dump += line;
+	}
+	in.close();	
+    } else if (mimetype == "text/pdf") {
+	string safefile = file;
+	string::size_type p = 0;
+	while (p < safefile.size()) {
+	    if (!isalnum(safefile[p])) safefile.insert(p++, "\\");
+            p++;
+	}
+	string tmp = "/tmp/omindex.txt";
+	unlink(tmp.c_str());
+	string cmd = "pdftotext " + safefile + " " + tmp;
+	if (system(cmd.c_str()) != 0) {
+	    cout << "pdftotext failed to extract text from \"" << file << "\" - skipping\n";
+	    return;
+	}
+	std::ifstream in(tmp.c_str());
+	unlink(tmp.c_str());
+	if (!in) {
+	    cout << "pdftotext failed to extract text from \"" << file << "\" - skipping\n";
+	    return;
+	}
+	while (!in.eof()) {
+	    string line;
+	    getline(in, line);
+	    dump += line;
+	}
+	in.close();
+    }
+
     OmStem stemmer("english");    
 
-    p.parse_html(text);
-
-    string dump = p.dump;
-
-    // replace newlines with spaces
-    size_t i = 0;    
-    while ((i = dump.find("\n", i)) != string::npos) dump[i] = ' ';
-
-    // Make the document
-    OmDocumentContents newdocument;
-
+    // Produce a sample
     string sample = dump.substr(0, 300);
     size_t space = sample.find_last_of(" \t\n");
     if (space != string::npos) sample.erase(space);
+    // replace newlines with spaces
+    size_t i = 0;    
+    while ((i = sample.find('\n', i)) != string::npos) sample[i] = ' ';
 
     // Put the data in the document
+    OmDocumentContents newdocument;
     string record = "url=" + url + "\nsample=" + sample;
-    if (p.title != "") record = record +"\ncaption=" + p.title;
+    if (title != "") record = record +"\ncaption=" + title;
     newdocument.data = record;
 
+    // Add postings for terms to the document
     om_termpos pos = 1;
-    pos = index_text(p.title, newdocument, stemmer, pos);
+    pos = index_text(title, newdocument, stemmer, pos);
     pos = index_text(dump, newdocument, stemmer, pos);
 
     // Add the document to the database
@@ -174,8 +215,13 @@ index_directory(const string &dir)
 	    continue;
 	}
 	if (S_ISREG(statbuf.st_mode)) {
-	    if (url.substr(url.size() - 5) == ".html")
-		index_file(url);
+	    string ext = url.substr(url.find_last_of('.'));
+	    if (ext == ".html" || ext == ".htm")
+		index_file(url, "text/html");
+	    else if (ext == ".txt")
+		index_file(url, "text/plain");	    
+	    else if (ext == ".pdf")
+		index_file(url, "text/pdf");	    
 	    continue;
 	}
 	cout << "Not a regular file \"" << file << "\" - skipping\n";
