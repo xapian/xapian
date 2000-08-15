69a2f4d6064c7a8db16ee2db24bfbd0c1335bc9d
Richard Boulton <richard@tartarus.org>
2000-08-15 10:21:43 +0000

    Added initial (empty) quartz_database.{cc,h} files, so that distcheck will work again.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2053 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/backends/quartz/quartz_database.cc
@@ -0,0 +1,139 @@
+/* quartz_database.cc: quartz database
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "config.h"
+#include "quartz_database.h"
+
+QuartzDatabase::QuartzDatabase(const DatabaseBuilderParams & params)
+{
+// initialise environment
+// Check version of BerkeleyDB available
+// Call DBENV->set_tmp_dir with a safe directory (with known permissions)
+// Make sure that environment is not in a network filesystem, eg NFS.
+// Specify DBRECOVER flag to DBENV->open.
+}
+
+QuartzDatabase::~QuartzDatabase()
+{
+}
+
+
+void
+QuartzDatabase::do_begin_session(om_timeout timeout)
+{
+}
+
+void
+QuartzDatabase::do_end_session()
+{
+}
+
+void
+QuartzDatabase::do_flush()
+{
+}
+
+
+void
+QuartzDatabase::do_begin_transaction()
+{
+    throw OmUnimplementedError("QuartzDatabase::do_begin_transaction() not yet implemented");
+}
+
+void
+QuartzDatabase::do_commit_transaction()
+{
+    throw OmUnimplementedError("QuartzDatabase::do_commit_transaction() not yet implemented");
+}
+
+void
+QuartzDatabase::do_cancel_transaction()
+{
+    throw OmUnimplementedError("QuartzDatabase::do_cancel_transaction() not yet implemented");
+}
+
+
+om_docid
+QuartzDatabase::do_add_document(const OmDocumentContents & document)
+{
+}
+
+void
+QuartzDatabase::do_delete_document(om_docid did)
+{
+}
+
+void
+QuartzDatabase::do_replace_document(om_docid did,
+				    const OmDocumentContents & document)
+{
+}
+
+
+OmDocumentContents
+QuartzDatabase::do_get_document(om_docid did)
+{
+}
+
+
+
+om_doccount 
+QuartzDatabase::get_doccount() const
+{
+}
+
+om_doclength
+QuartzDatabase::get_avlength() const
+{
+}
+
+om_doclength
+QuartzDatabase::get_doclength(om_docid did) const
+{
+}
+
+om_doccount
+QuartzDatabase::get_termfreq(const om_termname & tname) const
+{
+}
+
+bool
+QuartzDatabase::term_exists(const om_termname & tname) const
+{
+}
+
+
+LeafPostList *
+QuartzDatabase::open_post_list(const om_termname& tname) const
+{
+}
+
+LeafTermList *
+QuartzDatabase::open_term_list(om_docid did) const
+{
+}
+
+LeafDocument *
+QuartzDatabase::open_document(om_docid did) const
+{
+}
+
---
+++ xapian-core/backends/quartz/quartz_database.h
@@ -0,0 +1,82 @@
+/* quartz_database.h: C++ class definition for quartz database
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef OM_HGUARD_QUARTZ_DATABASE_H
+#define OM_HGUARD_QUARTZ_DATABASE_H
+
+#include <db_cxx.h>
+#include "database.h"
+
+/** A backend designed for efficient indexing and retrieval, using
+ *  compressed posting lists and the Berkeley database library (version
+ *  3.1).
+ *
+ *  BerkeleyDB is available from http://www.sleepycat.com/
+ */
+class QuartzDatabase : public IRDatabase {
+    friend class DatabaseBuilder;
+    private:
+	/** Create and open a quartz database.
+	 *
+	 *  @exception OmOpeningError thrown if database can't be opened.
+	 *
+	 *  @param params Parameters supplied by the user to specify the
+	 *                location of the database to open.  The meanings
+	 *                of these parameters are dependent on the database
+	 *                type.
+	 */
+	QuartzDatabase(const DatabaseBuilderParams & params);
+
+	//@{
+	/** Implementation of virtual methods: see IRDatabase for details.
+	 */
+	void do_begin_session(om_timeout timeout);
+	void do_end_session();
+	void do_flush();
+
+	void do_begin_transaction();
+	void do_commit_transaction();
+	void do_cancel_transaction();
+
+	om_docid do_add_document(const OmDocumentContents & document);
+	void do_delete_document(om_docid did);
+	void do_replace_document(om_docid did,
+				 const OmDocumentContents & document);
+	OmDocumentContents do_get_document(om_docid did);
+	//@}
+
+    public:
+	~QuartzDatabase();
+
+	// Virtual methods of IRDatabase
+	om_doccount  get_doccount() const;
+	om_doclength get_avlength() const;
+	om_doclength get_doclength(om_docid did) const;
+	om_doccount get_termfreq(const om_termname & tname) const;
+	bool term_exists(const om_termname & tname) const;
+
+	LeafPostList * open_post_list(const om_termname& tname) const;
+	LeafTermList * open_term_list(om_docid did) const;
+	LeafDocument * open_document(om_docid did) const;
+};
+
+#endif /* OM_HGUARD_QUARTZ_DATABASE_H */
