cf81f28012744ea253f9c2f9f3e33eced31a32f1
Richard Boulton <richard@tartarus.org>
2000-08-21 17:08:21 +0000

    Remove unneeded extra qualification.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2128 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/api/omsettings.cc
+++ xapian-core/api/omsettings.cc
@@ -84,7 +84,7 @@ class OmSettings::Internal {
 	/** Return stored settings as a string for use by
 	 *  OmSettings::get_description()
 	 */
-        string OmSettings::Internal::get_description() const;
+        string get_description() const;
 };
 
 ////////////////////////////////////////////////////////////////
