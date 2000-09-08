50517fc6e060d52d97a97f2685872938cece9ae6
Richard Boulton <richard@tartarus.org>
2000-09-08 15:36:04 +0000

    Delve can now display several termlists, several postlists, or lots of positionlists.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2241 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/delve.cc
+++ xapian-core/tests/delve.cc
@@ -42,16 +42,23 @@ main(int argc, char *argv[])
     const char *progname = argv[0];
     argv++;
     argc--;
-    om_docid recno = 0;
-    om_termname term;
+    
+    std::vector<om_docid> recnos;
+    std::vector<om_termname> terms;
+    string dbdir;
+
     bool syntax_error = false;
     while (argc && argv[0][0] == '-') {
 	if (argc >= 2 && strcmp(argv[0], "-r") == 0) {
-	    recno = atoi(argv[1]);
+	    recnos.push_back(atoi(argv[1]));
 	    argc -= 2;
 	    argv += 2;
 	} else if (argc >= 2 && strcmp(argv[0], "-t") == 0) {
-	    term = argv[1];
+	    terms.push_back(argv[1]);
+	    argc -= 2;
+	    argv += 2;
+	} else if (argc >= 2 && strcmp(argv[0], "-d") == 0) {
+	    dbdir = argv[1];
 	    argc -= 2;
 	    argv += 2;
 	} else {
@@ -60,67 +67,86 @@ main(int argc, char *argv[])
 	}
     }
 
-    if (syntax_error || argc != 1 || (recno == 0 && term.empty())) {
-	cout << "Syntax:\t" << progname << " -r <recno> <db>  for termlist\n";
-	cout << "or:\t" << progname << " -t <term> <db>  for posting list\n";
-	cout << "or:\t" << progname << " -t <term> -r <recno> <db> for position list\n";
+    if (syntax_error || argc != 0 || (recnos.empty() && terms.empty())) {
+	cout << "Syntax:\t" << progname << " <options>\n" <<
+		"\t-d <db>               to specify database\n" <<
+		"\t-r <recno>            for termlist\n" <<
+		"\t-t <term>             for posting list\n" <<
+		"\t-t <term> -r <recno>  for position list\n";
 	exit(1);
     }
     try {
 	OmSettings params;
 	params.set("backend", "auto");
-	params.set("auto_dir", argv[0]);
+	params.set("auto_dir", dbdir);
 	OmDatabase db(params);
 
-	if (!term.empty()) {
-	    OmStem stemmer("english");
-	    if (*(term.end() - 1) == '.') {
-		term = term.erase(term.size() - 1);
-	    } else {
-		term = stemmer.stem_word(term);
-	    }
-	    OmPostListIterator p = db.postlist_begin(term);
-	    OmPostListIterator pend = db.postlist_end(term);
-	    if (p == pend) {
-		cout << "term `" << term << "' not in database\n";
-		exit(0);
-	    }
-	    if (recno == 0) {
-		cout << "Posting List for term `" << term << "':";
-		while (p != pend) {
-		    cout << '\n' << *p;
-		    p++;
-		}
-		cout << endl;
-	    } else {
-		p.skip_to(recno);
-		if (p == pend || *p != recno) {
-		    cout << "term `" << term << "' doesn't index document #"
-			 << recno << endl;
-		    exit(0);
-		}
-		cout << "Position List for term `" << term
-		     << "', record #" << recno << ':';
-		OmPositionListIterator pos = p.positionlist_begin();
-		OmPositionListIterator posend = p.positionlist_end();
-		while (pos != posend) {
-		    cout << '\n' << *pos;
-		    pos++;
+	if (terms.empty()) {
+	    // Display termlists
+	    std::vector<om_docid>::const_iterator i;
+	    for (i = recnos.begin(); i != recnos.end(); i++) {
+		OmTermListIterator t = db.termlist_begin(*i);
+		OmTermListIterator tend = db.termlist_end(*i);
+		cout << "Term List for record #" << *i << ':';
+		while (t != tend) {
+		    cout << '\n' << *t;
+		    t++;
 		}
 		cout << endl;
 	    }
 	} else {
-	    OmTermListIterator t = db.termlist_begin(recno);
-	    OmTermListIterator tend = db.termlist_end(recno);
-	    cout << "Term List for record #" << recno << ':';
-	    while (t != tend) {
-		cout << '\n' << *t;
-		t++;
+	    std::vector<om_termname>::const_iterator j;
+	    for (j = terms.begin(); j != terms.end(); j++) {
+		om_termname term = *j;
+		OmStem stemmer("english");
+		if (*(term.end() - 1) == '.') {
+		    term = term.erase(term.size() - 1);
+		} else {
+		    term = stemmer.stem_word(term);
+		}
+		OmPostListIterator p = db.postlist_begin(term);
+		OmPostListIterator pend = db.postlist_end(term);
+		if (p == pend) {
+		    cout << "term `" << term << "' not in database\n";
+		    continue;
+		}
+		if (recnos.empty()) {
+		    // Display posting list
+		    cout << "Posting List for term `" << term << "':";
+		    while (p != pend) {
+			cout << '\n' << *p;
+			p++;
+		    }
+		    cout << endl;
+		} else {
+		    // Display position lists
+		    std::vector<om_docid>::const_iterator i;
+		    for (i = recnos.begin(); i != recnos.end(); i++) {
+			p.skip_to(*i);
+			if (p == pend || *p != *i) {
+			    cout << "term `" << term <<
+				    "' doesn't index document #" << *i << endl;
+			} else {
+			    cout << "Position List for term `" << term
+				    << "', record #" << *i << ':';
+			    try {
+				OmPositionListIterator pos = p.positionlist_begin();
+				OmPositionListIterator posend = p.positionlist_end();
+				while (pos != posend) {
+				    cout << '\n' << *pos;
+				    pos++;
+				}
+				cout << endl;
+			    } catch (OmError &e) {
+				cout << "Error: " << e.get_msg() << endl;
+			    }
+			}
+		    }
+		}
 	    }
-	    cout << endl;
 	}
     }
     catch (OmError &e) {
-	cout << e.get_msg() << endl;
+	cout << "Error: " << e.get_msg() << endl;
     }
 }
