a92d21da83ab05595548b1090d8bf669e2f27573
Chris Emerson <cemerson>
2000-09-25 14:38:56 +0000

    Added omprefix node.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2391 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/indexer/indexgraph/omprefixnode.cc
@@ -0,0 +1,53 @@
+/* omprefixnode.cc: Implementation of a prefixing node
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "omindexernode.h"
+#include "node_reg.h"
+
+class OmPrefixNode : public OmIndexerNode {
+    public:
+	OmPrefixNode(const OmSettings &config)
+		: OmIndexerNode(config)
+		{}
+    private:
+	void calculate() {
+	    OmIndexerMessage input = get_input_record("in");
+
+	    OmIndexerMessage output(new OmIndexerData("words",
+				      std::vector<OmIndexerData>()));
+
+	    std::string prefix = get_config_string("prefix");
+
+	    for (int i=0; i<input->get_vector_length(); ++i) {
+		output->append_element(
+	            OmIndexerData("word",
+			   prefix + input->get_element(i).get_string()));
+	    }
+
+	    set_output("out", output);
+	}
+};
+
+NODE_BEGIN(OmPrefixNode, omprefix)
+NODE_INPUT("in", "words", mt_vector)
+NODE_OUTPUT("out", "words", mt_vector)
+NODE_END()
