ca97790f9d4231e5c14feb74fb295e1f5297c1ce
Chris Emerson <cemerson>
2000-09-25 14:18:29 +0000

    Added omsplitternode and omlistconcatnode
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2390 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/indexer/indexgraph/omlistconcatnode.cc
@@ -0,0 +1,48 @@
+/* listconcatnode.cc: Implementation of a list concatenation node
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "omindexernode.h"
+#include "node_reg.h"
+
+class OmListConcatNode : public OmIndexerNode {
+    public:
+	OmListConcatNode(const OmSettings &config)
+		: OmIndexerNode(config)
+		{}
+    private:
+	void calculate() {
+	    OmIndexerMessage left = get_input_record("left");
+	    OmIndexerMessage right = get_input_record("right");
+
+	    for (int i=0; i<right->get_vector_length(); ++i) {
+		// FIXME use a multi-append function for efficiency?
+		left->append_element(right->get_element(i));
+	    }
+	    set_output("out", left);
+	}
+};
+
+NODE_BEGIN(OmListConcatNode, omlistconcat)
+NODE_INPUT("left", "ANY", mt_vector)
+NODE_INPUT("right", "ANY", mt_vector)
+NODE_OUTPUT("out", "ANY", mt_vector)
+NODE_END()
---
+++ xapian-core/indexer/indexgraph/omsplitternode.cc
@@ -0,0 +1,44 @@
+/* omsplitternode.cc: Implementation of the Splitter node
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "omindexernode.h"
+#include "node_reg.h"
+
+class OmSplitterNode : public OmIndexerNode {
+    public:
+	OmSplitterNode(const OmSettings &config)
+		: OmIndexerNode(config)
+		{}
+    private:
+	void calculate() {
+	    OmIndexerMessage msg = get_input_record("in");
+
+	    set_output("left", OmIndexerMessage(new OmIndexerData(*msg)));
+	    set_output("right", msg);
+	}
+};
+
+NODE_BEGIN(OmSplitterNode, omsplitter)
+NODE_INPUT("in", "ANY", mt_record)
+NODE_OUTPUT("left", "ANY", mt_record)
+NODE_OUTPUT("right", "ANY", mt_record)
+NODE_END()
