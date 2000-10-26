57c37e04a26c2bc0fab9719e1a6877e84d226dba
Chris Emerson <cemerson>
2000-10-26 17:34:33 +0000

    Added (currently incomplete) indexer example
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2526 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-examples/indexer/indexfile.cc
@@ -0,0 +1,58 @@
+/* indexfile.cc: Simple indexer
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ * 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include <om/om.h>
+
+int main(int argc, char *argv[])
+{
+    // Simplest possible options parsing: we just require three or more
+    // parameters.
+    if(argc != 3) {
+	cout << "usage: " << argv[0] <<
+		" <path to database> <file to index>" << endl;
+	exit(1);
+    }
+    
+    // Catch any OmError exceptions thrown
+    try {
+	// Make the indexer
+	OmIndexerBuilder builder;
+	AutoPtr<OmIndexer> indexer = builder.build_from_file("indexfile.xml");
+
+	// Make the database
+	OmSettings settings;
+	settings.set("backend", "sleepycat");
+	settings.set("sleepycat_dir", argv[1]);
+	OmWritableDatabase database(settings);
+
+	// Make the document by invoking the indexer
+	OmIndexerMessage filename(new OmIndexerData(argv[2]));
+	indexer->set_input(filename);
+	OmDocumentContents newdocument = indexer->get_output();
+
+	// Add the document to the database
+	database.add_document(newdocument);
+    }
+    catch(OmError &error) {
+	cout << "Exception: "  << error.get_msg() << endl;
+    }
+}
