9b1102a52e8fa4cf49f82047da08101c38606807
Richard Boulton <richard@tartarus.org>
2000-11-06 14:29:10 +0000

    methods with no arguments don't take (void) in C++
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2582 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/quartz/btree.h
+++ xapian-core/backends/quartz/btree.h
@@ -148,7 +148,7 @@ enum Btree_errors {
 };
 
 extern int Btree_find_key(struct Btree * B, byte * key, int key_len);
-extern struct Btree_item * Btree_item_create(void);
+extern struct Btree_item * Btree_item_create();
 extern int Btree_find_tag(struct Btree * B, byte * key, int key_len, struct Btree_item * t);
 extern void Btree_item_lose(struct Btree_item * kt);
 extern int Btree_add(struct Btree * B, byte * key, int key_len,
