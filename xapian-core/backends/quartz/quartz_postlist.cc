0a1165970f280180d606b29e05ce0d4eeb77ec3f
Richard Boulton <richard@tartarus.org>
2000-11-09 16:02:04 +0000

    Added implementation of posting lists.  Currently only has code for reading, and there's an omission of code to pack integers while preserving sort order.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2641 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/backends/quartz/quartz_postlist.cc
@@ -0,0 +1,208 @@
+/* quartz_positionlist.cc
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include "omdebug.h"
+#include "quartz_postlist.h"
+#include "quartz_utils.h"
+#include "quartz_table.h"
+#include "database.h"
+
+/** The format of a postlist is:
+ *
+ *  Split into chunks.  Key for first chunk is the termname (encoded as
+ *  length - name).  Key for susequent chunks is the same, followed by the
+ *  document ID of the first document in the chunk (encoded as length of
+ *  representation in first byte, and then docid).
+ *
+ *  A chunk (except for the first chunk) contains:
+ *
+ *  1)  difference between final docid in chunk and first docid.
+ *  2)  wdf, then doclength of first item.
+ *  3)  increment in docid to next item, followed by wdf and doclength of item
+ *  4)  (3) repeatedly.
+ * 
+ *  The first chunk begins with the number of entries, then the docid of the
+ *  first document, then continues as other chunks.
+ */
+QuartzPostList::QuartzPostList(RefCntPtr<const Database> this_db_,
+			       om_doclength avlength_,
+			       const QuartzTable * table_,
+			       const om_termname & tname_)
+	: this_db(this_db_),
+	  avlength(avlength_),
+	  table(table_),
+	  tname(tname_),
+	  cursor(table->cursor_get()),
+	  is_at_end(false)
+{
+    QuartzDbKey key;
+    key.value = pack_string(tname);
+    int found = cursor->find_entry(key);
+    if (!found) {
+	throw OmInvalidDataError("Attempt to open a postlist for term `" +
+				 tname +
+				 "', which can't be found in database.");
+    }
+    pos = cursor->current_tag.value.data();
+    end = pos + cursor->current_tag.value.size();
+
+    did = 1;
+
+    read_number_of_entries();
+    read_first_docid();
+    read_start_of_chunk();
+}
+
+// FIXME
+#define pack_uint_preserving_sort pack_uint
+#define unpack_uint_preserving_sort unpack_uint
+
+void
+QuartzPostList::make_key(const om_termname & tname,
+			 om_docid did,
+			 QuartzDbKey & key)
+{
+    key.value = pack_string(tname);
+    key.value += pack_uint_preserving_sort(did);
+}
+
+QuartzPostList::~QuartzPostList()
+{
+    delete cursor;
+}
+
+
+void
+QuartzPostList::report_read_error(const char * position)
+{
+    if (position == 0) {
+	// data ran out
+	throw OmDatabaseCorruptError("Data ran out unexpectedly when reading posting list.");
+    } else {
+	// overflow
+	throw OmRangeError("Value in posting list too large.");
+    }
+}
+
+void
+QuartzPostList::read_number_of_entries()
+{
+    if (!unpack_uint(&pos, end, &number_of_entries)) report_read_error(pos);
+}
+
+void
+QuartzPostList::read_first_docid()
+{
+    if (!unpack_uint(&pos, end, &did)) report_read_error(pos);
+}
+
+void
+QuartzPostList::read_start_of_chunk()
+{
+    om_docid increase_to_last;
+    if (!unpack_uint(&pos, end, &increase_to_last)) report_read_error(pos);
+    last_did_in_chunk = did + increase_to_last;
+
+    if (!unpack_uint(&pos, end, &wdf)) report_read_error(pos);
+    if (!unpack_uint(&pos, end, &doclength)) report_read_error(pos);
+}
+
+bool
+QuartzPostList::next_in_chunk()
+{
+    if (pos == end) return false;
+    om_docid did_increase;
+    if (!unpack_uint(&pos, end, &did_increase)) report_read_error(pos);
+    did += did_increase;
+    Assert(did <= last_did_in_chunk);
+
+    if (!unpack_uint(&pos, end, &wdf)) report_read_error(pos);
+    if (!unpack_uint(&pos, end, &doclength)) report_read_error(pos);
+
+    // Either not at last doc in chunk, or pos == end, but not both.
+    Assert(did < last_did_in_chunk || pos == end);
+    Assert(pos != end || did == last_did_in_chunk);
+
+    return true;
+}
+
+void
+QuartzPostList::next_chunk()
+{
+    cursor->next();
+    if (cursor->after_end()) {
+	is_at_end = true;
+	return;
+    }
+    const char * keypos = cursor->current_key.value.data();
+    const char * keyend = keypos + cursor->current_key.value.size();
+    std::string tname_in_key;
+
+    // Check we're still in same postlist
+    if (!unpack_string(&keypos, keyend, tname_in_key)) {
+	report_read_error(keypos);
+    }
+    if (tname_in_key != tname) {
+	is_at_end = true;
+	return;
+    }
+
+    om_docid newdid;
+    if (!unpack_uint_preserving_sort(&keypos, keyend, &newdid)) {
+	report_read_error(keypos);
+    }
+    if (newdid <= did) {
+	throw OmDatabaseCorruptError("Document ID in new chunk of postlist (" +
+		om_tostring(newdid) +
+		") is not greater than final document ID in previous chunk (" +
+		om_tostring(did) + ").");
+    }
+    did = newdid;
+
+    pos = cursor->current_tag.value.data();
+    end = pos + cursor->current_tag.value.size();
+    read_start_of_chunk();
+}
+
+PositionList *
+QuartzPostList::get_position_list()
+{
+    throw OmUnimplementedError("FIXME");
+}
+
+PostList *
+QuartzPostList::next(om_weight w_min)
+{
+    if (!next_in_chunk()) next_chunk();
+    return NULL;
+}
+
+PostList *
+QuartzPostList::skip_to(om_docid did, om_weight w_min)
+{
+    throw OmUnimplementedError("FIXME");
+}
+
+
+
+// Methods modifying position lists
+
---
+++ xapian-core/backends/quartz/quartz_postlist.h
@@ -0,0 +1,190 @@
+/* quartz_postlist.h: Postlists in quartz databases
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000 BrightStation PLC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#ifndef OM_HGUARD_QUARTZ_POSTLIST_H
+#define OM_HGUARD_QUARTZ_POSTLIST_H
+
+#include "config.h"
+#include "leafpostlist.h"
+#include "om/omtypes.h"
+#include <string>
+#include "quartz_table_entries.h"
+
+class QuartzTable;
+class QuartzCursor;
+class QuartzBufferedTable;
+class QuartzDatabase;
+
+/** A postlist in a quartz database.
+ */
+class QuartzPostList : public LeafPostList {
+    friend class QuartzDatabase;
+    private:
+	/** The database we are searching.  This pointer is held so that the
+	 *  database doesn't get deleted before us.
+	 */
+	RefCntPtr<const Database> this_db;
+
+	/// The average length of documents in this database.
+	om_doclength avlength;
+
+	/// The table containing the postlist.
+	const QuartzTable * table;
+
+	/// The termname for this postlist.
+	om_termname tname;
+
+
+	/// Cursor pointing to current chunk of postlist. 
+	QuartzCursor * cursor;
+
+	/// The last document id in this chunk;
+	om_docid last_did_in_chunk;
+
+	/// Position of iteration through current chunk.
+	const char * pos;
+
+	/// Byte after end of current chunk.
+	const char * end;
+
+
+        /// Document id we're currently at.
+	om_docid did;
+
+	/// The (absolute) length of the current document, 
+	om_termcount doclength;
+
+	/// The wdf of the current document.
+	om_termcount wdf;
+
+
+	/// Whether we've run off the end of the list yet.
+	bool is_at_end;
+
+	/// The number of entries in the posting list.
+	om_termcount number_of_entries;
+
+        /// Copying is not allowed.
+        QuartzPostList(const QuartzPostList &);
+
+        /// Assignment is not allowed.
+        void operator=(const QuartzPostList &);
+
+        /// Default constructor.
+        QuartzPostList(RefCntPtr<const Database> this_db_,
+		       om_doclength avlength_,
+		       const QuartzTable * table_,
+		       const om_termname & tname);
+
+	/// Make a key for accessing the postlist.
+	static void make_key(const om_termname & tname,
+			     om_docid did,
+			     QuartzDbKey & key);
+
+	/** Move to the next item in the chunk, if possible.
+	 *  If already at the end of the chunk, returns false.
+	 */
+	bool next_in_chunk();
+
+	/** Move to the next chunk.
+	 *
+	 *  If there are no more chunks in this postlist, this will set
+	 *  is_at_end to true.
+	 */
+	void next_chunk();
+
+	/** Read the number of entries in the posting list. 
+	 *  This must only be called when pos is pointing to the start of
+	 *  the first chunk of the posting list.
+	 */
+	void read_number_of_entries();
+
+	/** Read the docid of the first entry in the posting list.
+	 */
+	void read_first_docid();
+
+	/** Read the start of a chunk, including the first item in it.
+	 */
+	void read_start_of_chunk();
+
+	/** Report an error when reading the posting list.
+	 */
+	void report_read_error(const char * position);
+    public:
+
+        /// Destructor.
+        ~QuartzPostList();
+
+	/** Returns number of docs indexed by this term.
+	 *
+	 *  This is the length of the postlist.
+	 */
+	om_doccount   get_termfreq() const { return number_of_entries; }
+
+	/// Returns the current docid.
+	om_docid     get_docid() const { return did; }
+
+	/// Returns the normalised length of current document.
+	om_doclength get_doclength() const {
+	    return (om_doclength)doclength / avlength;
+	}
+
+	/** Returns the Within Document Frequency of the term in the current
+	 *  document.
+	 */
+	om_termcount get_wdf() const { return wdf; }
+
+	/** Get the list of positions of the term in the current document.
+	 */
+	PositionList *get_position_list();
+
+	/// Move to the next document.
+	PostList * next(om_weight w_min);
+
+	/// Skip to next document with docid >= docid.
+	PostList * skip_to(om_docid did, om_weight w_min);
+
+	/// Return true if and only if we're off the end of the list.
+	bool       at_end() const { return is_at_end; }
+
+	/// Get a description of the document.
+	std::string get_description() const;
+
+	////////////////////
+
+#if 0
+	/// Insert an entry
+	static void set_entry(QuartzBufferedTable * table,
+			      const om_termname & tname,
+			      om_docid did,
+			      om_termcount wdf,
+			      om_doclength doclen);
+
+	/// Delete an entry
+	static void delete_entry(QuartzBufferedTable * table,
+				 const om_termname & tname,
+				 om_docid did);
+#endif
+
+};
+
+#endif /* OM_HGUARD_QUARTZ_POSTLIST_H */
