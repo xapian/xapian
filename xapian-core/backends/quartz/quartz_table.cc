bbfd88524ad42f33437a5c6c2f26d4dbf0ca196a
Richard Boulton <richard@tartarus.org>
2000-12-15 11:48:06 +0000

    Fix segfault in quartz_table
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2924 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/quartz/quartz_table.cc
+++ xapian-core/backends/quartz/quartz_table.cc
@@ -614,7 +614,9 @@ QuartzBufferedTable::get_exact_entry(const QuartzDbKey &key,
 				     QuartzDbTag & tag) const
 {
     if (changed_entries.have_entry(key)) {
-	tag = *(changed_entries.get_tag(key));
+	QuartzDbTag * tagptr = changed_entries.get_tag(key);
+	if (tagptr == 0) return false;
+	tag = *tagptr;
 	return true;
     }
 
