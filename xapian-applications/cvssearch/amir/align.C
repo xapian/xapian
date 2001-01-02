6afb24a8a0c1793ffab672f5af8d37d1bab02c31
Amir Michail <amir>
2001-01-02 09:36:46 +0000

    added gpl copyright
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2950 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/cvssearch/amir/align.C
+++ xapian-applications/cvssearch/amir/align.C
@@ -1,3 +1,12 @@
+// align.C
+//
+// (c) 2001 Amir Michail (amir@users.sourceforge.net)
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+
 // align file1 file2
 
 #include <string>
