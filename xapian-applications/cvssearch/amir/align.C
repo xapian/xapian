e61ab5962c7443d9af2271df0a92572a6d51969a
Amir Michail <amir>
2001-01-13 00:57:18 +0000

    update
    git-svn-id: file:///home/xapian-svn/xapian/trunk@2994 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/cvssearch/amir/align.C
+++ xapian-applications/cvssearch/amir/align.C
@@ -9,6 +9,10 @@
 
 // align file1 file2
 
+#define GNU_DIFF_MARKER_COL 64
+#define DIFF_OUTPUT "/tmp/diff_output"
+#define DEBUG_MODE 1
+
 #include <string>
 #include <vector>
 #include <list>
@@ -25,7 +29,7 @@ public:
   Alignment( type &s, type &t ) : S(s), T(t) {
 
     // initialize V to zero's 
-    //    cerr << "Initializing." << endl;
+    //    if ( DEBUG_MODE ) cerr << "Initializing." << endl;
 
     for(int i = 0; i <= S.size(); i++ ) {
 
@@ -33,14 +37,14 @@ public:
       V.push_back(empty);
 
       for( int j = 0; j <= T.size(); j++ ) {
-	//	cerr << "i = " << i << " and j = " << j << endl;
+	//	if ( DEBUG_MODE ) cerr << "i = " << i << " and j = " << j << endl;
 	V[i].push_back(0);
       }
       assert( V[i].size() == T.size()+1 );
     }
     assert( V.size() == S.size()+1 );
 
-    //    cerr << "Done." << endl;
+    //    if ( DEBUG_MODE ) cerr << "Done." << endl;
 
   }
 
@@ -57,7 +61,7 @@ public:
     // recurrence
 
     for ( int i = 1; i <= S.size(); i++ ) {
-      if ( debug ) cerr << "..." << i << " / " << S.size() << endl;
+      if ( debug ) if ( DEBUG_MODE ) cerr << "..." << i << " / " << S.size() << endl;
       for ( int j = 1; j <= T.size(); j++ ) {
 	int v = V[i-1][j-1] + S.score( S[i], T[j] );
 	v = max( v, V[i-1][j] + S.score(S[i], T.space()) );
@@ -68,12 +72,12 @@ public:
   }
 
   void dump() {
-    cerr << "..dump.." << endl;
+    if ( DEBUG_MODE ) cerr << "..dump.." << endl;
     for (int i = 0; i <= S.size(); i++ ) {
       for (int j = 0; j <= T.size(); j++ ) {
-	cerr << V[i][j] << " ";
+	if ( DEBUG_MODE ) cerr << V[i][j] << " ";
       }
-      cerr << endl;
+      if ( DEBUG_MODE ) cerr << endl;
     }
   }
 
@@ -94,7 +98,7 @@ public:
     while ( i >0 || j > 0 ) {
 
       int v = V[i][j];
-      //      cerr << "v = " << v << endl;
+      //      if ( DEBUG_MODE ) cerr << "v = " << v << endl;
       
       if ( i>0 && j >0 && v == V[i-1][j-1] + S.score( S[i], T[j] ) ) {
 	s.push_front( S[i] );
@@ -118,7 +122,7 @@ public:
     list<string>::iterator is = s.begin();
     list<string>::iterator it = t.begin();
     for ( int i = 0; i < s.size(); i++ ) {
-      cerr << (*is) << "  |  " << (*it) << endl;
+      if ( DEBUG_MODE ) cerr << (*is) << "  |  " << (*it) << endl;
       is++;
       it++;
     }
@@ -185,7 +189,7 @@ public:
   LineSequence( const string& f ) {
     ifstream in(f.c_str());
     if ( ! in ) {
-      cerr << "Couldn't open " << f << endl;
+      if ( DEBUG_MODE ) cerr << "Couldn't open " << f << endl;
       abort();
     }
     
@@ -199,12 +203,20 @@ public:
       string line2 = trim(line);
 
       lines.push_back(line2);
-      // cerr << "pushed line -" << line << "-" << endl;
+      // if ( DEBUG_MODE ) cerr << "pushed line -" << line << "-" << endl;
     }
 
     in.close();
 
-    cerr << "...read " << lines.size() << " lines from " << f << endl;
+    if ( DEBUG_MODE ) cerr << "...read " << lines.size() << " lines from " << f << endl;
+  }
+
+  LineSequence() {
+  }
+
+  void addLine( const string& line ) {
+    string line2 = trim(line);
+    lines.push_back(line2);
   }
 
   int size() {
@@ -227,7 +239,7 @@ public:
 
     alignment.findOptimalAlignment();
 
-    //    cerr << "optimal alignment of " << l1 << " and " << l2 << " is " << alignment.optimalAlignmentValue() << endl;
+    //    if ( DEBUG_MODE ) cerr << "optimal alignment of " << l1 << " and " << l2 << " is " << alignment.optimalAlignmentValue() << endl;
     //    alignment.dump();
     return alignment.optimalAlignmentValue();
 
@@ -235,6 +247,110 @@ public:
 
 };
 
+void processDiffOutput( const string& f, LineSequence& l1, LineSequence& l2 ) {
+
+  ifstream in(f.c_str());
+  if ( ! in ) {
+    if ( DEBUG_MODE ) cerr << "Couldn't open " << f << endl;
+    abort();
+  }
+
+  int blockSize1 = 0;
+  int blockSize2 = 0;
+
+  bool foundChangeMarker = false;
+
+  int line_num_1 = 1;
+  int line_num_2 = 1;
+
+  int blockStart1 = 0;
+  int blockStart2 = 0;
+  
+  while ( ! in.eof() ) {
+    string line;
+    getline( in, line, '\n' );
+    
+    //    if ( DEBUG_MODE ) cerr << line << endl;
+    char marker = 0;
+    if ( line != "" ) {
+      marker = line[GNU_DIFF_MARKER_COL];
+    }
+    //    if ( DEBUG_MODE ) cerr << marker << endl;
+        
+    if ( marker == '|' ) {
+      foundChangeMarker = true;
+    }
+    if ( marker == '|' || marker == '<' || marker == '>' ) {
+
+      if ( marker == '|' ) {
+	blockSize1++;
+	blockSize2++;
+      } else if ( marker == '<' ) {
+	blockSize1++;
+      } else if ( marker == '>' ) {
+	blockSize2++;
+      }
+
+      if ( blockStart1 == 0 ) {
+	assert( blockStart2 == 0 );
+	blockStart1 = line_num_1;
+	blockStart2 = line_num_2;
+      }
+    } else {
+      if ( foundChangeMarker ) {
+	assert( blockSize1 > 0 );
+	assert( blockSize2 > 0 );
+
+	if ( DEBUG_MODE ) cerr << "\n\n\n*********** Found change block at lines " << blockStart1 << " and " << blockStart2 << endl;
+
+	LineSequence b1, b2;
+
+	// line numbers are with respect to line sequences...
+	if ( DEBUG_MODE ) cerr << "Block 1 source" << endl;
+	for (int i = blockStart1; i < blockStart1 + blockSize1; i++ ) {
+	  if ( DEBUG_MODE ) cerr << "=>" + l1[i] << endl;
+	  b1.addLine(l1[i]);
+	}
+
+	if ( DEBUG_MODE ) cerr << endl;
+	if ( DEBUG_MODE ) cerr << "Block 2 source" << endl;
+	for (int i = blockStart2; i < blockStart2 + blockSize2; i++ ) {
+	  if ( DEBUG_MODE ) cerr << "=>" + l2[i] << endl;
+	  b2.addLine(l2[i]);
+	}	
+
+	Alignment<LineSequence> alignment( b1, b2 );
+	alignment.findOptimalAlignment( );
+	if ( DEBUG_MODE ) cerr << "*** optimal alignment value is " << alignment.optimalAlignmentValue() << endl;
+	alignment.reconstruct();
+
+      }
+      blockStart1 = 0;
+      blockStart2 = 0;
+      blockSize1 = 0;
+      blockSize2 = 0;
+      foundChangeMarker = false;
+    }
+
+    if ( marker != '|' && marker != ' ' && marker != 0 ) {
+      if ( marker == '<' ) {
+	line_num_1++;
+      } else if ( marker == '>' ) {
+	line_num_2++;
+      } else {
+	if ( DEBUG_MODE ) cerr << "** found marker '" << marker << "'" << endl;
+	assert(0);
+      }
+    } else {
+      // blank line found in diff
+      line_num_1++;
+      line_num_2++;
+    }
+  }
+  
+  in.close();
+  
+}
 
 int main( int argc, char *argv[] ) {
   
@@ -243,13 +359,19 @@ int main( int argc, char *argv[] ) {
   string f1 = argv[1];
   string f2 = argv[2];
 
-  cerr << "Comparing " << f1 << " with " << f2 << "." << endl;
-  
+  if ( DEBUG_MODE ) cerr << "Comparing " << f1 << " with " << f2 << "." << endl;
+
+  // the first thing to do is to call GNU diff to get an approximate
+  // alignment
+
+  string diff_cmd = "diff --expand-tabs --ignore-space-change --side-by-side " + f1 + "  " + f2 + "> " + DIFF_OUTPUT;
+  int rc = system(diff_cmd.c_str());
+
+  if ( DEBUG_MODE ) cerr << "GNU diff returned " << rc << endl;
+
   LineSequence l1(f1);
   LineSequence l2(f2);
-  Alignment<LineSequence> alignment( l1, l2 );
-  alignment.findOptimalAlignment( 1 );
-  cerr << "*** optimal alignment value is " << alignment.optimalAlignmentValue() << endl;
-  alignment.reconstruct();
 
+  processDiffOutput( DIFF_OUTPUT, l1, l2 );
+  
 }
