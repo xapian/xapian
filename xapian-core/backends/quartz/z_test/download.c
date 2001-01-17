32caa3e64d8c46a6f781b2caee0169a3b7d55115
Richard Boulton <richard@tartarus.org>
2001-01-17 18:14:28 +0000

    Fix so that it will compile with C++ version.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3019 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/quartz/z_test/download.c
+++ xapian-core/backends/quartz/z_test/download.c
@@ -1,38 +1,41 @@
 
 #include <stdio.h>   /* main etc */
 #include "btree.h"
-
-#define true 1
-#define unless(E) if(!(E))
+#include "om/omerror.h"
 
 int main(int argc, char * argv[])
 {
-    struct Btree * B = Btree_open_to_read("B/");
-    struct Bcursor * BC = Bcursor_create(B);
-    struct Btree_item * item = Btree_item_create();
+    try {
+	Btree btree;
+	if (!btree.open_to_read("B/"))
+	    return 1;
 
-    int i;
+	AutoPtr<Bcursor> BC = Bcursor_create(&btree);
+	struct Btree_item * item = Btree_item_create();
 
-    Bcursor_find_key(BC, "", 0);
+	int i;
 
-    while(true)
-    {
-        unless (Bcursor_get_key(BC, item)) break;
-        Bcursor_get_tag(BC, item);
-        if (item->key_len != 0)
-        {   printf("+");
-            for (i = 0; i < item->key_len; i++) printf("%c", item->key[i]);
+	BC->find_key((byte *)"", 0);
 
-            printf(" ");
-            for (i = 0; i < item->tag_len; i++) printf("%c", item->tag[i]);
+	while(BC->get_key(item))
+	{
+	    BC->get_tag(item);
 
-            printf("\n");
-        }
+	    if (item->key_len != 0)
+	    {
+		printf("+");
+		for (i = 0; i < item->key_len; i++) printf("%c", item->key[i]);
+
+		printf(" ");
+		for (i = 0; i < item->tag_len; i++) printf("%c", item->tag[i]);
+
+		printf("\n");
+	    }
+	}
+
+	Btree_item_lose(item);
+    } catch(OmError & e) {
     }
-
-    Btree_item_lose(item);
-    Bcursor_lose(BC);
-    Btree_quit(B);
     return 0;
 }
 
