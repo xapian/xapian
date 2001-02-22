079762fbb84ed70c976bbf487c9cd408029907db
Andrew Y. Yao <andrewy>
2001-02-22 01:39:20 +0000

    Oops.. forgot to add this one.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3165 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-applications/cvssearch/db/cvs_db.C
@@ -0,0 +1,77 @@
+/************************************************************
+ *
+ *  cvs_db.C implementation.
+ *
+ *  (c) 2001 Andrew Yao (andrewy@users.sourceforge.net)
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  $Id$
+ *
+ ************************************************************/
+
+#include "cvs_db.h"
+
+int
+cvs_db::open(const string & filename) 
+{
+    int val = 0;
+    if (_opened || (val = do_open(filename)) == 0)
+    {
+        _opened = true;
+    }
+    return val;
+}
+
+int
+cvs_db::close(int flags = 0) {
+    try {
+        _opened = false;
+        return _db.close(flags);
+    }  catch (DbException& e ) {
+        cerr << "SleepyCat Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+int
+cvs_db::remove(const string & filename, int flags = 0) 
+{
+    try {
+        close();
+        return _db.remove(filename.c_str(), _db_name.c_str(), flags);
+    }  catch (DbException& e ) {
+            cerr << "SleepyCat Exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
+int
+cvs_db::sync()
+{
+   cout << "BEGIN SYNCRONISING" << endl;
+   int val = 0;
+   try {
+      if (_opened)
+      {
+         val = _db.sync(0);
+      }
+   } catch (DbException &e) {
+        cerr << "SleepyCat Exception: " << e.what() << endl;
+   }
+   cout << "DONE  SYNCRONISING" << endl;
+
+   return val;
+}
