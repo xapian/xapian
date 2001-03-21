d59a653049738e510a46efba7f493ec43efeead2
Andrew Y. Yao <andrewy>
2001-03-21 02:03:12 +0000

    update rev/bar to C/G fixed footnote bug.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3367 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/cvssearch/perl/Match-script.pl
+++ xapian-applications/cvssearch/perl/Match-script.pl
@@ -292,7 +292,7 @@ print "</body></html>";
 sub filename{
 	my ($name) = @_;
 	$num = scalar(keys %lineMAPinfo);
-	print Cvssearch::fileheader("<b>$num</b> Matched lines for <b>$name</b>", "Move over rev/bar to see comment; Click rev/bar to make it stick/unstick; Click matched line to see its context below");
+	print Cvssearch::fileheader("<b>$num</b> Matched lines for <b>$name</b>", "Move over C/G to see comment; Click C/G to make it stick/unstick; Click matched line to see its context below");
     
 }
 
