bb4f2219b4b2123e1428a3bda118a2df5fe1803c
Amir Michail <amir>
2001-04-12 03:42:06 +0000

    update
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3448 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/cvssearch/db/cvsminesearch.C
+++ xapian-applications/cvssearch/db/cvsminesearch.C
@@ -24,11 +24,44 @@
 
 // (1) no query words, no antecedent or consequent
 //
-//  simply returns most used classes & functions, ranked by % of commits that they are part of
+//  simply returns most used classes & functions
+//
+// SCORE: % of commits that they are part of
 //
 //  SEE:  rank_all_items 
 
-// 
+// (2) no query words, but have antecedent or consequent
+//
+// in this case, we consider rules A => B
+//
+//  SCORE:  log( conf(A=>B) / conf(B) ) x support(A,B)
+//
+//  Here conf(B) and support are computed with respect to the entire commit list.
+//
+//  SEE:  considerRule
+
+// (3) query words, no antecedent or consequent
+//
+// returns classes/functions that tend to be used given the query Q
+//
+// SCORE:  log ((% of commits containing Q) / (% of all commits containing the item)) x support(A,Q)
+//
+// SEE:  rank_single_items
+
+
+// (4) query words, but have antecedent or consequent
+//
+// in this case, we consider rules A => B
+//
+//  SCORE:  log( conf(A=>B) / conf(B) ) x support(A,B)
+//
+//  Here conf(B) is computed with respect to the entire commit list.
+//  support(A,B) is computed only with respect to the commits containing Q.
+//
+//  SEE:  considerRule
+//
+//  NOTE:  We do not require confidence of rule to increase in presence of Q.
+
 
 
 
@@ -80,7 +113,6 @@ void rank_single_items(        map< string, int>& item_count,
 	int con_count = atoi( (char*) data.get_data() );
 	double conf_a = 100.0*(double) con_count / (double) total_commit_transactions;
 	//	cerr << "...conf of consequent " << conf_a << endl;
-#warning "division too strict, so let's try log"
 	c = log(c / conf_a);
 	//	cerr << "...conf imp " << c << endl;
 	
