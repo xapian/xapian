8384ae67586320de1d70cf2b818021836b009e9d
Amir Michail <amir>
2001-06-30 23:59:24 +0000

    minor
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3592 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/cvssearch/perl/TopComment-script.pl
+++ xapian-applications/cvssearch/perl/TopComment-script.pl
@@ -49,7 +49,7 @@ if(param()){
     print "<a href=\"$cvscompare?root=$root&pkg=$pkg\" target=\"s\">[$pkg]</a></b>\n";
     print "<td align=center><b>Similar Commits in [<a href=\"$related?query=$pkg\@$commit_id%20in:$pkg&root=$root\" target=s>$pkg</a> / <a href=\"$related?query=$pkg\@$commit_id&root=$root\" target=s>All Packages</a>]</b></td>\n";
     print "<td align=right><a href=\"./Query.cgi\" target=_top>Search Again</a></td>\n";
-    print "<tr></tr>\n";
+#    print "<tr></tr>\n";
 #    print "<td align=left><b>Similar Commits in [<a href=\"$related?query=$pkg\@$commit_id%20in:$pkg&root=$root\" target=s>$pkg</a> / <a href=\"$related?query=$pkg\@$commit_id&root=$root\" target=s>All Packages</a>]</b></td>\n";
     print "</tr>\n";
     print "</table>\n";
