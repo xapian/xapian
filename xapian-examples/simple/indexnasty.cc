488aad04ac057e6ce5e2bfa404d42e528d1e831d
Sam Liddicott <sam@liddicott.com>
2001-08-23 16:08:48 +0000

    This is a simple indexer of a complicated file format that would be better served by xml or some other means, but it does immediatly let people play with adding complicated records and prefixes and such to a db.
    I know it's kack, no complaints, but comments in their season are welcome
    
    
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3617 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-examples/simple/indexnasty.cc
@@ -0,0 +1,218 @@
+/* indexnasty.cc based on simpleindex.cc and omindex.cc
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000,2001 BrightStation PLC
+ * Copyright 2001 Sam Liddicott
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+/*
+
+File format is multiple segments separated by blank lines, each segment
+representing a record.
+
+A segment consists of multiple single line fields with a header,
+
+<header>=<data>
+
+A head may optionally be prefixed with ~, followed by an optional ':' 
+terminated prefix, followed by am optional field name; 
+
+i.e.
+  [~[prefix:]][fieldname]=
+
+Prefix and fieldname may be null.
+If ~ is specified then the data is stemmed for terms.
+If a prefix is specified then terms are prefixed.
+If neither ~ or a prefix or a : are specified the field is not used for terms
+
+If a fieldname is specified then the data is stored as a line in the document
+data field.
+
+e.g.
+
+~P:P1=Stem terms with prefix P, also store in field P1
+Q:Q1=Don't stem terms but give prefix Q, also store in field Q1
+~:R1=Stem terms, no prefix and sore as R1
+:S1=No Stem terms, no prefix and store as S1
+~T:=Stem terms, prefix as T, no store
+U:=No stem terms , prefix U, no store
+~:=Stem terms, no prefix, no store
+:=No stem terms, no prefix, no store
+X=No terms, but still store
+
+~:Text=Jack and Jill went up the hill
+
+*/
+
+#include <memory>
+#include <fstream>
+#include <om/om.h>
+
+om_docid IndexNastyFile(string Filepath, OmWritableDatabase &database, OmStem &stemmer);
+
+int main(int argc, char *argv[])
+{
+    // Simplest possible options parsing: we just require three or more
+    // parameters.
+    if(argc < 2) {
+	std::cout << "usage: " << argv[0] <<
+		" <path to database> [<filename>]" <<
+		std::endl;
+	exit(1);
+    }
+    
+    // Catch any OmError exceptions thrown
+    try {
+	// Make the database
+	OmSettings settings;
+	settings.set("backend", "quartz");
+	settings.set("quartz_dir", argv[1]);
+	OmWritableDatabase database(settings);
+
+	OmStem stemmer("english"); 
+
+	// Read file/s
+        om_docid docid;
+	if (argc==2) docid=IndexNastyFile("",database,stemmer);
+        else for (int i=2; i < argc; i++) {
+          docid=IndexNastyFile(argv[i],database,stemmer);
+        }
+
+    }
+    catch(OmError &error) {
+	std::cout << "Exception: "  << error.get_msg() << std::endl;
+    }
+}
+
+static void
+lowercase_term(om_termname &term)
+{
+    om_termname::iterator i = term.begin();
+    while(i != term.end()) {
+        *i = tolower(*i);
+        i++;
+    }
+} 
+
+static om_termpos
+index_text(const string &s, OmDocument &doc, OmStem &stemmer, om_termpos pos)
+{
+    size_t i, j = 0, k;
+    while ((i = s.find_first_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                "abcdefghijklmnopqrstuvwxyz", j))
+           != string::npos) {
+ 
+        j = s.find_first_not_of("ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                "abcdefghijklmnopqrstuvwxyz"
+                                "0123456789", i);
+        k = s.find_first_not_of("+-", j);
+        if (k == string::npos || !isalnum(s[k])) j = k;
+        om_termname term = s.substr(i, j - i);
+        lowercase_term(term);
+        if (isupper(s[i]) || isdigit(s[i])) {
+          doc.add_posting(term, pos);
+        }
+ 
+        term = stemmer.stem_word(term);
+        doc.add_posting(term, pos++);
+        i = j + 1;
+    }
+    return pos;
+}                           
+
+om_docid
+IndexNastyFile(string Filepath, OmWritableDatabase &database, OmStem &stemmer) {
+  std::ifstream stream(Filepath.c_str());
+  if (! stream) {
+    std::cout << "Can't open file " << Filepath << std::endl;
+    return 0;
+  }
+
+  // Make the document
+  OmDocument newdocument;
+  om_docid docid=0;
+
+
+  while(! stream.eof()) {
+    docid=0;
+    string data="";
+    int wordcount=0;
+    string line;
+
+    while (getline(stream,line)) {
+      if (! line.length()) break;
+
+      bool stem, term;
+      string prefix="";
+      string field="";
+      string header;
+      int index=0;
+      int cursor=0;
+
+      if ( (cursor=line.find_first_of("=",index))!=string::npos) {
+        int hindex=0;
+        int hcursor;
+        header=line.substr(index,cursor-index);
+        if (! header.length()) docid=atoi(line.substr(cursor+1).c_str());
+  std::cout << "Docid " << docid << " " << line.substr(cursor+1).c_str() << std::endl;
+        // if header has no : but has ~ then stem no prefix no store
+        // if header has no : but has not ~ then just store 
+
+        if (stem=(header[hindex]=='~')) hindex++;
+
+        // now scan up to colon to get terms prefix
+        if (term=( (hcursor=header.find_first_of(":",hindex)) !=string::npos)) {
+          prefix=header.substr(hindex,hcursor-hindex);
+          field=header.substr(hcursor+1,header.length()-hcursor-1);
+        } else { // No ':' 
+          if (term=stem) prefix=header.substr(hindex,header.length()-hindex);
+          else field=header;
+        }
+
+        cursor++; // skip past '='
+        string text=line.substr(cursor,line.length()-cursor);
+
+        if (field.length()) data+=field+"="+text+"\n";
+
+        // now index field if required
+        if (term) {
+          if (stem) index_text(text, newdocument, stemmer, wordcount);
+          else newdocument.add_posting(prefix+text,wordcount++);
+        }
+      } else std::cout << "Bad line: " << cursor << " " << line << std::endl;
+    }
+
+    // Put the data in the document
+    newdocument.set_data(data);
+
+    // Add the document to the database
+    if (docid) {
+      try {
+        database.replace_document(docid,newdocument);
+      } catch (...) {
+        docid=database.add_document(newdocument);
+      }
+    }
+    else docid=database.add_document(newdocument);
+
+    std::cout << docid << std::endl;
+  }
+
+  return docid;
+}
