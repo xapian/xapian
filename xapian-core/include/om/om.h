ead9e1d66918b83f925efb4f97a02f29c8b33228
Olly Betts <olly@survex.com>
2002-04-04 19:19:39 +0000

    Removed XML indexer stuff from the main xapian library.  All the removed bits are now in the indexer subdirectory, ready to be reassembled into a supplementary library.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3816 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/om/om.h
+++ xapian-core/include/om/om.h
@@ -2,6 +2,7 @@
  *
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
+ * Copyright 2002 Ananova Ltd
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -43,17 +44,6 @@
 #include "om/omenquire.h"
 #include "om/omquery.h"
 #include "om/omexpanddecider.h"
-
-/* Indexing */
-#include "om/omindexer.h"
-#include "om/omindexerbuilder.h"
-#include "om/omindexercommon.h"
-#include "om/omindexerdesc.h"
-
-/* Extending the indexing system */
-#include "om/omindexermessage.h"
-#include "om/omindexernode.h"
-#include "om/omnodedescriptor.h"
 
 /* Stemming */
 #include "om/omstem.h"
---
+++ xapian-core/indexer/om_indexer.h
@@ -0,0 +1,12 @@
+/* Indexing */
+#include "om/omindexer.h"
+#include "om/omindexerbuilder.h"
+#include "om/omindexercommon.h"
+#include "om/omindexerdesc.h"
+
+/* Extending the indexing system */
+#include "om/omindexermessage.h"
+#include "om/omindexernode.h"
+#include "om/omnodedescriptor.h"
+
+
