1f6e52b359be0f89ffb3c958408303b50d1b3485
Olly Betts <olly@survex.com>
2002-05-14 11:38:40 +0000

    omindex.cc: cleaned up a little.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@3995 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omindex.cc
+++ xapian-applications/omega/omindex.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001 James Aylett
- * Copyright 2001 Ananova Ltd
+ * Copyright 2001,2002 Ananova Ltd
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -50,6 +50,8 @@ using std::cout;
 using std::cerr;
 using std::endl;
 using std::find;
+using std::ifstream;
+using std::auto_ptr;
 
 #define OMINDEX "omindex"
 #define VERSION "1.0"
@@ -177,7 +179,7 @@ p_notplusminus(unsigned int c)
 static om_termpos
 index_text(const string &s, OmDocument &doc, OmStem &stemmer, om_termpos pos)
 {
-    std::string::const_iterator i, j = s.begin(), k;
+    string::const_iterator i, j = s.begin(), k;
     while ((i = find_if(j, s.end(), p_alnum)) != s.end()) {
         j = find_if(i, s.end(), p_notalnum);
         k = find_if(j, s.end(), p_notplusminus);
@@ -208,7 +210,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
     }
 
     if (mimetype == "text/html") {
-	std::ifstream in(file.c_str());
+	ifstream in(file.c_str());
 	if (!in) {
 	    cout << "can't open \"" << file << "\" - skipping\n";
 	    return;
@@ -231,7 +233,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
 	keywords = p.keywords;
 	sample = p.sample;
     } else if (mimetype == "text/plain") {
-	std::ifstream in(file.c_str());
+	ifstream in(file.c_str());
 	if (!in) {
 	    cout << "can't open \"" << file << "\" - skipping\n";
 	    return;
@@ -256,7 +258,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
 	    cout << "pdftotext failed to extract text from \"" << file << "\" - skipping\n";
 	    return;
 	}
-	std::ifstream in(tmp.c_str());
+	ifstream in(tmp.c_str());
 	unlink(tmp.c_str());
 	if (!in) {
 	    cout << "pdftotext failed to extract text from \"" << file << "\" - skipping\n";
@@ -282,7 +284,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
 	    cout << "pstotext failed to extract text from \"" << file << "\" - skipping\n";
 	    return;
 	}
-	std::ifstream in(tmp.c_str());
+	ifstream in(tmp.c_str());
 	unlink(tmp.c_str());
 	if (!in) {
 	    cout << "pstotext failed to extract text from \"" << file << "\" - skipping\n";
@@ -317,7 +319,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
     // Put the data in the document
     OmDocument newdocument;
     string record = "url=" + baseurl + url + "\nsample=" + sample;
-    if (title != "") record = record + "\ncaption=" + title;
+    if (!title.empty()) record = record + "\ncaption=" + title;
     record = record + "\ntype=" + mimetype;
     newdocument.set_data(record);
 
@@ -359,7 +361,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod)
     if (dupes==DUPE_replace && db->term_exists("U" + baseurl + url)) {
 	// This document has already been indexed - update!
 	try {
-	    std::auto_ptr<OmEnquire> enq = std::auto_ptr<OmEnquire>(new OmEnquire(*db));
+	    auto_ptr<OmEnquire> enq = auto_ptr<OmEnquire>(new OmEnquire(*db));
 	    enq->set_query(OmQuery("U" + baseurl + url));
 	    OmMSet mset = enq->get_mset(0, 1);
 	    try {
@@ -402,9 +404,7 @@ index_directory(const string &dir, const map<string, string>& mime_map)
 	    continue;
 	}
 	if (S_ISDIR(statbuf.st_mode)) {
-	    if (!recurse) {
-		continue;
-	    }
+	    if (!recurse) continue;
 	    try {
 		index_directory(url, mime_map);
 	    }
@@ -436,41 +436,18 @@ main(int argc, char **argv)
 {
     // getopt
     char* optstring = "hvd:D:U:M:l";
-    struct option longopts[8];
-    int longindex, getopt_ret;
+    struct option longopts[8] = {
+	{ "help",	0,			NULL, 'h' },
+	{ "version",	0,			NULL, 'v' },
+	{ "duplicates",	required_argument,	NULL, 'd' },
+	{ "db",		required_argument,	NULL, 'D' },
+	{ "url",	required_argument,	NULL, 'U' },
+	{ "mime-type",	required_argument,	NULL, 'M' },
+	{ "no-recurse",	0,			NULL, 'l' },
+	{ 0, 0, NULL, 0 }
+    };
 
-    longopts[0].name = "help";
-    longopts[0].has_arg = 0;
-    longopts[0].flag = NULL;
-    longopts[0].val = 'h';
-    longopts[1].name = "version";
-    longopts[1].has_arg = 0;
-    longopts[1].flag = NULL;
-    longopts[1].val = 'v';
-    longopts[2].name = "duplicates";
-    longopts[2].has_arg = required_argument;
-    longopts[2].flag = NULL;
-    longopts[2].val = 'd';
-    longopts[3].name = "db";
-    longopts[3].has_arg = required_argument;
-    longopts[3].flag = NULL;
-    longopts[3].val = 'D';
-    longopts[4].name = "url";
-    longopts[4].has_arg = required_argument;
-    longopts[4].flag = NULL;
-    longopts[4].val = 'U';
-    longopts[5].name = "mime-type";
-    longopts[5].has_arg = required_argument;
-    longopts[5].flag = NULL;
-    longopts[5].val = 'M';
-    longopts[6].name = "no-recurse";
-    longopts[6].has_arg = 0;
-    longopts[6].flag = NULL;
-    longopts[6].val = 'l';
-    longopts[7].name = 0;
-    longopts[7].has_arg = 0;
-    longopts[7].flag = 0;
-    longopts[7].val = 0;
+    int longindex, getopt_ret;
 
     map<string, string> mime_map = map<string, string>();
     mime_map["txt"] = "text/plain";
@@ -488,29 +465,28 @@ main(int argc, char **argv)
 	switch (getopt_ret) {
 	case 'h':
 	    cout << OMINDEX << endl
-		 << "Usage: " << argv[0] << " [OPTION] --db DATABASE "
-		 << endl << "\t--url BASEURL [BASEDIRECTORY] DIRECTORY" << endl << endl
-		 << "Index static website data via the filesystem." << endl
-		 << "  -d, --duplicates\tset duplicate handling" << endl
-		 << "  \t\t\tone of `ignore', `replace', `duplicate'" << endl
-		 << "  -D, --db\t\tpath to database to use" << endl
-		 << "  -U, --url\t\tbase url DIRECTORY represents" << endl
-	         << "  -M, --mime-type\tadditional MIME mapping ext:type" << endl
-		 << "  -l, --no-recurse\tonly process the given directory" << endl
-		 << "  -h, --help\t\tdisplay this help and exit" << endl
-		 << "  -v --version\t\toutput version and exit" << endl << endl
-		 << "Report bugs via the web interface at:" << endl
-		 << "<http://sourceforge.net/tracker/?func=add&group_id=35626&atid=414875>" << endl;
+		 << "Usage: " << argv[0] << " [OPTION] --db DATABASE\n"
+		 << "\t--url BASEURL [BASEDIRECTORY] DIRECTORY\n\n"
+		 << "Index static website data via the filesystem.\n"
+		 << "  -d, --duplicates\tset duplicate handling\n"
+		 << "  \t\t\tone of `ignore', `replace', `duplicate'\n"
+		 << "  -D, --db\t\tpath to database to use\n"
+		 << "  -U, --url\t\tbase url DIRECTORY represents\n"
+	         << "  -M, --mime-type\tadditional MIME mapping ext:type\n"
+		 << "  -l, --no-recurse\tonly process the given directory\n"
+		 << "  -h, --help\t\tdisplay this help and exit\n"
+		 << "  -v, --version\t\toutput version and exit\n\n"
+		 << "Report bugs via the web interface at:\n"
+		 << "<http://xapian.org/bugs/>" << endl;
 	    return 0;
-	    break;
 	case 'v':
-	    cout << OMINDEX << " (omega) " << VERSION << endl;
-	    cout << "(c) Copyright 1999,2000,2001 BrightStation PLC." << endl;
-	    cout << "(c) Copyright 2001 James Aylett" << endl << endl;
-	    cout << "This is free software, and may be redistributed under" << endl;
-	    cout << "the terms of the GNU Public License." << endl;
+	    cout << OMINDEX << " (omega) " << VERSION << "\n"
+		 << "Copyright (c) 1999,2000,2001 BrightStation PLC.\n"
+		 << "Copyright (c) 2001 James Aylett\n"
+		 << "Copyright (c) 2001,2002 Ananova Ltd\n\n"
+		 << "This is free software, and may be redistributed under\n"
+		 << "the terms of the GNU Public License." << endl;
 	    return 0;
-	    break;
 	case 'd': // how shall we handle duplicate documents?
 	    switch (optarg[0]) {
 	    case 'd':
@@ -530,11 +506,12 @@ main(int argc, char **argv)
 	case 'M':
 	    {
 		char* s;
-		if ((s = strchr(optarg, ':'))!=NULL && s[1]!=0) {
-		    mime_map[string(optarg, s - optarg)] = string(s+1);
+		if ((s = strchr(optarg, ':')) != NULL && s[1] != '\0') {
+		    mime_map[string(optarg, s - optarg)] = string(s + 1);
 		} else {
-		    cerr << "Illegal MIME mapping '" << optarg << "'" << endl;
-		    cerr << "Should be of the form ext:type, eg txt:text/plain" << endl;
+		    cerr << "Illegal MIME mapping '" << optarg << "'\n"
+			 << "Should be of the form ext:type, eg txt:text/plain"
+			 << endl;
 		    return 1;
 		}
 	    }
@@ -552,11 +529,11 @@ main(int argc, char **argv)
 	}
     }
 
-    if (dbpath=="") {
+    if (dbpath.empty()) {
 	cerr << OMINDEX << ": you must specify a database with --db.\n";
 	return 1;
     }
-    if (baseurl=="") {
+    if (baseurl.empty()) {
 	cerr << OMINDEX << ": you must specify a base URL with --url.\n";
 	return 1;
     }
@@ -564,7 +541,7 @@ main(int argc, char **argv)
     // (//thing is different to /thing). We could probably make this
     // safe a different way, by ensuring that we don't put a leading '/'
     // on leafnames when scanning a directory, but this will do.
-    if (baseurl[baseurl.length()-1]=='/') {
+    if (baseurl[baseurl.length() - 1] == '/') {
 	cout << "baseurl has trailing '/' ... removing ... " << endl;
 	baseurl = baseurl.substr(0, baseurl.length()-1);
     }
@@ -574,9 +551,9 @@ main(int argc, char **argv)
 	return 1;
     }
     root = argv[optind];
-    if (optind+2 == argc) {
-	indexroot = argv[optind+1]; // relative to root
-	if (indexroot[0]!='/') {
+    if (optind + 2 == argc) {
+	indexroot = argv[optind + 1]; // relative to root
+	if (indexroot.empty() || indexroot[0] != '/') {
 	    indexroot = "/" + indexroot;
 	}
     } else {
@@ -598,10 +575,10 @@ main(int argc, char **argv)
 	//      cout << "\n\nNow we have " << db->get_doccount() << " documents.\n";
 	delete db;
     }
-    catch (OmError &e) {
+    catch (const OmError &e) {
 	cout << "Exception: " << e.get_msg() << endl;
     }
-    catch (string &s) {
+    catch (const string &s) {
 	cout << "Exception: " << s << endl;
     }
     catch (const char *s) {
