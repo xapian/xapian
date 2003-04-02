24a90788ad9fc611649d8a81c5ce59bd15948920
Olly Betts <olly@survex.com>
2003-04-02 15:55:37 +0000

    tests/internaltest.cc: More Sun C++ fixes. PLATFORMS: Updated.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@4497 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/internaltest.cc
+++ xapian-core/tests/internaltest.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002 Olly Betts
+ * Copyright 2002,2003 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -69,8 +69,10 @@ static bool test_except1()
 }
 
 class duffnew_test {
-   duffnew_test() { *foo = 0; }
-   char foo[7];
+  public:
+    duffnew_test() { *foo = 0; }
+  private:
+    char foo[7];
 };
 
 duffnew_test *duff_allocation = 0;
