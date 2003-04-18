b0b1d19cdbf1aa769cd270552722073918a5cf91
Olly Betts <olly@survex.com>
2003-04-18 18:19:10 +0000

    configure.in,include/Makefile.am,include/om/.cvsignore, include/om/Makefile.am,include/om/om.h,include/om/om.h.in, include/xapian/.cvsignore,include/xapian/Makefile.am, include/xapian/dir_contents,include/xapian/version.h.in: C++ ABI version checking is now done by xapian/version.h.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@4529 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/include/om/om.h
@@ -0,0 +1,57 @@
+// om.h: Include all externally visible parts of om
+//
+// ----START-LICENCE----
+// Copyright 1999,2000,2001 BrightStation PLC
+// Copyright 2002 Ananova Ltd
+// Copyright 2002,2003 Olly Betts
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// -----END-LICENCE-----
+
+#ifndef OM_HGUARD_OM_H
+#define OM_HGUARD_OM_H
+
+// Set defines for library version and check C++ ABI versions match
+#include <xapian/version.h>
+
+#include <string>
+typedef std::string om_termname;
+
+// Types and exceptions
+#include "om/omtypes.h"
+#include "om/omerror.h"
+#include "om/omerrorhandler.h"
+
+// Data access
+#include "om/omdocument.h"
+#include "om/omdatabase.h"
+#include "om/ompostlistiterator.h"
+#include "om/ompositionlistiterator.h"
+#include "om/omtermlistiterator.h"
+#include "om/omvalueiterator.h"
+
+// Searching
+#include "om/omenquire.h"
+#include "om/omquery.h"
+#include "om/omexpanddecider.h"
+
+// Stemming
+#include "om/omstem.h"
+
+// Output
+#include "om/omoutput.h"
+
+#endif
