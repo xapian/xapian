7558644dc6ba2e42ed517ec8fd3515d9b6e63d29
Olly Betts <olly@survex.com>
2003-05-26 21:54:44 +0000

    delve.cc: Corrected odd use of std::string::erase() (instead of `str = str.erase(n, m)' just use `str.erase(n,m)').
    git-svn-id: file:///home/xapian-svn/xapian/trunk@4597 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-examples/delve.cc
+++ xapian-examples/delve.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002 Olly Betts
+ * Copyright 2002,2003 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -202,7 +202,7 @@ main(int argc, char *argv[])
 	    om_termname term = *i;
 	    OmStem stemmer("english");
 	    if (*(term.end() - 1) == '.') {
-		term = term.erase(term.size() - 1);
+		term.erase(term.size() - 1);
 	    } else {
 		term = stemmer.stem_word(term);
 	    }
