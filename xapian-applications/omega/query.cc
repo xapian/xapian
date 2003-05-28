85ec9c94a3a11723719d15d6904b86105747c80f
Olly Betts <olly@survex.com>
2003-05-28 23:01:20 +0000

    Makefile.am,date.cc,date.h,query.cc: Split code to build a date range filter into a separate file.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@4619 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/query.cc
+++ xapian-applications/omega/query.cc
@@ -47,14 +47,13 @@
 #include <unistd.h>
 #elif defined HAVE_FTIME
 #include <sys/timeb.h>
-#else
-#include <time.h>
 #endif
 
 #ifdef HAVE_PCRE
 #include <pcre.h>
 #endif
 
+#include "date.h"
 #include "utils.h"
 #include "omega.h"
 #include "query.h"
@@ -229,85 +228,6 @@ void add_bterm(const string &term) {
     filter_map.insert(make_pair(prefix, term));
 }
 
-static int
-last_day(int y, int m)
-{
-    static const int l[13] = {
-	0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-    };
-    if (m != 2) return l[m];
-    return 28 + (y % 4 == 0); // good until 2100
-}
-
-static OmQuery
-date_range_filter(int y1, int m1, int d1, int y2, int m2, int d2)
-{
-    char buf[9];
-    sprintf(buf, "%04d%02d", y1, m1);
-    vector<OmQuery> v;
-
-    int d_last = last_day(y1, m1);
-    int d_end = d_last;
-    if (y1 == y2 && m1 == m2 && d2 < d_last) {
-	d_end = d2;
-    }
-    // Deal with any initial partial month
-    if (d1 > 1 || d_end < d_last) {
-    	for ( ; d1 <= d_end ; d1++) {
-	    sprintf(buf + 6, "%02d", d1);
-	    v.push_back(OmQuery('D' + string(buf)));
-	}
-    } else {
-	v.push_back(OmQuery('M' + string(buf)));
-    }
-    
-    if (y1 == y2 && m1 == m2) {
-	return OmQuery(OmQuery::OP_OR, v.begin(), v.end());
-    }
-
-    int m_last = (y1 < y2) ? 12 : m2 - 1;
-    while (++m1 <= m_last) {
-	sprintf(buf + 4, "%02d", m1);
-	v.push_back(OmQuery('M' + string(buf)));
-    }
-	
-    if (y1 < y2) {
-	while (++y1 < y2) {
-	    sprintf(buf, "%04d", y1);
-	    v.push_back(OmQuery('Y' + string(buf)));
-	}
-	sprintf(buf, "%04d", y2);
-	for (m1 = 1; m1 < m2; m1++) {
-	    sprintf(buf + 4, "%02d", m1);
-	    v.push_back(OmQuery('M' + string(buf)));
-	}
-    }
-	
-    sprintf(buf + 4, "%02d", m2);
-
-    // Deal with any final partial month
-    if (d2 < last_day(y2, m2)) {
-    	for (d1 = 1 ; d1 <= d2; d1++) {
-	    sprintf(buf + 6, "%02d", d1);
-	    v.push_back(OmQuery('D' + string(buf)));
-	}
-    } else {
-	v.push_back(OmQuery('M' + string(buf)));
-    }
-
-    return OmQuery(OmQuery::OP_OR, v.begin(), v.end());
-}
-
-static void
-parse_date(string date, int *y, int *m, int *d)
-{
-    // FIXME: for now only support YYYYMMDD (e.g. 20011119)
-    // and don't error check
-    *y = atoi(date.substr(0, 4).c_str());
-    *m = atoi(date.substr(4, 2).c_str());
-    *d = atoi(date.substr(6, 2).c_str());
-}
-
 static void
 run_query()
 {
@@ -355,72 +275,10 @@ run_query()
     }
 
     if (!date_start.empty() || !date_end.empty() || !date_span.empty()) {
-	int y1, m1, d1, y2, m2, d2;
-	if (!date_span.empty()) {
-	    time_t secs = atoi(date_span.c_str()) * (24 * 60 * 60);
-	    if (!date_end.empty()) {
-		parse_date(date_end, &y2, &m2, &d2);
-		struct tm t;
-		t.tm_year = y2 - 1900;
-		t.tm_mon = m2 - 1;
-		t.tm_mday = d2;
-		t.tm_hour = 12;
-		t.tm_min = t.tm_sec = 0;
-		t.tm_isdst = -1;
-		time_t then = mktime(&t) - secs;
-		struct tm *t2 = localtime(&then);
-		y1 = t2->tm_year + 1900;
-		m1 = t2->tm_mon + 1;
-		d1 = t2->tm_mday;
-	    } else if (!date_start.empty()) {
-		parse_date(date_start, &y1, &m1, &d1);	
-		struct tm t;
-		t.tm_year = y1 - 1900;
-		t.tm_mon = m1 - 1;
-		t.tm_mday = d1;
-		t.tm_hour = 12;
-		t.tm_min = t.tm_sec = 0;
-		t.tm_isdst = -1;
-		time_t end = mktime(&t) + secs;
-		struct tm *t2 = localtime(&end);
-		y2 = t2->tm_year + 1900;
-		m2 = t2->tm_mon + 1;
-		d2 = t2->tm_mday;
-	    } else {
-		time_t end = time(NULL);
-		struct tm *t = localtime(&end);
-		y2 = t->tm_year + 1900;
-		m2 = t->tm_mon + 1;
-		d2 = t->tm_mday;
-		time_t then = end - secs;
-		struct tm *t2 = localtime(&then);
-		y1 = t2->tm_year + 1900;
-		m1 = t2->tm_mon + 1;
-		d1 = t2->tm_mday;
-	    }
-	} else {
-	    if (date_start.empty()) {
-		y1 = 1970;
-		m1 = 1;
-		d1 = 1;
-	    } else {
-		parse_date(date_start, &y1, &m1, &d1);	
-	    }
-	    if (date_end.empty()) {
-		time_t now = time(NULL);
-		struct tm *t = localtime(&now);
-		y2 = t->tm_year + 1900;
-		m2 = t->tm_mon + 1;
-		d2 = t->tm_mday;
-	    } else {
-		parse_date(date_end, &y2, &m2, &d2);
-	    }
-	}
-
 	query = OmQuery(OmQuery::OP_FILTER,
 		       	query,
 			OmQuery(OmQuery::OP_OR,
-			date_range_filter(y1, m1, d1, y2, m2, d2),
+			date_range_filter(date_start, date_end, date_span),
 			OmQuery("Dlatest")));
     }
 
