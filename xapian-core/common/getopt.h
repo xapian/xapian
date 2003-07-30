0d34b005d9dec1a6eebc9e68784bafcf21d8f612
Olly Betts <olly@survex.com>
2003-07-30 16:06:38 +0000

    common/getopt.h: Another try - the previous one failed on FreeBSD.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@4842 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/getopt.h
+++ xapian-core/common/getopt.h
@@ -139,6 +139,30 @@ struct option
    `getopt'.  */
 
 #if (defined __STDC__ && __STDC__) || defined(__cplusplus)
+# ifdef __GNU_LIBRARY__
+/* Many other libraries have conflicting prototypes for getopt, with
+   differences in the consts, in stdlib.h.  To avoid compilation
+   errors, only prototype getopt for the GNU C library.  */
+extern int getopt (int /*__argc*/, char *const */*__argv*/, const char *__shortopts);
+# else /* not __GNU_LIBRARY__ */
+#  ifdef __cplusplus
+// In C++ "getopt ()" means it takes no argument, which is no good.  So
+// we define getopt as a call to the internal function which actually
+// does the work!
+
+// Include stdlib.h first so we don't mess with any getopt prototype that
+// might be there if stdlib.h is included after this header...
+// Also stdio.h and unistd.h - Solaris also prototypes getopt there...
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#define getopt(ARGC, ARGV, OPTSTRING) _getopt_internal(ARGC, ARGV, OPTSTRING,\
+	(const struct option *) 0, (int *) 0, 0)
+#  else
+extern int getopt ();
+#  endif
+# endif /* __GNU_LIBRARY__ */
+
 # ifndef __need_getopt
 extern int getopt_long (int /*__argc*/, char *const */*__argv*/, const char *__shortopts,
 			const struct option *__longopts, int *__longind);
@@ -152,26 +176,6 @@ extern int _getopt_internal (int /*__argc*/, char *const */*__argv*/,
 			     const struct option *__longopts, int *__longind,
 			     int __long_only);
 # endif
-
-# ifdef __GNU_LIBRARY__
-/* Many other libraries have conflicting prototypes for getopt, with
-   differences in the consts, in stdlib.h.  To avoid compilation
-   errors, only prototype getopt for the GNU C library.  */
-extern int getopt (int /*__argc*/, char *const */*__argv*/, const char *__shortopts);
-# else /* not __GNU_LIBRARY__ */
-#  ifdef __cplusplus
-// In C++ "getopt ()" means it takes no argument, which is no good.  So
-// we inline a call to _getopt_internal instead.  The dummy default argument
-// avoids any conflict with any C prototype of getopt in system headers.
-inline int getopt(int __argc, char *const * __argv, const char *__shortopts,
-		  bool dummy = false) {
-    return _getopt_internal(__argc, __argv, __shortopts,
-		       (const struct option *) 0, (int *) 0, 0);
-}
-#  else
-extern int getopt ();
-#  endif
-# endif /* __GNU_LIBRARY__ */
 #else /* not __STDC__ */
 extern int getopt ();
 # ifndef __need_getopt
