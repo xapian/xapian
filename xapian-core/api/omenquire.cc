759bd461d108957cb32d63ab9cbd7899145b2cc4
Olly Betts <olly@survex.com>
2004-03-06 01:56:41 +0000

    api/omenquire.cc: Another tweak for Sun's C++ compiler.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@5053 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/api/omenquire.cc
+++ xapian-core/api/omenquire.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001,2002 Ananova Ltd
- * Copyright 2002,2003 Olly Betts
+ * Copyright 2002,2003,2004 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -698,7 +698,7 @@ Enquire::Internal::get_mset(Xapian::doccount first, Xapian::doccount maxitems,
     }
 
     // FIXME: make match take a refcntptr
-    MultiMatch match(db, query->internal.get(), *rset, collapse_key,
+    ::MultiMatch match(db, query->internal.get(), *rset, collapse_key,
 		     percent_cutoff, weight_cutoff,
 		     sort_forward, sort_key, sort_bands,
 		     bias_halflife, bias_weight, errorhandler,
