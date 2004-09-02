ea2e40b5088691c622659bb9f33e8dfbc970d355
Olly Betts <olly@survex.com>
2004-09-02 16:57:10 +0000

    matcher/andpostlist.cc: Initialise lmax and rmax to 0.  Hopefully this will fix SIGFPE on apitest's qterminfo2 on alpha linux.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@5460 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/andpostlist.cc
+++ xapian-core/matcher/andpostlist.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2003 Olly Betts
+ * Copyright 2003,2004 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -79,7 +79,8 @@ AndPostList::AndPostList(PostList *left_, PostList *right_,
 			 MultiMatch *matcher_,
 			 Xapian::doccount dbsize_,
 			 bool replacement)
-	: BranchPostList(left_, right_, matcher_), head(0), dbsize(dbsize_)
+	: BranchPostList(left_, right_, matcher_), head(0), lmax(0), rmax(0),
+	  dbsize(dbsize_)
 {
     DEBUGCALL(MATCH, void, "AndPostList", left_ << ", " << right_ << ", " << matcher_ << ", " << dbsize_ << ", " << replacement);
     if (replacement) {
