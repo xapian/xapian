e3af957c476e956e05b1c055dc01cf8af78176a8
Olly Betts <olly@survex.com>
2004-10-19 20:06:44 +0000

    tcl8/Makefile.am: Wrap in a tcl8 namespace "Xapian". tcl8/Makefile.am,tcl8/smoketest.tcl: Added a smoketest.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@5654 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-bindings/tcl8/smoketest.tcl
@@ -0,0 +1,34 @@
+# Simple test that we can load the xapian module and run a simple test
+#
+# Copyright (C) 2004 Olly Betts
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+
+load [file join ".libs" xapian.so]
+xapian::Stem stem "english"
+xapian::Document doc
+doc set_data "is there anybody out there?"
+doc add_term "XYzzy"
+doc add_posting [stem stem_word "is"] 1
+doc add_posting [stem stem_word "there"] 2
+doc add_posting [stem stem_word "anybody"] 3
+doc add_posting [stem stem_word "out"] 4
+doc add_posting [stem stem_word "there"] 5
+xapian::WritableDatabase db [xapian::inmemory_open]
+db add_document doc
+if { [db get_doccount] != 1 } {
+    exit 1
+}
