e84e66269d9599ce266eb62ea1904b32c25d3252
Olly Betts <olly@survex.com>
2004-11-01 05:44:50 +0000

    matcher/multimatch.cc: Fixed compilation problem on alpha Linux.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@5695 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/multimatch.cc
+++ xapian-core/matcher/multimatch.cc
@@ -880,7 +880,7 @@ MultiMatch::get_mset(Xapian::doccount first, Xapian::doccount maxitems,
 	    // another approach: Xapian::doccount new_est = items.size() * (1 - percent_factor) / (1 - min_item.wt / greatest_wt);
 	    Xapian::doccount new_est;
 	    new_est = Xapian::doccount((1 - percent_factor) * matches_estimated);
-	    matches_estimated = max(Xapian::doccount(new_est), items.size());
+	    matches_estimated = max(size_t(new_est), items.size());
 	    // and another: items.size() + (1 - greatest_wt * percent_factor / min_item.wt) * (matches_estimated - items.size());
 
 	    // Very likely an underestimate, but we can't really do better without
