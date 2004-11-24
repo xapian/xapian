ca0a7f1049f63f89361f587b426c61730b169122
Olly Betts <olly@survex.com>
2004-11-24 02:21:57 +0000

    omindex.cc: Improved HTML to text conversion - the parser now knows that some tags should be regarded as word breaks and some shouldn't (previously all tags were treated as word breaks).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@5825 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omindex.cc
+++ xapian-applications/omega/omindex.cc
@@ -84,6 +84,7 @@ class MyHtmlParser : public HtmlParser {
     public:
 	bool in_script_tag;
 	bool in_style_tag;
+	bool pending_space;
     	string title, sample, keywords, dump;
 	bool indexing_allowed;
 	void process_text(const string &text);
@@ -92,6 +93,7 @@ class MyHtmlParser : public HtmlParser {
 	MyHtmlParser() :
 		in_script_tag(false),
 		in_style_tag(false),
+		pending_space(false),
 		indexing_allowed(true) { }
 };
 
@@ -103,15 +105,16 @@ class MyHtmlParser : public HtmlParser {
 void
 MyHtmlParser::process_text(const string &text)
 {
-    // some tags are meaningful mid-word so this is simplistic at best...
-
     if (!in_script_tag && !in_style_tag) {
 	string::size_type b = 0;
 	while ((b = text.find_first_not_of(WHITESPACE, b)) != string::npos) {
-	    if (!dump.empty()) dump += ' ';
+	    if (pending_space || b != 0)
+		if (!dump.empty()) dump += ' ';
+	    pending_space = true;
 	    string::size_type e = text.find_first_of(WHITESPACE, b);
 	    if (e == string::npos) {
 		dump += text.substr(b);
+		pending_space = false;
 		break;
 	    }
 	    dump += text.substr(b, e - b);
@@ -131,56 +134,187 @@ MyHtmlParser::opening_tag(const string &tag, const map<string,string> &p)
     }
     cout << ">\n";
 #endif
-    
-    if (tag == "meta") {
-	map<string, string>::const_iterator i, j;
-	if ((i = p.find("content")) != p.end()) {
-	    if ((j = p.find("name")) != p.end()) {
-		string name = j->second;
-		lowercase_term(name);
-		if (name == "description") {
-		    if (sample.empty()) {
-			sample = i->second;
-			decode_entities(sample);
-		    }
-		} else if (name == "keywords") {
-		    if (!keywords.empty()) keywords += ' ';
-		    string tmp = i->second;
-		    decode_entities(tmp);
-		    keywords += tmp;
-		} else if (name == "robots") {
-		    string val = i->second;
-		    decode_entities(val);
-		    lowercase_term(val);
-		    if (val.find("none") != string::npos ||
-			val.find("noindex") != string::npos) {
-			indexing_allowed = false;
-			throw true;
+    if (tag.empty()) return;
+    switch (tag[0]) {
+	case 'a':
+	    if (tag == "address") pending_space = true;
+	    break;
+	case 'b':
+	    if (tag == "body") {
+		dump = "";
+		break;
+	    }
+	    if (tag == "blockquote" || tag == "br") pending_space = true;
+	    break;
+	case 'c':
+	    if (tag == "center") pending_space = true;
+	    break;
+	case 'd':
+	    if (tag == "dd" || tag == "dir" || tag == "div" || tag == "dl" ||
+		tag == "dt") pending_space = true;
+	    break;
+	case 'e':
+	    if (tag == "embed") pending_space = true;
+	    break;
+	case 'f':
+	    if (tag == "fieldset" || tag == "form") pending_space = true;
+	    break;
+	case 'h':
+	    if (tag == "hr") pending_space = true;
+	    break;
+	case 'i':
+	    if (tag == "iframe" || tag == "img" || tag == "isindex" ||
+		tag == "input") pending_space = true;
+	    break;
+	case 'k':
+	    if (tag == "keygen") pending_space = true;
+	    break;
+	case 'l':
+	    if (tag == "legend" || tag == "li" || tag == "listing")
+		pending_space = true;
+	    break;
+	case 'm':
+	    if (tag == "meta") {
+		map<string, string>::const_iterator i, j;
+		if ((i = p.find("content")) != p.end()) {
+		    if ((j = p.find("name")) != p.end()) {
+			string name = j->second;
+			lowercase_term(name);
+			if (name == "description") {
+			    if (sample.empty()) {
+				sample = i->second;
+				decode_entities(sample);
+			    }
+			} else if (name == "keywords") {
+			    if (!keywords.empty()) keywords += ' ';
+			    string tmp = i->second;
+			    decode_entities(tmp);
+			    keywords += tmp;
+			} else if (name == "robots") {
+			    string val = i->second;
+			    decode_entities(val);
+			    lowercase_term(val);
+			    if (val.find("none") != string::npos ||
+				val.find("noindex") != string::npos) {
+				indexing_allowed = false;
+				throw true;
+			    }
+			}
 		    }
 		}
+		break;
 	    }
-	}
-    } else if (tag == "script") {
-	in_script_tag = true;
-    } else if (tag == "style") {
-	in_style_tag = true;
-    } else if (tag == "body") {
-	dump = "";
+	    if (tag == "marquee" || tag == "menu" || tag == "multicol")
+		pending_space = true;
+	    break;
+	case 'o':
+	    if (tag == "ol" || tag == "option") pending_space = true;
+	    break;
+	case 'p':
+	    if (tag == "p" || tag == "pre" || tag == "plaintext")
+		pending_space = true;
+	    break;
+	case 'q':
+	    if (tag == "q") pending_space = true;
+	    break;
+	case 's':
+	    if (tag == "style") {
+		in_style_tag = true;
+		break;
+	    }
+	    if (tag == "script") {
+		in_script_tag = true;
+		break;
+	    }
+	    if (tag == "select") pending_space = true;
+	    break;
+	case 't':
+	    if (tag == "table" || tag == "td" || tag == "textarea" ||
+		tag == "th") pending_space = true;
+	    break;
+	case 'u':
+	    if (tag == "ul") pending_space = true;
+	    break;
+	case 'x':
+	    if (tag == "xmp") pending_space = true;
+	    break;
     }
 }
 
 void
 MyHtmlParser::closing_tag(const string &tag)
 {
-    if (tag == "title") {
-	title = dump;
-	dump = "";
-    } else if (tag == "script") {
-	in_script_tag = false;
-    } else if (tag == "style") {
-	in_style_tag = false;
-    } else if (tag == "body") {
-	throw true;
+    if (tag.empty()) return;
+    switch (tag[0]) {
+	case 'a':
+	    if (tag == "address") pending_space = true;
+	    break;
+	case 'b':
+	    if (tag == "body") {
+		throw true;
+	    }
+	    if (tag == "blockquote" || tag == "br") pending_space = true;
+	    break;
+	case 'c':
+	    if (tag == "center") pending_space = true;
+	    break;
+	case 'd':
+	    if (tag == "dd" || tag == "dir" || tag == "div" || tag == "dl" ||
+		tag == "dt") pending_space = true;
+	    break;
+	case 'f':
+	    if (tag == "fieldset" || tag == "form") pending_space = true;
+	    break;
+	case 'h':
+	    if (tag == "hr") pending_space = true;
+	    break;
+	case 'i':
+	    if (tag == "iframe") pending_space = true;
+	    break;
+	case 'l':
+	    if (tag == "legend" || tag == "li" || tag == "listing")
+		pending_space = true;
+	    break;
+	case 'm':
+	    if (tag == "marquee" || tag == "menu") pending_space = true;
+	    break;
+	case 'o':
+	    if (tag == "ol" || tag == "option") pending_space = true;
+	    break;
+	case 'p':
+	    if (tag == "p" || tag == "pre") pending_space = true;
+	    break;
+	case 'q':
+	    if (tag == "q") pending_space = true;
+	    break;
+	case 's':
+	    if (tag == "style") {
+		in_style_tag = false;
+		break;
+	    }
+	    if (tag == "script") {
+		in_script_tag = false;
+		break;
+	    }
+	    if (tag == "select") pending_space = true;
+	    break;
+	case 't':
+	    if (tag == "title") {
+		if (title.empty()) {
+		    title = dump;
+		    dump = "";
+		}
+		break;
+	    }
+	    if (tag == "table" || tag == "td" || tag == "textarea" ||
+		tag == "th") pending_space = true;
+	    break;
+	case 'u':
+	    if (tag == "ul") pending_space = true;
+	    break;
+	case 'x':
+	    if (tag == "xmp") pending_space = true;
+	    break;
     }
 }
 
