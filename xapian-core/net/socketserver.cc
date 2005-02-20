1a0b563c0e02bbdda95532bae2cc192a6ba7a2b5
Olly Betts <olly@survex.com>
2005-02-20 19:47:21 +0000

    net/socketserver.cc: Fixed typo in debug code.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6011 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/net/socketserver.cc
+++ xapian-core/net/socketserver.cc
@@ -3,7 +3,7 @@
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001,2002 Ananova Ltd
- * Copyright 2002,2003,2004 Olly Betts
+ * Copyright 2002,2003,2004,2005 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -244,7 +244,7 @@ SocketServer::run()
 	    if (timing) {
 		cout << "Total working time = " << total
 		    << " usecs. (socketserver.cc)\n";
-		cout << "Total waiting time = " << totalidlei
+		cout << "Total waiting time = " << totalidle
 		    << " usecs. (socketserver.cc)\n";
 	    }
 #endif
