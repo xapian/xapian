7a85ce463dffda5b287d56ac7187bc314356e9c4
Olly Betts <olly@survex.com>
2005-03-01 02:04:22 +0000

    bin/quartzcompact.cc: Extended to allow merging several quartz databases to produce a single compact quartz database.  This allows for faster building - simple index in chunks, then merge the chunks.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6054 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/bin/quartzcompact.cc
+++ xapian-core/bin/quartzcompact.cc
@@ -1,10 +1,10 @@
-/* quartzcompact.cc: Make a quartzdatabase with full compaction.  This makes
- * it smaller and faster to search if you aren't going to be updating it.
+/* quartzcompact.cc: Compact one or more quartz databases to produce a new
+ * quartzdatabase.  By default the resultant database has full compaction
+ * with revision 1, which makes it especially fast to search.
  *
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004 Olly Betts
+ * Copyright 2002,2003,2004,2005 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -27,6 +27,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <queue>
 
 #include <errno.h>
 #include <stdio.h> // for rename()
@@ -35,7 +36,8 @@
 
 #include "btree.h"
 #include "bcursor.h"
-#include <xapian/version.h>
+#include "quartz_utils.h"
+#include <xapian.h>
 
 #include "gnu_getopt.h"
 
@@ -45,11 +47,78 @@ static void
 usage(const char * progname)
 {
     cout << "Usage: " << progname
-	 << " [OPTION] <path to source database> "
+	 << " [OPTION] <path to source database>... "
 	    "<path to destination database>\n\n"
 	    "  -n, --no-full  Disable full compaction"
 	 << endl;
 }
+
+class MyBcursor : private Bcursor {
+        Xapian::docid offset;
+    public:
+	string key, tag;
+	Xapian::docid firstdid;
+	Xapian::termcount tf, cf;
+
+	MyBcursor(Btree *in, Xapian::docid offset_)
+	    : Bcursor(in), offset(offset_)
+	{
+	    find_entry("");
+	    next();
+	}
+
+	bool next() {
+	    if (!Bcursor::next()) return false;
+	    // We put all chunks into the non-initial chunk form here,
+	    // then fix up the first chunks in the merged database as we
+	    // merge.
+	    read_tag();
+	    key = current_key;
+	    tag = current_tag;
+	    tf = cf = 0;
+	    // Adjust key if this is *NOT* an initial chunk.
+	    // key is: pack_string_preserving_sort(tname)
+	    // plus optionally: pack_uint_preserving_sort(did)
+	    const char * d = key.data();
+	    string tname;
+	    if (!unpack_string_preserving_sort(&d, d + key.size(), tname))
+		abort();
+	    bool first_chunk = (d == key.data() + key.size());
+	    if (!first_chunk) {
+		size_t tmp = d - key.data();
+		if (!unpack_uint_preserving_sort(&d, d + key.size(), &firstdid))
+		    abort();
+		firstdid += offset;
+		key.erase(tmp);
+	    }
+	    // Adjust key and header of tag if this *IS* an initial chunk.
+	    if (first_chunk) {
+		d = tag.data();
+		const char *e = d + tag.size();
+		if (!unpack_uint(&d, e, &tf))
+		    abort();
+		if (!unpack_uint(&d, e, &cf))
+		    abort();
+		if (!unpack_uint(&d, e, &firstdid))
+		    abort();
+		firstdid += 1;
+		firstdid += offset;
+		tag.erase(0, d - tag.data());
+	    }
+	    return true;
+	}
+};
+
+class MyBcursorGt {
+  public:
+    /** Return true if and only if a's key is strictly greater than b's key.
+     */
+    bool operator()(const MyBcursor *a, const MyBcursor *b) {
+	if (a->key > b->key) return true;
+	if (a->key != b->key) return false;
+	return (a->firstdid > b->firstdid);
+    }
+};
 
 int
 main(int argc, char **argv)
@@ -80,60 +149,62 @@ main(int argc, char **argv)
         }
     }
 
-    if (argc - optind != 2) {
+    if (argc - optind < 3) {
 	usage(argv[0]);
 	exit(1);
     }
 
-    // Path to the source databases
-    const char *srcdir = argv[optind];
     // Path to the database to create
-    const char *destdir = argv[optind + 1];
+    const char *destdir = argv[argc - 1];
 
-    if (strcmp(srcdir, destdir) == 0) {
-	cout << argv[0] << ": source and destination may not be the same directory" << endl;
-	exit(1);
+    // Check destdir isn't the same as any source directory...
+    for (int i = optind; i < argc - 1; ++i) {
+	const char *srcdir = argv[i];
+	if (strcmp(srcdir, destdir) == 0) {
+	    cout << argv[0] << ": destination may not be the same as any source directory" << endl;
+	    exit(1);
+	}
     }
 
+    // Create the directory for the database, if it doesn't exist already
+    if (mkdir(destdir, 0755) == -1) {
+	// Check if mkdir failed because there's already a directory there
+	// or for some other reason - we also get EEXIST if there's a file
+	// with that name.
+	if (errno == EEXIST) {
+	    struct stat sb;
+	    if (stat(destdir, &sb) == 0 && S_ISDIR(sb.st_mode))
+		errno = 0;
+	    else
+		errno = EEXIST; // stat might have changed it
+	}
+	if (errno) {
+	    cerr << argv[0] << ": couldn't create directory `"
+		 << destdir << "': " << strerror(errno) << endl;
+	    exit(1);
+	}
+    }
+
+    quartz_totlen_t tot_totlen = 0;
     try {
-	// Create the directory for the database, if it doesn't exist already
-	if (mkdir(destdir, 0755) == -1) {
-	    // Check if mkdir failed because there's already a directory there
-	    // or for some other reason - we also get EEXIST if there's a file
-	    // with that name.
-	    if (errno == EEXIST) {
-		struct stat sb;
-		if (stat(destdir, &sb) == 0 && S_ISDIR(sb.st_mode))
-		    errno = 0;
-		else
-		    errno = EEXIST; // stat might have changed it
-	    }
-	    if (errno) {
-		cerr << argv[0] << ": couldn't create directory `"
-		     << destdir << "': " << strerror(errno) << endl;
-		exit(1);
-	    }
+	const char * tables[] = {
+	    "record", "termlist", "position", "value"
+	};
+
+	struct stat sb;
+
+	vector<Xapian::docid> offset(argc - 1 - optind);
+	Xapian::docid tot_off = 0;
+	for (int i = optind; i < argc - 1; ++i) {
+	    Xapian::Database db(argv[i]);
+	    Xapian::docid last = db.get_lastdocid();
+	    offset[i - optind] = tot_off;
+	    tot_off += last;
+	    // FIXME: prune unused docids off the start and end of each range...
 	}
 
-	const char * tables[] = {
-	    "record", "postlist", "termlist", "position", "value"
-	};
 	for (const char **t = tables;
 	     t != tables + sizeof(tables)/sizeof(tables[0]); ++t) {
-	    cout << *t << " ..." << flush;
-
-	    struct stat sb;
-
-	    string src(srcdir);
-	    src += '/';
-	    src += *t;
-	    src += '_';
-	    Btree in(src, true);
-	    in.open();
-
-	    off_t in_size = 0;
-	    if (stat(src + "DB", &sb) == 0) in_size = sb.st_size / 1024;
-
 	    string dest = destdir;
 	    dest += '/';
 	    dest += *t;
@@ -144,28 +215,205 @@ main(int argc, char **argv)
 	    out.open();
 	    out.set_full_compaction(full_compaction);
 
-	    if (in.get_entry_count()) {
-		Bcursor BC(&in);
-		BC.find_entry("");
-		while (BC.next()) {
-		    BC.read_tag();
-		    out.add(BC.current_key, BC.current_tag);
+	    off_t in_size = 0;
+
+	    for (int i = optind; i < argc - 1; ++i) {
+		size_t j = i - optind;
+		cout << "\r" << *t << " " << j << "/" << argc - 1 - optind
+		     << " ..." << flush;
+
+		Xapian::docid off = offset[j];
+		const char *srcdir = argv[i];
+		string src(srcdir);
+		src += '/';
+		src += *t;
+		src += '_';
+		Btree in(src, true);
+		in.open();
+
+		if (stat(src + "DB", &sb) == 0) in_size += sb.st_size / 1024;
+
+		if (in.get_entry_count()) {
+		    Bcursor BC(&in);
+		    BC.find_entry("");
+		    while (BC.next()) {
+			BC.read_tag();
+			string key = BC.current_key;
+			switch (**t) {
+			    case 'r': // (r)ecord
+				if (key == string("", 1)) {
+				    // Handle the METAINFO key.
+				    Xapian::docid did;
+				    quartz_totlen_t totlen;
+				    const char * data = BC.current_tag.data();
+				    const char * end = data + BC.current_tag.size();
+				    if (!unpack_uint(&data, end, &did)) {
+					throw Xapian::DatabaseCorruptError("Record containing meta information is corrupt.");
+				    }
+				    if (!unpack_uint_last(&data, end, &totlen)) {
+					throw Xapian::DatabaseCorruptError("Record containing meta information is corrupt.");
+				    }
+				    tot_totlen += totlen;
+				    if (tot_totlen < tot_totlen) {
+					throw "totlen wrapped!";
+				    }
+				    if (i == argc - 2) {
+					string tag = pack_uint(tot_off);
+					tag += pack_uint_last(tot_totlen);
+					out.add(string("", 1), tag);
+				    }
+				    continue;
+				}
+				/* FALLTHRU */
+			    case 't': // (t)ermlist
+			    case 'v': // (v)alue
+				if (off != 0) {
+				    const char * d = key.data();
+				    Xapian::docid did;
+				    if (!unpack_uint_last(&d, d + key.size(), &did))
+					abort();
+				    key = pack_uint_last(did + off);
+				}
+				break;
+			    case 'p': // (p)osition
+				if (off != 0) {
+				    // key is: pack_uint(did) + tname
+				    const char * d = key.data();
+				    Xapian::docid did;
+				    if (!unpack_uint(&d, d + key.size(), &did))
+					abort();
+				    did += off;
+				    size_t tnameidx = d - key.data();
+				    key = pack_uint(did) + key.substr(tnameidx);
+				}
+				break;
+			}
+			out.add(key, BC.current_tag);
+		    }
 		}
 	    }
 	    out.commit(1);
 
 	    if (in_size != 0 && stat(dest + "DB", &sb) == 0) {
 		off_t out_size = sb.st_size / 1024;
-		cout << '\r' << *t << ": Reduced by "
-		     << 100 * double(in_size - out_size) / in_size << "% "
-		     << in_size - out_size << "K (" << in_size << "K -> "
-		     << out_size << "K)" << endl;
+		if (out_size <= in_size) {
+		    cout << '\r' << *t << ": Reduced by "
+			 << 100 * double(in_size - out_size) / in_size << "% "
+			 << in_size - out_size << "K (" << in_size << "K -> "
+			 << out_size << "K)" << endl;
+		} else {
+		    cout << '\r' << *t << ": INCREASED by "
+			 << 100 * double(out_size - in_size) / in_size << "% "
+			 << out_size - in_size << "K (" << in_size << "K -> "
+			 << out_size << "K)" << endl;
+		}
 	    } else {
 		cout << '\r' << *t << ": Done" << endl;
 	    }
 	}
+
+	// The postlist requires an N-way merge, adjusting the headers
+	// of various blocks.
+
+	cout << "postlist ..." << flush;
+
+	string pdest = destdir;
+	pdest += "/postlist_";
+
+	Btree out(pdest, false);
+	out.create(8192);
+	out.open();
+	out.set_full_compaction(full_compaction);
+
+	off_t in_size = 0;
+
+	vector<Btree *> btrees;
+
+	priority_queue<MyBcursor *, std::vector<MyBcursor *>, MyBcursorGt> pq;
+	for (int i = optind; i < argc - 1; ++i) {
+	    Xapian::docid off = offset[i - optind];
+	    const char *srcdir = argv[i];
+	    string src(srcdir);
+	    src += "/postlist_";
+
+	    Btree *in = new Btree(src, true);
+	    in->open();
+	    if (in->get_entry_count()) {
+		btrees.push_back(in);
+		MyBcursor * bc = new MyBcursor(in, off);
+		pq.push(bc);
+	    } else {
+		delete in;
+	    }
+
+	    if (stat(src + "DB", &sb) == 0) in_size += sb.st_size / 1024;
+	}
+
+	string last_key;
+	Xapian::termcount tf, cf;
+	vector<pair<Xapian::docid, string> > tags;
+	while (true) {
+	    MyBcursor * bc;
+	    if (!pq.empty()) {
+		bc = pq.top();
+		pq.pop();
+	    }
+	    if (pq.empty() || bc->key != last_key) {
+		if (!tags.empty()) {
+		    string first_tag = pack_uint(tf);
+		    first_tag += pack_uint(cf);
+		    first_tag += pack_uint(tags[0].first - 1);
+		    string tag = tags[0].second;
+		    tag[0] = (tags.size() == 1) ? '1' : '0';
+		    first_tag += tag;
+		    out.add(last_key, first_tag);
+		    vector<pair<Xapian::docid, string> >::const_iterator i;
+		    i = tags.begin();
+		    while (++i != tags.end()) {
+			string key = last_key;
+			key += pack_uint_preserving_sort(i->first);
+			tag = i->second;
+			tag[0] = (i + 1 == tags.end()) ? '1' : '0';
+			out.add(key, tag);
+		    }
+		}
+		tags.clear();
+		if (pq.empty()) break;
+		tf = cf = 0;
+		last_key = bc->key;
+	    }
+	    tf += bc->tf;
+	    cf += bc->cf;
+	    tags.push_back(make_pair(bc->firstdid, bc->tag));
+	    if (bc->next()) {
+		pq.push(bc);
+	    } else {
+		delete bc;
+	    }
+	}
+
+	for (vector<Btree *>::const_iterator b = btrees.begin();
+	     b != btrees.end(); ++b) {
+	    delete *b;
+	}
+	btrees.clear();
+
+	out.commit(1);
+
+	if (in_size != 0 && stat(pdest + "DB", &sb) == 0) {
+	    off_t out_size = sb.st_size / 1024;
+	    cout << "\rpostlist: Reduced by "
+		<< 100 * double(in_size - out_size) / in_size << "% "
+		<< in_size - out_size << "K (" << in_size << "K -> "
+		<< out_size << "K)" << endl;
+	} else {
+	    cout << "\rpostlist: Done" << endl;
+	}
+
 	// Copy meta file
-	string src(srcdir);
+	// FIXME: may need to do something smarter that just copying an
+	// arbitrary meta file if the meta file format changes...
+	string src(argv[optind]);
 	src += "/meta";
 	string dest = destdir;
 	dest += "/meta.tmp";
