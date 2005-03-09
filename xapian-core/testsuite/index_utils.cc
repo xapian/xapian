8c117e064b56cbb52d5bf6d2446afd90e01392b7
Olly Betts <olly@survex.com>
2005-03-09 15:23:44 +0000

    testsuite/index_utils.cc: Fixed ^x to actually decode hex values correctly.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6073 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/testsuite/index_utils.cc
+++ xapian-core/testsuite/index_utils.cc
@@ -2,7 +2,7 @@
  *
  * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2003,2004 Olly Betts
+ * Copyright 2003,2004,2005 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -26,6 +26,8 @@
 #include "utils.h"
 #include <iostream>
 
+using namespace std;
+
 void lowercase_term(string &term)
 {
     string::iterator i = term.begin();
@@ -33,7 +35,7 @@ void lowercase_term(string &term)
 	*i = C_tolower(*i);
 	i++;
     }
-    std::string::size_type a;
+    string::size_type a;
     while ((a = term.find("^s")) != term.npos) {
 	term.replace(a, 2, " ");
     }
@@ -44,62 +46,68 @@ void lowercase_term(string &term)
 	term.replace(a, 2, "\\");
     }
     while ((a = term.find("^t")) != term.npos) {
-	term.replace(a, 2, std::string("\t", 1));
+	term.replace(a, 2, "\t");
     }
     while ((a = term.find("^0")) != term.npos) {
-	term.replace(a, 2, std::string("\0", 1));
+	term.replace(a, 2, string("\0", 1));
     }
     while ((a = term.find("^x")) != term.npos) {
 	if (a > term.size() - 4) return;
 	char b = term[a + 2];
 	char c = term[a + 3];
-	if (b <= '9') b = b - '0';
-	else if (b <= 'Z') b = b - 'A';
-	else b = b - 'a';
-	if (c <= '9') c = c - '0';
-	else if (c <= 'Z') c = c - 'A';
-	else c = c - 'a';
+	if (C_isdigit(b))
+	    b -= '0';
+	else if (C_isupper(b) <= 'Z')
+	    b -= ('A' - 10);
+	else
+	    b -= ('a' - 10);
+	if (C_isdigit(c))
+	    c -= '0';
+	else if (C_isupper(c) <= 'Z')
+	    c -= ('A' - 10);
+	else
+	    c -= ('a' - 10);
 	b = b * 16 + c;
-	term.replace(a, 4, std::string(&b, 1));
+	term.replace(a, 4, string(&b, 1));
     }
 }
 
 // Keep only the characters in keep
 // FIXME - make this accept character ranges in "keep"
-void select_characters(string &term, const std::string & keep)
+void select_characters(string &term, const string & keep)
 {
-    std::string chars;
-    if(keep.size() == 0) {
-	chars ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789^";
+    string chars;
+    if (keep.empty()) {
+	chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+		"0123456789^";
     } else {
 	chars = keep;
     }
-    std::string::size_type pos;
-    while((pos = term.find_first_not_of(chars)) != std::string::npos)
-    {
-	std::string::size_type endpos = term.find_first_of(chars, pos);
+    string::size_type pos;
+    while ((pos = term.find_first_not_of(chars)) != string::npos) {
+	string::size_type endpos = term.find_first_of(chars, pos);
 	term.erase(pos, endpos - pos);
     }
 }
 
 // Read a paragraph from stream.
-void get_paragraph(std::istream &input, std::string &para) {
+void get_paragraph(istream &input, string &para) {
     para = "";
-    std::string line;
+    string line;
     unsigned linecount = 0;
     do {
-	std::getline(input, line);
+	getline(input, line);
 	para += line;
 	para += "\n";
 	linecount ++;
-	if(linecount > 30) break;;
-    } while(line.find_first_not_of(" \t\f") != std::string::npos || linecount < 3);
+	if (linecount > 30) break;
+    } while (line.find_first_not_of(" \t\r\f") != string::npos || linecount < 3);
 }
 
-// Read a line from stream.
-void get_a_line(std::istream &input, std::string &line) {
-    while(input) {
-	std::getline(input, line);
-	if(line.find_first_not_of(" \t\f") != std::string::npos) break;
+// Read a non-blank line from stream.
+void get_a_line(istream &input, string &line) {
+    while (input) {
+	getline(input, line);
+	if (line.find_first_not_of(" \t\r\f") != string::npos) break;
     }
 }
