14ab60c9ef95346151a0514e5ace49ead8959390
Olly Betts <olly@survex.com>
2005-04-05 01:13:13 +0000

    docs/overview.html,docs/install.html,configure.ac,Makefile.am, xapian.spec.in,examples/Makefile.am: Moved xapian-examples module to examples subdirectory of xapian-core.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6097 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/examples/quest.cc
@@ -0,0 +1,119 @@
+/* quest.cc - Command line search tool using Xapian::QueryParser.
+ *
+ * Copyright (C) 2004, 2005 Olly Betts
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+#include <config.h>
+
+#include <xapian.h>
+#include <xapian/queryparser.h>
+
+#include <iostream>
+
+#include "gnu_getopt.h"
+
+using namespace std;
+
+static void
+show_help(const char * argv0)
+{
+    cout << "Usage: " << argv0 << " [OPTIONS] 'QUERY'\n"
+	 << "NB: query should be quoted to protect it from the shell.\n"
+	 << "\n"
+	 << "  -d, --db=DIRECTORY  database to search (multiple databases may be specified)\n"
+	 << "  -m, --msize=MSIZE   maximum number of matches to return\n"
+	 << "  -h, --help          display this help and exit\n"
+	 << "  -v, --version       output version information and exit\n";
+}
+
+int
+main(int argc, char **argv)
+{
+    static const struct option long_opts[] = {
+	{ "db",		required_argument, 0, 'd' },
+	{ "msize",	required_argument, 0, 'm' },
+	{ "help",	no_argument, 0, 'h' },
+	{ "version",	no_argument, 0, 'v' },
+	{ NULL,		0, 0, 0}
+    };
+
+    int msize = 10;
+
+    bool have_database = false;
+
+    try {
+	Xapian::Database db;
+
+	int c;
+	while ((c = gnu_getopt_long(argc, argv, "hvm:d:", long_opts, 0)) != EOF)
+	{
+	    switch (c) {
+		case 'm':
+		    msize = atoi(optarg);
+		    break;
+		case 'd':
+		    db.add_database(Xapian::Database(optarg));
+		    have_database = true;
+		    break;
+		case 'v':
+		    cout << argv[0] << " (" << PACKAGE_NAME << ") "
+			 << VERSION << endl;
+		    exit(0);
+		case 'h':
+		    show_help(argv[0]);
+		    exit(0);
+		case ':': // missing parameter
+		case '?': // unknown option
+		    show_help(argv[0]);
+		    exit(1);
+	    }
+	}
+
+	if (!have_database || argc - optind != 1) {
+	    show_help(argv[0]);
+	    exit(1);
+	}
+
+	Xapian::Enquire enquire(db);
+
+	try {
+	    Xapian::QueryParser parser;
+	    parser.set_database(db);
+	    parser.set_default_op(Xapian::Query::OP_OR);
+	    parser.set_stemmer(Xapian::Stem("english"));
+	    parser.set_stemming_options(Xapian::QueryParser::STEM_SOME);
+	    // FIXME: pass Xapian::Stopper instead of NULL...
+	    //parser.set_stopper(NULL);
+	    enquire.set_query(parser.parse_query(argv[optind]));
+	} catch (const char * error_msg) {
+	    cout << "Couldn't parse query: " << error_msg << endl;
+	    exit(1);
+	}
+
+	Xapian::MSet mset = enquire.get_mset(0, msize);
+	for (Xapian::MSetIterator i = mset.begin(); i != mset.end(); i++) {
+	    Xapian::Document doc = i.get_document();
+	    string data = doc.get_data();
+	    cout << *i << " [" << i.get_percent() << "%]\n" << data << "\n";
+	}
+	cout << flush;
+    } catch (const Xapian::Error & err) {
+	cout << err.get_msg() << endl;
+	exit(1);
+    }
+}
---
+++ xapian-core/examples/simpleindex.cc
@@ -0,0 +1,120 @@
+/* simpleindex.cc: Index each paragraph in a textfile as a document.
+ *
+ * ----START-LICENCE----
+ * Copyright 1999,2000,2001 BrightStation PLC
+ * Copyright 2002 Ananova Ltd
+ * Copyright 2002,2003,2004 Olly Betts
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ * -----END-LICENCE-----
+ */
+
+#include <xapian.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace Xapian;
+using namespace std;
+
+#include <ctype.h>
+
+// Put a limit on the size of terms to help prevent the index being bloated
+// by useless junk terms
+static const unsigned int MAX_PROB_TERM_LENGTH = 64;
+
+inline static bool
+p_alnum(unsigned int c)
+{
+    return isalnum(c);
+}
+
+inline static bool
+p_notalnum(unsigned int c)
+{
+    return !isalnum(c);
+}
+
+inline static bool
+p_notplusminus(unsigned int c)
+{
+    return c != '+' && c != '-';
+}
+
+static void
+lowercase_term(string &term)
+{
+    string::iterator i = term.begin();
+    while (i != term.end()) {
+	*i = tolower(*i);
+	i++;
+    }
+}
+
+int main(int argc, char **argv)
+try {
+    if (argc != 2) {
+	cout << "usage: " << argv[0] << " <path to database>" << endl;
+	exit(1);
+    }
+
+    // Open the database
+    WritableDatabase database(argv[1], DB_CREATE_OR_OPEN);
+    
+    Stem stemmer("english");
+    string para;
+    while (true) {
+	string line;
+	if (cin.eof()) {
+	    if (para.empty()) break;
+	} else {
+	    getline(cin, line); 
+	}
+	if (line.empty()) {
+	    if (!para.empty()) {
+		Document doc;
+		doc.set_data(para);
+
+		termcount pos = 0;
+		string::iterator i = para.begin();
+		while ((i = find_if(i, para.end(), p_alnum)) != para.end()) {
+		    string::iterator j = find_if(i, para.end(), p_notalnum);
+		    string::iterator k = find_if(j, para.end(), p_notplusminus);
+		    if (k == para.end() || !isalnum(*k)) j = k;
+		    string::size_type len = j - i;
+		    if (len <= MAX_PROB_TERM_LENGTH) {
+			string term = para.substr(i - para.begin(), len);
+			lowercase_term(term);
+			term = stemmer.stem_word(term);
+			doc.add_posting(term, pos++);
+		    }
+		    i = j;
+		}
+
+		// Add the document to the database
+		database.add_document(doc);
+
+		para = "";
+	    }
+	}
+	if (!para.empty()) para += ' ';
+	para += line;
+    }
+} catch (const Error &error) {
+    cerr << "Exception: "  << error.get_msg() << endl;
+    exit(1);
+}
