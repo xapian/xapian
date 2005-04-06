31b1c9d07f342d4d3221bb2c27f0c0017a446e4e
Olly Betts <olly@survex.com>
2005-04-06 01:56:48 +0000

    omindex.cc: Tweak the hashing of URLs so that it works the same way on all platforms (previously it would depend on sizeof(long)). This means an incompatibility with any existing database built on a platform where sizeof(long) > 4 where URLs were hashed (i.e. URLs were > 228 bytes if sizeof(long) == 8), but we really want databases to be portable between platforms.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6120 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omindex.cc
+++ xapian-applications/omega/omindex.cc
@@ -88,9 +88,10 @@ p_notalnum(unsigned int c)
 
 /* Hash is computed as an unsigned long, and then converted to a
  * string by writing 6 bits of it to each output byte.  So length is
- * ceil(sizeof(unsigned long) * 8 / 6).
+ * ceil(4 * 8 / 6) (we use 4 rather than sizeof(unsigned long) so
+ * that the hash is the same regardless of the platform).
  */
-#define HASH_LEN ((sizeof(unsigned long) * 8 + 5) / 6)
+#define HASH_LEN ((4 * 8 + 5) / 6)
 
 /* Make a hash of a string - this isn't a very good hashing algorithm, but
  * it's fast.  A collision would result in a document overwriting a different
@@ -101,12 +102,17 @@ hash_string(const string &s)
 {
     unsigned long int h = 1;
     for (string::const_iterator i = s.begin(); i != s.end(); ++i) {
-	h += (h << 5) + static_cast<unsigned long int>(*i);
+	h += (h << 5) + static_cast<unsigned char>(*i);
     }
+    h &= 0xffffffff; // In case sizeof(unsigned long) > 4
+    // FIXME: It's quirky that we make leading zeros ' ' here, but "embedded"
+    // zeros become char(33) below.  Not a problem, but perhaps change ' ' to
+    // char(33) if we need to break backwards compatiblity for some other
+    // reason.
     string result = string(HASH_LEN, ' ');
     int i = 0;
     while (h != 0) {
-	char ch = (char)((h & 63) + 33);
+	char ch = char((h & 63) + 33);
 	result[i++] = ch;
 	h = h >> 6;
     }
@@ -115,8 +121,8 @@ hash_string(const string &s)
 
 /* Make a term for a url, ensuring that it's not longer than the maximum
  * length.  The term is "U"+baseurl+url, unless this would be too long, in
- * which case it is truncated to the maximum length - the length of the hash,
- * and has a hash of the truncated part appended.
+ * which case it is truncated to (maximum length - length of the hash),
+ * and has a hash of the removed part appended.
  */
 static string
 make_url_term(const string &url)
