91389be67da52e6ea1c504884dc4ef7127940e04
Olly Betts <olly@survex.com>
2005-08-30 11:06:43 +0000

    Let perl know Search::Xapian::WritableDatabase is a subclass of Search::Xapian::Database by adding the latter to the former's @INC.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11218 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- search-xapian/Xapian/WritableDatabase.pm
+++ search-xapian/Xapian/WritableDatabase.pm
@@ -8,7 +8,7 @@ use Carp;
 require Exporter;
 require DynaLoader;
 
-our @ISA = qw(Exporter DynaLoader);
+our @ISA = qw(Exporter DynaLoader Search::Xapian::Database);
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
