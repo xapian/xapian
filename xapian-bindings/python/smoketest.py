7201f5cc9d16815147d032dbc450934750cbd7cb
Olly Betts <olly@survex.com>
2005-12-31 06:42:50 +0000

    python/smoketest.py: Add more tests.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6445 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/python/smoketest.py
+++ xapian-bindings/python/smoketest.py
@@ -1,4 +1,5 @@
-# Simple test that we can load the xapian module and run a simple test
+# Simple test to ensure that we can load the xapian module and exercise basic
+# functionality successfully.
 #
 # Copyright (C) 2004,2005 Olly Betts
 #
@@ -22,6 +23,8 @@ import xapian
 
 try:
     stem = xapian.Stem("english")
+    if stem.get_description() != "Xapian::Stem(english)":
+        sys.exit(1)
     doc = xapian.Document()
     doc.set_data("is there anybody out there?")
     doc.add_term("XYzzy")
@@ -36,9 +39,28 @@ try:
         sys.exit(1)
     terms = ["smoke", "test", "terms"]
     query = xapian.Query(xapian.Query.OP_OR, terms)
+    if query.get_description() != "Xapian::Query((smoke OR test OR terms))":
+        sys.exit(1)
     query1 = xapian.Query(xapian.Query.OP_PHRASE, ("smoke", "test", "tuple"))
-    query2 = xapian.Query(xapian.Query.OP_XOR, (query, query1, "string"))
+    if query1.get_description() != "Xapian::Query((smoke PHRASE 3 test PHRASE 3 tuple))":
+        sys.exit(1)
+    query2 = xapian.Query(xapian.Query.OP_XOR, (xapian.Query("smoke"), query1, "string"))
+    if query2.get_description() != "Xapian::Query((smoke XOR (smoke PHRASE 3 test PHRASE 3 tuple) XOR string))":
+        sys.exit(1)
     subqs = ["a", "b"]
     query3 = xapian.Query(xapian.Query.OP_OR, subqs)
+    if query3.get_description() != "Xapian::Query((a OR b))":
+        sys.exit(1)
+    enq = xapian.Enquire(db)
+    enq.set_query(xapian.Query(xapian.Query.OP_OR, "there", "is"))
+    mset = enq.get_mset(0, 10)
+    if mset.size() != 1:
+        sys.exit(1)
+    terms = " ".join(enq.get_matching_terms(mset.get_hit(0)))
+    print terms
+    if terms != "is there":
+        sys.exit(1)
 except:
     sys.exit(1)
+
+# vim:syntax=python:set expandtab:
