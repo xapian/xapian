37d5d068ef0a3208085e635965ab7ece9ac6d061
Olly Betts <olly@survex.com>
2005-12-31 06:41:34 +0000

    php/smoketest.php: Add more tests.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6444 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/php/smoketest.php
+++ xapian-bindings/php/smoketest.php
@@ -1,5 +1,6 @@
 <?php
-/* Simple test that we can load the xapian module and run a simple test
+/* Simple test to ensure that we can load the xapian module and exercise basic
+ * functionality successfully.
  *
  * Copyright (C) 2004,2005 Olly Betts
  *
@@ -21,6 +22,7 @@
 
 dl("xapian.so");
 $stem = new_Stem("english");
+if (Stem_get_description($stem) != "Xapian::Stem(english)") exit(1);
 $doc = new_Document();
 Document_set_data($doc, "is there anybody out there?");
 Document_add_term($doc, "XYzzy");
@@ -30,11 +32,24 @@ Document_add_posting($doc, Stem_stem_word($stem, "anybody"), 3);
 Document_add_posting($doc, Stem_stem_word($stem, "out"), 4);
 Document_add_posting($doc, Stem_stem_word($stem, "there"), 5);
 $db = inmemory_open();
+// Check virtual function dispatch.
+if (WritableDatabase_get_description($db) != Database_get_description($db))
+    exit(1);
 WritableDatabase_add_document($db, $doc);
 if (Database_get_doccount($db) != 1) exit(1);
-$terms = array("smoke", "test", "terms")
-$query = new_Query(Query_OP_OR, $terms)
-$query1 = new_Query(Query_OP_PHRASE, array("smoke", "test", "tuple"))
-$query2 = new_Query(Query_OP_XOR, array($query, $query1, "string"))
-$subqs = array("a", "b")
-$query3 = new_Query(Query_OP_OR, $subqs)
+$terms = array("smoke", "test", "terms");
+$query = new_Query(Query_OP_OR, $terms);
+if (Query_get_description($query) != "Xapian::Query((smoke OR test OR terms))") exit(1);
+$query1 = new_Query(Query_OP_PHRASE, array("smoke", "test", "tuple"));
+if (Query_get_description($query1) != "Xapian::Query((smoke PHRASE 3 test PHRASE 3 tuple))") exit(1);
+$query2 = new_Query(Query_OP_XOR, array(new_Query("smoke"), $query1, "string"));
+if (Query_get_description($query2) != "Xapian::Query((smoke XOR (smoke PHRASE 3 test PHRASE 3 tuple) XOR string))") exit(1);
+$subqs = array("a", "b");
+$query3 = new_Query(Query_OP_OR, $subqs);
+if (Query_get_description($query3) != "Xapian::Query((a OR b))") exit(1);
+$enq = new_Enquire($db);
+Enquire_set_query($enq, new_Query(Query_OP_OR, "there", "is"));
+$mset = Enquire_get_mset($enq, 0, 10);
+if (MSet_size($mset) != 1) exit(1);
+$terms = join(" ", Enquire_get_matching_terms($enq, MSet_get_hit($mset, 0)));
+if ($terms != "is there") exit(1);
