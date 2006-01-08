3d81df74a5960b1497dbc2120f3fa28a24c0652c
Olly Betts <olly@survex.com>
2006-01-08 03:52:47 +0000

    testsuite/backendmanager.cc: Fix conditional compilation of flint backend to use XAPIAN_BUILD_BACKEND_FLINT instead of XAPIAN_BUILD_BACKEND_QUARTZ.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6494 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/testsuite/backendmanager.cc
+++ xapian-core/testsuite/backendmanager.cc
@@ -1,9 +1,8 @@
 /* backendmanager.cc: manage backends for testsuite
  *
- * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004,2005 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,9 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
- * -----END-LICENCE-----
  */
 
 #include <config.h>
@@ -132,7 +130,7 @@ BackendManager::set_dbtype(const string &type)
 #endif
 #endif
     } else if (type == "flint") {
-#ifdef XAPIAN_BUILD_BACKEND_QUARTZ
+#ifdef XAPIAN_BUILD_BACKEND_FLINT
 	do_getdb = &BackendManager::getdb_flint;
 	do_getwritedb = &BackendManager::getwritedb_flint;
 	rmdir(".flint");
