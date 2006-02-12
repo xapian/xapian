b95b265cadfa26b4a4ac88ec92bf316cddac8f0f
Olly Betts <olly@survex.com>
2006-02-12 07:28:51 +0000

    java/native/Query.cc: Fixed memory leak in Query-from-array-of-strings constructor.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6556 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/java/native/Query.cc
+++ xapian-bindings/java/native/Query.cc
@@ -1,5 +1,6 @@
 /**
  Copyright (c) 2003, Technology Concepts & Design, Inc.
+ Copyright (c) 2006, Olly Betts
  All rights reserved.
 
  Redistribution and use in source and binary forms, with or without modification, are permitted
@@ -112,6 +113,7 @@ JNIEXPORT jlong JNICALL Java_org_xapian_XapianJNI_query_1new__I_3Ljava_lang_Stri
         jsize size = env->GetArrayLength(terms);
         string *array = toArray(env, terms, size);
         Query *q = new Query(op_table[op-1], array, array+size);
+	delete array;
         return _query->put(q);
     CATCH(-1)
 }
