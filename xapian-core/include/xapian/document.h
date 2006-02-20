490c2de3fd2cadcb5c0c2d58f7ca6454facd08cf
Olly Betts <olly@survex.com>
2006-02-20 15:48:28 +0000

    include/xapian/document.h: __attribute__((deprecated)) doesn't work on method definitions with default parameters on GCC 3.2 so fix header to overload instead.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6603 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian/document.h
+++ xapian-core/include/xapian/document.h
@@ -1,10 +1,9 @@
 /** \file document.h
  * \brief API for working with documents
  */
-/* ----START-LICENCE----
- * Copyright 1999,2000,2001 BrightStation PLC
+/* Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004 Olly Betts
+ * Copyright 2002,2003,2004,2006 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -18,9 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
- * -----END-LICENCE-----
  */
 
 #ifndef XAPIAN_INCLUDED_DOCUMENT_H
@@ -123,8 +121,16 @@ class Document {
 	 *  @deprecated	This method is deprecated and present only for backward
 	 *  compatibility.  Use add_term() instead.
 	 */
-	XAPIAN_DEPRECATED(void add_term_nopos(const std::string & term,
-			    Xapian::termcount wdfinc = 1)) {
+	XAPIAN_DEPRECATED(void add_term_nopos(const std::string & term)) {
+	    add_term(term, 1);
+	}
+
+	/** Old name for add_term().
+	 *
+	 *  @deprecated	This method is deprecated and present only for backward
+	 *  compatibility.  Use add_term() instead.
+	 */
+	XAPIAN_DEPRECATED(void add_term_nopos(const std::string & term, Xapian::termcount wdfinc)) {
 	    add_term(term, wdfinc);
 	}
 
