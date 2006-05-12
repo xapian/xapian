109e8a9a01e36a75de12b775b94f4960b1158375
Olly Betts <olly@survex.com>
2006-05-12 02:56:21 +0000

    python/docs/examples/simpleexpand.py: Don't define MAX_PROB_TERM_LENGTH as it isn't used.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@6905 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/python/docs/examples/simpleexpand.py
+++ xapian-bindings/python/docs/examples/simpleexpand.py
@@ -3,9 +3,8 @@
 # $Id$
 # Simple command-line query expand program
 #
-# ----START-LICENCE----
 # Copyright 2003 James Aylett
-# Copyright 2004 Olly Betts
+# Copyright 2004,2006 Olly Betts
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -19,14 +18,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
 # USA
-# -----END-LICENCE-----
 
 import sys
 import xapian
-
-MAX_PROB_TERM_LENGTH = 64
 
 if len(sys.argv) < 3:
     print >> sys.stderr, "usage: %s <path to database> [<search terms>] [-- <relevant docids>]" % sys.argv[0]
