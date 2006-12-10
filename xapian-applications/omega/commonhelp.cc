3f98c46c451defd5b34636cd7dd59a5fca65e810
Olly Betts <olly@survex.com>
2006-12-10 03:06:09 +0000

    commonhelp.cc: "--help" now says that the default stemming language is "english"
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7558 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/commonhelp.cc
+++ xapian-applications/omega/commonhelp.cc
@@ -32,8 +32,9 @@ void print_package_info(const char *name) {
 }
 
 void print_stemmer_help(const char * spaces) {
-    cout << "  -s, --stemmer=LANG  " << spaces << "set the stemming language ('none' disables stemming)\n";
+    cout << "  -s, --stemmer=LANG  " << spaces << "set the stemming language, the default is 'english'\n";
     string wrap = "Possible values: " + Xapian::Stem::get_available_languages();
+    wrap += " (pass 'none' to disable stemming)";
     const size_t WRAP_AT = 79 - 22 - strlen(spaces);
     while (wrap.size() > WRAP_AT) {
 	size_t i;
