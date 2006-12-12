4d68a179f8ce33afa4566a3d9107241d61ce066c
Richard Boulton <richard@tartarus.org>
2006-12-12 21:07:08 +0000

    * tests/api_nodb.cc,api/omqueryinternal.cc,api/omquery.cc: Fix some   copyright assertions I missed.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7564 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/api/omquery.cc
+++ xapian-core/api/omquery.cc
@@ -3,6 +3,7 @@
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001,2002 Ananova Ltd
  * Copyright 2003,2004,2005,2006 Olly Betts
+ * Copyright 2006 Lemur Consulting Ltd
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
