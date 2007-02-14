8321dab22aee1af80fc92a45df713a6bd15674fb
Olly Betts <olly@survex.com>
2007-02-14 07:50:11 +0000

    Makefile.am,api/Makefile,api/Makefile.am,api/Makefile.mk, configure.ac:  Convert api subdirectory to non-recursive make.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7662 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/api/Makefile.mk
@@ -0,0 +1,19 @@
+noinst_HEADERS +=\
+	api/maptermlist.h
+
+EXTRA_DIST +=\
+	api/dir_contents\
+	api/Makefile
+
+libxapian_la_SOURCES +=\
+	api/errorhandler.cc\
+	api/omdatabase.cc\
+	api/omdocument.cc\
+	api/omenquire.cc\
+	api/ompositionlistiterator.cc\
+	api/ompostlistiterator.cc\
+	api/omquery.cc\
+	api/omqueryinternal.cc\
+	api/omtermlistiterator.cc\
+	api/omvalueiterator.cc\
+	api/version.cc
