928b8ffebc2ba41a4661f632986fadf916c5b76a
Olly Betts <olly@survex.com>
2007-03-03 02:25:04 +0000

    utf8test.cc: Add single utf-8 sequence decoding tests.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7843 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/utf8test.cc
+++ xapian-applications/omega/utf8test.cc
@@ -1,6 +1,6 @@
 /* utf8test.cc: test the Utf8Iterator class
  *
- * Copyright (C) 2006 Olly Betts
+ * Copyright (C) 2006,2007 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -34,14 +34,30 @@ struct testcase {
 static const testcase testcases[] = {
     { "abcd", "abcd" }, // Sanity check!
     { "a\x80""bcd", "a\xc2\x80""bcd" },
-    { "a\xa0", "a\xc2\xa0" }, 
+    { "a\xa0", "a\xc2\xa0" },
+    { 0, 0 }
+};
+
+struct testcase2 {
+    const char * a;
+    unsigned long n;
+};
+
+static const testcase2 testcases2[] = {
+    { "a", 97 },
+    { "\x80", 128 },
+    { "\xa0", 160 },
+    { "\xc2\x80", 128 },
+    { "\xc2\xa0", 160 },
+    { "\xf0\xa8\xa8\x8f", 166415 },
+    { 0, 0 }
 };
 
 int
 main()
 {
     const testcase * p;
-    for (p = testcases; p < testcases + sizeof(testcases) / sizeof(testcases[0]); ++p) {
+    for (p = testcases; p->a; ++p) {
 	size_t a_len = strlen(p->a);
 	Utf8Iterator a(p->a, a_len);
 
@@ -59,6 +75,24 @@ main()
 
 	if (a != Utf8Iterator() || b != Utf8Iterator()) {
 	    cerr << "\"" << p->a << "\" and \"" << p->b << "\" weren't the same length" << endl;
+	    exit(1);
+	}
+    }
+
+    const testcase2 * q;
+    for (q = testcases2; q->a; ++q) {
+	Utf8Iterator a(q->a, strlen(q->a));
+
+	if (a == Utf8Iterator()) {
+	    cerr << "\"" << p->a << "\" yields no characters" << endl;
+	    exit(1);
+	}
+	if (*a != q->n) {
+	    cerr << "*a yields " << *a << ", expected " << q->n << endl;
+	    exit(1);
+	}
+	if (++a != Utf8Iterator()) {
+	    cerr << "\"" << p->a << "\" yields more than one characters" << endl;
 	    exit(1);
 	}
     }
