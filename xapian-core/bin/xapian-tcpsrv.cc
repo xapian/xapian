59fd7650e51fd447a79d84fbe440aa93e84ac886
Olly Betts <olly@survex.com>
2007-03-03 19:47:26 +0000

    bin/xapian-tcpsrv.cc: Put "e.get_errno()" in a variable for clearer code.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7851 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/bin/xapian-tcpsrv.cc
+++ xapian-core/bin/xapian-tcpsrv.cc
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001,2002 Ananova Ltd
- * Copyright 2002,2003,2004,2006 Olly Betts
+ * Copyright 2002,2003,2004,2006,2007 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -185,9 +185,10 @@ int main(int argc, char **argv) {
 	}
     } catch (const Xapian::Error &e) {
 	cerr << e.get_type() << ": " << e.get_msg();
-	if (e.get_errno()) cerr << " (errno:" << strerror(e.get_errno()) << ")";
+	int my_errno = e.get_errno();
+	if (my_errno) cerr << " (errno:" << strerror(my_errno) << ')';
 	cerr << endl;
-	if (e.get_errno() == EADDRINUSE) exit(69); // EX_UNAVAILABLE
+	if (my_errno == EADDRINUSE) exit(69); // EX_UNAVAILABLE
 	exit(1);
     } catch (const exception &e) {
 	cerr << "Caught standard exception: " << e.what() << endl;
