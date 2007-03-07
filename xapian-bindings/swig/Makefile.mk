bcf3802e473d9f363062c1f1356c66e337472cdc
Richard Boulton <richard@tartarus.org>
2007-03-07 16:28:19 +0000

    * xapian.i,swig/xapian.i: Move swig source file to a subdirectory, 	  in preparation for splitting the language dependent stuff out. 	  This commit shouldn't change the result of running swig. 	* swig/Makefile.mk: New makefile fragment, included by each 	  language-specific SWIG-using Makefile.am, which defines the paths 	  and flags to use when running SWIG.  Also contains the 	  MACOSX_DEPLOYMENT_TARGET stuff which was previously replicated in 	  each SWIG-using Makefile.am 	* csharp/Makefile.am,ruby/Makefile.am,python/Makefile.am, 	  php/Makefile.am,guile/Makefile.am,tcl8/Makefile.am: Include 	  swig/Makefile.mk to get the path to the language independent swig 	  sources, and standard paths and flags to supply to SWIG.  Update 	  dependencies and rules for generating the xapian_wrap.cc files to 	  use the values from swig/Makefile.mk.  Remove the 	  MACOSX_DEPLOYMENT_TARGET stuff (now in swig/Makefile.mk). 	* configure.ac: Reference swig/xapian.i from AC_CONFIG_SRCDIR 	  instead of xapian.i 	* Makefile.am: Reference swig/xapian.i in EXTRA_DIST 	  instead of xapian.i
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7905 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-bindings/swig/Makefile.mk
@@ -0,0 +1,18 @@
+## Makefile fragment included from each language binding directory which uses
+## SWIG.  Any makefile rules or variables which should be set for all SWIG
+## bindings should be placed here.
+
+if OVERRIDE_MACOSX_DEPLOYMENT_TARGET
+# This requires GNU make, but apparently that's the default on OS X.
+export MACOSX_DEPLOYMENT_TARGET=@OVERRIDE_MACOSX_DEPLOYMENT_TARGET@
+endif
+
+SWIG_mainsource = \
+	$(srcdir)/../swig/xapian.i
+
+SWIG_sources = \
+	$(SWIG_mainsource)
+
+SWIG_includes = \
+	-I$(srcdir)
+
