4797ee492cc37774626af0d1bb88f867123108f7
Olly Betts <olly@survex.com>
2007-03-24 17:18:24 +0000

    omega.cc: Prefer static char[] to static char * (gives better generated code).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@7951 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omega.cc
+++ xapian-applications/omega/omega.cc
@@ -3,7 +3,7 @@
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001 James Aylett
  * Copyright 2001,2002 Ananova Ltd
- * Copyright 2002,2003,2004,2006 Olly Betts
+ * Copyright 2002,2003,2004,2006,2007 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -45,7 +45,7 @@
 
 using namespace std;
 
-static const char * DEFAULT_STEM_LANGUAGE = "english";
+static const char DEFAULT_STEM_LANGUAGE[] = "english";
 
 Xapian::Enquire * enquire;
 Xapian::Database db;
