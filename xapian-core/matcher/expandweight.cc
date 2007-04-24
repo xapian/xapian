d6ceaaa2c14aed3cce4c895367c5139cba2abdc5
Olly Betts <olly@survex.com>
2007-04-24 00:26:32 +0000

    matcher/bm25weight.cc,matcher/expandweight.cc: Fix typo in comments: 'releveant' -> 'relevant'.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8380 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/expandweight.cc
+++ xapian-core/matcher/expandweight.cc
@@ -111,7 +111,7 @@ OmExpandWeight::get_weight(const OmExpandBits &bits,
 	    // termfreq must be at least rtermfreq since there are at least
 	    // rtermfreq documents indexed by this term.  And it can't be
 	    // more than (dbsize - rsize + rtermfreq) since the number
-	    // of releveant documents not indexed by this term can't be
+	    // of relevant documents not indexed by this term can't be
 	    // more than the number of documents not indexed by this term.
 	    if (termfreq < rtermfreq) {
 		termfreq = rtermfreq;
