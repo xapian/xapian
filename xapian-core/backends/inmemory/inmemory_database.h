5b40e80d326b2b00a601dadb12901103f9b1e79a
Olly Betts <olly@survex.com>
2007-04-24 01:57:33 +0000

    backends/flint/flint_database.h, backends/inmemory/inmemory_database.h, backends/quartz/quartz_database.h: MSVC doesn't handle using properly.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8381 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/inmemory/inmemory_database.h
+++ xapian-core/backends/inmemory/inmemory_database.h
@@ -270,7 +270,7 @@ class InMemoryDatabase : public Xapian::Database::Internal {
 	// a problem as we only try to call them through the base class
 	// (where they aren't hidden) but some compilers generate a warning
 	// about the hiding.
-#if !defined __GNUC__ || __GNUC__ > 2
+#if (!defined __GNUC__ && !defined _MSC_VER) || __GNUC__ > 2
 	using Xapian::Database::Internal::delete_document;
 	using Xapian::Database::Internal::replace_document;
 #endif
