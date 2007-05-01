839dfba142af2224d21d8373460734ea8145ae14
Olly Betts <olly@survex.com>
2007-05-01 14:20:16 +0000

    matcher/branchpostlist.h: Don't check for NULL pointer before calling delete.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8403 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/branchpostlist.h
+++ xapian-core/matcher/branchpostlist.h
@@ -1,9 +1,8 @@
 /* branchpostlist.h: virtual base class for branched types of postlist
  *
- * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2003,2004 Olly Betts
+ * Copyright 2003,2004,2007 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,9 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
- * -----END-LICENCE-----
  */
 
 #ifndef OM_HGUARD_BRANCHPOSTLIST_H
@@ -73,8 +71,8 @@ class BranchPostList : public PostList {
 		: l(l_), r(r_), matcher(matcher_) {}
 
 	virtual ~BranchPostList() {
-	    if (l) delete l;
-	    if (r) delete r;
+	    delete l;
+	    delete r;
 	}
 
 	/** Most branch postlists won't be able to supply position lists.
