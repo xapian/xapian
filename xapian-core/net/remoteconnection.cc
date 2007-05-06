4fe938644cc10b25bdd9449f68271c636bebbcdf
Olly Betts <olly@survex.com>
2007-05-06 01:29:51 +0000

    net/remoteconnection.cc: Protect _set_invalid_parameter_handler, etc by _MSC_VER >= 1400 (MSVC 2005) and __STDC_SECURE_LIB__ (undocumented but cribbed from the Python sources).  Refactor into a handy class (MSVCIgnoreInvalidParameter) which just needs to be instantiated in the scope where you want to allow invalid parameters to be passed.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8465 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/net/remoteconnection.cc
+++ xapian-core/net/remoteconnection.cc
@@ -33,13 +33,12 @@
 
 #ifndef __WIN32__
 # include "safesysselect.h"
-# ifdef _MSC_VER
-#  include <stdlib.h> // For _set_invalid_parameter_handler().
-# endif
-#endif
 
-#ifdef __WIN32__
-#ifdef _MSC_VER
+# ifdef _MSC_VER && _MSC_VER >= 1400 && defined __STDC_SECURE_LIB__
+#  define XAPIAN_MSVC_INVALID_PARAMETER_HANDLER
+#  include <stdlib.h> // For _set_invalid_parameter_handler(), etc.
+#  include <crtdbg.h> // For _CrtSetReportMode, etc.
+
 /** A dummy invalid parameter handler which ignores the error. */
 static void dummy_handler(const wchar_t*,
 			  const wchar_t*,
@@ -48,23 +47,41 @@ static void dummy_handler(const wchar_t*,
 			  uintptr_t)
 {
 }
-#endif
+
+// Recent versions of MSVC call an "_invalid_parameter_handler" if a
+// CRT function receives an invalid parameter.  However, there are cases
+// where this is totally reasonable.  To avoid the application dying,
+// you just need to instantiate the MSVCIgnoreInvalidParameter class in
+// the scope where you want MSVC to ignore invalid parameters.
+class MSVCIgnoreInvalidParameter {
+    _invalid_parameter_handler old_handler;
+    int old_report_mode;
+
+  public:
+    MSVCIgnoreInvalidParameter() {
+	// Install a dummy handler to avoid the program dying.
+	old_handler = _set_invalid_parameter_handler(dummy_handler);
+	// Make sure that no dialog boxes appear.
+	old_report_mode = _CrtSetReportMode(_CRT_ASSERT, 0);
+    }
+
+    ~MSVCIgnoreInvalidParameter() {
+	// Restore the previous settings.
+	_set_invalid_parameter_handler(old_handler);
+	_CrtSetReportMode(_CRT_ASSERT, old_report_mode);
+    }
+};
+# else
+// Mingw seems to be free of this insanity, so for this and older MSVC versions
+// define a dummy class to allow MSVCIgnoreInvalidParameter to be used
+// unconditionally.
+struct MSVCIgnoreInvalidParameter {};
+# endif
 
 /// Convert an fd (which might be a socket) to a WIN32 HANDLE.
 static HANDLE fd_to_handle(int fd) {
-#ifdef _MSC_VER
-    // Recent versions of MSVC call an "_invalid_parameter_handler" if
-    // _get_osfhandle() is about to return INVALID_HANDLE_VALUE.  This is a
-    // case we expect and handle, so we have to install a dummy handler to
-    // avoid the program dying.  Mingw seems to be free of this insanity (and
-    // even if it gets sucked in by the runtime DLL, it currently doesn't have
-    // the header support to compile this code).
-    _invalid_parameter_handler old_handler = _set_invalid_parameter_handler(dummy_handler);
-#endif
+    MSVCIgnoreInvalidParameter invalid_handle_value_is_ok;
     HANDLE handle = (HANDLE)_get_osfhandle(fd);
-#ifdef _MSC_VER
-    _set_invalid_parameter_handler(old_handler);
-#endif
     // On WIN32, a socket fd isn't the same as a non-socket fd - in fact
     // it's already a HANDLE!
     return (handle != INVALID_HANDLE_VALUE ? handle : (HANDLE)fd);
