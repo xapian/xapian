b20e80c7e05a0a6d4f85cbf9e8c09dba66342857
Olly Betts <olly@survex.com>
2007-05-08 15:18:22 +0000

    include/xapian/deprecated.h: Use `__attribute__((__deprecated__))' instead of `__attribute__((deprecated))' so we're even robust against some clown doing `#define deprecated foo'.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8504 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian/deprecated.h
+++ xapian-core/include/xapian/deprecated.h
@@ -29,8 +29,8 @@
 // it.
 #ifndef XAPIAN_DEPRECATED
 # if defined __GNUC__ && (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ > 0))
-// __attribute__((deprecated)) is supported by GCC 3.1 and later.
-#  define XAPIAN_DEPRECATED(D) D __attribute__((deprecated))
+// __attribute__((__deprecated__)) is supported by GCC 3.1 and later.
+#  define XAPIAN_DEPRECATED(D) D __attribute__((__deprecated__))
 # elif defined _MSC_VER && _MSC_VER >= 1300
 // __declspec(deprecated) is supported by MSVC 7.0 and later.
 #  define XAPIAN_DEPRECATED(D) __declspec(deprecated) D
