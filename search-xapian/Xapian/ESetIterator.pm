52ec5ffb91c3bac34fd0917f5e56e6deef7d9a7c
Olly Betts <olly@survex.com>
2007-05-25 22:57:58 +0000

    Fix missing apostrophe in widely cut-and-pasted comment.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11283 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- search-xapian/Xapian/ESetIterator.pm
+++ search-xapian/Xapian/ESetIterator.pm
@@ -9,7 +9,7 @@ require Exporter;
 require DynaLoader;
 
 our @ISA = qw(Exporter DynaLoader);
-# Items to export into callers namespace by default. Note: do not export
+# Items to export into caller's namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
