7bcb24c49c962fe4f78ea536467dacddc749537e
Olly Betts <olly@survex.com>
2007-05-27 15:15:05 +0000

    python/generate-python-exceptions.in: Remove __str__ method from %extend of Error class, since C++ now provides an Error::get_description() method which is automatically renamed to __str__. python/pythontest.py,python/smoketest.py: New C++ Error::get_description() method gives different output to the old Python-specific __str__, so update testcases to match. python/generate-python-exceptions.in: Mark generated file using `@configure_input@'.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8687 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/python/smoketest.py
+++ xapian-bindings/python/smoketest.py
@@ -155,7 +155,7 @@ def test_all():
     expect(count, 0, "Unexpected number of entries in doc.values")
 
     # Check exception handling for Xapian::DocNotFoundError
-    expect_exception(xapian.DocNotFoundError, "Docid 3 not found", db.get_document, 3)
+    expect_exception(xapian.DocNotFoundError, "DocNotFoundError: Docid 3 not found", db.get_document, 3)
 
     expect(xapian.Query.OP_ELITE_SET, 10, "Unexpected value for OP_ELITE_SET")
 
@@ -195,7 +195,7 @@ def test_all():
 
     # Check QueryParser parsing error.
     qp = xapian.QueryParser()
-    expect_exception(xapian.QueryParserError, "Syntax: <expression> AND <expression>", qp.parse_query, "test AND")
+    expect_exception(xapian.QueryParserError, "QueryParserError: Syntax: <expression> AND <expression>", qp.parse_query, "test AND")
 
     # Check QueryParser pure NOT option
     qp = xapian.QueryParser()
