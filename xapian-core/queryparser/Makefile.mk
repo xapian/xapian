1d9c59dad38febe39bf801d491c9791d7216c59e
Richard Boulton <richard@tartarus.org>
2007-05-29 20:00:12 +0000

    queryparser/Makefile.mk: Fix multiple target rule for generating the queryparser source files in parallel builds, as described in HACKING, and as done for the SWIG rules in xapian-bindings. queryparser/: Add queryparser_internal.lock and queryparser_internal.stamp to SVN ignores.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8750 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/queryparser/Makefile.mk
+++ xapian-core/queryparser/Makefile.mk
@@ -31,12 +31,27 @@ if MAINTAINER_MODE
 queryparser/lemon: queryparser/lemon.c
 	$(CC_FOR_BUILD) -o queryparser/lemon $(srcdir)/queryparser/lemon.c
 
+queryparser/queryparser_internal.cc queryparser/queryparser_token.h: queryparser/queryparser_internal.stamp
+## Recover from the removal of $@.  A full explanation of these rules is in the
+## automake manual under the heading "Multiple Outputs".
+	@if test -f $@; then :; else \
+	  trap 'rm -rf queryparser/queryparser_internal.lock queryparser/queryparser_internal.stamp' 1 2 13 15; \
+	  if mkdir queryparser/queryparser_internal.lock 2>/dev/null; then \
+	    rm -f queryparser/queryparser_internal.stamp; \
+	    $(MAKE) $(AM_MAKEFLAGS) queryparser/queryparser_internal.stamp; \
+	    rmdir queryparser/queryparser_internal.lock; \
+	  else \
+	    while test -d queryparser/queryparser_internal.lock; do sleep 1; done; \
+	    test -f queryparser/queryparser_internal.stamp; exit $$?; \
+	  fi; \
+	fi
 # Lemon carefully avoids touching queryparser_token.h if it hasn't changed,
 # but only the generated file queryparser_internal.cc depends
 # on it, so it's better to touch it so we can have a
 # dependency to generate it.
-queryparser/queryparser_internal.cc queryparser/queryparser_token.h: queryparser/queryparser.lemony queryparser/queryparser.lt queryparser/lemon
+queryparser/queryparser_internal.stamp: queryparser/queryparser.lemony queryparser/queryparser.lt queryparser/lemon
 	queryparser/lemon -q -oqueryparser/queryparser_internal.cc -hqueryparser/queryparser_token.h $(srcdir)/queryparser/queryparser.lemony && touch queryparser/queryparser_token.h
+	touch $@
 
 BUILT_SOURCES += $(lemon_built_sources)
 CLEANFILES += $(lemon_built_sources)\
