1e4f9285012f890e1c965a9c2dcbcf7c9a43c7a9
Olly Betts <olly@survex.com>
2007-05-31 22:02:51 +0000

    examples/simplesearch.cc: Replacement "simplesearch" example which uses the QueryParser.  That makes for a simpler example, and it's much more typical of what most users will want to do.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@8785 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/examples/simplesearch.cc
+++ xapian-core/examples/simplesearch.cc
@@ -1,14 +1,14 @@
-/* simplesearch.cc: Simple command-line search program
- * (See quest for a more complete example with sophisticated query parsing).
+/** @file simplesearch.cc
+ * @brief Simple command-line search utility.
  *
- * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2007 Olly Betts
+ * See "quest" for a more sophisticated example.
+ */
+/* Copyright (C) 2007 Olly Betts
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,66 +17,64 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
 #include <xapian.h>
 
-#include <stdlib.h>
-
 #include <iostream>
 #include <string>
-#include <vector>
 
-using namespace Xapian;
+#include <stdlib.h> // For exit().
+
 using namespace std;
 
-int main(int argc, char **argv)
-{
-    // Simplest possible options parsing: we just require two or more
-    // parameters.
+int
+main(int argc, char **argv)
+try {
+    // We require at least two command line arguments.
     if (argc < 3) {
-	cout << "usage: " << argv[0] <<
-		" <path to database> <search terms>" << endl;
+	cout << "Usage: " << argv[0] << " PATH_TO_DATABASE QUERY" << endl;
 	exit(1);
     }
 
-    // Catch any Error exceptions thrown
-    try {
-	// Open the database
-	Database db(argv[1]);
+    // Open the database for searching.
+    Xapian::Database db(argv[1]);
 
-	// Start an enquire session
-	Enquire enquire(db);
+    // Start an enquire session.
+    Xapian::Enquire enquire(db);
 
-	Stem stemmer("english");
-	vector<string> stemmed_terms;
-
-	argv += 2;
-	while (*argv) {
-	    stemmed_terms.push_back(stemmer(*argv++));
-	}
-
-	// Build a query by OR-ing together all the terms
-	Query query(Query::OP_OR, stemmed_terms.begin(), stemmed_terms.end());
-	cout << "Performing query `" << query.get_description() << "'" << endl;
-
-	// Give the query object to the enquire session
-	enquire.set_query(query);
-
-	// Get the top 10 results of the query
-	MSet matches = enquire.get_mset(0, 10);
-
-	// Display the results
-	cout << matches.get_matches_estimated() << " results found" << endl;
-
-	for (MSetIterator i = matches.begin(); i != matches.end(); ++i) {
-	    cout << "ID " << *i << " " << i.get_percent() << "% ["
-		 << i.get_document().get_data() << "]" << endl;
-	}
-    } catch (const Error &error) {
-	cout << "Exception: "  << error.get_description() << endl;
-	exit(1);
+    // Combine the rest of the command line arguments with spaces between
+    // them, so that simple queries don't have to be quoted at the shell
+    // level.
+    string query_string(argv[2]);
+    argv += 3;
+    while (*argv) {
+	query_string += ' ';
+	query_string += *argv++;
     }
+
+    // Parse the query string to produce a Xapian::Query object.
+    Xapian::QueryParser qp;
+    Xapian::Stem stemmer("english");
+    qp.set_stemmer(stemmer);
+    qp.set_database(db);
+    qp.set_stemming_strategy(Xapian::QueryParser::STEM_SOME);
+    Xapian::Query query = qp.parse_query(query_string);
+    cout << "Parsed query is: " << query.get_description() << endl;
+
+    // Find the top 10 results for the query.
+    enquire.set_query(query);
+    Xapian::MSet matches = enquire.get_mset(0, 10);
+
+    // Display the results.
+    cout << matches.get_matches_estimated() << " results found:" << endl;
+
+    for (Xapian::MSetIterator i = matches.begin(); i != matches.end(); ++i) {
+	cout << i.get_rank() + 1 << ": " << i.get_percent() << "% docid=" << *i
+	     << " [" << i.get_document().get_data() << "]\n\n";
+    }
+} catch (const Xapian::Error &e) {
+    cout << e.get_description() << endl;
+    exit(1);
 }
