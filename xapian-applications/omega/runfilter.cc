9d607994e84b3d970d0129ab2adb46642fa4a71e
Olly Betts <olly@survex.com>
2007-09-08 18:26:11 +0000

    configure.ac,runfilter.cc: Impose a 5 minute CPU time limit on filter programs to prevent problems if a filter program goes into an infinite loop on a malformed input.  Partly addresses bug#111.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@9243 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/runfilter.cc
+++ xapian-applications/omega/runfilter.cc
@@ -19,14 +19,27 @@
 
 #include <config.h>
 
+#include <iostream>
 #include <string>
 
+#include "safeerrno.h"
 #include <sys/types.h>
 #include <stdio.h>
 #include "safefcntl.h"
+
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
+#endif
+#ifdef HAVE_SYS_RESOURCE_H
+# include <sys/resource.h>
+#endif
 #ifdef HAVE_SYS_WAIT_H
 # include <sys/wait.h>
 #endif
+#ifdef HAVE_SYS_SOCKET_H
+# include <sys/socket.h>
+#endif
+#include "safeunistd.h"
 
 #include "runfilter.h"
 
@@ -50,6 +63,68 @@ string
 stdout_to_string(const string &cmd)
 {
     string out;
+#if defined HAVE_FORK && defined HAVE_SOCKETPAIR && defined HAVE_SETRLIMIT
+    // We want to be able to get the exit status of the child process.
+    signal(SIGCHLD, SIG_DFL);
+
+    int fds[2];
+    if (socketpair(AF_UNIX, SOCK_STREAM, PF_UNSPEC, fds) < 0)
+	throw ReadError();
+
+    pid_t child = fork();
+    if (child == 0) {
+	// We're the child process.
+
+	// Close the parent's side of the socket pair.
+	close(fds[0]);
+
+	// Connect stdout to our side of the socket pair.
+	dup2(fds[1], 1);
+
+	// Impose some pretty generous resource limits to prevent run-away
+	// filter programs from causing problems.
+
+	// Limit CPU time to 300 seconds (5 minutes).
+	struct rlimit cpu_limit = { 300, RLIM_INFINITY } ;
+	setrlimit(RLIMIT_CPU, &cpu_limit);
+
+	execl("/bin/sh", "/bin/sh", "-c", cmd.c_str(), (void*)NULL);
+	_exit(-1);
+    }
+
+    // We're the parent process.
+
+    // Close the child's side of the socket pair.
+    close(fds[1]);
+    if (child == -1) {
+	// fork() failed.
+	close(fds[0]);
+	throw ReadError();
+    }
+
+    int fd = fds[0];
+
+    while (true) {
+	char buf[4096];
+	ssize_t res = read(fd, buf, sizeof(buf));
+	if (res == 0) break;
+	if (res == -1) {
+	    if (errno != EINTR) {
+		close(fd);
+		int status;
+		(void)waitpid(child, &status, 0);
+		throw ReadError();
+	    }
+	}
+	out.append(buf, res);
+    }
+
+    close(fd);
+    int status;
+    if (waitpid(child, &status, 0) == -1) {
+	throw ReadError();
+    }
+#else
     FILE * fh = popen(cmd.c_str(), "r");
     if (fh == NULL) throw ReadError();
     while (!feof(fh)) {
@@ -62,10 +137,17 @@ stdout_to_string(const string &cmd)
 	out.append(buf, len);
     }
     int status = pclose(fh);
+#endif
+
     if (status != 0) {
 	if (WIFEXITED(status) && WEXITSTATUS(status) == 127) {
 	    throw NoSuchFilter();
 	}
+#ifdef SIGXCPU
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGXCPU) {
+	    cerr << "Filter process consumed too much CPU time" << endl;
+	}
+#endif
 	throw ReadError();
     }
     return out;
