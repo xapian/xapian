a30bc0dc299ef1c75b35bcec68947af58df5a7cd
Richard Boulton <richard@tartarus.org>
2007-09-15 22:17:03 +0000

    Add EXTRADISTCHECK_CONFIGURE_FLAGS which should allow distcheck to pass successfully.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@9284 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-maintainer-tools/buildbot/xapian_factories.py
+++ xapian-maintainer-tools/buildbot/xapian_factories.py
@@ -87,15 +87,19 @@ def gen_svn_clean_factory(baseURL):
     f.addStep(step.Configure, command = ["./configure", "PYTHON_LIB=`pwd`/tmp_pylib", "PHP_EXTENSION_DIR=`pwd`/tmp_phplib"])
     extraargs = (
         "XAPIAN_TESTSUITE_OUTPUT=plain", "VALGRIND=",
-        "rubylibdir=`pwd`/tmp_rubylib",
-        "rubylibarchdir=`pwd`/tmp_rubylibarch",
-        "tcllibdir=`pwd`/tmp_tcllib"
+        "rubylibdir=\"`pwd`/tmp_rubylib\"",
+        "rubylibarchdir=\"`pwd`/tmp_rubylibarch\"",
+        "tcllibdir=\"`pwd`/tmp_tcllib\""
+    )
+    extradistargs = (
+        "EXTRADISTCHECK_CONFIGURE_FLAGS=" +
+        "'PYTHON_LIB=\"`pwd`/tmp_pylib\" PHP_EXTENSION_DIR=\"`pwd`/tmp_phplib\"'",
     )
     f.addStep(step.Compile, command=("make",) + extraargs)
     f.addStep(step.Test, name="check", command=("make", "check") + extraargs)
     f.addStep(step.Test, name="distcheck", command=("make", "distcheck") + extraargs, workdir='build/xapian-core')
     f.addStep(step.Test, name="distcheck", command=("make", "distcheck") + extraargs, workdir='build/xapian-applications/omega')
-    f.addStep(step.Test, name="distcheck", command=("make", "distcheck") + extraargs, workdir='build/xapian-bindings')
+    f.addStep(step.Test, name="distcheck", command=("make", "distcheck") + extraargs + extradistargs, workdir='build/xapian-bindings')
     return f
 
 def gen_svn_updated_win_factory(baseURL):
