e7702c9824e1c578c73913592e3d334d71cee180
Richard Boulton <richard@tartarus.org>
2007-09-29 09:12:38 +0000

    Attempt to fix paths for windows build.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@9376 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-maintainer-tools/buildbot/scripts/compile_with_vc7.bat
+++ xapian-maintainer-tools/buildbot/scripts/compile_with_vc7.bat
@@ -1,6 +1,13 @@
 rem Set up environment for Visual C++ 2005 Express Edition
 call "C:\Program Files\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
 
+set PYTHON_DIR=C:\Python25
+set PERL_DIR=C:\Perl\bin
+set ZLIB_DIR=D:\zlib123-dll
+
+set EXTRAPATHS=PYTHON_DIR=%PYTHON_DIR PERL_DIR=%PERL_DIR% ZLIB_DIR=%ZLIB_DIR% ZLIB_INCLUDE_DIR=%ZLIB_DIR%\include ZLIB_LIB_DIR=%ZLIB_DIR%\lib ZLIB_BIN_DIR=%ZLIB_DIR%
+set CHECKEXTRAPATHS=XAPIAN_TESTSUITE_OUTPUT=plain %EXTRAPATHS%
+
 rem Build "makedepend" and put it in place
 cd makedepend
 nmake -f makedepend.mak
@@ -10,17 +17,17 @@ cd ..
 nmake COPYMAKFILES
 
 rem Compile and test each module
-nmake PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
-nmake CHECK XAPIAN_TESTSUITE_OUTPUT=plain PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
+nmake %EXTRAPATHS%
+nmake CHECK %CHECKEXTRAPATHS%
 
 cd ....\xapian-bindings\python
-nmake PYTHON_DIR=C:\Python25 PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
-nmake CHECK XAPIAN_TESTSUITE_OUTPUT=plain PYTHON_DIR=C:\Python25 PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
+nmake %EXTRAPATHS%
+nmake CHECK %CHECKEXTRAPATHS%
 
 cd ....\xapian-bindings\perl
-nmake PERL_DIR=C:\Perl\bin PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
-nmake CHECK XAPIAN_TESTSUITE_OUTPUT=plain PERL_DIR=C:\Perl\bin  ZLIB_DIR=D:\zlib123-dll
+nmake %EXTRAPATHS%
+nmake CHECK %CHECKEXTRAPATHS%
 
 cd ..\..\xapian-omega
-nmake PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
-nmake CHECK PERL_DIR=C:\Perl\bin ZLIB_DIR=D:\zlib123-dll
+nmake %EXTRAPATHS%
+nmake CHECK %CHECKEXTRAPATHS%
