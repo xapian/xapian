728153d9cf3b65ed209c6eda8c4d3efc73e31e5b
Olly Betts <olly@survex.com>
2007-10-10 23:33:42 +0000

    matcher/branchpostlist.h: Fix comment typo.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@9441 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/branchpostlist.h
+++ xapian-core/matcher/branchpostlist.h
@@ -73,10 +73,10 @@ class BranchPostList : public PostList {
 
 	virtual ~BranchPostList();
 
-	/** get_wdf() for branch postlists returns the sub of the wdfs of the
+	/** get_wdf() for branch postlists returns the sum of the wdfs of the
 	 *  sub postlists.  The wdf isn't really meaningful in many situations,
 	 *  but if the lists are being combined as a synonym we want the sum of
-	 *  the wdfs: so we do that in general.
+	 *  the wdfs, so we do that in general.
 	 */
 	virtual Xapian::termcount get_wdf() const;
 };
