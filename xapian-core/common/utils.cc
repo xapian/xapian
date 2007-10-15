df79feb1b8370ecb79f3103c27c58aff49329a67
Olly Betts <olly@survex.com>
2007-10-15 04:10:23 +0000

    common/utils.cc,common/utils.h: Add within_DBL_EPSILON() function which returns true if its two double arguments differ by less than DBL_EPSILON (currently not used anywhere).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@9462 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/utils.cc
+++ xapian-core/common/utils.cc
@@ -25,6 +25,8 @@
 #include "utils.h"
 
 #include <stdio.h>
+#include <cfloat>
+#include <cmath>
 
 using namespace std;
 
@@ -413,6 +415,10 @@ const unsigned char up_tab[257] =
     "\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf"
     "\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef"
     "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
+
+bool within_DBL_EPSILON(double a, double b) {
+    return fabs(a - b) >= DBL_EPSILON;
+}
 
 }
 }
