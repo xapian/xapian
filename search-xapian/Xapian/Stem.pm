91cccc80de46f9568c3497893bbe491373dba955
Olly Betts <olly@survex.com>
2007-10-31 05:32:10 +0000

    Improve documentation of Stem class.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11319 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- search-xapian/Xapian/Stem.pm
+++ search-xapian/Xapian/Stem.pm
@@ -3,14 +3,16 @@ package Search::Xapian::Stem;
 # In a new thread, copy objects of this class to unblessed, undef values.
 sub CLONE_SKIP { 1 }
 
+1;
+
 =head1 NAME
 
 Search::Xapian::Stem - Snowball stemmer
 
 =head1 SYNOPSIS
 
-    my $stemmer=Search::Xapian::Stem->new('norwegian');
-    my $stemmed_foo=$stemmer->stem_word($foo);
+    my $stemmer = Search::Xapian::Stem->new('norwegian');
+    my $stemmed_foo = $stemmer->stem_word($foo);
 
 =head1 DESCRIPTION
 
@@ -24,14 +26,12 @@ Constructor. Takes the language to stem as as parameter.
 
 =item stem_word <word>
 
-Returns a stemmed version of the given word.
+Returns the stemmed form of the given word.
 
 =back
 
 =head1 SEE ALSO
 
-L<Search::Xapian>, L<Search::Xapian::QueryParser>
+L<Search::Xapian>, L<Search::Xapian::QueryParser>, L<Search::Xapian::TermGenerator>
 
 =cut
-
-1;
