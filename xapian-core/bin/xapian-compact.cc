4738985c20b95bf4a11b6ac8e4c8c5511dde3e90
Olly Betts <olly@survex.com>
2008-03-05 06:52:09 +0000

    Backport change from trunk: bin/xapian-compact.cc: Fix compaction for database which contains user metadata keys.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.0@10167 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/bin/xapian-compact.cc
+++ xapian-core/bin/xapian-compact.cc
@@ -73,6 +73,12 @@ is_metainfo_key(const string & key)
     return key.size() == 1 && key[0] == '\0';
 }
 
+static inline bool
+is_user_metainfo_key(const string & key)
+{
+    return key.size() > 1 && key[0] == '\0' && key[1] != '\xff';
+}
+
 class PostlistCursor : private FlintCursor {
     Xapian::docid offset;
 
@@ -102,6 +108,7 @@ class PostlistCursor : private FlintCursor {
 	tag = current_tag;
 	tf = cf = 0;
 	if (is_metainfo_key(key)) return true;
+	if (is_user_metainfo_key(key)) return true;
 	// Adjust key if this is *NOT* an initial chunk.
 	// key is: pack_string_preserving_sort(tname)
 	// plus optionally: pack_uint_preserving_sort(did)
@@ -193,6 +200,23 @@ merge_postlists(FlintTable * out, vector<Xapian::docid>::const_iterator offset,
     out->add(string("", 1), tag);
 
     string last_key;
+    while (true) {
+	PostlistCursor * cur = NULL;
+	if (!pq.empty()) {
+	    cur = pq.top();
+	    pq.pop();
+	}
+	if (!is_user_metainfo_key(cur->key)) {
+	    pq.push(cur);
+	    break;
+	}
+	if (cur->next()) {
+	    pq.push(cur);
+	} else {
+	    delete cur;
+	}
+    }
+
     Xapian::termcount tf = 0, cf = 0; // Initialise to avoid warnings.
     vector<pair<Xapian::docid, string> > tags;
     while (true) {
@@ -201,6 +225,7 @@ merge_postlists(FlintTable * out, vector<Xapian::docid>::const_iterator offset,
 	    cur = pq.top();
 	    pq.pop();
 	}
+	Assert(cur == NULL || !is_user_metainfo_key(cur->key));
 	if (cur == NULL || cur->key != last_key) {
 	    if (!tags.empty()) {
 		string first_tag = pack_uint(tf);
