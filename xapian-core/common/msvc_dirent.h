7095c1e254b47a371758e4d9ca7ad7fda929f3e9
Olly Betts <olly@survex.com>
2008-03-05 09:07:40 +0000

    common/msvc_dirent.cc,common/msvc_dirent.h: Move licence boilerplate up front where we have it in every other file.  Move header guards to the usual location.  Detail the fix we've made for not setting errno to ENOENT erroneously.  Push the setting of orig_errno down a bit.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@10172 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/msvc_dirent.h
+++ xapian-core/common/msvc_dirent.h
@@ -1,18 +1,28 @@
 /** @file msvc_dirent.h
  *  @brief Implementation of dirent functions using WIN32 API.
  */
-#ifndef XAPIAN_INCLUDED_MSVC_DIRENT_H
-#define XAPIAN_INCLUDED_MSVC_DIRENT_H
-
 /*
 
     Declaration of POSIX directory browsing functions and types for Win32.
 
     Author:  Kevlin Henney (kevlin@acm.org, kevlin@curbralan.com)
     History: Created March 1997. Updated June 2003.
-    Rights:  See end of file.
+
+    Copyright Kevlin Henney, 1997, 2003. All rights reserved.
+
+    Permission to use, copy, modify, and distribute this software and its
+    documentation for any purpose is hereby granted without fee, provided
+    that this copyright and permissions notice appear in all copies and
+    derivatives.
     
+    This software is supplied "as is" without express or implied warranty.
+
+    But that said, if there are any problems please get in touch.
+
 */
+    
+#ifndef XAPIAN_INCLUDED_MSVC_DIRENT_H
+#define XAPIAN_INCLUDED_MSVC_DIRENT_H
 
 #ifdef __cplusplus
 extern "C"
@@ -30,21 +40,6 @@ DIR           *opendir(const char *);
 int           closedir(DIR *);
 struct dirent *readdir(DIR *);
 void          rewinddir(DIR *);
-
-/*
-
-    Copyright Kevlin Henney, 1997, 2003. All rights reserved.
-
-    Permission to use, copy, modify, and distribute this software and its
-    documentation for any purpose is hereby granted without fee, provided
-    that this copyright and permissions notice appear in all copies and
-    derivatives.
-    
-    This software is supplied "as is" without express or implied warranty.
-
-    But that said, if there are any problems please get in touch.
-
-*/
 
 #ifdef __cplusplus
 }
