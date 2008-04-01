c068742cd80fecac2735b338d987892eb89ba547
Olly Betts <olly@survex.com>
2008-04-01 03:46:25 +0000

    backends/flint/flint_record.cc,backends/flint/flint_values.cc, common/,docs/quickstartexpand.cc.html,docs/quickstartindex.cc.html, docs/quickstartsearch.cc.html,matcher/: Update the FSF address for the small number of files which still have the old one.  Remove the "START_LICENCE" and "END_LICENCE" markers from the small number of files which still have them.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@10283 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/xorpostlist.h
+++ xapian-core/matcher/xorpostlist.h
@@ -17,9 +17,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
  * USA
- * -----END-LICENCE-----
  */
 
 #ifndef OM_HGUARD_XORPOSTLIST_H
