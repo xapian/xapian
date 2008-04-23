c050d5ab059fada7f0088cce327d9f3e7e045f8f
Olly Betts <olly@survex.com>
2008-04-23 00:59:57 +0000

    api/postingsource.cc: Another missing file.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@10370 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/api/postingsource.cc
@@ -0,0 +1,36 @@
+#include <config.h>
+
+#include "xapian/postingsource.h"
+
+namespace Xapian {
+
+PostingSource::~PostingSource() { }
+
+Xapian::weight
+PostingSource::get_maxweight() const
+{
+    return 0;
+}
+
+Xapian::weight
+PostingSource::get_weight() const
+{
+    return 0;
+}
+
+void
+PostingSource::skip_to(Xapian::docid did, Xapian::weight w)
+{
+    while (!at_end() && get_docid() < did) {
+	next(w);
+    }
+}
+
+void
+PostingSource::check(Xapian::docid did, Xapian::weight w, bool & valid)
+{
+    valid = true;
+    skip_to(did, w);
+}
+
+}
