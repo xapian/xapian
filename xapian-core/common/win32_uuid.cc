1b17c1d2c52400f96a1daa7bc1c619a729e05188
Olly Betts <olly@survex.com>
2008-11-14 16:54:42 +0000

    common/win32_uuid.cc: A few code clean-ups.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11629 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/win32_uuid.cc
+++ xapian-core/common/win32_uuid.cc
@@ -22,13 +22,23 @@
 
 #include "win32_uuid.h"
 
-#include <xapian/error.h>
+#include "xapian/error.h"
+
+#include <cstring>
+
+using namespace std;
+
+/// The size of a UUID in bytes.
+const size_t UUID_SIZE = 16;
+
+/// The size of a UUID string in bytes (not including trailing '\0').
+const size_t UUID_STRING_SIZE = 36;
 
 void
 uuid_generate(uuid_t uu)
 {
     UUID uuid;
-    if (UuidCreate(&uuid) != RPC_S_OK) {
+    if (rare(UuidCreate(&uuid) != RPC_S_OK)) {
 	// Throw a DatabaseCreateError, since we can't make a UUID.  The
 	// windows API documentation is a bit unclear about the situations in
 	// which this can happen, but if this behaviour causes a problem, an
@@ -36,7 +46,7 @@ uuid_generate(uuid_t uu)
 	// situation.
 	throw Xapian::DatabaseCreateError("Cannot create UUID");
     }
-    memcpy(uu, &uuid, 16);
+    memcpy(uu, &uuid, UUID_SIZE);
 }
 
 int
@@ -45,31 +55,34 @@ uuid_parse(const char * in, uuid_t uu)
     UUID uuid;
     if (UuidFromString((unsigned char*)in, &uuid) != RPC_S_OK)
 	return -1;
-    memcpy(uu, &uuid, 16);
+    memcpy(uu, &uuid, UUID_SIZE);
     return 0;
 }
 
-void uuid_unparse_lower(const uuid_t uu, char *  out)
+void uuid_unparse_lower(const uuid_t uu, char * out)
 {
     UUID uuid;
     char *uuidstr;
-    memcpy(&uuid, uu, 16);
-    if (UuidToString(&uuid, (unsigned char **)(&uuidstr)) != RPC_S_OK)
-	return;
-    int uuidlen = strlen(uuidstr);
-    strncpy( out, strlwr(uuidstr), uuidlen );
+    memcpy(&uuid, uu, UUID_SIZE);
+    if (rare(UuidToString(&uuid, (unsigned char **)(&uuidstr)) != RPC_S_OK)) {
+	// The only documented (or really conceivable) error code is
+	// RPC_S_OUT_OF_MEMORY.
+	throw std::bad_alloc();
+    }
+    memcpy(out, strlwr(uuidstr), UUID_STRING_SIZE);
+    out[UUID_STRING_SIZE] = '\0';
     RpcStringFree((unsigned char**)(&uuidstr));
 }
 
 void uuid_clear(uuid_t uu)
 {
-    memset(uu, 0x00, 16);
+    memset(uu, 0, UUID_SIZE);
 }
 
 int uuid_is_null(const uuid_t uu)
 {
     int i = 0;
-    while (i < 16) {
+    while (i < UUID_SIZE) {
 	if (uu[i++])
 	    return 0;
     }
