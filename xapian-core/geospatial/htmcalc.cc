1e3267fee63439bfc4d2d4d65f8ad86a20bab6f5
Richard Boulton <richard@tartarus.org>
2008-12-08 09:32:34 +0000

    geospatial/htmcalc.cc: Fuller HTM implementation.
    git-svn-id: file:///home/xapian-svn/xapian/branches/geospatial@11687 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/geospatial/htmcalc.cc
+++ xapian-core/geospatial/htmcalc.cc
@@ -23,9 +23,61 @@
 
 #include "xapian/geospatial.h"
 
+#include "bitstream.h"
 #include "stringutils.h"
 
+#include <cfloat>
+#include <cmath>
+#include <vector>
+
 using namespace Xapian;
+
+/** A vector, representing a lat long on the unit sphere.
+ *
+ *  X axis is from centre of earth to 0 latitude & longitude.
+ *  Y axis is from centre of earth to 0 latitude, 90 longitude.
+ */
+struct HTMVector {
+    double x, y, z;
+
+    HTMVector(double x_, double y_, double z_)
+	    : x(x_), y(y_), z(z_)
+    {
+    }
+
+    HTMVector(const LatLongCoord & coord)
+    {
+	double equator_d = cos(coord.latitude * M_PI / 180.0);
+	x = cos(coord.longitude * M_PI / 180.0) * equator_d;
+	y = sin(coord.longitude * M_PI / 180.0) * equator_d;
+	z = sin(coord.latitude * M_PI / 180.0);
+    }
+
+    void normalise()
+    {
+	double l = sqrt(x * x + y * y + z * z);
+	x /= l;
+	y /= l;
+	z /= l;
+    }
+
+    HTMVector operator +(const HTMVector & other) const
+    {
+	return HTMVector(x + other.x, y + other.y, z + other.z);
+    }
+
+    HTMVector operator ^(const HTMVector & other) const
+    {
+	return HTMVector(y * other.z - z * other.y,
+			 z * other.x - x * other.z,
+			 x * other.y - y * other.x);
+    }
+
+    double operator *(const HTMVector & other) const
+    {
+	return x * other.x + y * other.y + z * other.z;
+    }
+};
 
 HTMCalculator::HTMCalculator(int min_depth_,
 			     int max_depth_)
@@ -34,24 +86,101 @@ HTMCalculator::HTMCalculator(int min_depth_,
 {
 }
 
+// Return true iff v is in triangle a, b, c.
+// a, b, c must be in anticlockwise order, seen from above.
+static bool
+in_triangle(const HTMVector & a,
+	    const HTMVector & b,
+	    const HTMVector & c,
+	    const HTMVector & v)
+{
+    if ((a ^ b) * v < -DBL_EPSILON) return false;
+    if ((b ^ c) * v < -DBL_EPSILON) return false;
+    if ((c ^ a) * v < -DBL_EPSILON) return false;
+    return true;
+}
+
+static const double initial_vectors[6][3] = {
+    {0.0,  0.0,  1.0},
+    {1.0,  0.0,  0.0},
+    {0.0,  1.0,  0.0},
+    {-1.0,  0.0,  0.0},
+    {0.0, -1.0,  0.0},
+    {0.0,  0.0, -1.0}
+};
+
+// Triangles at at first level
+static const int initial_triangles[8][3] = {
+    {5, 2, 1}, // S0
+    {5, 3, 2}, // S1
+    {5, 4, 3}, // S2
+    {5, 1, 4}, // S3
+    {0, 4, 1}, // N0
+    {0, 3, 4}, // N1
+    {0, 2, 3}, // N2
+    {0, 1, 2}  // N3
+};
+
+// Triangles at a new level
+// 0,1,2 are the points of the current triangle
+// 3,4,5 are the new points, at midpoints opposite the corresponding vertex
+static const int new_triangles[4][3] = {
+    {0, 5, 4},
+    {1, 3, 5},
+    {2, 4, 3},
+    {3, 4, 5}
+};
+
 void
 HTMCalculator::get_nodes_for_location(HTMNodeSet & nodes,
 				      const LatLongCoord & location) const
 {
-    // FIXME - implement
-    (void) nodes;
-    (void) location;
+    std::vector<HTMVector> v;
+    v.reserve(6);
+    HTMVector loc(location);
+    int i;
+    for (i = 0; i != 6; ++i) {
+	v[i] = HTMVector(initial_vectors[i][0],
+			 initial_vectors[i][1],
+			 initial_vectors[i][2]);
+    }
+
+    for (i = 0; i != 8; ++i) {
+	if (in_triangle(v[initial_triangles[i][0]],
+			v[initial_triangles[i][1]],
+			v[initial_triangles[i][2]],
+			loc)) {
+	    break;
+	}
+    }
+
+    BitWriter node;
+    node.encode(i, 8);
+    int level = 0;
+    if (level >= min_depth) {
+	nodes.insert(std::string(1, char(level)) + node.slush());
+    }
+
+    // For each level, construct the new 3 points, find which of the 4 triangles we're in, and add to node.
+
+    for (int depth = 0; depth <= max_depth; ++depth) {
+	if (depth >= min_depth) {
+	    nodes.insert(std::string(1, char(level)) + node.slush());
+	}
+    }
 }
 
 void
 HTMCalculator::simplify(HTMNodeSet & nodes) const
 {
+    // FIXME - implement
     (void) nodes;
 }
 
 void
 HTMCalculator::reduce(HTMNodeSet & nodes, int max_nodes) const
 {
+    // FIXME - implement
     (void) nodes;
     (void) max_nodes;
 }
@@ -63,6 +192,7 @@ HTMCalculator::get_nodes_covering_circle(HTMNodeSet & nodes,
 					 const LatLongMetric * metric,
 					 int desired_nodes) const
 {
+    // FIXME - implement
     (void) nodes;
     (void) centre;
     (void) radius;
