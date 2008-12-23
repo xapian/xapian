e4ad6ed2df616de7c5bb46b7682935a71b641b47
Olly Betts <olly@survex.com>
2008-12-23 08:00:25 +0000

    Remove bogus directory I accidentally created
    git-svn-id: file:///home/xapian-svn/xapian/tags/1.0.10@11780 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- 1.0/xapian-applications/cvssearch/db/stoplist.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-#include <set>
-
-extern std::set<std::string> stopSet;
-extern void stoplist_init();
--- 1.0/xapian-core/acinclude.m4
+++
@@ -1,3 +0,0 @@
-dnl acinclude.m4
-m4_include(m4/rjb_find_stlport.m4)
-m4_include(m4/type_socklen_t.m4)
