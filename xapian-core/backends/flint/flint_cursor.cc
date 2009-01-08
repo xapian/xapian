67c282d0fa51c48d8a9ae36724f6abc7e887049e
Olly Betts <olly@survex.com>
2009-01-08 05:16:55 +0000

    backends/chert/chert_cursor.cc,backends/chert/chert_postlist.cc, backends/flint/flint_cursor.cc,backends/flint/flint_postlist.cc, backends/multi/multi_postlist.cc,common/omassert.h,configure.ac, matcher/multimatch.cc: Change preprocessor defines controlling assertions from XAPIAN_DEBUG->XAPIAN_ASSERTIONS and XAPIAN_DEBUG_PARANOID->XAPIAN_ASSERTIONS_PARANOID so their purpose is clearer.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11815 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/flint/flint_cursor.cc
+++ xapian-core/backends/flint/flint_cursor.cc
@@ -1,7 +1,7 @@
 /* flint_cursor.cc: Btree cursor implementation
  *
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2006,2007,2008 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -31,7 +31,7 @@
 #include "omassert.h"
 #include "omdebug.h"
 
-#ifdef XAPIAN_DEBUG_VERBOSE
+#ifdef XAPIAN_ASSERTIONS_VERBOSE
 static string
 hex_display_encode(const string & input)
 {
--- xapian-core/matcher/multimatch.cc
+++ xapian-core/matcher/multimatch.cc
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001,2002 Ananova Ltd
- * Copyright 2002,2003,2004,2005,2006,2007,2008 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009 Olly Betts
  * Copyright 2003 Orange PCS Ltd
  * Copyright 2003 Sam Liddicott
  * Copyright 2007,2008 Lemur Consulting Ltd
@@ -750,7 +750,7 @@ MultiMatch::get_mset(Xapian::doccount first, Xapian::doccount maxitems,
 			Assert(items.back().wt < min_weight);
 			items.pop_back();
 		    }
-#ifdef XAPIAN_DEBUG_PARANOID
+#ifdef XAPIAN_ASSERTIONS_PARANOID
 		    vector<Xapian::Internal::MSetItem>::const_iterator i;
 		    for (i = items.begin(); i != items.end(); ++i) {
 			Assert(i->wt >= min_weight);
@@ -843,7 +843,7 @@ MultiMatch::get_mset(Xapian::doccount first, Xapian::doccount maxitems,
 		Assert(items.back().wt < min_wt);
 		items.pop_back();
 	    }
-#ifdef XAPIAN_DEBUG_PARANOID
+#ifdef XAPIAN_ASSERTIONS_PARANOID
 	    vector<Xapian::Internal::MSetItem>::const_iterator j;
 	    for (j = items.begin(); j != items.end(); ++j) {
 		Assert(j->wt >= min_wt);
