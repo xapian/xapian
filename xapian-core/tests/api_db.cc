86a2acd4a6033abf6ec4f119f0b5a5798b13b1ab
Olly Betts <olly@survex.com>
2009-01-20 06:22:39 +0000

    tests/api_db.cc: Prefer value.empty() to value == "".
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11825 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/api_db.cc
+++ xapian-core/tests/api_db.cc
@@ -719,7 +719,7 @@ DEFINE_TESTCASE(collapsekey1, backend) {
 	Xapian::MSetIterator i = mymset.begin();
 	for ( ; i != mymset.end(); ++i) {
 	    string value = i.get_document().get_value(value_no);
-	    TEST(values[value] == 0 || value == "");
+	    TEST(values[value] == 0 || value.empty());
 	    values[value] = *i;
 	}
     }
