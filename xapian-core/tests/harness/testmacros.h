c8fc8f57fd448eea589683b9b1d8d92e9eb814a0
Olly Betts <olly@survex.com>
2009-02-26 07:50:50 +0000

    tests/harness/Makefile.mk,tests/harness/testmacros.h, tests/harness/testsuite.h: Add new "TEST_REL" macro which can test for condition using any relational operator and report a failure clearly yet concisely.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11947 6b2f1b92-a4f3-0310-943f-de3cbe41748b

---
+++ xapian-core/tests/harness/testmacros.h
@@ -0,0 +1,30 @@
+/** @file testmacros.h
+ * @brief Macros for testing conditions hold.
+ */
+/* Copyright (C) 2009 Olly Betts
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef XAPIAN_INCLUDED_TESTMACROS_H
+#define XAPIAN_INCLUDED_TESTMACROS_H
+
+/// Test a relation holds,e.g. TEST_REL(a,>,b);
+#define TEST_REL(A,REL,B) \
+    TEST_AND_EXPLAIN((A) REL (B), \
+	"Evaluates to: " << (A) << " "#REL" " << (B))
+
+#endif // XAPIAN_INCLUDED_TESTMACROS_H
--- xapian-core/tests/harness/testsuite.h
+++ xapian-core/tests/harness/testsuite.h
@@ -1,7 +1,7 @@
 /* testsuite.h: a generic test suite engine
  *
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2005,2006,2007,2008 Olly Betts
+ * Copyright 2002,2003,2005,2006,2007,2008,2009 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -294,5 +294,8 @@ extern bool TEST_EQUAL_DOUBLE_(double a, double b);
 	" were " << (a) << " and " << (b))
 
 #define DEFINE_TESTCASE(S,COND) bool test_##S()
+
+// Newer test macros:
+#include "testmacros.h"
 
 #endif // OM_HGUARD_TESTSUITE_H
