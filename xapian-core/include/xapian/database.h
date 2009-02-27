9bf6f21c13c605cc569194610083584def6432a2
Olly Betts <olly@survex.com>
2009-02-27 02:01:12 +0000

    include/xapian/database.h: WritableDatabase::flush() can't throw DatabaseLockError.  WritableDatabase's ctor can throw at least DatabaseCorruptError or DatabaseLockError.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@11960 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian/database.h
+++ xapian-core/include/xapian/database.h
@@ -3,7 +3,7 @@
  */
 /* Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004,2005,2006,2007,2008 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009 Olly Betts
  * Copyright 2006,2008 Lemur Consulting Ltd
  *
  * This program is free software; you can redistribute it and/or
@@ -456,6 +456,12 @@ class XAPIAN_VISIBILITY_DEFAULT WritableDatabase : public Database {
 	 *  - Xapian::DB_CREATE_OR_OVERWRITE overwrite existing db; create if
 	 *    none exists
 	 *  - Xapian::DB_OPEN open for read/write; fail if no db exists
+	 *
+	 *  @exception Xapian::DatabaseCorruptError will be thrown if the
+	 *             database is in a corrupt state.
+	 *
+	 *  @exception Xapian::DatabaseLockError will be thrown if a lock
+	 *	       couldn't be acquired on the database.
 	 */
 	WritableDatabase(const std::string &path, int action);
 
@@ -513,9 +519,6 @@ class XAPIAN_VISIBILITY_DEFAULT WritableDatabase : public Database {
 	 *
 	 *  @exception Xapian::DatabaseCorruptError will be thrown if the
 	 *             database is in a corrupt state.
-	 *
-	 *  @exception Xapian::DatabaseLockError will be thrown if a lock
-	 *	       couldn't be acquired on the database.
 	 */
 	void flush();
 
