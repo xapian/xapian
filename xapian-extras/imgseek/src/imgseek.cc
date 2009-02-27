5b003d2ce5b9b4b60987845f7a9ca10f4e229fbf
Richard Boulton <richard@tartarus.org>
2009-02-27 10:02:32 +0000

    imgseek/src/imgseek.cc: Fix namespace for RangeAccelerator.
    git-svn-id: file:///home/xapian-svn/xapian/branches/imgseek@11966 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-extras/imgseek/src/imgseek.cc
+++ xapian-extras/imgseek/src/imgseek.cc
@@ -408,19 +408,19 @@ ImgTerms::ImgTerms(const std::string& prefix_, Xapian::valueno v1, Xapian::value
   colour_vals.push_back(v2);
   colour_vals.push_back(v3);
   //Y
-  colour_average_accels.push_back(RangeAccelerator(prefix+"A0",
-                                                   colour_vals[0],
-                                                   0.0, 1.0, 1.0/255.0));
+  colour_average_accels.push_back(Xapian::RangeAccelerator(prefix+"A0",
+							   colour_vals[0],
+							   0.0, 1.0, 1.0/255.0));
   //I
-  colour_average_accels.push_back(RangeAccelerator(prefix+"A1",
-                                                   colour_vals[1],
-                                                   -0.523, 0.523, 
-                                                   (0.523*2.0)/255.0));
+  colour_average_accels.push_back(Xapian::RangeAccelerator(prefix+"A1",
+							   colour_vals[1],
+							   -0.523, 0.523, 
+							   (0.523*2.0)/255.0));
   //Q
-  colour_average_accels.push_back(RangeAccelerator(prefix+"A2",
-                                                   colour_vals[2],
-                                                   -0.596, 0.596, 
-                                                   (0.596*2.0)/255.0));
+  colour_average_accels.push_back(Xapian::RangeAccelerator(prefix+"A2",
+							   colour_vals[2],
+							   -0.596, 0.596, 
+							   (0.596*2.0)/255.0));
 }
 
 void 
