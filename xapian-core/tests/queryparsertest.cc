d095760870380fe8837eb4ec0f3886fb3b7bdc22
Olly Betts <olly@survex.com>
2009-03-05 09:54:52 +0000

    Backport change from trunk: include/xapian/queryparser.h: Add QueryParser::FLAG_DEFAULT to make it easier to add flags to those set by default. tests/queryparsertest.cc: Use FLAG_DEFAULT in a few places to serve as a feature test.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.0@12092 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/queryparsertest.cc
+++ xapian-core/tests/queryparsertest.cc
@@ -1586,11 +1586,7 @@ static bool test_qp_synonym1()
 	expect += p->expect;
 	expect += ')';
 	Xapian::Query q;
-	q = qp.parse_query(p->query,
-			   Xapian::QueryParser::FLAG_AUTO_SYNONYMS |
-			   Xapian::QueryParser::FLAG_BOOLEAN |
-			   Xapian::QueryParser::FLAG_LOVEHATE |
-			   Xapian::QueryParser::FLAG_PHRASE );
+	q = qp.parse_query(p->query, qp.FLAG_AUTO_SYNONYMS|qp.FLAG_DEFAULT);
 	tout << "Query: " << p->query << endl;
 	TEST_STRINGS_EQUAL(q.get_description(), expect);
     }
@@ -1633,9 +1629,7 @@ static bool test_qp_synonym2()
 	Xapian::Query q;
 	q = qp.parse_query(p->query,
 			   Xapian::QueryParser::FLAG_AUTO_MULTIWORD_SYNONYMS |
-			   Xapian::QueryParser::FLAG_BOOLEAN |
-			   Xapian::QueryParser::FLAG_LOVEHATE |
-			   Xapian::QueryParser::FLAG_PHRASE );
+			   Xapian::QueryParser::FLAG_DEFAULT);
 	tout << "Query: " << p->query << endl;
 	TEST_STRINGS_EQUAL(q.get_description(), expect);
     }
@@ -1786,10 +1780,7 @@ static bool test_qp_stem_scale1()
     syn.resize(syn.size() - 1);
 
     double time1, time2;
-    unsigned defflags =
-	    Xapian::QueryParser::FLAG_PHRASE |
-	    Xapian::QueryParser::FLAG_BOOLEAN |
-	    Xapian::QueryParser::FLAG_LOVEHATE;
+    unsigned defflags = Xapian::QueryParser::FLAG_DEFAULT;
     unsigned synflags = defflags |
 	    Xapian::QueryParser::FLAG_SYNONYM |
 	    Xapian::QueryParser::FLAG_AUTO_MULTIWORD_SYNONYMS;
