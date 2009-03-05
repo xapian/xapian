3f45b2fd976cedbc43ea2270fc183680232376cb
Richard Boulton <richard@tartarus.org>
2009-03-05 10:53:42 +0000

    imgseek/Makefile.mk: Remove reference to missing dir_contents file.
    git-svn-id: file:///home/xapian-svn/xapian/branches/imgseek@12105 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-extras/imgseek/Makefile.mk
+++ xapian-extras/imgseek/Makefile.mk
@@ -8,9 +8,6 @@ noinst_HEADERS +=\
 	imgseek/src/haar.h\
 	imgseek/src/jpegloader.h
 
-EXTRA_DIST +=\
-	imgseek/dir_contents
-
 libxapian_imgseek_la_SOURCES +=\
 	imgseek/src/haar.cc\
 	imgseek/src/image_terms.cc\
