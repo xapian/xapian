642a587f46de5261db5f471d08b98cc01d785318
Olly Betts <olly@survex.com>
2009-03-08 05:37:41 +0000

    matcher/extraweightpostlist.h: Add forward declaration of Xapian::Weight since we use "Xapian::Weight *".
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12133 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/extraweightpostlist.h
+++ xapian-core/matcher/extraweightpostlist.h
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001 Ananova Ltd
- * Copyright 2003,2004,2007 Olly Betts
+ * Copyright 2003,2004,2007,2009 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -24,6 +24,10 @@
 #define OM_HGUARD_EXTRAWEIGHTPOSTLIST_H
 
 #include "multimatch.h"
+
+namespace Xapian {
+    class Weight;
+}
 
 /// A postlist which adds on an extra weight contribution
 class ExtraWeightPostList : public PostList {
