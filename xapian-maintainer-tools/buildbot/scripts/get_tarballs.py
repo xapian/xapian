ef38033c2d14cf92a57306a856a6b07cfc36c53d
Richard Boulton <richard@tartarus.org>
2009-03-24 18:20:04 +0000

    Update another URL in the script.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12249 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-maintainer-tools/buildbot/scripts/get_tarballs.py
+++ xapian-maintainer-tools/buildbot/scripts/get_tarballs.py
@@ -100,7 +100,7 @@ os.rename(os.path.join(builddir, 'win32msvc'),
           os.path.join(builddir, 'xapian-core', 'win32'))
 
 # Get the scripts for building on our windows server, too:
-fd = urllib2.urlopen('http://svn.xapian.org/trunk/xapian-maintainer-tools/buildbot/scripts/compile_with_vc7.bat?revision=HEAD')
+fd = urllib2.urlopen('http://svn.xapian.org/*checkout*/trunk/xapian-maintainer-tools/buildbot/scripts/compile_with_vc7.bat')
 data = fd.read()
 fd.close()
 fd = open(os.path.join(builddir, 'xapian-core', 'win32', 'compile_with_vc7.bat'), 'wb')
