fc7965ad18dab85c7bfbfb639dcbabb08bfb52a4
Olly Betts <olly@survex.com>
2009-05-05 12:23:49 +0000

    - Add new translated version of the simple examples from the Python   bindings (no simplematchdecider yet). - Add more fully featured examples: full-indexer.pl and full-searcher.pl.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.0/search-xapian@12608 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- examples/simplesearch.pl
+++ examples/simplesearch.pl
@@ -1,48 +1,76 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+#
+# Simple command-line search script.
+#
+# Copyright (C) 2003 James Aylett
+# Copyright (C) 2004,2007,2009 Olly Betts
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
 
 use strict;
+use Search::Xapian (':all');
 
-use ExtUtils::testlib;
-use Getopt::Std;
-use Search::Xapian qw(:ops);
-
-use vars qw( %opts );
-getopts('d:t:nh', \%opts);
-
-if( exists $opts{h} ) {
-  print <<EOF;
-Synopsis: Searches a Xapian database for a particular term.
-Usage: $0 -d testdb -t help
-Options:
-        -d : database directory
-        -t : search terms
-        -h : displays this help screen
-        -q : quiet; does not display document data
-EOF
-  exit 0;
+# We require at least two command line arguments.
+if (scalar @ARGV < 2) {
+    print STDERR "Usage: $0 PATH_TO_DATABASE QUERY\n";
+    exit(1);
 }
 
-if( !$opts{d} or !$opts{t} ) {
-  print "Usage: $0 -d [database dir] -t [search terms]\n";
-  print "Try $0 -h for further information\n";
-  exit 0;
+eval {
+    # Open the database for searching.
+    my $database = Search::Xapian::Database->new(shift @ARGV);
+
+    # Start an enquire session.
+    my $enquire = Search::Xapian::Enquire->new($database);
+
+    # Combine the rest of the command line arguments with spaces between
+    # them, so that simple queries don't have to be quoted at the shell
+    # level.
+    my $query_string = join ' ', @ARGV;
+
+    # Parse the query string to produce a Xapian::Query object.
+    my $qp = Search::Xapian::QueryParser->new();
+    my $stemmer = Search::Xapian::Stem->new("english");
+    $qp->set_stemmer($stemmer);
+    $qp->set_database($database);
+    $qp->set_stemming_strategy(STEM_SOME);
+    my $query = $qp->parse_query($query_string);
+    print "Parsed query is: $query\n";
+
+    # Find the top 10 results for the query.
+    $enquire->set_query($query);
+    my $mset = $enquire->get_mset(0, 10);
+
+    # Display the results.
+    printf "%i results found.\n", $mset->get_matches_estimated();
+    printf "Results 1-%i:\n", $mset->size();
+
+    # In 1.0.13.0 and newer you'll be able to replace this with:
+    # foreach my $m ($mset->matches()) {
+    foreach my $m (tie_mset($mset)) {
+        printf "%i: %i%% docid=%i [%s]\n", $m->get_rank() + 1, $m->get_percent(), $m->get_docid(), $m->get_document()->get_data();
+    }
+};
+if ($@) {
+    print STDERR "Exception: $@\n";
+    exit(1);
 }
 
-my $db = Search::Xapian::Database->new( $opts{d} );
-my @terms = split ',', $opts{t};
-my $enq = $db->enquire( OP_OR, @terms );
-
-printf "Parsing query '%s'\n", $enq->get_query()->get_description();
-
-my @matches = $enq->matches(0, 10);
-
-print scalar(@matches) . " results found\n";
-
-foreach my $match ( @matches ) {
-  printf "ID %d %d%%", $match->get_docid(), $match->get_percent();
-  if( !defined( $opts{n} ) ) {
-    my $doc = $match->get_document();
-    printf " [ %s ]", $doc->get_data();
-  }
-  print "\n";
+sub tie_mset {
+    my @a;
+    tie( @a, 'Search::Xapian::MSet::Tied', shift );
+    return @a;
 }
