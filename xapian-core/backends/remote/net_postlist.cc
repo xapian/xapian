31bb19ca64c8b3ad15e22a684a3b7e803b87d9e5
Richard Boulton <richard@tartarus.org>
2009-05-05 07:19:52 +0000

    backends/remote/net_postlist.cc: Remove a copyright line - the changes made here were reverted (and possibly never committed).
    git-svn-id: file:///home/xapian-svn/xapian/branches/opsynonym@12594 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/remote/net_postlist.cc
+++ xapian-core/backends/remote/net_postlist.cc
@@ -3,7 +3,6 @@
  */
 /* Copyright (C) 2007 Lemur Consulting Ltd
  * Copyright (C) 2007,2008,2009 Olly Betts
- * Copyright (C) 2009 Lemur Consulting Ltd
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
