f4aa8b1a67ff31fff5c382219b0731d7089f1f2e
Olly Betts <olly@survex.com>
2009-05-14 13:23:58 +0000

    Backport change from trunk: net/serialise.cc: Build up string using +=.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.0@12729 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/net/serialise.cc
+++ xapian-core/net/serialise.cc
@@ -137,8 +137,11 @@ unserialise_error(const string &serialised_error, const string &prefix,
 
 #include <xapian/errordispatch.h>
 
-    msg = "Unknown remote exception type " + type + ": " + msg;
-    throw Xapian::InternalError(msg, context);
+    string newmsg = "Unknown remote exception type ";
+    newmsg += type;
+    newmsg += ": ";
+    newmsg += msg;
+    throw Xapian::InternalError(newmsg, context);
 }
 
 string serialise_stats(const Stats &stats)
