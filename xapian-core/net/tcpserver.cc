b737e730c34700865fb14ec26b89a63981688c18
Olly Betts <olly@survex.com>
2009-05-20 14:20:57 +0000

    net/tcpclient.cc,net/tcpserver.cc: Fix xapian-tcpsrv --interface option to work on MacOS X (ticket#373).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12769 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/net/tcpserver.cc
+++ xapian-core/net/tcpserver.cc
@@ -167,6 +167,7 @@ TcpServer::get_listening_socket(const std::string & host, int port,
     }
 
     struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     if (host.empty()) {
@@ -191,7 +192,7 @@ TcpServer::get_listening_socket(const std::string & host, int port,
 		);
 	}
 
-	memcpy(&addr.sin_addr, hostent->h_addr, sizeof(addr.sin_addr));
+	memcpy(&addr.sin_addr, hostent->h_addr, hostent->h_length);
     }
 
     retval = bind(socketfd,
