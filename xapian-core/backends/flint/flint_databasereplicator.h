7ad7ad31fa5b0a78642a624bee00a5ba1e84334e
Olly Betts <olly@survex.com>
2009-05-21 08:10:33 +0000

    backends/chert/chert_databasereplicator.h, backends/flint/flint_databasereplicator.h: Correct parent class name in doxygen comments.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12783 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/flint/flint_databasereplicator.h
+++ xapian-core/backends/flint/flint_databasereplicator.h
@@ -48,7 +48,7 @@ class FlintDatabaseReplicator : public Xapian::DatabaseReplicator {
     public:
 	FlintDatabaseReplicator(const std::string & db_dir_);
 
-	/** Virtual methods of Database::Internal. */
+	/** Virtual methods of DatabaseReplicator. */
 	//@{
 	bool check_revision_at_least(const std::string & rev,
 				     const std::string & target) const;
