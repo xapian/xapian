527eae72ad9384f4778dcf242fd3953e604625e8
Olly Betts <olly@survex.com>
2009-05-25 12:58:14 +0000

    cgiparam.cc,commonhelp.cc: Include the corresponding header.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12836 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/cgiparam.cc
+++ xapian-applications/omega/cgiparam.cc
@@ -3,7 +3,7 @@
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2001 James Aylett
  * Copyright 2001 Ananova Ltd
- * Copyright 2002,2003 Olly Betts
+ * Copyright 2002,2003,2009 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -22,6 +22,8 @@
  */
 
 #include <config.h>
+
+#include "cgiparam.h"
 
 #include <stdio.h>
 #include <limits.h>
