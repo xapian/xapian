3f47fab02357f744f3605f58c3d5da2c2cb8c1ea
Olly Betts <olly@survex.com>
2009-07-04 16:37:33 +0000

    queryparser/lemon.c: Merge upstream "Check-in Number: 6755" - no functional changes in generated code for our parser.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@12987 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/queryparser/lemon.c
+++ xapian-core/queryparser/lemon.c
@@ -9,8 +9,8 @@
 ** Modified to add "-o" and "-h" command line options.  Olly Betts 2005-02-14
 ** Modified to fix a number of compiler warnings.  Olly Betts 2007-02-20
 **
-** Synced with upstream CVS rev 1.65:
-** http://www.sqlite.org/cvstrac/fileview?f=sqlite/tool/lemon.c&v=1.65
+** Synced with upstream CVS rev 1.67:
+** http://www.sqlite.org/cvstrac/fileview?f=sqlite/tool/lemon.c&v=1.67
 */
 #include <stdio.h>
 #include <stdarg.h>
@@ -3651,7 +3651,7 @@ int mhflag;     /* Output in makeheaders format if true */
 
   /* Generate the defines */
   fprintf(out,"#define YYCODETYPE %s\n",
-    minimum_size_type(0, lemp->nsymbol+5)); lineno++;
+    minimum_size_type(0, lemp->nsymbol+1)); lineno++;
   fprintf(out,"#define YYNOCODE %d\n",lemp->nsymbol+1);  lineno++;
   fprintf(out,"#define YYACTIONTYPE %s\n",
     minimum_size_type(0, lemp->nstate+lemp->nrule+5));  lineno++;
@@ -3916,8 +3916,8 @@ int mhflag;     /* Output in makeheaders format if true */
         fprintf(out, "      /* TERMINAL Destructor */\n"); lineno++;
         once = 0;
       }
-      fprintf(out,"    case %d: /* %s */\n",
-              sp->index, sp->name); lineno++;
+      fprintf(out,"    case %d: /* %s */ yytestcase(yymajor==%d);\n",
+              sp->index, sp->name, sp->index); lineno++;
     }
     for(i=0; i<lemp->nsymbol && lemp->symbols[i]->type!=TERMINAL; i++);
     if( i<lemp->nsymbol ){
@@ -3936,8 +3936,8 @@ int mhflag;     /* Output in makeheaders format if true */
         fprintf(out, "      /* Default NON-TERMINAL Destructor */\n"); lineno++;
         once = 0;
       }
-      fprintf(out,"    case %d: /* %s */\n",
-              sp->index, sp->name); lineno++;
+      fprintf(out,"    case %d: /* %s */ yytestcase(yymajor==%d);\n",
+              sp->index, sp->name, sp->index); lineno++;
       dflt_sp = sp;
     }
     if( dflt_sp!=0 ){
@@ -3948,8 +3948,8 @@ int mhflag;     /* Output in makeheaders format if true */
   for(i=0; i<lemp->nsymbol; i++){
     struct symbol *sp = lemp->symbols[i];
     if( sp==0 || sp->type==TERMINAL || sp->destructor==0 ) continue;
-    fprintf(out,"    case %d: /* %s */\n",
-            sp->index, sp->name); lineno++;
+    fprintf(out,"    case %d: /* %s */ yytestcase(yymajor==%d);\n",
+            sp->index, sp->name, sp->index); lineno++;
 
     /* Combine duplicate destructors into a single case */
     for(j=i+1; j<lemp->nsymbol; j++){
@@ -3957,8 +3957,8 @@ int mhflag;     /* Output in makeheaders format if true */
       if( sp2 && sp2->type!=TERMINAL && sp2->destructor
           && sp2->dtnum==sp->dtnum
           && strcmp(sp->destructor,sp2->destructor)==0 ){
-         fprintf(out,"    case %d: /* %s */\n",
-                 sp2->index, sp2->name); lineno++;
+         fprintf(out,"    case %d: /* %s */ yytestcase(yymajor==%d);\n",
+                 sp2->index, sp2->name, sp2->index); lineno++;
          sp2->destructor = 0;
       }
     }
@@ -3996,7 +3996,7 @@ int mhflag;     /* Output in makeheaders format if true */
       if( rp2->code==rp->code ){
         fprintf(out,"      case %d: /* ", rp2->index);
         writeRuleText(out, rp2);
-        fprintf(out," */\n"); lineno++;
+        fprintf(out," */ yytestcase(yyruleno==%d);\n", rp2->index); lineno++;
         rp2->code = 0;
       }
     }
