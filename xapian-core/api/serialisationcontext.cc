94dada69677397a8eb1e4ba8c1d5831bc08c2c57
Richard Boulton <richard@tartarus.org>
2009-07-14 14:55:45 +0000

    api/serialisationcontext.cc: Add MultipleMatchSpy and TermCountMatchSpy to serialisation context.
    git-svn-id: file:///home/xapian-svn/xapian/branches/matchspy@13049 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/api/serialisationcontext.cc
+++ xapian-core/api/serialisationcontext.cc
@@ -143,7 +143,11 @@ SerialisationContext::Internal::add_defaults()
     postingsources[source->name()] = source;
 
     Xapian::MatchSpy * spy;
+    spy = new Xapian::MultipleMatchSpy();
+    matchspies[spy->name()] = spy;
     spy = new Xapian::ValueCountMatchSpy();
+    matchspies[spy->name()] = spy;
+    spy = new Xapian::TermCountMatchSpy();
     matchspies[spy->name()] = spy;
 }
 
