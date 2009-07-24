41c7d61ab9bb6e87bf2693e344905ff5346ece77
Richard Boulton <richard@tartarus.org>
2009-07-24 08:32:25 +0000

    Update to include timings from latest logging patch.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@13155 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-maintainer-tools/perftest/draw_positions.py
+++ xapian-maintainer-tools/perftest/draw_positions.py
@@ -10,10 +10,10 @@ class FileAccesses(object):
         self.max_offset = 0
         self.data = []
 
-    def append(self, start_time, offset, elapsed, optype):
-        self.data.append((start_time, offset, elapsed, optype))
-        if self.max_offset < offset:
-            self.max_offset = offset
+    def append(self, start_time, elapsed, offset, count, optype):
+        self.data.append((start_time, elapsed, offset, count, optype))
+        if self.max_offset < offset + count:
+            self.max_offset = offset + count
 
     def __iter__(self):
         return iter(self.data)
@@ -21,15 +21,23 @@ class FileAccesses(object):
 class AllAccesses(object):
     def __init__(self):
         self.data = {}
-        self.total_time = 0
+        self.total_io_time = 0
+        self.start_time = None;
+        self.total_time = 0;
 
-    def append(self, filename, offset, elapsed, optype):
+    def append(self, filename, start_time, elapsed, offset, count, optype):
+        if self.start_time is None:
+            self.start_time = start_time
+        start_time -= self.start_time
+
         fileaccesses = self.data.get(filename, None)
         if fileaccesses is None:
             fileaccesses = FileAccesses()
             self.data[filename] = fileaccesses
-        fileaccesses.append(self.total_time, offset, elapsed, optype)
-        self.total_time += elapsed
+
+        fileaccesses.append(start_time, elapsed, offset, count, optype)
+        self.total_time = max(self.total_time, start_time + elapsed)
+        self.total_io_time += elapsed
 
     def files_count(self):
         return len(self.data)
@@ -53,10 +61,7 @@ class DrawPanel(wx.Panel):
         # bind the panel to the paint event
         wx.EVT_PAINT(self, self.onPaint)
 
-        self.data = AllAccesses()
-        for filename, offset, elapsed, optype in data:
-            #elapsed = 0.0005
-            self.data.append(filename, offset, elapsed, optype)
+        self.data = data
 
     def onPaint(self, event=None):
         # this is the wx drawing surface/canvas
@@ -75,7 +80,7 @@ class DrawPanel(wx.Panel):
         yscale = float(hgt - top - bot - titlehgt * len(self.data)) / self.data.sum_max_offset()
 
         pointwid = 1
-        print "Drawing"
+        print "Drawing: green=read<1ms, red=read>1ms, blue=write, lightblue=sync"
         dc.SetPen(wx.Pen("grey", 1))
         dc.SetBrush(wx.Brush("#eeeeee"))
         dc.DrawRectangle(0, 0, wid, hgt)
@@ -94,7 +99,7 @@ class DrawPanel(wx.Panel):
 
             maxoffset_sum += data.max_offset
 
-            for starttime, offset, elapsed, optype in data:
+            for starttime, elapsed, offset, count, optype in data:
                 xpos = left + xscale * starttime
                 pointwid = xscale * elapsed
                 if pointwid < 1: pointwid = 1
@@ -106,7 +111,7 @@ class DrawPanel(wx.Panel):
                     dc.DrawLine(xpos, hgt - y, xpos + pointwid, hgt - y)
 
                 elif optype == 'r':
-                    if elapsed < 0.001:
+                    if elapsed < 1000:
                         dc.SetPen(wx.Pen("dark green", 1))
                         counts[0] += 1
                     else:
@@ -122,8 +127,6 @@ class DrawPanel(wx.Panel):
         print "Drawn: [green, red, blue] =", counts
         if counts[1] > 0:
             print "blue/red =", float(counts[0]) / counts[1]
-        print "Total time: %f" % (self.data.total_time,)
-        print "Avg time: %f" % (self.data.total_time / (counts[0] + counts[1] + counts[2]))
 
 def show_data(app, data, title):
     frame = wx.Frame(None, -1, title, size=(1500, 1000))
@@ -134,30 +137,35 @@ def read_data(filename):
     fd = open(filename)
     fds = {}
     try:
-        data = []
+        data = AllAccesses()
         just_seeked = None
         for line in fd:
             line = line.strip()
-            if line[0] == 'o':
-                fdnum, filename = line[1:].split(',', 1)
-                fds[int(fdnum)] = filename
+            optype = line[0]
+            fdnum, start, elapsed, line = line[1:].split(',', 3)
+            fdnum = int(fdnum)
+            start = int(start)
+            elapsed = int(elapsed)
+
+            if optype == 'o':
+                filename = line
+                fds[fdnum] = filename
                 continue
 
-            if line[0] == 'r':
-                fdnum, offset, len = map(int, line[1:].split(',', 2))
-                filename = fds.get(fdnum, str(fdnum))
-                data.append((filename, offset, len, line[0]))
+            filename = fds.get(fdnum, str(fdnum))
+
+            if optype == 'r':
+                offset, count = map(int, line.split(',', 1))
+                data.append(filename, start, elapsed, offset, count, optype)
                 continue
 
-            if line[0] == 'w':
-                fdnum, offset, len = map(int, line[1:].split(',', 2))
-                filename = fds.get(fdnum, str(fdnum))
-                data.append((filename, offset, len, line[0]))
+            if optype == 'w':
+                offset, count = map(int, line.split(',', 1))
+                data.append(filename, start, elapsed, offset, count, optype)
                 continue
 
-            if line[0] == 's':
-                fdnum = int(line[1:-1])
-                data.append((filename, 0, 0, line[0]))
+            if optype == 's':
+                data.append(filename, start, elapsed, 0, 0, optype)
                 continue
 
         return data
@@ -169,5 +177,7 @@ if __name__ == '__main__':
     # data file should be produced by strace -T
     filename = sys.argv[1]
     data = read_data(filename)
+    print "Total IO time = %fs" % (float(data.total_io_time) / 1000000.0)
+    print "Total time    = %fs" % (float(data.total_time) / 1000000.0)
     show_data(app, data, "%d io calls, from %s" % (len(data), filename))
     app.MainLoop()
