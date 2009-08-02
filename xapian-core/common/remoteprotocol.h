67a22f3743a7dae493e503ca5e2c6e66bf3b199d
Olly Betts <olly@survex.com>
2009-08-02 15:48:06 +0000

    common/remoteprotocol.h: Note the versions in which recent protocol versions were introduced.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@13253 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/common/remoteprotocol.h
+++ xapian-core/common/remoteprotocol.h
@@ -39,9 +39,9 @@
 // 30.4: New query operator OP_SCALE_WEIGHT.
 // 30.5: New MSG_GETMSET which expects MSet's percent_factor to be returned.
 // 30.6: Support for OP_VALUE_GE and OP_VALUE_LE in query serialisation
-// 31: Clean up for Xapian 1.1.0
-// 32: Serialise termfreq and reltermfreqs together in serialise_stats.
-// 33: Support for passing matchspies over the remote connection.
+// 31: 1.1.0 Clean up for Xapian 1.1.0
+// 32: 1.1.1 Serialise termfreq and reltermfreqs together in serialise_stats.
+// 33: 1.1.4 Support for passing matchspies over the remote connection.
 #define XAPIAN_REMOTE_PROTOCOL_MAJOR_VERSION 33
 #define XAPIAN_REMOTE_PROTOCOL_MINOR_VERSION 0
 
