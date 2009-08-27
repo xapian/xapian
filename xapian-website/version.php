bd75145cbdfc423653a92eda4d0a03a5863b0c2a
James Aylett <james@tartarus.org>
2009-08-27 14:30:53 +0000

    Manually merge updates from CVS: new release, and Fink now packages Python bindings.
    git-svn-id: file:///home/xapian-svn/xapian/branches/2009-documentation-changes@13422 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-website/version.php
+++ xapian-website/version.php
@@ -1,4 +1,4 @@
-<? $version = "1.0.14"; $branch = "1.0"; $release_date = "2009-07-21";
+<? $version = "1.0.15"; $branch = "1.0"; $release_date = "2009-08-26";
    $perl_minor = ".0";
 
    $version_d = "1.1.2"; $release_date_d = "2009-07-23";
