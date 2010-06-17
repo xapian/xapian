7ba49f115b6b443f4ff26d0e85a9b5657e1d4086
Olly Betts <olly@survex.com>
2010-06-17 11:50:57 +0000

    api/valuerangeproc.cc: Add support for open-ended ranges (ticket#480). docs/queryparser.html,docs/valueranges.rst: Document. tests/queryparsertest.cc: Add feature tests.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@14714 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/queryparsertest.cc
+++ xapian-core/tests/queryparsertest.cc
@@ -1173,6 +1173,9 @@ static const test test_value_range1_queries[] = {
     { "hello -5..7", "(hello:(pos=1) FILTER VALUE_RANGE 1 -5 7)" },
     { "-5..7 hello", "(hello:(pos=1) FILTER VALUE_RANGE 1 -5 7)" },
     { "\"time flies\" 09:00..12:30", "((time:(pos=1) PHRASE 2 flies:(pos=2)) FILTER VALUE_RANGE 1 09:00 12:30)" },
+    // Feature test for single-ended ranges (ticket#480):
+    { "..b", "VALUE_RANGE 1  b" },
+    { "a..", "VALUE_GE 1 a" },
     { NULL, NULL }
 };
 
@@ -1212,6 +1215,9 @@ static const test test_value_range2_queries[] = {
     { "20070201..20070228", "VALUE_RANGE 1 20070201 20070228" },
     { "$10..20", "VALUE_RANGE 4 \255 \261" },
     { "$10..$20", "VALUE_RANGE 4 \255 \261" },
+    // Feature test for single-ended ranges (ticket#480):
+    { "$..20", "VALUE_RANGE 4  \261" },
+    { "$10..", "VALUE_GE 4 \255" },
     { "12..42kg", "VALUE_RANGE 5 \256 \265@" },
     { "12kg..42kg", "VALUE_RANGE 5 \256 \265@" },
     { "12kg..42", "VALUE_RANGE 3 12kg 42" },
@@ -1219,6 +1225,9 @@ static const test test_value_range2_queries[] = {
     { "1999-03-12..2020-12-30", "VALUE_RANGE 1 19990312 20201230" },
     { "1999/03/12..2020/12/30", "VALUE_RANGE 1 19990312 20201230" },
     { "1999.03.12..2020.12.30", "VALUE_RANGE 1 19990312 20201230" },
+    // Feature test for single-ended ranges (ticket#480):
+    { "..2020.12.30", "VALUE_RANGE 1  20201230" },
+    { "1999.03.12..", "VALUE_GE 1 19990312" },
     { "12/03/99..12/04/01", "VALUE_RANGE 1 19990312 20010412" },
     { "03-12-99..04-14-01", "VALUE_RANGE 1 19990312 20010414" },
     { "(test:a..test:b hello)", "(hello:(pos=1) FILTER VALUE_RANGE 3 test:a test:b)" },
