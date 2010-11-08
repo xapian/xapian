630a2a8e536540b4629d3a410af7b858b5697a20
Olly Betts <olly@survex.com>
2010-11-08 03:15:37 +0000

    metaxmlparse.h: Initialise field member variable.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@15156 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/metaxmlparse.h
+++ xapian-applications/omega/metaxmlparse.h
@@ -1,6 +1,6 @@
 /* metaxmlparse.h: subclass of HtmlParser for parsing OpenDocument's meta.xml.
  *
- * Copyright (C) 2006,2009 Olly Betts
+ * Copyright (C) 2006,2009,2010 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,7 +25,7 @@
 class MetaXmlParser : public HtmlParser {
     enum { NONE, KEYWORDS, TITLE, SAMPLE } field;
   public:
-    MetaXmlParser() { }
+    MetaXmlParser() : field(NONE) { }
     void process_text(const string &text);
     void opening_tag(const string &tag);
     void closing_tag(const string &tag);
