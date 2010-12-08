b76315fa0c559645093a1173b063bf98a9a00d50
Olly Betts <olly@survex.com>
2010-12-08 10:22:06 +0000

    Backport change from trunk: omindex.cc: Escape wildcard patterns being passed to unzip - in the unlikely event that one of these matched files in or under the current directory, we might fail to extract all the files we wanted to.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.0@15208 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omindex.cc
+++ xapian-applications/omega/omindex.cc
@@ -396,7 +396,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod, off_t siz
 	} else if (startswith(tail, "spreadsheetml.")) {
 	    args = " xl/sharedStrings.xml";
 	} else if (startswith(tail, "presentationml.")) {
-	    args = " ppt/slides/slide*.xml";
+	    args = " ppt/slides/slide\\*.xml";
 	} else {
 	    // Don't know how to index this type.
 	    cout << "unknown Office 2007 MIME subtype - skipping" << endl;
@@ -518,7 +518,7 @@ index_file(const string &url, const string &mimetype, time_t last_mod, off_t siz
 	}
     } else if (mimetype == "application/vnd.ms-xpsdocument") {
 	string safefile = shell_protect(file);
-	string cmd = "unzip -p " + safefile + " Documents/1/Pages/*.fpage";
+	string cmd = "unzip -p " + safefile + " Documents/1/Pages/\\*.fpage";
 	try {
 	    XpsXmlParser xpsparser;
 	    dump = stdout_to_string(cmd);
