3c5a73e2b1691072b5a3bdbcf42e2c163c151324
Olly Betts <olly@survex.com>
2011-02-28 12:11:38 +0000

    xmlparse.cc: Add whitespace between chunks of text extracted from Microsoft Office 2007 formats.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@15350 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/xmlparse.cc
+++ xapian-applications/omega/xmlparse.cc
@@ -1,6 +1,6 @@
 /* xmlparse.cc: subclass of HtmlParser for parsing XML.
  *
- * Copyright (C) 2006,2009 Olly Betts
+ * Copyright (C) 2006,2009,2011 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,9 @@ XmlParser::opening_tag(const string &)
 }
 
 void
-XmlParser::closing_tag(const string &)
+XmlParser::closing_tag(const string &tag)
 {
+    // For OpenDocument, .docx, .xlsx, .pptx respectively.
+    if (tag == "text:p" || tag == "w:t" || tag == "t" || tag == "a:t")
+	pending_space = true;
 }
