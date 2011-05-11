a3106eb548f11c1b3ce26063da0be19e111d5460
Olly Betts <olly@survex.com>
2011-05-11 01:31:34 +0000

    net/progclient.cc,net/tcpserver.cc: Need <cstdio> for sprintf on __WIN32__ and cygwin.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@15461 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/net/progclient.cc
+++ xapian-core/net/progclient.cc
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2003,2004,2005,2006,2007,2010 Olly Betts
+ * Copyright 2003,2004,2005,2006,2007,2010,2011 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -38,6 +38,7 @@
 # include <sys/socket.h>
 # include <sys/wait.h>
 #else
+# include <cstdio> // For sprintf().
 # include <io.h>
 #endif
 
--- xapian-core/net/tcpserver.cc
+++ xapian-core/net/tcpserver.cc
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -50,6 +50,7 @@
 #include <iostream>
 
 #include <cstring>
+#include <cstdio> // For sprintf() on __WIN32__ or cygwin.
 #include <sys/types.h>
 
 using namespace std;
