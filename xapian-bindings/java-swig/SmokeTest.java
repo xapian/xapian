63933142e3da3b85134c46cd5bb0971fd1868d64
Olly Betts <olly@survex.com>
2011-06-23 08:47:27 +0000

    Backport change from trunk: java-swig/Makefile.am,java-swig/SmokeTest.java: Put the SWIG-generated Java bindings in package org.xapian.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.2@15778 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/java-swig/SmokeTest.java
+++ xapian-bindings/java-swig/SmokeTest.java
@@ -17,8 +17,7 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
 // USA
 
-// FIXME: need to put bindings in a package
-//import org.xapian.*;
+import org.xapian.*;
 
 // FIXME: need to sort out throwing wrapped Xapian::Error subclasses
 //import org.xapian.errors.*;
