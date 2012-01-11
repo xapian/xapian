39d1ff7d4ce3b04f06eb1899a3c550ac741d3567
Olly Betts <olly@survex.com>
2012-01-11 13:06:30 +0000

    matcher/phrasepostlist.h: Remove lone "----START-LICENCE----" - the rest got removed ages ago.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@16310 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/matcher/phrasepostlist.h
+++ xapian-core/matcher/phrasepostlist.h
@@ -1,6 +1,5 @@
 /* phrasepostlist.h: Return only items where terms are near or form a phrase
  *
- * ----START-LICENCE----
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2003,2004,2005 Olly Betts
  * Copyright 2009 Lemur Consulting Ltd
