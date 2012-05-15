a2dd308fdcbfa1e39fd0810a4b1430e900aec4d8
Olly Betts <olly@survex.com>
2012-05-15 11:13:16 +0000

    backends/chert/chert_cursor.cc: Fix incorrect use of "delete" to "delete []".  The type is POD, so I suspect this mistake didn't actually cause any problems, and it only happens when a cursor gets rebuilt because the B-tree has gained a level, so it's a rather rare occurrence.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@16570 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/chert/chert_cursor.cc
+++ xapian-core/backends/chert/chert_cursor.cc
@@ -1,7 +1,7 @@
 /* chert_cursor.cc: Btree cursor implementation
  *
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2012 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -87,7 +87,7 @@ ChertCursor::rebuild()
 	    C[i].p = old_C[i].p;
 	    C[i].n = BLK_UNUSED;
 	}
-	delete old_C;
+	delete [] old_C;
 	for (int j = level; j < new_level; j++) {
 	    C[j].p = new byte[B->block_size];
 	    C[j].n = BLK_UNUSED;
