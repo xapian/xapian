48f0cceec9c26cbb2050f29878f8abb52b1c6ab6
Olly Betts <olly@survex.com>
2012-06-04 07:10:14 +0000

    python/smoketest2.py: Test xapian.open_stub() is wrapped.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@16688 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-bindings/python/smoketest2.py
+++ xapian-bindings/python/smoketest2.py
@@ -95,6 +95,12 @@ def test_all():
     # Check database factory functions are wrapped as expected:
 
     expect_exception(xapian.DatabaseOpeningError, None,
+                     xapian.open_stub, "nosuchdir/nosuchdb")
+    # WritableDatabase form not wrapped currently.
+    #expect_exception(xapian.DatabaseCreateError, None,
+    #                 xapian.open_stub, "nosuchdir/nosuchdb", xapian.DB_CREATE)
+
+    expect_exception(xapian.DatabaseOpeningError, None,
                      xapian.brass_open, "nosuchdir/nosuchdb")
     expect_exception(xapian.DatabaseCreateError, None,
                      xapian.brass_open, "nosuchdir/nosuchdb", xapian.DB_CREATE)
