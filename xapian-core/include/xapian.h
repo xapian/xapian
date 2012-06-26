274e5c9c7d2deb847bacf52b65d3fb85ea268319
Olly Betts <olly@survex.com>
2012-06-26 08:52:47 +0000

    Backport change from trunk: include/xapian.h: Add check for Qt headers being included before us and defining 'slots' as a macro - if they are, give a clear error advising how to work around this.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.2@16769 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian.h
+++ xapian-core/include/xapian.h
@@ -1,7 +1,7 @@
 /** @file xapian.h
  *  @brief Public interfaces for the Xapian library.
  */
-// Copyright (C) 2003,2004,2005,2007,2008,2009,2010 Olly Betts
+// Copyright (C) 2003,2004,2005,2007,2008,2009,2010,2012 Olly Betts
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -19,6 +19,16 @@
 
 #ifndef XAPIAN_INCLUDED_XAPIAN_H
 #define XAPIAN_INCLUDED_XAPIAN_H
+
+// Qt headers '#define slots' by default, which clashes with us using it as a
+// class member name.  Including <xapian.h> first is a simple workaround, or
+// you can use 'no_keywords' to stop Qt polluting the global macro namespace,
+// as described here:
+//
+// http://qt-project.org/doc/qt-5.0/signalsandslots.html#using-qt-with-3rd-party-signals-and-slots
+#if defined slots && defined Q_OBJECT
+# error "Include <xapian.h> before Qt headers, or put 'CONFIG += no_keywords' in your .pro file and use Q_SLOTS instead of slots, etc"
+#endif
 
 // Set defines for library version and check C++ ABI versions match.
 #include <xapian/version.h>
