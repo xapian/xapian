3d7ffb7d98178a119439b8201f0279e0e96bebb6
Olly Betts <olly@survex.com>
2012-09-25 08:10:18 +0000

    include/xapian/enquire.h: Remove FIXMEs about converting MSetIterator and ESetIterator to use Internal classes - as HACKING notes, the current implementation is that way for performance reasons.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@16828 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian/enquire.h
+++ xapian-core/include/xapian/enquire.h
@@ -261,7 +261,6 @@ class XAPIAN_VISIBILITY_DEFAULT MSet {
 /** An iterator pointing to items in an MSet.
  *  This is used for access to individual results of a match.
  */
-//FIXME:dc: This iterator should be converted to use an Internal class
 class XAPIAN_VISIBILITY_DEFAULT MSetIterator {
     private:
 	friend class MSet;
@@ -486,7 +485,6 @@ class XAPIAN_VISIBILITY_DEFAULT ESet {
 	std::string get_description() const;
 };
 
-//FIXME:dc: This iterator should be converted to use an Internal class
 /** Iterate through terms in the ESet */
 class XAPIAN_VISIBILITY_DEFAULT ESetIterator {
     private:
