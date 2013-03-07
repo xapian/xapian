3ae9e4354f7533d2434d3705f53785ebfd2e6fe6
Olly Betts <olly@survex.com>
2013-03-07 00:42:57 +0000

    tests/api_anydb.cc: Added tradweight4 testcase to test TradWeight with an RSet for weighting documents.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17095 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/api_anydb.cc
+++ xapian-core/tests/api_anydb.cc
@@ -2250,6 +2250,32 @@ DEFINE_TESTCASE(tradweight1, backend) {
     return true;
 }
 
+// Test TradWeight when weighting documents using an RSet.
+// Simply changed the weighting scheme used by rset2 testcase.
+DEFINE_TESTCASE(tradweight4, backend) {
+    Xapian::Database mydb(get_database("apitest_rset"));
+    Xapian::Enquire enquire(mydb);
+    Xapian::Query myquery = query(Xapian::Query::OP_OR, "cuddly", "people");   
+    
+    enquire.set_query(myquery); 
+    enquire.set_weighting_scheme(Xapian::TradWeight()); 
+
+    Xapian::MSet mymset1 = enquire.get_mset(0, 10);    
+
+    Xapian::RSet myrset;
+    myrset.add_document(2);
+
+    Xapian::MSet mymset2 = enquire.get_mset(0, 10, &myrset);
+
+    mset_expect_order(mymset1, 1, 2);    
+    // Document 2 should have higher weight than document 1 despite the wdf of
+    // "people" being 1 because "people" indexes a document in the RSet whereas
+    // "cuddly" (wdf=2) does not.
+    mset_expect_order(mymset2, 2, 1);
+    
+    return true;
+}
+
 // Feature test for Database::get_uuid().
 DEFINE_TESTCASE(uuid1, backend && !multi) {
     SKIP_TEST_FOR_BACKEND("inmemory");
