603b48d880bd766af5ae33a2757d2edca6d333da
Olly Betts <olly@survex.com>
2013-05-24 09:24:48 +0000

    weight.cc: Check parameters to $set{weighting,bm25 ...} and $set{weighting,trad ...} converted OK.  Based on patch from Aarsh Shah.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17304 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/weight.cc
+++ xapian-applications/omega/weight.cc
@@ -1,7 +1,8 @@
 /** @file weight.cc
  * @brief Set the weighting scheme for Omega
  */
-/* Copyright (C) 2009 Olly Betts
+/* Copyright (C) 2009,2013 Olly Betts
+ * Copyright (C) 2013 Aarsh Shah
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,8 +27,22 @@
 
 #include <cstdlib>
 #include "safeerrno.h"
+#include "common/noreturn.h"
 
 using namespace std;
+
+XAPIAN_NORETURN(static void
+parameter_error(const char * param, const string & scheme));
+
+static void
+parameter_error(const char * msg, const string & scheme)
+{
+    string m(msg);
+    m += ": '";
+    m += scheme;
+    m += "'";
+    throw m;
+}
 
 static bool
 double_param(const char ** p, double * ptr_val)
@@ -54,17 +69,28 @@ set_weighting_scheme(Xapian::Enquire & enq, const map<string, string> & opt,
 
 	if (startswith(scheme, "bm25")) {
 	    const char *p = scheme.c_str() + 4;
-	    if (*p == '\0' || C_isspace((unsigned char)*p)) {
+	    if (*p == '\0') {
+		enq.set_weighting_scheme(Xapian::BM25Weight());
+		return;
+	    }
+	    if (C_isspace((unsigned char)*p)) {
 		double k1 = 1;
 		double k2 = 0;
 		double k3 = 1;
 		double b = 0.5;
 		double min_normlen = 0.5;
-		(void)(double_param(&p, &k1) &&
-		    double_param(&p, &k2) &&
-		    double_param(&p, &k3) &&
-		    double_param(&p, &b) &&
-		    double_param(&p, &min_normlen));
+		if (!double_param(&p, &k1))
+		    parameter_error("Parameter 1 (k1) is invalid", scheme);
+		if (*p && !double_param(&p, &k2))
+		    parameter_error("Parameter 2 (k2) is invalid", scheme);
+		if (*p && !double_param(&p, &k3))
+		    parameter_error("Parameter 3 (k3) is invalid", scheme);
+		if (*p && !double_param(&p, &b))
+		    parameter_error("Parameter 4 (b) is invalid", scheme);
+		if (*p && !double_param(&p, &min_normlen))
+		    parameter_error("Parameter 5 (min_normlen) is invalid", scheme);
+		if (*p)
+		    parameter_error("Extra data after parameter 5", scheme);
 		Xapian::BM25Weight wt(k1, k2, k3, b, min_normlen);
 		enq.set_weighting_scheme(wt);
 		return;
@@ -73,9 +99,16 @@ set_weighting_scheme(Xapian::Enquire & enq, const map<string, string> & opt,
 
 	if (startswith(scheme, "trad")) {
 	    const char *p = scheme.c_str() + 4;
-	    if (*p == '\0' || C_isspace((unsigned char)*p)) {
-		double k = 1;
-		double_param(&p, &k);
+	    if (*p == '\0') {
+		enq.set_weighting_scheme(Xapian::TradWeight());
+		return;
+	    }
+	    if (C_isspace((unsigned char)*p)) {
+		double k;
+		if (!double_param(&p, &k))
+		    parameter_error("Parameter is invalid", scheme);
+		if (*p)
+		    parameter_error("Extra data after parameter", scheme);
 		enq.set_weighting_scheme(Xapian::TradWeight(k));
 		return;
 	    }
