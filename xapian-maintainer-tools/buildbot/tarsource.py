01132f3d159b96c23c8a590bd7e83dc95a71bcf3
Olly Betts <olly@survex.com>
2013-06-14 12:15:50 +0000

    Update to versions currently in use
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17321 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-maintainer-tools/buildbot/tarsource.py
+++ xapian-maintainer-tools/buildbot/tarsource.py
@@ -10,7 +10,8 @@ from twisted.internet.task import LoopingCall
 from buildbot import util
 from buildbot.changes import base
 from buildbot.changes.changes import Change
-from buildbot.process.step import Source, RemoteShellCommand
+from buildbot.process.buildstep import RemoteShellCommand
+from buildbot.steps.source import Source
 
 import re
 
