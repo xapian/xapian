e296d5d8fe514cdef92f13e833d802172ec979a2
Olly Betts <olly@survex.com>
2013-09-16 10:48:14 +0000

    Backport change from trunk: tests/harness/testsuite.cc,tests/harness/testsuite.h: If -v/--verbose is specified more than once, show the diagnostic output for passing tests as well as failing/skipped ones.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.2@17465 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/harness/testsuite.h
+++ xapian-core/tests/harness/testsuite.h
@@ -1,7 +1,7 @@
 /* testsuite.h: a generic test suite engine
  *
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2005,2006,2007,2008,2009 Olly Betts
+ * Copyright 2002,2003,2005,2006,2007,2008,2009,2013 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -75,10 +75,12 @@ struct test_desc {
 
 /// The global verbose flag.
 //
-//  If verbose is set, then the test harness will display diagnostic output
-//  for tests which fail or skip.  Individual tests may use this flag to avoid
-//  needless generation of diagnostic output in cases when it's expensive.
-extern bool verbose;
+//  If verbose is non-zero, then the test harness will display diagnostic output
+//  for tests which fail or skip.  If it is > 1, then the diagnostic output will
+//  also be displayed for tests which pass.  Individual tests may use this flag
+//  to avoid needless generation of diagnostic output in cases when it's
+//  expensive.
+extern int verbose;
 
 /// The exception type we were expecting in TEST_EXCEPTION.
 //  Used to detect if such an exception was mishandled by the
