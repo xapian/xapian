5339f603922aaa22aae8d8ca318a4b55ab3916d8
Olly Betts <olly@survex.com>
2013-10-13 23:25:22 +0000

    Backport change from trunk: omindex.cc: Group-readable files which are owner-readable but not world-readable should still get a "readable by owner" term added. Reported by Emmanuel Garette.
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.2@17572 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-applications/omega/omindex.cc
+++ xapian-applications/omega/omindex.cc
@@ -910,7 +910,6 @@ index_file(const string &file, const string &url, DirectoryIterator & d,
 	    const char * group = d.get_group();
 	    if (group) {
 		newdocument.add_boolean_term(string("I#") + group);
-		inc_tag_added = true;
 	    }
 	}
 	const char * owner = d.get_owner();
