a6b8bc925cd32859782783373cbdb5d463bdd6d3
Olly Betts <olly@survex.com>
2013-12-23 03:35:57 +0000

    unicode/: Add the source data file and scripts to generate tclUniData.cc.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17768 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/unicode/Makefile.mk
+++ xapian-core/unicode/Makefile.mk
@@ -3,7 +3,19 @@ noinst_HEADERS +=\
 
 EXTRA_DIST +=\
 	unicode/dir_contents\
-	unicode/Makefile
+	unicode/Makefile\
+	unicode/UnicodeData-README.txt\
+	unicode/UnicodeData.txt\
+	unicode/uniParse.tcl
+
+if MAINTAINER_MODE
+unicode/tclUniData.cc: unicode/uniParse.tcl unicode/UnicodeData.txt
+	rm -f unicode/UnicodeData-6.3.0.txt
+	ln -s $(abs_srcdir)/unicode/UnicodeData.txt unicode/UnicodeData-6.3.0.txt
+	tclsh $(srcdir)/unicode/uniParse.tcl $(srcdir)/unicode/UnicodeData-6.3.0.txt unicode
+
+BUILT_SOURCES += unicode/tclUniData.cc
+endif
 
 lib_src +=\
 	unicode/description_append.cc\
