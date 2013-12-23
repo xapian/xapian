da2b2a602cb2c46e88232fab08f76ee2c1fd5305
Olly Betts <olly@survex.com>
2013-12-23 02:20:49 +0000

    Backport change from trunk: include/xapian/enquire.h: Note that 1.2.17 is required for the correct sense of the reverse parameter to Enquire methods set_sort_by_relevance_then_value() and set_sort_by_relevance_then_key().
    git-svn-id: file:///home/xapian-svn/xapian/branches/1.2@17765 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/include/xapian/enquire.h
+++ xapian-core/include/xapian/enquire.h
@@ -905,6 +905,12 @@ class XAPIAN_VISIBILITY_DEFAULT Enquire {
 	 * @param sort_key  value number to sort on.
 	 *
 	 * @param reverse   If true, reverses the sort order of sort_key.
+	 *		    Beware that in 1.2.16 and earlier, the sense
+	 *		    of this parameter was incorrectly inverted
+	 *		    and inconsistent with the other set_sort_by_...
+	 *		    methods.  This was fixed in 1.2.17, so make that
+	 *		    version a minimum requirement if this detail
+	 *		    matters to your application.
 	 */
 	void set_sort_by_relevance_then_value(Xapian::valueno sort_key,
 					      bool reverse);
@@ -924,7 +930,12 @@ class XAPIAN_VISIBILITY_DEFAULT Enquire {
 	 * @param sorter    The functor to use for generating keys.
 	 *
 	 * @param reverse   If true, reverses the sort order of the generated
-	 *		    keys.
+	 *		    keys.  Beware that in 1.2.16 and earlier, the sense
+	 *		    of this parameter was incorrectly inverted
+	 *		    and inconsistent with the other set_sort_by_...
+	 *		    methods.  This was fixed in 1.2.17, so make that
+	 *		    version a minimum requirement if this detail
+	 *		    matters to your application.
 	 */
 	void set_sort_by_relevance_then_key(Xapian::KeyMaker * sorter,
 					    bool reverse);
