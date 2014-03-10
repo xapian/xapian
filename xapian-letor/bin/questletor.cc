942335d42ab3bcdebe93183b281acdf997ecb97e
VcamX <vcamx3@gmail.com>
2014-03-10 02:00:36 -0700

    Modify questletor to be compatible with new Letor class and optimize code

--- xapian-letor/bin/questletor.cc
+++ xapian-letor/bin/questletor.cc
@@ -25,18 +25,17 @@
 #include <xapian.h>
 #include <xapian/letor.h>
 
-#include <cstdlib>
-#include <cstring>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <map>
 #include <set>
-#include <math.h>
+
+#include <cstdlib>
+#include <cstring>
+#include <cstdio> 
+#include <cmath>
 
 #include "gnu_getopt.h"
 
@@ -45,11 +44,11 @@ using namespace std;
 #define PROG_NAME "letor"
 #define PROG_DESC "Xapian command line search tool with Lerning to Rank Facility"
 
-typedef std::pair<Xapian::docid, double> MyPair;
+typedef std::pair<Xapian::docid, double> docid_to_score;
 
 struct MyTestCompare {
-    bool operator()(const MyPair& firstPair, const MyPair& secondPair) const {
-	return firstPair.second < secondPair.second;
+    bool operator()(const docid_to_score& firstPair, const docid_to_score& secondPair) const {
+        return firstPair.second < secondPair.second;
     }
 };
 
@@ -85,14 +84,14 @@ main(int argc, char **argv)
 try {
     const char * opts = "d:m:s:p:b:hv";
     static const struct option long_opts[] = {
-	{ "db",		required_argument, 0, 'd' },
-	{ "msize",	required_argument, 0, 'm' },
-	{ "stemmer",	required_argument, 0, 's' },
-	{ "prefix",	required_argument, 0, 'p' },
-	{ "boolean-prefix",	required_argument, 0, 'b' },
-	{ "help",	no_argument, 0, 'h' },
-	{ "version",	no_argument, 0, 'v' },
-	{ NULL,		0, 0, 0}
+        { "db",             required_argument, 0, 'd' },
+        { "msize",          required_argument, 0, 'm' },
+        { "stemmer",        required_argument, 0, 's' },
+        { "prefix",         required_argument, 0, 'p' },
+        { "boolean-prefix", required_argument, 0, 'b' },
+        { "help",           no_argument,       0, 'h' },
+        { "version",        no_argument,       0, 'v' },
+        { NULL,             0,                 0, 0   }
     };
 
     Xapian::SimpleStopper mystopper(sw, sw + sizeof(sw) / sizeof(sw[0]));
@@ -103,61 +102,68 @@ try {
 
     Xapian::Database db;
     Xapian::QueryParser parser;
-	parser.add_prefix("title","S");
-	parser.add_prefix("subject","S");
+    parser.add_prefix("title", "S");
+    parser.add_prefix("subject", "S");
 
     int c;
     while ((c = gnu_getopt_long(argc, argv, opts, long_opts, 0)) != -1) {
-	switch (c) {
-	    case 'm':
-		msize = atoi(optarg);
-		break;
-	    case 'd':
-		db.add_database(Xapian::Database(optarg));
-		have_database = true;
-		break;
-	    case 's':
-		try {
-		    stemmer = Xapian::Stem(optarg);
-		} catch (const Xapian::InvalidArgumentError &) {
-		    cerr << "Unknown stemming language '" << optarg << "'.\n"
-			    "Available language names are: "
-			 << Xapian::Stem::get_available_languages() << endl;
-		    exit(1);
-		}
-		break;
-	    case 'b': case 'p': {
-		const char * colon = strchr(optarg, ':');
-		if (colon == NULL) {
-		    cerr << argv[0] << ": need ':' when setting prefix" << endl;
-		    exit(1);
-		}
-		string prefix(optarg, colon - optarg);
-		string termprefix(colon + 1);
-		if (c == 'b') {
-		    parser.add_boolean_prefix(prefix, termprefix);
-		} else {
-		    parser.add_prefix(prefix, termprefix);
-		}
-		break;
-	    }
-	    case 'v':
-		cout << PROG_NAME" - "PACKAGE_STRING << endl;
-		exit(0);
-	    case 'h':
-		cout << PROG_NAME" - "PROG_DESC"\n\n";
-		show_usage();
-		exit(0);
-	    case ':': // missing parameter
-	    case '?': // unknown option
-		show_usage();
-		exit(1);
-	}
+        switch (c) {
+            case 'm':
+                msize = atoi(optarg);
+                break;
+            case 'd':
+                db.add_database(Xapian::Database(optarg));
+                have_database = true;
+                break;
+            case 's':
+                try {
+                    stemmer = Xapian::Stem(optarg);
+                } catch (const Xapian::InvalidArgumentError &) {
+                    cerr << "Unknown stemming language '" << optarg << "'.\n"
+                        "Available language names are: "
+                     << Xapian::Stem::get_available_languages() << endl;
+                    exit(1);
+                }
+                break;
+            case 'b':
+            case 'p':
+            {
+                const char * colon = strchr(optarg, ':');
+                if (colon == NULL)
+                {
+                    cerr << argv[0] << ": need ':' when setting prefix" << endl;
+                    exit(1);
+                }
+                
+                string prefix(optarg, colon - optarg);
+                string termprefix(colon + 1);
+                if (c == 'b')
+                {
+                    parser.add_boolean_prefix(prefix, termprefix);
+                }
+                else
+                {
+                    parser.add_prefix(prefix, termprefix);
+                }
+                break;
+            }
+            case 'v':
+                cout << PROG_NAME" - "PACKAGE_STRING << endl;
+                exit(0);
+            case 'h':
+                cout << PROG_NAME" - "PROG_DESC"\n\n";
+                show_usage();
+                exit(0);
+            case ':': // missing parameter
+            case '?': // unknown option
+                show_usage();
+                exit(1);
+        }
     }
 
     if (argc - optind != 1) {
-	show_usage();
-	exit(1);
+        show_usage();
+        exit(1);
     }
 
     parser.set_database(db);
@@ -170,32 +176,35 @@ try {
     string qq=argv[optind];
     istringstream iss(argv[optind]);
     string title="title:";
+
     while(iss) {
-	string t;
-	iss >> t;
-	if (t=="")
-	    break;
-	string temp="";
-	temp.append(title);
-	temp.append(t);
-	temp.append(" ");
-	temp.append(qq);
-	qq=temp;
+        string t;
+        iss >> t;
+        if (t=="")
+            break;
+        string temp="";
+        temp.append(title);
+        temp.append(t);
+        temp.append(" ");
+        temp.append(qq);
+        qq=temp;
     }
     cout << "Final Query " << qq << "\n";
 
     Xapian::Query query = parser.parse_query(qq,
-					     parser.FLAG_DEFAULT|
-					     parser.FLAG_SPELLING_CORRECTION);
+                         parser.FLAG_DEFAULT|
+                         parser.FLAG_SPELLING_CORRECTION);
+
     const string & correction = parser.get_corrected_query_string();
+    
     if (!correction.empty())
-	cout << "Did you mean: " << correction << "\n\n";
+        cout << "Did you mean: " << correction << "\n\n";
 
     cout << "Parsed Query: " << query.get_description() << endl;
 
     if (!have_database) {
-	cout << "No database specified so not running the query." << endl;
-	exit(0);
+        cout << "No database specified so not running the query." << endl;
+        exit(0);
     }
 
     Xapian::Enquire enquire(db);
@@ -203,50 +212,58 @@ try {
 
     Xapian::MSet mset = enquire.get_mset(0, msize);
 
-    Xapian::TermIterator qt,qt_end,temp,temp_end,docterms,docterms_end;
-    Xapian::PostingIterator p,pend;
+    // Xapian::TermIterator qt,qt_end,temp,temp_end,docterms,docterms_end;
+    // Xapian::PostingIterator p,pend;
 
     Xapian::Letor ltr;
 
     ltr.set_database(db);
     ltr.set_query(query);
-    ltr.create_ranker(1);
 
-    ltr.prepare_training_file("/home/encoder/gsoc/inex/topics.txt.short","/home/encoder/gsoc/inex/2010-assessments/inex2010-article.qrels",100);
+    string query_file = "~/Downloads/INEX/query.txt";
+    string qrel_file  = "~/Downloads/INEX/qrel.txt";
+    int n_entries     = 100;
+
+    ltr.create_ranker(Xapian::Letor::RANKER_SVM);
+    ltr.set_training_file_svm(query_file, qrel_file, n_entries);
+    ltr.prepare_training_file();
+
     //int num_features = 40;
     //ltr.prepare_training_file_listwise("filename",num_features);
-
     //ltr.letor_learn_model(4,0);
+    
     ltr.letor_learn_model();
-    map<Xapian::docid,double> letor_mset = ltr.letor_score(mset);
+    map<Xapian::docid, double> letor_mset = ltr.letor_score(mset);
 
-    set<MyPair,MyTestCompare> s;
-    map<Xapian::docid, double>::iterator iter = letor_mset.begin();
-    map<Xapian::docid, double>::iterator endIter = letor_mset.end();
+    set<docid_to_score, MyTestCompare> s;
 
-    for (; iter != endIter; ++iter) {
-       s.insert(*iter);
+    for (map<Xapian::docid, double>::iterator mset_it = letor_mset.begin();
+        mset_it != letor_mset.end(); ++mset_it)
+    {
+       s.insert(*mset_it);
     }
 
-    set<MyPair,MyTestCompare>::iterator it;
+    int rank = 1;
+    for (set<docid_to_score, MyTestCompare>::iterator it = s.end();
+        it != s.begin(); it--)
+    {
+        cout << "Item: " << rank << "\t" << it->second << "\n";
 
-    int rank=1;
-    for (it = s.end(); it != s.begin(); it--) {
-	cout << "Item: " << rank << "\t" << (*it).second << "\n";
+        Xapian::Document doc = db.get_document(it->first);
+        cout << doc.get_data() << "\n";
 
-	Xapian::Document doc = db.get_document((*it).first);
-	cout << doc.get_data() << "\n";
-	rank++;
+        ++rank;
     }
 
     cout << flush;
-} catch (const Xapian::QueryParserError & e) {
+}
+catch (const Xapian::QueryParserError & e)
+{
     cout << "Couldn't parse query: " << e.get_msg() << endl;
     exit(1);
-} catch (const Xapian::Error & err) {
+}
+catch (const Xapian::Error & err)
+{
     cout << err.get_description() << endl;
     exit(1);
 }
-
-
-
