f37aa0ef348b7db6bf018366c69620023490b165
Olly Betts <olly@survex.com>
2014-04-09 22:23:34 +0000

    api/documentterm.h,api/omdocument.cc: Don't store the termname in OmDocumentTerm - we were only using it in get_description() output and an exception message.  Speeds up indexing etext.txt using simpleindex by 0.4%, and should reduce memory usage a bit too. (Change inspired by comments from Vishesh Handa on xapian-devel).
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17946 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/api/documentterm.h
+++ xapian-core/api/documentterm.h
@@ -38,18 +38,13 @@ class OmDocumentTerm {
     public:
     /** Make a new term.
      *
-     *  @param tname_ The name of the new term.
      *  @param wdf_   Initial wdf.
      */
-    OmDocumentTerm(const string & tname_, Xapian::termcount wdf_)
-	: tname(tname_), wdf(wdf_)
+    explicit OmDocumentTerm(Xapian::termcount wdf_)
+	: wdf(wdf_)
     {
-	LOGCALL_CTOR(DB, "OmDocumentTerm", tname_ | wdf_);
+	LOGCALL_CTOR(DB, "OmDocumentTerm", wdf_);
     }
-
-    /** The name of this term.
-     */
-    string tname;
 
     /** Within document frequency of the term.
      *  This is the number of occurrences of the term in the document.
