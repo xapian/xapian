1950701454450205f87bf678df4650eb4ace406d
Olly Betts <olly@survex.com>
2014-04-29 04:33:22 +0000

    tests/harness/unixcmds.cc: Drop code supporting Microsoft Windows 9x which reached EOL in 2006.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@17963 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/harness/unixcmds.cc
+++ xapian-core/tests/harness/unixcmds.cc
@@ -1,7 +1,7 @@
 /** @file unixcmds.cc
  *  @brief C++ function versions of useful Unix commands.
  */
-/* Copyright (C) 2003,2004,2007,2012 Olly Betts
+/* Copyright (C) 2003,2004,2007,2012,2014 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -56,10 +56,9 @@ checked_system(const string & cmd)
 /// Recursively copy a directory.
 void cp_R(const std::string &src, const std::string &dest) {
 #ifdef __WIN32__
-    // xcopy should be available on both NT and 95 derivatives.  We create
-    // the target directory first to avoid being prompted as to whether we
-    // want to create a directory or a file (which makes no sense when
-    // copying a directory, but that's how xcopy seems to work!)
+    // We create the target directory first to avoid being prompted as to
+    // whether we want to create a directory or a file (which makes no sense
+    // when copying a directory, but that's how xcopy seems to work!)
     mkdir(dest.c_str());
     string cmd("xcopy /E /Y");
 #else
@@ -77,21 +76,6 @@ void cp_R(const std::string &src, const std::string &dest) {
 #endif
 }
 
-#ifdef __WIN32__
-static bool running_on_win9x() {
-    static int win9x = -1;
-    if (win9x == -1) {
-	OSVERSIONINFO info;
-	memset(&info, 0, sizeof(OSVERSIONINFO));
-	info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
-	if (GetVersionEx(&info)) {
-	    win9x = (info.dwPlatformId == VER_PLATFORM_WIN32_WINDOWS);
-	}
-    }
-    return win9x;
-}
-#endif
-
 /// Remove a directory and contents, just like the Unix "rm -rf" command.
 void rm_rf(const string &filename) {
     // Check filename exists and is actually a directory
@@ -99,9 +83,7 @@ void rm_rf(const string &filename) {
 	return;
 
 #ifdef __WIN32__
-    string cmd = running_on_win9x() ?
-	"deltree /y" : // for 95-like systems.
-	"rd /s /q"; // for NT-like systems.
+    string cmd("rd /s /q");
 #else
     string cmd("rm -rf");
 #endif
