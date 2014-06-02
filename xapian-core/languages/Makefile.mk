7152b17289ad126ae2643451517dfd9badedcdc7
Olly Betts <olly@survex.com>
2014-06-02 11:40:50 +0000

    languages/Makefile.mk: Ship collate-sbl script.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@18053 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/languages/Makefile.mk
+++ xapian-core/languages/Makefile.mk
@@ -49,6 +49,7 @@ snowball_headers =\
 	languages/compiler/syswords2.h
 
 EXTRA_DIST += $(snowball_sources) $(snowball_headers) $(snowball_algorithms) $(snowball_built_sources)\
+	languages/collate-sbl\
 	languages/sbl-dispatch.h\
 	languages/dir_contents\
 	languages/Makefile\
