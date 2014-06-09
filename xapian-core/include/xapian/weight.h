7701bbcaff142d5a0d253729975825e3f0705d7f
kiran <aarsh@ubuntu.ubuntu-domain>
2014-06-09 01:40:23 -0400

    BB2 Optimization Phase1 - Header File.

--- xapian-core/include/xapian/weight.h
+++ xapian-core/include/xapian/weight.h
@@ -843,6 +843,13 @@ class XAPIAN_VISIBILITY_DEFAULT BB2Weight : public Weight {
     /// The factor to multiply with the weight.
     double factor;
 
+    /// The constant values to be used in get_sumpart().
+    double c_product_avlen;
+    double B_constant;
+    double wt;
+    double stirling_constant_1;
+    double stirling_constant_2;
+
     BB2Weight * clone() const;
 
     void init(double factor);
