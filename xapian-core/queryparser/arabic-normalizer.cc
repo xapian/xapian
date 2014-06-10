fbc2ea187018ec9a0d4a21e8623408800b408371
Assem Chelli <assem.ch@gmail.com>
2014-06-11 00:43:04 +0100

    updates on the normalizer - add docstrings to the header - use strings with utf8-iterator instead of array of unsingeds - fix indentation - improve the order and add all normalization conditions

--- xapian-core/queryparser/arabic-normalizer.cc
+++ xapian-core/queryparser/arabic-normalizer.cc
@@ -5,53 +5,53 @@
 
 #include <algorithm>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-unsigned* ArabicNormalizer::normalize(const unsigned* word) {
-	std::vector<unsigned> new_word;
-	unsigned c;
-	int word_size = sizeof(word)/sizeof(unsigned);
-	for(int i=0; i< word_size; ++i) { // TODO order them by proba of happening
-		c=word[i];
-		// normalize LAM ALEF forms
-		if ( find(ARABIC_LIGUATURES, ARABIC_LIGUATURES + (sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)), c) != ARABIC_LIGUATURES + sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)) {
-			new_word.push_back(ARABIC_LAM);
-			new_word.push_back(ARABIC_ALEF);
-		}
-		// normalize shaped letters
-		else if  (ARABIC_SHAPING_MAP.find(c)!= ARABIC_SHAPING_MAP.end()) {  // TODO dont repeat the find operation
-			new_word.push_back(ARABIC_SHAPING_MAP.find(c)->second);
-		}
-		// strip the tatweel or kasheeda
-		else if (c == ARABIC_TATWEEL ) {
-			// ignore it
-		}
-		// strip arabic-specific punctuations
-		else if (c == ARABIC_COMMA || c == ARABIC_SEMICOLON || c == ARABIC_QUESTION) {
-			// ignore it
-		}
-		// strip diacritics
-		else if ( (find(ARABIC_TASHKEEL, ARABIC_TASHKEEL + (sizeof(ARABIC_TASHKEEL)/sizeof(unsigned)), c) != ARABIC_TASHKEEL + sizeof(ARABIC_TASHKEEL)/sizeof(unsigned))  && this->normalize_diacritics ) {
-			// ignore it
-		}
-		else if (c ==  ARABIC_TEH_MARBUTA && this->normalize_similar_spellings) {
-			new_word.push_back(ARABIC_HEH);
-		}
-		else if (c ==  ARABIC_ALEF_MAKSURA && this->normalize_similar_spellings){
-			new_word.push_back(ARABIC_YEH);
-		}
-		else if ( find(ARABIC_ALEFAT, ARABIC_ALEFAT + (sizeof(ARABIC_ALEFAT)/sizeof(unsigned)) , c) !=  ARABIC_ALEFAT + (sizeof(ARABIC_ALEFAT)/sizeof(unsigned))) {
-			new_word.push_back(ARABIC_ALEF_HAMZA_ABOVE);
-		}
-		else if ( find(ARABIC_HAMZAT, ARABIC_HAMZAT + (sizeof(ARABIC_HAMZAT)/sizeof(unsigned)) , c)  != ARABIC_HAMZAT + (sizeof(ARABIC_HAMZAT)/sizeof(unsigned))) {
-			new_word.push_back(ARABIC_ALEF);
-		}
-		// nothing to do
-		else {
-			new_word.push_back(c);
-		};
-	};
 
-	return &new_word[0];
+std::string ArabicNormalizer::normalize(const std::string word) {
+    std::string new_word;
+    Xapian::Utf8Iterator i(word);
+    for(; i != Xapian::Utf8Iterator(); ++i) { // TODO order them by proba of happening
+        unsigned ch = *i;
+        // normalize LAM ALEF forms
+        if (this->normalize_shaping && find(ARABIC_LIGUATURES, ARABIC_LIGUATURES + (sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)), ch) != ARABIC_LIGUATURES + sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)) {
+            Xapian::Unicode::append_utf8(new_word, ARABIC_LAM);
+            Xapian::Unicode::append_utf8(new_word, ARABIC_ALEF);
+        }
+        // normalize shaped letters
+        else if (this->normalize_shaping && ARABIC_SHAPING_MAP.find(ch)!= ARABIC_SHAPING_MAP.end()) {  // TODO dont repeat the find operation
+            new_word.push_back(ARABIC_SHAPING_MAP.find(ch)->second);
+        }
+        // strip diacritics
+        else if (this->normalize_diacritics && (find(ARABIC_TASHKEEL, ARABIC_TASHKEEL + (sizeof(ARABIC_TASHKEEL)/sizeof(unsigned)), ch) != ARABIC_TASHKEEL + sizeof(ARABIC_TASHKEEL)/sizeof(unsigned))  && this->normalize_diacritics ) {
+            // ignore it
+        }
+        // strip the tatweel or kasheeda
+        else if (this->normalize_shaping && ch == ARABIC_TATWEEL ) {
+            // ignore it
+        }
+        // strip arabic-specific punctuation
+        else if (this->normalize_punctuation && (ch == ARABIC_COMMA || ch == ARABIC_SEMICOLON || ch == ARABIC_QUESTION)) {
+            // ignore it
+        }
+        else if (this->normalize_similar_spellings && ch ==  ARABIC_TEH_MARBUTA ) {
+            Xapian::Unicode::append_utf8(new_word, ARABIC_HEH);
+        }
+        else if (this->normalize_similar_spellings && ch ==  ARABIC_ALEF_MAKSURA){
+            Xapian::Unicode::append_utf8(new_word, ARABIC_YEH);
+        }
+        else if (this->normalize_similar_spellings && find(ARABIC_ALEFAT, ARABIC_ALEFAT + (sizeof(ARABIC_ALEFAT)/sizeof(unsigned)) , ch) !=  ARABIC_ALEFAT + (sizeof(ARABIC_ALEFAT)/sizeof(unsigned))) {
+            Xapian::Unicode::append_utf8(new_word, ARABIC_ALEF_HAMZA_ABOVE);
+        }
+        else if (this->normalize_hamza && find(ARABIC_HAMZAT, ARABIC_HAMZAT + (sizeof(ARABIC_HAMZAT)/sizeof(unsigned)) , ch)  != ARABIC_HAMZAT + (sizeof(ARABIC_HAMZAT)/sizeof(unsigned))) {
+            Xapian::Unicode::append_utf8(new_word, ARABIC_ALEF);
+        }
+        // nothing to do
+        else {
+            Xapian::Unicode::append_utf8(new_word, ch);
+        }
+    }
+    return new_word;
 }
--- xapian-core/queryparser/arabic-normalizer.h
+++ xapian-core/queryparser/arabic-normalizer.h
@@ -24,39 +24,60 @@
 
 #include "xapian/visibility.h"
 
-/// Arabic Normalizer
-class XAPIAN_VISIBILITY_DEFAULT ArabicNormalizer {
+#include <string>
 
+/** @brief Arabic Normalizer, a  class to normalize Arabic text
+ * its role is:
+ * - keep the letters unshaped, separate Lam-Alef and eliminate Tatweel
+ * - strip Arabic-specific punctuation marks
+ * - strip diacritics or vocalization marks
+ * - normalize all Hamza forms to "Alef with Hamza above"
+ * - normalize similar spelled letters, like: Teh marbouta and Heh, Alef maqsoura and Yah
+ * - eliminate letters that can't be written using a keyboard
+ */
+class XAPIAN_VISIBILITY_DEFAULT ArabicNormalizer {
   private:
 	bool normalize_shaping;
+	bool normalize_punctuation;
 	bool normalize_diacritics;
 	bool normalize_hamza;
 	bool normalize_similar_spellings;
 	bool normalize_extra_symbols;
 
-
   public:
+	/** @brief Constructor with no parameters, use it for all normalization types.
+	 */
 	ArabicNormalizer () {
-
 		this->normalize_shaping = 1;
+		this->normalize_punctuation = 1;
 		this->normalize_diacritics = 1;
 		this->normalize_hamza = 1;
 		this->normalize_similar_spellings = 1;
 		this->normalize_extra_symbols = 1;
-	};
+	}
 
-	ArabicNormalizer (bool shaping, bool diacritics, bool hamza, bool similar_spellings, bool extra_symbols) {
+	/** @brief Constructor with parameters, use it to specify what you want to be normalized and what not.
+	 *
+	 *  Parameters:
+	 *  @param shaping allow the normalization of shaping, fixing Lam-Alef issue and strip Tatweel
+	 *  @param punctuation allow stripping Arabic-specific punctuation marks
+	 *  @param diacritics allow stripping diacritics or vocalization marks
+	 *  @param hamza allow normalization of all Hamza forms to "Alef with Hamza above"
+	 *  @param similar_spellings allow normalization of similar spelled letters, like: Teh marbouta and Heh, Alef maqsoura and Yah
+	 *  @param extra_symbols allow elimination of letters that can't be written using a keyboard
+	 *
+	 */
+	ArabicNormalizer (bool shaping, bool punctuation, bool diacritics, bool hamza, bool similar_spellings, bool extra_symbols) {
 		this->normalize_shaping = shaping;
+		this->normalize_punctuation = punctuation;
 		this->normalize_diacritics = diacritics;
 		this->normalize_hamza = hamza;
 		this->normalize_similar_spellings = similar_spellings;
 		this->normalize_extra_symbols = extra_symbols;
-  };
+  }
 
-
-	unsigned* normalize(const unsigned* word);
+	std::string normalize(const std::string word);
 
 };
-
 
 #endif // XAPIAN_INCLUDED_ARABICNORMALIZER_Hأ‬
