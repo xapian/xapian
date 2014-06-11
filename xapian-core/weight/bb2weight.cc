5881a6fb8576d308d9be20986489709aa9e76ce8
aarshkshah1992 <aarshkshah1992@gmail.com>
2014-06-11 07:27:10 -0400

    Change in copyright.

--- xapian-core/weight/bb2weight.cc
+++ xapian-core/weight/bb2weight.cc
@@ -1,7 +1,7 @@
 /** @file bb2weight.cc
  * @brief Xapian::BB2Weight class - the BB2 weighting scheme of the DFR framework.
  */
-/* Copyright (C) 2013 Aarsh Shah
+/* Copyright (C) 2013, 2014 Aarsh Shah
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
