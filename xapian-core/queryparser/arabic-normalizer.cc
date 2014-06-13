419027a52efec58eb3aedbc421c5a3ac89d690e9
Assem Chelli <assem.ch@gmail.com>
2014-06-13 03:41:54 +0100

    Fix the normalization of unshaped letters
    - I updated it since I forget to update it when I moved from using
    unsigned vectors to using string with utf8iterator to iterate them
    
    - It should be done for all letters before applying other actions so I
    moved it alone not with the "if else if" chain

--- xapian-core/queryparser/arabic-normalizer.cc
+++ xapian-core/queryparser/arabic-normalizer.cc
@@ -15,14 +15,15 @@ std::string ArabicNormalizer::normalize(const std::string word) {
     Xapian::Utf8Iterator i(word);
     for(; i != Xapian::Utf8Iterator(); ++i) { // TODO order them by proba of happening
         unsigned ch = *i;
+        // convert letters to their unshaped form
+        if (normalize_shaping && ARABIC_SHAPING_MAP.find(ch)!= ARABIC_SHAPING_MAP.end()) {  // TODO dont repeat the find operation
+            ch = ARABIC_SHAPING_MAP[ch];
+        }
+
         // normalize LAM ALEF forms
         if (normalize_shaping && find(ARABIC_LIGUATURES, ARABIC_LIGUATURES + (sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)), ch) != ARABIC_LIGUATURES + sizeof(ARABIC_LIGUATURES)/sizeof(unsigned)) {
             Xapian::Unicode::append_utf8(new_word, ARABIC_LAM);
             Xapian::Unicode::append_utf8(new_word, ARABIC_ALEF);
-        }
-        // normalize shaped letters
-        else if (normalize_shaping && ARABIC_SHAPING_MAP.find(ch)!= ARABIC_SHAPING_MAP.end()) {  // TODO dont repeat the find operation
-            new_word.push_back(ARABIC_SHAPING_MAP.find(ch)->second);
         }
         // strip diacritics
         else if (normalize_diacritics && (find(ARABIC_TASHKEEL, ARABIC_TASHKEEL + (sizeof(ARABIC_TASHKEEL)/sizeof(unsigned)), ch) != ARABIC_TASHKEEL + sizeof(ARABIC_TASHKEEL)/sizeof(unsigned))  && normalize_diacritics ) {
