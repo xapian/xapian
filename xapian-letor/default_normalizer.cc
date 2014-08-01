69dc5eb7444d333d3c3c1fe6f8aaa90ccca145e4
VcamX <vcamx3@gmail.com>
2014-08-01 15:15:41 +0800

    Add copyright information for default_normalizer.
    Also clean up code.

--- xapian-letor/default_normalizer.cc
+++ xapian-letor/default_normalizer.cc
@@ -1,5 +1,21 @@
-/* default_normalizer.cc: The default Normalizer -- used to normalize RankList.
+/* default_normalizer.cc: The default normalizer.
  *
+ * Copyright (C) 2014 Jiarong Wei
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110=1301
+ * USA
  */
 
 #include <xapian.h>
@@ -15,6 +31,7 @@
 using namespace Xapian;
 using std::string;
 using std::vector;
+
 
 static RankList
 DefaultNormalizer::normalize(RankList rlist_) {
@@ -51,5 +68,3 @@ DefaultNormalizer::normalize(RankList rlist_) {
 
 	return rlist;
 }
-
-#endif /* DEFAULT_NORMALIZER_H */
