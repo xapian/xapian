90b9a1078c915231ac237bc17f9bdc6de89c040d
VcamX <vcamx3@gmail.com>
2014-08-11 18:46:31 +0800

    Update Makefile for compiling.

--- xapian-letor/bin/Makefile.mk
+++ xapian-letor/bin/Makefile.mk
@@ -1,29 +1,51 @@
 bin_PROGRAMS +=\
-	bin/questletor\
-	bin/xapian-letor-update
+	bin/letor-request\
+	bin/letor-prepare\
+	bin/letor-train\
+	bin/letor-select
 
-bin_questletor_SOURCES =\
-	bin/questletor.cc\
+bin_letor_request_SOURCES =\
+	bin/letor-request.cc\
 	common/getopt.cc\
 	common/gnu_getopt.h
-bin_questletor_LDADD = libxapianletor.la
+bin_letor_request_LDADD = libxapianletor.la
 
-bin_xapian_letor_update_SOURCES =\
-	bin/xapian-letor-update.cc\
+bin_letor_prepare_SOURCES =\
+	bin/letor-prepare.cc\
 	common/getopt.cc\
 	common/gnu_getopt.h
-bin_xapian_letor_update_LDADD = $(XAPIAN_LIBS)
+bin_letor_prepare_LDADD = libxapianletor.la
+
+bin_letor_train_SOURCES =\
+	bin/letor-train.cc\
+	common/getopt.cc\
+	common/gnu_getopt.h
+bin_letor_train_LDADD = libxapianletor.la
+
+bin_letor_select_SOURCES =\
+	bin/letor-select.cc\
+	common/getopt.cc\
+	common/gnu_getopt.h
+bin_letor_select_LDADD = libxapianletor.la
 
 if !MAINTAINER_NO_DOCS
 dist_man_MANS +=\
-	bin/questletor.1\
-	bin/xapian-letor-update.1
+	bin/letor-request.1\
+	bin/letor-prepare.1\
+	bin/letor-train.1\
+	bin/letor-select.1
 endif
 
 if DOCUMENTATION_RULES
-bin/questletor.1: bin/questletor$(EXEEXT) makemanpage
-	./makemanpage bin/questletor $(srcdir)/bin/questletor.cc bin/questletor.1
+bin/letor-request.1: bin/letor-request$(EXEEXT) makemanpage
+	./makemanpage bin/letor-request$(srcdir)/bin/letor-request.cc bin/letor-request.1
 
-bin/xapian-letor-update.1: bin/xapian-letor-update$(EXEEXT) makemanpage
-	./makemanpage bin/xapian-letor-update $(srcdir)/bin/xapian-letor-update.cc bin/xapian-letor-update.1
+bin/letor-prepare.1: bin/letor-prepare$(EXEEXT) makemanpage
+	./makemanpage bin/letor-prepare$(srcdir)/bin/letor-prepare.cc bin/letor-prepare.1
+
+bin/letor-train.1: bin/letor-train$(EXEEXT) makemanpage
+	./makemanpage bin/letor-train$(srcdir)/bin/letor-train.cc bin/letor-train.1
+
+bin/letor-select.1: bin/letor-select$(EXEEXT) makemanpage
+	./makemanpage bin/letor-select$(srcdir)/bin/letor-select.cc bin/letor-select.1
 endif
