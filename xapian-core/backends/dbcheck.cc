3ebf25887a3d7314ff337ca01b58d438fa5f10bf
Olly Betts <olly@survex.com>
2014-08-29 21:06:49 +0000

    backends/dbcheck.cc: Fix checking a single brass table.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@18214 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/dbcheck.cc
+++ xapian-core/backends/dbcheck.cc
@@ -256,7 +256,7 @@ Database::check(const string & path, int opts, std::ostream *out)
 	    version_file.read();
 	    // Set the last docid to its maximum value to suppress errors.
 	    Xapian::docid db_last_docid = static_cast<Xapian::docid>(-1);
-	    errors = check_brass_table(tablename.c_str(), filename,
+	    errors = check_brass_table(tablename.c_str(), dir,
 				       version_file, opts,
 				       doclens, db_last_docid, out);
 #endif
