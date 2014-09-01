2ded0efda9d8e2e6c916c66be6fbbbae9d667bfe
Olly Betts <olly@survex.com>
2014-09-01 19:01:06 +0000

    backends/brass/: Track doccount in the database stats.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@18219 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/backends/brass/brass_dbcheck.cc
+++ xapian-core/backends/brass/brass_dbcheck.cc
@@ -112,6 +112,7 @@ check_brass_table(const char * tablename, const string &db_dir,
 		cursor->read_tag();
 		// Check format of the METAINFO key.
 		totlen_t total_doclen;
+		Xapian::doccount doccount;
 		Xapian::docid last_docid;
 		Xapian::termcount doclen_lbound;
 		Xapian::termcount doclen_ubound;
@@ -119,7 +120,11 @@ check_brass_table(const char * tablename, const string &db_dir,
 
 		const char * data = cursor->current_tag.data();
 		const char * end = data + cursor->current_tag.size();
-		if (!unpack_uint(&data, end, &last_docid)) {
+		if (!unpack_uint(&data, end, &doccount)) {
+		    if (out)
+			*out << "Tag containing meta information is corrupt (couldn't read doccount)." << endl;
+		    ++errors;
+		} else if (!unpack_uint(&data, end, &last_docid)) {
 		    if (out)
 			*out << "Tag containing meta information is corrupt (couldn't read last_docid)." << endl;
 		    ++errors;
