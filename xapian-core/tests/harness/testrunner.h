4a175ed2fbb9499791758a314fddf04aecd2d446
Olly Betts <olly@survex.com>
2014-09-01 22:04:10 +0000

    tests/harness/testrunner.h: Drop "flint" flag - flint was removed from trunk some time ago.
    git-svn-id: file:///home/xapian-svn/xapian/trunk@18224 6b2f1b92-a4f3-0310-943f-de3cbe41748b

--- xapian-core/tests/harness/testrunner.h
+++ xapian-core/tests/harness/testrunner.h
@@ -2,7 +2,7 @@
  * @brief Run multiple tests for different backends.
  */
 /* Copyright 2008 Lemur Consulting Ltd
- * Copyright 2008,2009 Olly Betts
+ * Copyright 2008,2009,2014 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -110,9 +110,6 @@ class TestRunner {
 
     /// True if the backend is the chert backend.
     bool chert;
-
-    /// True if the backend is the flint backend.
-    bool flint;
 
     /// Virtual destructor - needed for abstract class.
     virtual ~TestRunner();
