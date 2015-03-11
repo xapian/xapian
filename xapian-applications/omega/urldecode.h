b5935ac7e56e6423caa4625493da3777c7d3ac1b
Olly Betts <olly@survex.com>
2015-03-12 00:13:25 +1300

    urldecode.h,urlenctest.cc: $prettyurl now decodes valid UTF-8 sequences.  Fixes #550 and #644, reported by catkin and terencz.
    (cherry picked from commit 6f34d49778aec8da52dd173c0e88d6d54b8aa51a)

--- xapian-applications/omega/urldecode.h
+++ xapian-applications/omega/urldecode.h
@@ -194,9 +194,100 @@ operator!=(const StdinItor& a, const StdinItor& b)
 // This prettifying is aimed at URLs produced by omindex, so we don't currently
 // try to decode the query or fragment parts of the URL at all.  We can probably
 // safely decode the query in a similar way, but also leaving &+= alone.
-#define URL_PRESERVE "?#" "%" "/"
 
-#define URL_PRESERVE_BEFORE_PATH ":[]@"
+enum {
+    // Always unsafe.
+    UNSAFE,
+    // Always safe.
+    OK,
+    // Always safe (and 8, 9, a, b, A or B).
+    OK89AB,
+    // Safe after a '/'.
+    INPATH,
+    // Start of a 2 byte UTF-8 sequence.
+    SEQ2,
+    // Start of a 3 byte UTF-8 sequence.
+    SEQ3,
+    // Start of a 4 byte UTF-8 sequence.
+    SEQ4
+};
+
+static const char url_chars[256] = {
+    // 0x00-0x07
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x08-0x0f
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x10-0x17
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x18-0x1f
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // ' '	!	"	#	$	%	&	'
+    OK,		OK,	OK,	UNSAFE,	OK,	UNSAFE,	OK,	OK,
+    // (	)	*	+	,	-	.	/
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	UNSAFE,
+    // 0	1	2	3	4	5	6	7
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	OK,
+    // 8	9	:	;	<	=	>	?
+    OK89AB,	OK89AB,	INPATH,	OK,	OK,	OK,	OK,	UNSAFE,
+    // @	A	B	C	D	E	F	G
+    INPATH,	OK89AB,	OK89AB,	OK,	OK,	OK,	OK,	OK,
+    // H	I	J	K	L	M	N	O
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	OK,
+    // P	Q	R	S	T	U	V	W
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	OK,
+    // X	Y	Z	[	\	]	^	_
+    OK,		OK,	OK,	INPATH,	OK,	INPATH,	OK,	OK,
+    // `	a	b	c	d	e	f	g
+    OK,		OK89AB,	OK89AB,	OK,	OK,	OK,	OK,	OK,
+    // h	i	j	k	l	m	n	o
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	OK,
+    // p	q	r	s	t	u	v	w
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	OK,
+    // x	y	z	{	|	}	~	0x7f
+    OK,		OK,	OK,	OK,	OK,	OK,	OK,	UNSAFE,
+    // 0x80	0x81	0x82	0x83	0x84	0x85	0x86	0x87
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x88	0x89	0x8a	0x8b	0x8c	0x8d	0x8e	0x8f
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x90	0x91	0x92	0x93	0x94	0x95	0x96	0x97
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0x98	0x99	0x9a	0x9b	0x9c	0x9d	0x9e	0x9f
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xa0	0xa1	0xa2	0xa3	0xa4	0xa5	0xa6	0xa7
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xa8	0xa9	0xaa	0xab	0xac	0xad	0xae	0xaf
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xb0	0xb1	0xb2	0xb3	0xb4	0xb5	0xb6	0xb7
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xb8	0xb9	0xba	0xbb	0xbc	0xbd	0xbe	0xbf
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xc0	0xc1	0xc2	0xc3	0xc4	0xc5	0xc6	0xc7
+    UNSAFE,	UNSAFE,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,
+    // 0xc8	0xc9	0xca	0xcb	0xcc	0xcd	0xce	0xcf
+    SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,
+    // 0xd0	0xd1	0xd2	0xd3	0xd4	0xd5	0xd6	0xd7
+    SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,
+    // 0xd8	0xd9	0xda	0xdb	0xdc	0xdd	0xde	0xdf
+    SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,	SEQ2,
+    // 0xe0	0xe1	0xe2	0xe3	0xe4	0xe5	0xe6	0xe7
+    SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,
+    // 0xe8	0xe9	0xea	0xeb	0xec	0xed	0xee	0xef
+    SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,	SEQ3,
+    // 0xf0	0xf1	0xf2	0xf3	0xf4	0xf5	0xf6	0xf7
+    SEQ4,	SEQ4,	SEQ4,	SEQ4,	SEQ4,	UNSAFE,	UNSAFE,	UNSAFE,
+    // 0xf8	0xf9	0xfa	0xfb	0xfc	0xfd	0xfe	0xff
+    UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE,	UNSAFE
+};
+
+// Test if the 3 characters of s from offset i are '%', one of [89abAB]
+// and a hex digit.
+inline bool
+encoded_ucont(const std::string & s, size_t i)
+{
+    return s[i] == '%' &&
+	url_chars[static_cast<unsigned char>(s[i + 1])] == OK89AB &&
+	isxdigit(static_cast<unsigned char>(s[i + 2]));
+}
 
 /** Prettify a URL.
  *
@@ -228,39 +319,95 @@ url_prettify(std::string & url)
     url.reserve(in.size());
     while (true) {
 	// We've checked there are at least two bytes after the '%' already.
-	if (isxdigit(in[pcent + 1]) && isxdigit(in[pcent + 2])) {
+	if (isxdigit(static_cast<unsigned char>(in[pcent + 1])) &&
+	    isxdigit(static_cast<unsigned char>(in[pcent + 2]))) {
 	    int ch = (hex_decode_(in[pcent + 1]) << 4);
 	    ch |= hex_decode_(in[pcent + 2]);
-	    // FIXME: It would be nice to unescape top bit set bytes, at least
-	    // when they form valid UTF-8 sequences.
-	    bool safe;
-	    if (ch < 0x20 || ch >= 0x7f || std::strchr(URL_PRESERVE, ch)) {
-		// Not safe to decode.
-		safe = false;
-	    } else if (std::strchr(URL_PRESERVE_BEFORE_PATH, ch)) {
-		// ':' is safe to decode if there is a single '/' earlier in
-		// the URL.
-		if (slash == std::string::npos) {
-		    // Lazily set slash to the position of the first single '/'.
-		    const char * d = in.data();
-		    const void * s = d;
-		    slash = 0;
-		    while (true) {
-			s = std::memchr(d + slash, '/', pretty_limit - slash);
-			if (s == NULL) {
-			    slash = in.size();
-			    break;
-			}
-			slash = reinterpret_cast<const char *>(s) - d;
-			if (slash == in.size() - 1 || d[slash + 1] != '/')
-			    break;
-			++slash;
-			while (++slash < in.size() - 1 && d[slash] == '/') { }
+	    bool safe = true;
+	    switch (url_chars[ch]) {
+		case UNSAFE:
+		    safe = false;
+		    break;
+		case SEQ2:
+		    if (in.size() - (pcent + 2) < 3 ||
+			!encoded_ucont(in, pcent + 3)) {
+			safe = false;
+			break;
 		    }
-		}
-		safe = (pcent > slash);
-	    } else {
-		safe = true;
+		    url.append(in, start, pcent - start);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    start = pcent;
+		    break;
+		case SEQ3:
+		    if (in.size() - (pcent + 2) < 3 * 2 ||
+			!encoded_ucont(in, pcent + 3) ||
+			!encoded_ucont(in, pcent + 6) ||
+			(ch == 0xe0 && in[pcent + 4] <= '9')) {
+			safe = false;
+			break;
+		    }
+		    url.append(in, start, pcent - start);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    start = pcent;
+		    break;
+		case SEQ4:
+		    if (in.size() - (pcent + 2) < 3 * 3 ||
+			!encoded_ucont(in, pcent + 3) ||
+			!encoded_ucont(in, pcent + 6) ||
+			!encoded_ucont(in, pcent + 9) ||
+			(ch == 0xf0 && in[pcent + 4] == '8') ||
+			(ch == 0xf4 && in[pcent + 4] >= '9')) {
+			safe = false;
+			break;
+		    }
+		    url.append(in, start, pcent - start);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    url += char(ch);
+		    pcent += 3;
+		    ch = (hex_decode_(in[pcent + 1]) << 4);
+		    ch |= hex_decode_(in[pcent + 2]);
+		    start = pcent;
+		    break;
+		case INPATH:
+		    // ':' is safe to decode if there is a single '/' earlier in
+		    // the URL.
+		    if (slash == std::string::npos) {
+			// Lazily set slash to the position of the first single '/'.
+			const char * d = in.data();
+			const void * s = d;
+			slash = 0;
+			while (true) {
+			    s = std::memchr(d + slash, '/', pretty_limit - slash);
+			    if (s == NULL) {
+				slash = in.size();
+				break;
+			    }
+			    slash = reinterpret_cast<const char *>(s) - d;
+			    if (slash == in.size() - 1 || d[slash + 1] != '/')
+				break;
+			    ++slash;
+			    while (++slash < in.size() - 1 && d[slash] == '/') { }
+			}
+		    }
+		    safe = (pcent > slash);
+		    break;
 	    }
 
 	    if (safe) {
