1a26a2fcd4f25ad66900c7fc032870f71dc9405f
Olly Betts <olly@survex.com>
2015-05-29 19:48:26 +1200

    tests/api_wrdb.cc: Revert disabling of part of adddoc5 for clang, as that should now work now the method in question isn't marked as XAPIAN_PURE_FUNCTION.

--- xapian-core/tests/api_wrdb.cc
+++ xapian-core/tests/api_wrdb.cc
@@ -426,13 +426,8 @@ DEFINE_TESTCASE(adddoc5, writable) {
 		TEST_EQUAL(i.get_wdf(), j.get_wdf());
 		// Actually use termfreq to stop compiler optimising away the
 		// call to get_termfreq().
-#ifndef __clang__
-		// clang's exception handling is buggy, and this exception
-		// leaks through catches in the test harness.  Confirmed as a
-		// bug by a clang developer, but it seems no fix is in sight.
 		TEST_EXCEPTION(Xapian::InvalidOperationError,
 			       if (i.get_termfreq()) return false);
-#endif
 		TEST_NOT_EQUAL(0, j.get_termfreq());
 		if (*i == "foobar") {
 		    // termfreq of foobar is 2
