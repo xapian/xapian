35a8b5261da8e9164b77f845a3f9106578a4a4f4
Olly Betts <olly@survex.com>
2015-09-17 11:42:49 +1200

    Makefile.am,datetime.cc,datetime.h,metaxmlparse.cc,myhtmlparse.cc: Factor out parse_datetime() function.

--- xapian-applications/omega/myhtmlparse.cc
+++ xapian-applications/omega/myhtmlparse.cc
@@ -23,16 +23,14 @@
 
 #include "myhtmlparse.h"
 
+#include "datetime.h"
 #include "keyword.h"
 #include "my-html-tok.h"
 #include "stringutils.h"
 #include "utf8convert.h"
 
 #include <cctype>
-#include <cstdlib>
 #include <cstring>
-#include <time.h>
-#include "timegm.h"
 
 using namespace std;
 
@@ -44,83 +42,6 @@ lowercase_string(string &str)
     for (string::iterator i = str.begin(); i != str.end(); ++i) {
 	*i = C_tolower(*i);
     }
-}
-
-static time_t
-parse_datetime(const string & s)
-{
-    struct tm t;
-    const char * p = s.c_str();
-    char * q;
-    if (s.find('T') != string::npos || s.find('-') != string::npos) {
-	// E.g. "2013-01-17T09:10:55Z"
-	t.tm_year = strtoul(p, &q, 10) - 1900;
-	p = q;
-	if (*p == '-') {
-	    t.tm_mon = strtoul(p + 1, &q, 10) - 1;
-	    p = q;
-	} else {
-	    t.tm_mon = 0;
-	}
-	if (*p == '-') {
-	    t.tm_mday = strtoul(p + 1, &q, 10);
-	    p = q;
-	} else {
-	    t.tm_mday = 1;
-	}
-	if (*p == 'T') {
-	    t.tm_hour = strtoul(p + 1, &q, 10);
-	    p = q;
-	    if (*p == ':') {
-		t.tm_min = strtoul(p + 1, &q, 10);
-		p = q;
-	    } else {
-		t.tm_min = 0;
-	    }
-	    if (*p == ':') {
-		t.tm_sec = strtoul(p + 1, &q, 10);
-		p = q;
-	    } else {
-		t.tm_sec = 0;
-	    }
-	} else {
-	    t.tm_hour = t.tm_min = t.tm_sec = 0;
-	}
-	if (*p == 'Z') {
-	    // FIXME: always assume UTC for now...
-	}
-    } else {
-	// As produced by LibreOffice HTML export.
-	// E.g.
-	// "20130117;09105500" == 2013-01-17T09:10:55
-	// "20070903;200000000000" == 2007-09-03T00:02:00
-	// "20070831;5100000000000" == 2007-08-31T00:51:00
-	unsigned long v = strtoul(p, &q, 10);
-	if (v == 0) {
-	    // LibreOffice sometimes exports "0;0".  A date of "0" is
-	    // clearly invalid.
-	    return time_t(-1);
-	}
-	p = q;
-	t.tm_mday = v % 100;
-	v /= 100;
-	t.tm_mon = v % 100 - 1;
-	t.tm_year = v / 100 - 1900;
-	if (*p == ';') {
-	    ++p;
-	    v = strtoul(p, &q, 10);
-	    v /= (q - p > 10) ? 1000000000 : 100;
-	    t.tm_sec = v % 100;
-	    v /= 100;
-	    t.tm_min = v % 100;
-	    t.tm_hour = v / 100;
-	} else {
-	    t.tm_hour = t.tm_min = t.tm_sec = 0;
-	}
-    }
-    t.tm_isdst = -1;
-
-    return timegm(&t);
 }
 
 void
