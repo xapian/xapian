cbcbd8d3adf35e35a437bd9847186e322df6473c
Daniel Kopecek <dnk1618@gmail.com>
2015-09-30 10:48:57 +0200

    Test chert and glass backends in api_encryption test

--- xapian-core/tests/api_encryption.cc
+++ xapian-core/tests/api_encryption.cc
@@ -51,11 +51,94 @@ using namespace std;
 const std::string test_encryption_key = "pQHQNZrTghIryAukoxkavLQupfDqZTKnKqFceofiKlNJaCkBsCynRWxQnJlVjGBF";
 const std::string test_encryption_cipher = "Serpent/XTS";
 
-DEFINE_TESTCASE(encrypted_db, chert && !remote && writable) {
+DEFINE_TESTCASE(encrypted_db_chert, chert && !remote && writable) {
   SKIP_TEST_FOR_BACKEND("inmemory");
 
-  Xapian::WritableDatabase db(".testdb_encrypted",
+  Xapian::WritableDatabase db(".chert_encrypted",
       Xapian::DB_CREATE_OR_OVERWRITE|Xapian::DB_BACKEND_CHERT, 8192,
+      &test_encryption_key, test_encryption_cipher);
+
+  // Read-only tests:
+  {
+    TEST_EQUAL(db.get_doccount(), 0);
+    Xapian::Enquire enquire(db);
+    enquire.set_query(Xapian::Query("word"));
+    Xapian::MSet mset = enquire.get_mset(0, 10);
+    TEST(mset.empty());
+  }
+  {
+    TEST_EQUAL(db.get_doccount(), 0);
+    Xapian::Enquire enquire(db);
+    enquire.set_query(Xapian::Query("word"));
+    Xapian::MSet mset = enquire.get_mset(0, 10);
+    TEST(mset.empty());
+  }
+
+  // Writable tests:
+  {
+    Xapian::WritableDatabase database(db);
+    Xapian::docid did;
+    Xapian::Document document_in;
+    document_in.set_data("Foobar rising");
+    document_in.add_value(7, "Value7");
+    document_in.add_value(13, "Value13");
+    document_in.add_posting("foo", 1);
+    document_in.add_posting("bar", 2);
+
+    {
+      did = database.add_document(document_in);
+      TEST_EQUAL(did, 1);
+      TEST_EQUAL(database.get_doccount(), 1);
+      TEST_EQUAL(database.get_avlength(), 2);
+    }
+
+    {
+      document_in.remove_term("foo");
+      document_in.add_posting("baz", 1);
+
+      database.replace_document(1, document_in);
+
+      database.delete_document(1);
+
+      TEST_EQUAL(database.get_doccount(), 0);
+      TEST_EQUAL(database.get_avlength(), 0);
+      TEST_EQUAL(database.get_termfreq("foo"), 0);
+      TEST_EQUAL(database.get_collection_freq("foo"), 0);
+      TEST_EQUAL(database.get_termfreq("bar"), 0);
+      TEST_EQUAL(database.get_collection_freq("bar"), 0);
+      TEST_EQUAL(database.get_termfreq("baz"), 0);
+      TEST_EQUAL(database.get_collection_freq("baz"), 0);
+    }
+  }
+
+  {
+    TEST_EQUAL(db.get_doccount(), 0);
+    db.add_document(Xapian::Document());
+    TEST_EQUAL(db.get_doccount(), 1);
+    db.add_document(Xapian::Document());
+    TEST_EQUAL(db.get_doccount(), 2);
+  }
+
+  {
+    for (Xapian::doccount i = 0; i < 2100; ++i) {
+      Xapian::Document doc;
+      for (Xapian::termcount t = 0; t < 100; ++t) {
+        string term("foo");
+        term += char(t ^ 70 ^ i);
+        doc.add_posting(term, t);
+      }
+      db.add_document(doc);
+    }
+    TEST(db.term_exists("foo1"));
+  }
+  return true;
+}
+
+DEFINE_TESTCASE(encrypted_db_glass, glass && !remote && writable) {
+  SKIP_TEST_FOR_BACKEND("inmemory");
+
+  Xapian::WritableDatabase db(".glass_encrypted",
+      Xapian::DB_CREATE_OR_OVERWRITE|Xapian::DB_BACKEND_GLASS, 8192,
       &test_encryption_key, test_encryption_cipher);
 
   // Read-only tests:
