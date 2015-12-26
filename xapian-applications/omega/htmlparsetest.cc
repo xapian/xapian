99164f4678c910c3197885a73f327fb6b7c58bef
Olly Betts <olly@survex.com>
2015-12-27 11:02:33 +1300

    Add test coverage for parsing of HTML entities

--- xapian-applications/omega/htmlparsetest.cc
+++ xapian-applications/omega/htmlparsetest.cc
@@ -1,6 +1,6 @@
 /* htmlparsetest.cc: test the MyHtmlParser class
  *
- * Copyright (C) 2006,2008,2011,2012,2013 Olly Betts
+ * Copyright (C) 2006,2008,2011,2012,2013,2015 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -60,6 +60,10 @@ static const testcase tests[] = {
     { "a<html>b<head>c<title>bad</title>d</head>e<body>f</body>g<body>h</body>i</html>j<body>k", "abcdefghijk", "bad", "", "" },
     { "check<object id='foo'>for<applet foo=\"bar\" />spaces<br> in <p>\tout</p>put\r\n", "check for spaces\rin\rout\rput", "", "", "" },
     { "tab:<table><tr><th>col 1</th><th>col 2</th></tr><tr><td>test</td><td><img src='foo.jpg'> <img src='bar.jpg'></td></tr><tr><td colspan=2>hello world</td></tr></table>done", "tab:\rcol 1\tcol 2\rtest\rhello world\rdone", "", "", "" },
+    // Test entities.
+    { "<html><body>1 &lt; 2, 3 &gt; 2</body></html>", "1 < 2, 3 > 2", "", "", "" },
+    { "<html><body>&amp;amp;</body></html>", "&amp;", "", "", "" },
+    { "&#65;&#x40;&gt", "A@>", "", "", "" },
     { 0, 0, 0, 0, 0 }
 };
 
