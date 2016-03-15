d50e9a17546dfc77af0b33626e5278d5cb4e5a01
Olly Betts <olly@survex.com>
2016-03-16 12:45:53 +1300

    Make Literal class constructors explicit

--- xapian-core/common/pretty.h
+++ xapian-core/common/pretty.h
@@ -1,7 +1,7 @@
 /** @file pretty.h
  * @brief Convert types to pretty representations
  */
-/* Copyright (C) 2010,2011,2012,2014 Olly Betts
+/* Copyright (C) 2010,2011,2012,2014,2016 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -44,8 +44,8 @@ struct PrettyOStream {
 
 struct Literal {
     const char * _lit;
-    Literal(const char * lit) : _lit(lit) { }
-    Literal(const std::string & s) : _lit(s.c_str()) { }
+    explicit Literal(const char * lit) : _lit(lit) { }
+    explicit Literal(const std::string & s) : _lit(s.c_str()) { }
 };
 
 /// Default is to output as std::ostream would.
