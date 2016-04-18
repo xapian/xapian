a6e55b9c3aef92a63a067d4f7ff4dbea9645c53b
Ayush <ayushtomar@gmail.com>
2016-04-18 15:35:16 -0400

    Update documentation for public API headers

--- xapian-letor/include/xapian-letor/featuremanager.h
+++ xapian-letor/include/xapian-letor/featuremanager.h
@@ -1,6 +1,7 @@
-/* featuremanager.h: The feature manager file.
- *
- * Copyright (C) 2012 Parth Gupta
+/** @file featuremanager.h
+ *  @brief The feature manager file
+ */
+/* Copyright (C) 2012 Parth Gupta
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -44,38 +45,60 @@ class FeatureVector;
 class XAPIAN_VISIBILITY_DEFAULT FeatureManager {
 
   public:
-
+    /// @private @internal Class representing the FeatureManager internals.
     class Internal;
-
+    /// @private @internal Reference counted internals.
     Xapian::Internal::intrusive_ptr<Internal> internal;
 
+    /// Default constructor
     FeatureManager();
 
+    /// Copy constructor
     FeatureManager(const FeatureManager & o);
 
+    /// Assignment
     FeatureManager & operator=(const FeatureManager & o);
 
+    /// Destructor
     ~FeatureManager();
 
+    /** Calculates features array for a each document paired with a query
+     *  @return features array of length 19 in <int, double> format
+     */
     std::map<int,double> transform(const Document &doc, double &weight_);
 
+    /** Calculates ranklist (0 qid:10032 1:0.130742 2:0.000000 ... 18:0.750000 19:1.000000 #docid = 1123323)
+     *  for an mset retrieved from a query. Feature 19 is the BM25 weight.
+     */
     Xapian::RankList create_rank_list(const Xapian::MSet & mset,std::string & qid);
 
-    map<string, map<string,int> > load_relevance(const std::string & qrel_file);    
-    
+    /** Loads up a qrel file.
+     *  @return a map: < qid <docid, relevance_judgement> >
+     */
+    map<string, map<string,int> > load_relevance(const std::string & qrel_file);
+
+    /** Creates instance of FeatureVector class that stores features_array, relevance label and corresponding document id
+     *  @return a FeatureVector instance containing features_array, relevance label and corresponding document id
+     */
     Xapian::FeatureVector create_feature_vector(map<int,double> fvals, int &label, std::string & did);
-    
+
+    /** Creates instance of FeatureVector class that stores features_array, relevance label and corresponding document id
+     *  @return a FeatureVector instance containing features_array, relevance label and corresponding document id
+     */
     std::string getdid(const Document &doc);
-    
+
+    /** Get relvance label corresponding to a document (docid) and query (qid) in qrel
+     *  @return Relavance label corresponding to docid and qid in qrel
+     */
     int getlabel(map<string, map<string, int> > qrel, const Document &doc, std::string & qid);
 
     static const int fNum = 20;
 
-    // accessors
+    /// Specify the database to use for retrieval. This database will be used directly by internal class.
     void set_database(const Database &db);
-    // const Database &get_database();
+
+    /// Specify the query. This will be used by the internal class.
     void set_query(const Query &query);
-    // const Query &get_query();
 
 };
 
--- xapian-letor/include/xapian-letor/letor_features.h
+++ xapian-letor/include/xapian-letor/letor_features.h
@@ -1,6 +1,7 @@
-/* letor_features.h: The feature manager file.
- *
- * Copyright (C) 2012 Parth Gupta
+/** @file letor_features.h
+ *  @brief The feature manager file for Learning to Rank
+ */
+ /* Copyright (C) 2012 Parth Gupta
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -34,15 +35,83 @@ namespace Xapian {
 class XAPIAN_VISIBILITY_DEFAULT Features {
  public:
 
+    /** This method finds the frequency of the query terms in the specified documents. This method is a helping method and statistics gathered through
+     *  this method are used in feature value calculation. It return the frequency of the terms of query in std::map<string, long int> form.
+     *
+     *  @param  doc     This is the document in which we want the frequency of the terms in query. Generally in Letor, this document is taken
+     *          from the Xapian::MSet retrieved from the first retrieval.
+     *  @param  query   This is the query for which terms we want frequency in the document.
+     */
     map<string,long int> termfreq(const Xapian::Document & doc,const Xapian::Query & query);
 
+    /** This method calculates the inverse document frequency(idf) of query terms in the database. It returns the idf of each term in
+     *  std::map<string, double> form.
+     *
+     *  Note: idf of a term 't' is calculated as below:
+     *
+     *  idf(t) = log(N/df(t))
+     *                                  Where,
+     *                                  N = Total number of documents in database and
+     *                                  df(t) = number of documents containing term 't'
+     *
+     *  @param  db      specify the database being used for search to calculate idf values.
+     *  @param  query   query being used in the retrieval
+     */
     map<string,double> inverse_doc_freq(const Xapian::Database & db, const Xapian::Query & query);
 
+    /** This method calculates the length of the documents as number of 'terms'. It calculates the length for three different
+     *  parts: title, body and whole document. This information is returned in the std::map<string, long int> format.
+     *  It can be accessed as below:
+     *
+     *  @code
+     *  map<string, long int> len;
+     *  len["title"];
+     *  len["body"];
+     *  len["whole"];
+     *  @endcode
+     *
+     *  @param  db      Database containing that document.
+     *  @param  doc     The document whose length is to be found.
+     */
     map<string,long int> doc_length(const Xapian::Database & db, const Xapian::Document & doc);
 
+    /** This method calculates the length of the collection in number of terms for different parts like 'title', 'body' and 'whole'. This is calculated
+     *  as a stored user metadata in omindex otherwise it is calculated out of scratch (this might take some time depending upon the size of the
+     *  database. Length information is stored in std::map<string, long int> format and can be accessed as below:
+     *
+     *  @code
+     *  map<string, long int> len;
+     *  len["title"];
+     *  len["body"];
+     *  len["whole"];
+     *  @endcode
+     *
+     *  @param  db      Database used for retrieval.
+     *
+     */
     map<string,long int> collection_length(const Xapian::Database & db);
 
+    /** This method calculates the frequency of query terms in the whole database. The information is stored in std::map<string, long int> format and
+     *  used during the feature calculation methods.
+     *
+     *  @param  db      Database to be used
+     *  @param  query   Query being searched.
+     */
     map<string,long int> collection_termfreq(const Xapian::Database & db, const Xapian::Query & query);
+
+
+    /** It calculated the feature value for the query-document pair. These feature calculation methods uses the data generated using above defined
+     *  methods like termfreq, inverse_doc_freq, doc_length, coll_freq and collection_length.
+     *
+     *  These features are depicted in overview documentation of Letor.
+     *
+     *  @param  query   Query being used.
+     *  @param  tf      Map generated using Letor::termfreq() method.
+     *  @param  ch      Specifies the part of document for which the value needs to be calculated. Values:
+     *          't'     Title only
+     *          'b'     Body only
+     *          'w'     Whole document
+     */
 
     double calculate_f1(const Xapian::Query & query, map<string,long int> & tf,char ch);
 
