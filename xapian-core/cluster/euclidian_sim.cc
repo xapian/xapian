6ebc9facb44c8a4dadf8664e2fcabca50dc73dbc
Richhiey <richhiey.thomas@gmail.com>
2016-08-07 23:47:29 +0530

    Change euclidian_sim.cc

--- xapian-core/cluster/euclidian_sim.cc
+++ xapian-core/cluster/euclidian_sim.cc
@@ -40,86 +40,10 @@ EuclidianDistance::get_description() {
     return "Euclidian Distance metric";
 }
 
+/** TODO
+ *  Implement the euclidian distance metric for the current API
+ */
 double
 EuclidianDistance::similarity(Point a, Point b) {
-    Point x, y;
-    x=a;
-    y=b;
     return 0.0;
 }
-/*
-double
-EuclidianDistance::similarity1(TermIterator a_begin,
-			      TermIterator a_end,
-			      TermIterator b_begin,
-			      TermIterator b_end)
-{
-    LOGCALL(API, double, "EuclidianDistance::similarity()", tlg | a_begin | a_end | b_begin | b_end);
-    vector<pair<string, double> > docvec1;
-    vector<pair<string, double> > docvec2;
-    docvec1.clear();
-    docvec2.clear();
-
-    double doccount = tlg.get_doccount();
-    TermIterator titer = a_begin;
-
-    // Creating document vector for first document
-    for (; titer != a_end; titer++) {
-	double tf, wt;
-	tf = 1 + log(titer.get_wdf());
-	double idf;
-	double termfreq = tlg.get_termfreq(*titer);
-	idf = log(doccount/termfreq);
-	wt = tf*idf;
-	docvec1.push_back(make_pair(*titer, wt));
-    }
-
-    titer = b_begin;
-    // Creating document vector for the second document
-    for (; titer != b_end; titer++) {
-	double tf, wt;
-	tf = 1 + log(titer.get_wdf());
-	double idf;
-	double termfreq = tlg.get_termfreq(*titer);
-	idf = log(doccount/termfreq);
-	wt = tf*idf;
-	docvec2.push_back(make_pair(*titer, wt));
-    }
-
-    vector<pair<string, double> >::iterator ta;
-    vector<pair<string, double> >::iterator tb;
-    double result = 0;
-
-    // Finding euclidian distance between the above document vectors
-    for (ta = docvec1.begin(); ta != docvec1.end(); ta++) {
-	bool found = false;
-	double sum = 0;
-	for (tb = docvec2.begin(); tb != docvec2.end(); tb++) {
-	    if (tb->first == ta->first) {
-		found = true;
-		break;
-	    }
-	}
-
-	if (found)
-	    sum += (tb->second - ta->second)*(tb->second - ta->second);
-	else
-	    sum += (ta->second)*(ta->second);
-	result += sum;
-    }
-
-    for (ta = docvec2.begin(); ta != docvec2.end(); ta++) {
-	bool found = false;
-	double sum = 0;
-	for (tb = docvec1.begin(); tb != docvec1.end(); tb++) {
-	    if (tb->first == ta->first) {
-		found = true;
-		break;
-	    }
-	}
-	if (!found)
-	    sum += (ta->second)*(ta->second);
-	result += sum;
-    }
-    return result;
-}*/
