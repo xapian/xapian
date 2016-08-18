07e441e51e43ec9571dcf3e445f3c4af42795f07
Olly Betts <olly@survex.com>
2016-08-18 16:21:47 +1200

    PL2,PL2+: Fix calculation of upper_bound when P2>0
    P2 is typically negative, but for a very common term it can be positive
    And then we should use wdfn_lower not wdfn_upper to adjust P_max.
    
    I've been unable to find a simple reproducer for this, so no regression
    test.
    
    (cherry picked from commit dd6b35900358d49ec41a32578a651d4ee2bc0a6f)

--- xapian-core/weight/pl2weight.cc
+++ xapian-core/weight/pl2weight.cc
@@ -2,7 +2,7 @@
  * @brief Xapian::PL2Weight class - the PL2 weighting scheme of the DFR framework.
  */
 /* Copyright (C) 2013 Aarsh Shah
- * Copyright (C) 2013,2014 Olly Betts
+ * Copyright (C) 2013,2014,2016 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -82,8 +82,9 @@ PL2Weight::init(double)
     lower_bound = get_wqf() * P_min / (wdfn_upper + 1.0);
 
     // Calculate the upper bound on the weight.
-    double P_max =
-	P1 + (wdfn_upper + 0.5) * log2(wdfn_upper) - P2 * wdfn_upper;
+    double P_max = P1 + (wdfn_upper + 0.5) * log2(wdfn_upper);
+    // P2 is typically negative, but for a very common term it can be positive.
+    P_max -= P2 * (P2 < 0 ? wdfn_upper : wdfn_lower);
     upper_bound = get_wqf() * P_max / (wdfn_lower + 1.0);
 
     upper_bound -= lower_bound;
