9090b0444bc45125000cd9efb5d8a58e9ebbc892
Olly Betts <olly@survex.com>
2016-08-23 19:45:16 +1200

    Add comment to explain empty catch blocks
    (cherry picked from commit ad5acd7279ff01f7043ce1517a4f765e18e4141d)

--- xapian-core/tests/api_weight.cc
+++ xapian-core/tests/api_weight.cc
@@ -146,7 +146,7 @@ DEFINE_TESTCASE(inl2weight1, !backend) {
 	    FAIL_TEST("Serialised inl2weight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised inl2weight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
 
     return true;
@@ -211,7 +211,7 @@ DEFINE_TESTCASE(ifb2weight1, !backend) {
 	    FAIL_TEST("Serialised IfB2Weight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised IfB2Weight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
     return true;
 }
@@ -274,7 +274,7 @@ DEFINE_TESTCASE(ineb2weight1, !backend) {
 	    FAIL_TEST("Serialised ineb2weight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised ineb2weight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
 
     return true;
@@ -341,7 +341,7 @@ DEFINE_TESTCASE(bb2weight1, !backend) {
 	    FAIL_TEST("Serialised BB2Weight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised BB2Weight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
     return true;
 }
@@ -469,7 +469,7 @@ DEFINE_TESTCASE(pl2weight1, !backend) {
 	    FAIL_TEST("Serialised PL2Weight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised PL2Weight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
     return true;
 }
@@ -565,7 +565,7 @@ DEFINE_TESTCASE(tfidfweight2, !backend) {
 	    FAIL_TEST("Serialised TfIdfWeight with junk appended unserialised to empty name!");
 	FAIL_TEST("Serialised TfIdfWeight with junk appended unserialised OK");
     } catch (const Xapian::SerialisationError &) {
-
+	// Good!
     }
     return true;
 }
