01f93e954bb456ee77bdf0513ff6335742361ffa
Olly Betts <olly@survex.com>
2017-01-11 17:51:15 +1300

    omindex: Don't use meta description as sample by default
    Now we have dynamic snippets (via $snippet), the body text is a better
    default.  Also some HTML generators put unhelpful content in the meta
    description.  To get the previous behaviour, use the new omindex command
    line option: --sample=description

--- xapian-applications/omega/myhtmlparse.h
+++ xapian-applications/omega/myhtmlparse.h
@@ -1,7 +1,7 @@
 /* myhtmlparse.h: subclass of HtmlParser for extracting text
  *
  * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2006,2008,2010,2011,2012,2013,2016 Olly Betts
+ * Copyright 2002,2003,2004,2006,2008,2010,2011,2012,2013,2016,2017 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -39,6 +39,7 @@ class MyHtmlParser : public HtmlParser {
 	bool indexing_allowed;
 	bool ignoring_metarobots;
 	bool charset_from_meta;
+	bool description_as_sample;
 	string title, sample, keywords, dump, author, topic;
 	time_t created;
 	string * target;
@@ -56,6 +57,7 @@ class MyHtmlParser : public HtmlParser {
 		indexing_allowed(true),
 		ignoring_metarobots(false),
 		charset_from_meta(false),
+		description_as_sample(false),
 		created(time_t(-1)),
 		target(&dump) { }
 
@@ -66,6 +68,7 @@ class MyHtmlParser : public HtmlParser {
 	    indexing_allowed = true;
 	    ignoring_metarobots = false;
 	    charset_from_meta = false;
+	    description_as_sample = false;
 	    title.resize(0);
 	    sample.resize(0);
 	    keywords.resize(0);
