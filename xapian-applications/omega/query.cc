15bd5ee748c81d8b61b4583c6232ee66ae53d437
shivanshuiitg <shivanshu5100@gmail.com>
2017-01-23 21:27:41 +0530

    FileSizeRangeProcessor is now in use in Omega instead of NumberRangeProcessor in file xapian/xapian-applications/omega/query.cc FileSizeRangeProcessor gets the size of file as follows:      B - Bytes      K - Kilo bytes      M - Mega bytes      G - Giga bytes Examples of valid range:-    10K..1M    10..20K Examples of invalid range:-    10K..20    10..20    Range containing unit of memory other than B,K,M,G is also invalid. i.e. 10A..20K (A is not valid unit)

--- xapian-applications/omega/query.cc
+++ xapian-applications/omega/query.cc
@@ -112,7 +112,9 @@ static Xapian::Query query;
 Xapian::Query::op default_op = Xapian::Query::OP_AND; // default matching mode
 
 static Xapian::QueryParser qp;
-static Xapian::NumberRangeProcessor * size_rp = NULL;
+//static Xapian::NumberRangeProcessor * size_rp = NULL;
+// Using FileSizeRangeProcessor instead of NumberRangeProcessor
+static Xapian::FileSizeRangeProcessor * size_rp = NULL;
 static Xapian::Stem *stemmer = NULL;
 
 static string eval_file(const string &fmtfile);
@@ -320,8 +322,10 @@ set_probabilistic(const string &oldp)
     qp.set_default_op(default_op);
     qp.set_database(db);
     // FIXME: provide a custom RP which handles size:10..20K, etc.
-    if (!size_rp)
-	size_rp = new Xapian::NumberRangeProcessor(VALUE_SIZE, "size:");
+    if (!size_rp){
+    //size_rp = new Xapian::NumberRangeProcessor(VALUE_SIZE, "size:");
+    size_rp = new Xapian::FileSizeRangeProcessor(VALUE_SIZE, "size:");
+	}
     qp.add_rangeprocessor(size_rp);
     map<string, string>::const_iterator pfx = option.lower_bound("prefix,");
     for (; pfx != option.end() && startswith(pfx->first, "prefix,"); ++pfx) {
