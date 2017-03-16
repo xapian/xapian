d0042e1127539172635fd9e3d7972f2240c083e5
AyushP123 <f2012714@goa.bits-pilani.ac.in>
2017-03-16 22:44:30 +0530

    fixup! Add featurelist_internal

--- xapian-letor/api/Makefile.mk
+++ xapian-letor/api/Makefile.mk
@@ -1,9 +1,11 @@
 noinst_HEADERS +=\
-	api/featurevector_internal.h
+	api/featurevector_internal.h\
+	api/featurelist_internal.h
 
 EXTRA_DIST +=\
 	api/Makefile
 
 lib_src +=\
 	api/featurelist.cc\
-	api/featurevector.cc
+	api/featurevector.cc\
+	ap/featurelist_internal.cc
