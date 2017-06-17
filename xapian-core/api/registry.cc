2248fb881c5a40d23d6b1ca3c10f84f8be442701
ivmarkp <vivekpal.dtu@gmail.com>
2017-06-17 20:31:32 +0530

    fixup! Register weighting schemes with short names as well

--- xapian-core/api/registry.cc
+++ xapian-core/api/registry.cc
@@ -120,15 +120,18 @@ register_object(map<string, T*> & registry1, map<string, T*> & registry2,
 	throw Xapian::InvalidOperationError("Unable to register object - name() method returned empty string");
     }
 
-    string short_name = obj.name();
-    if (rare(short_name.empty())) {
-	throw Xapian::InvalidOperationError("Unable to register object - short_name() method returned empty string");
-    }
-
     pair<typename map<string, T *>::iterator, bool> r1;
     r1 = registry1.insert(make_pair(name, static_cast<T*>(NULL)));
+
     pair<typename map<string, T *>::iterator, bool> r2;
-    r2 = registry2.insert(make_pair(short_name, static_cast<T*>(NULL)));
+    string short_name = obj.short_name();
+    if (!short_name.empty()) {
+	r2 = registry2.insert(make_pair(short_name, static_cast<T*>(NULL)));
+
+	if (r1.second != r2.second || (!r1.second && r2.first->second != r1.first->second)) {
+	    throw Xapian::InvalidOperationError("Unable to register object - weighting scheme with the same name but a different short name already registered");
+	}
+    }
 
     if (!r1.second) {
 	// Existing element with this key, so replace the pointer with NULL
@@ -150,7 +153,10 @@ register_object(map<string, T*> & registry1, map<string, T*> & registry2,
     }
 
     r1.first->second = clone;
-    r2.first->second = clone;
+
+    if (!short_name.empty()) {
+	r2.first->second = clone;
+    }
 }
 
 template<class T>
@@ -329,14 +335,13 @@ const Xapian::Weight *
 Registry::get_weighting_scheme(const string & name) const
 {
     LOGCALL(API, const Xapian::Weight *, "Xapian::Registry::get_weighting_scheme", name);
-    const Xapian::Weight * wt = lookup_object(internal->wtschemes, name);
-    const Xapian::Weight * wt_short = lookup_object(internal->wtschemes_short, name);
     if (!name.empty() && C_islower(name[0])) {
-	if (wt_short == NULL)
-	    RETURN(wt);
-	RETURN(wt_short);
+	const Xapian::Weight * wt_short = lookup_object(internal->wtschemes_short, name);
+	if (wt_short != NULL) {
+	    RETURN(wt_short);
+	}
     }
-    RETURN(wt);
+    RETURN(lookup_object(internal->wtschemes, name));
 }
 
 void
