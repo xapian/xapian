xapian/xapian

commit 3b5b40132fed3b30819dfe85c17cac5e002e83be

Fix incorrect class/method names in debug logging

xapian-core/backends/databasereplicator.cc
@@ -45,7 +45,7 @@ DatabaseReplicator::~DatabaseReplicator()
 DatabaseReplicator *
 DatabaseReplicator::open(const string & path)
 {
-    LOGCALL_STATIC_VOID(DB, "DatabaseReplicator::DatabaseReplicator", path);
+    LOGCALL(DB, DatabaseReplicator *, "DatabaseReplicator::open", path);
 
 #ifdef XAPIAN_HAS_GLASS_BACKEND
     if (file_exists(path + "/iamglass")) {

xapian-core/backends/glass/glass_postlist.cc
@@ -1284,7 +1284,7 @@ void
 GlassPostListTable::get_used_docid_range(Xapian::docid & first,
 					 Xapian::docid & last) const
 {
-    LOGCALL(DB, Xapian::docid, "GlassPostList::get_used_docid_range", "&first, &used");
+    LOGCALL(DB, Xapian::docid, "GlassPostListTable::get_used_docid_range", "&first, &used");
     AutoPtr<GlassCursor> cur(cursor_get());
     if (!cur->find_entry(pack_glass_postlist_key(string()))) {
 	// Empty database.

xapian-letor/ranker/listnet_ranker.cc
@@ -145,7 +145,7 @@ ListNETRanker::train_model(const std::vector<Xapian::FeatureVector> & training_d
 
 void
 ListNETRanker::save_model_to_metadata(const string & model_key) {
-    LOGCALL_VOID(API, "ListNETRanker::save_model_to_file", model_key);
+    LOGCALL_VOID(API, "ListNETRanker::save_model_to_metadata", model_key);
     Xapian::WritableDatabase letor_db(get_database_path());
     string key = model_key;
     if (key.empty()) {
@@ -159,7 +159,7 @@ ListNETRanker::save_model_to_metadata(const string & model_key) {
 
 void
 ListNETRanker::load_model_from_metadata(const string & model_key) {
-    LOGCALL_VOID(API, "ListNETRanker::load_model_from_file", model_key);
+    LOGCALL_VOID(API, "ListNETRanker::load_model_from_metadata", model_key);
     Xapian::Database letor_db(get_database_path());
     string key = model_key;
     if (key.empty()) {

xapian-letor/ranker/svmranker.cc
@@ -151,7 +151,7 @@ SVMRanker::train_model(const std::vector<Xapian::FeatureVector> & training_data)
 void
 SVMRanker::save_model_to_metadata(const string & model_key)
 {
-    LOGCALL_VOID(API, "SVMRanker::save_model_to_file", model_key);
+    LOGCALL_VOID(API, "SVMRanker::save_model_to_metadata", model_key);
     Xapian::WritableDatabase letor_db(get_database_path());
     string key = model_key;
     if (key.empty()) {
@@ -163,7 +163,7 @@ SVMRanker::save_model_to_metadata(const string & model_key)
 void
 SVMRanker::load_model_from_metadata(const string & model_key)
 {
-    LOGCALL_VOID(API, "SVMRanker::load_model_from_file", model_key);
+    LOGCALL_VOID(API, "SVMRanker::load_model_from_metadata", model_key);
     Xapian::Database letor_db(get_database_path());
     string key = model_key;
     if (key.empty()) {

commit 7622298c85aa3552b1649f7923178a3ec3da2e75

Fix debug logging for static method
Inadvertently broken by 3b5b40132fed3b30819dfe85c17cac5e002e83be
(so this breakage never made it into a release).

xapian-core/backends/databasereplicator.cc
@@ -45,7 +45,7 @@ DatabaseReplicator::~DatabaseReplicator()
 DatabaseReplicator *
 DatabaseReplicator::open(const string & path)
 {
-    LOGCALL(DB, DatabaseReplicator *, "DatabaseReplicator::open", path);
+    LOGCALL_STATIC(DB, DatabaseReplicator *, "DatabaseReplicator::open", path);
 
 #ifdef XAPIAN_HAS_GLASS_BACKEND
     if (file_exists(path + "/iamglass")) {