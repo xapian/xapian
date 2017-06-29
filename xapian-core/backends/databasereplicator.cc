4a38d8edd9cfcfaaf74c79f3b5a09651068d8f14
Olly Betts <olly@survex.com>
2017-06-29 14:08:25 +1200

    Fix debug logging for static method
    Inadvertently broken by 3b5b40132fed3b30819dfe85c17cac5e002e83be
    (so this breakage never made it into a release).
    
    (cherry picked from commit 7622298c85aa3552b1649f7923178a3ec3da2e75)

--- xapian-core/backends/databasereplicator.cc
+++ xapian-core/backends/databasereplicator.cc
@@ -48,7 +48,7 @@ DatabaseReplicator::~DatabaseReplicator()
 DatabaseReplicator *
 DatabaseReplicator::open(const string & path)
 {
-    LOGCALL(DB, DatabaseReplicator *, "DatabaseReplicator::open", path);
+    LOGCALL_STATIC(DB, DatabaseReplicator *, "DatabaseReplicator::open", path);
 
 #ifdef XAPIAN_HAS_CHERT_BACKEND
     if (file_exists(path + "/iamchert")) {
