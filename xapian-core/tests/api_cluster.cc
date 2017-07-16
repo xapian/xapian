ed8437cfa3b36dc5e3a4f470e54de19f284febbd
richhiey1996 <richhiey.thomas@gmail.com>
2017-07-16 16:09:24 -0400

    Added generated database which uses TermGenerator

--- xapian-core/tests/api_cluster.cc
+++ xapian-core/tests/api_cluster.cc
@@ -29,14 +29,38 @@
 #include "testsuite.h"
 #include "testutils.h"
 
+static void
+make_generated_db(Xapian::WritableDatabase &db, const std::string &)
+{
+    const std::unordered_set<std::string>
+    test_strings({"This line is about a cluster. Cluster is important and is everywhere",
+		  "We need to search for special cluster. Cluster cluster cluster",
+		  "Computer cluster is a special example of a cluster. Used to search fast",
+		  "Another example of cluster is a star cluster. Star cluster has a lot of stars"});
+
+    Xapian::TermGenerator indexer;
+    Xapian::Stem stemmer("english");
+    indexer.set_stemmer(stemmer);
+
+    for (std::unordered_set<std::string>::const_iterator it = test_strings.begin(); it != test_strings.end(); ++it)
+    {
+	Xapian::Document document;
+	const std::string &document_data = *it;
+	document.set_data(document_data);
+	indexer.set_document(document);
+	indexer.index_text(document_data);
+	db.add_document(document);
+    }
+}
 /** Test for cosine distance
  *  Cosine distance = 1 - (cosine of the angle between two vectors).
  *  Thus, if two vectors are equal, the distance between them will be zero
  *  and if two vectors are unequal, the distance will be 1 >= dist >= 0.
  */
-DEFINE_TESTCASE(cosine_distance1, backend)
+DEFINE_TESTCASE(cosine_distance1, generated)
 {
-    Xapian::Enquire enquire(get_database("apitest_cluster"));
+    Xapian::Database db = get_database("cosine_distance1", make_generated_db);
+    Xapian::Enquire enquire(db);
     enquire.set_query(Xapian::Query("cluster"));
 
     Xapian::MSet matches = enquire.get_mset(0, 4);
@@ -63,9 +87,9 @@ DEFINE_TESTCASE(cosine_distance1, backend)
 /** Round Robin Test
  *  Test that none of the returned clusters are empty
  */
-DEFINE_TESTCASE(round_robin1, backend)
+DEFINE_TESTCASE(round_robin1, generated)
 {
-    Xapian::Database db(get_database("apitest_cluster"));
+    Xapian::Database db = get_database("round_robin1", make_generated_db);
     Xapian::Enquire enq(db);
     enq.set_query(Xapian::Query("cluster"));
     Xapian::MSet matches = enq.get_mset(0, 4);
