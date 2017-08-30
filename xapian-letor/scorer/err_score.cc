b721df0cf0ec4022157e79ca2ab2f953745ddbd5
AyushP123 <ayushpandey34@yahoo.co.in>
2017-08-30 10:21:32 +0530

    fixup! Add ERR Scorer to xapian-letor

--- xapian-letor/scorer/err_score.cc
+++ xapian-letor/scorer/err_score.cc
@@ -50,35 +50,28 @@ ERRScore::score(const std::vector<FeatureVector> & fvv) const
 {
     LOGCALL(API, double, "ERRScore::score", fvv);
     if (fvv.empty()) {
-	throw Xapian::InvalidArgumentError("Empty argument supplied");
+	return 0;
     }
-    int length = fvv.size();
-
-    /* used to store values which change from labels to
-     * satisfaction probability
-     */
-    double intermediate_values[length];
+    size_t length = fvv.size();
     double max_label = fvv[0].get_label();
 
     // store the labels set	by the user in intermediate_values.
-    for (int i = 0; i < length; ++i) {
+    for (size_t i = 0; i < length; ++i) {
 	double label = fvv[i].get_label();
-	intermediate_values[i] = label;
 	max_label = max(max_label, label);
     }
 
     // Accumulated probability, which is updated for each document.
     double p = 1;
     double err_score = 0;
-    int max_value = exp2(max_label);
-    for (int rank = 1; rank <= length; ++rank) {
-
+    double max_value = exp2(max_label);
+    for (size_t rank = 1; rank <= length; ++rank) {
 	/* Compute the probability of relevance for the document.
 	 * Probability of relevance is calculated in accordance with the gain
 	 * function for the Discounted Cumulative Gain in the paper:
 	 * http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.74.9057&rep=rep1&type=pdf
 	 */
-	intermediate_values[rank - 1] = (exp2(intermediate_values[rank - 1]) -
+	double relevance_probability = (exp2(fvv[rank - 1].get_label()) -
 					 1) / max_value;
 
        /* err_score = summation over all the documents
@@ -88,8 +81,8 @@ ERRScore::score(const std::vector<FeatureVector> & fvv) const
 	* The paper assumes discrete relevances but continous relevances can
 	* be scored using this scorer.
 	*/
-	err_score = err_score + (p * intermediate_values[rank - 1] / rank);
-	p = p * (1 - intermediate_values[rank - 1]);
+	err_score = err_score + (p * relevance_probability / rank);
+	p = p * (1 - relevance_probability);
     }
 
     return err_score;
