08409d766df7cfa201c1d0c9dcd6aa8468dfe7d3
Olly Betts <olly@survex.com>
2017-09-01 18:13:40 +1200

    Reimplement MultiPostList
    The new implementation uses a heap to merge more efficiently.

--- xapian-core/backends/multi/multi_postlist.cc
+++ xapian-core/backends/multi/multi_postlist.cc
@@ -1,222 +1,253 @@
-/* multi_postlist.cc: interface to multiple database access
+/** @file multi_postlist.cc
+ * @brief Class for merging PostList objects from subdatabases.
+ */
+/* Copyright (C) 2007,2008,2009,2011,2017 Olly Betts
  *
- * Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2007,2008,2009,2011 Olly Betts
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
 #include <config.h>
 
 #include "multi_postlist.h"
 
-#include "debuglog.h"
+#include <xapian/database.h>
+
+#include "api/leafpostlist.h"
+#include "backends/multi.h"
 #include "omassert.h"
 
-#ifdef XAPIAN_ASSERTIONS_PARANOID
-#include "xapian/database.h"
-#endif
+#include <algorithm>
 
-MultiPostList::MultiPostList(std::vector<LeafPostList *> & pls,
-			     const Xapian::Database &this_db_)
-	: postlists(pls),
-	  this_db(this_db_),
-	  finished(false),
-	  currdoc(0)
+using namespace std;
+
+MultiPostList::MultiPostList(const Xapian::Database& db,
+                            const string& term)
+    : current(0), n_shards(0), postlists(NULL), docids_size(0), docids(NULL)
 {
-    multiplier = pls.size();
+    // The 0 and 1 cases should be handled by our caller.
+    AssertRel(db.internal.size(), >=, 2);
+    postlists = new PostList*[db.internal.size()];
+    try {
+       docids = new Xapian::docid[db.internal.size()];
+       for (auto&& sub_db : db.internal) {
+           postlists[n_shards] = sub_db->open_post_list(term);
+           ++n_shards;
+       }
+    } catch (...) {
+       while (n_shards)
+           delete postlists[--n_shards];
+       delete [] postlists;
+       delete [] docids;
+       throw;
+    }
 }
-
 
 MultiPostList::~MultiPostList()
 {
-    std::vector<LeafPostList *>::iterator i;
-    for (i = postlists.begin(); i != postlists.end(); ++i) {
-	delete *i;
-    }
-    postlists.clear();
+    while (n_shards)
+       delete postlists[--n_shards];
+    delete [] postlists;
 }
 
 Xapian::doccount
 MultiPostList::get_termfreq_min() const
 {
-    // Should never get called.
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
     Assert(false);
     return 0;
 }
-
 Xapian::doccount
 MultiPostList::get_termfreq_max() const
 {
-    return MultiPostList::get_termfreq_min();
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
+    Assert(false);
+    return 0;
 }
 
 Xapian::doccount
 MultiPostList::get_termfreq_est() const
 {
-    return MultiPostList::get_termfreq_min();
-}
-
-double
-MultiPostList::get_maxweight() const
-{
-    return MultiPostList::get_weight();
-}
-
-double
-MultiPostList::get_weight() const
-{
-    // Should never get called.
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
     Assert(false);
     return 0;
 }
 
 double
-MultiPostList::recalc_maxweight()
+MultiPostList::get_maxweight() const
 {
-    return MultiPostList::get_weight();
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
+    Assert(false);
+    return 0;
 }
 
 Xapian::docid
 MultiPostList::get_docid() const
 {
-    LOGCALL(DB, Xapian::docid, "MultiPostList::get_docid", NO_ARGS);
-    Assert(!at_end());
-    Assert(currdoc != 0);
-    RETURN(currdoc);
+    return docids[0];
 }
 
 Xapian::termcount
 MultiPostList::get_doclength() const
 {
-    LOGCALL(DB, Xapian::termcount, "MultiPostList::get_doclength", NO_ARGS);
-    Assert(!at_end());
-    Assert(currdoc != 0);
-    Xapian::termcount result = postlists[(currdoc - 1) % multiplier]->get_doclength();
-    AssertEqParanoid(result, this_db.get_doclength(get_docid()));
-    RETURN(result);
+    return postlists[shard_number(docids[0], n_shards)]->get_doclength();
 }
 
 Xapian::termcount
 MultiPostList::get_unique_terms() const
 {
-    LOGCALL(DB, Xapian::termcount, "MultiPostList::get_unique_terms", NO_ARGS);
-    Assert(!at_end());
-    Assert(currdoc != 0);
-    Xapian::termcount result = postlists[(currdoc - 1) % multiplier]->get_unique_terms();
-    AssertEqParanoid(result, this_db.get_unique_terms(get_docid()));
-    RETURN(result);
+    return postlists[shard_number(docids[0], n_shards)]->get_unique_terms();
 }
 
 Xapian::termcount
 MultiPostList::get_wdf() const
 {
-    return postlists[(currdoc - 1) % multiplier]->get_wdf();
+    return postlists[shard_number(docids[0], n_shards)]->get_wdf();
 }
 
-PositionList *
-MultiPostList::open_position_list() const
+double
+MultiPostList::get_weight() const
 {
-    return postlists[(currdoc - 1) % multiplier]->open_position_list();
-}
-
-PostList *
-MultiPostList::next(double w_min)
-{
-    LOGCALL(DB, PostList *, "MultiPostList::next", w_min);
-    Assert(!at_end());
-
-    Xapian::docid newdoc = 0;
-    Xapian::docid offset = 1;
-    std::vector<LeafPostList *>::iterator i;
-    for (i = postlists.begin(); i != postlists.end(); ++i) {
-	if (!(*i)->at_end()) {
-	    Xapian::docid id = ((*i)->get_docid() - 1) * multiplier + offset;
-	    // Check if it needs to be advanced
-	    if (currdoc >= id) {
-		(*i)->next(w_min);
-		if (!(*i)->at_end()) {
-		    id = ((*i)->get_docid() - 1) * multiplier + offset;
-		    if (newdoc == 0 || id < newdoc) newdoc = id;
-		}
-	    } else {
-		if (newdoc == 0 || id < newdoc) newdoc = id;
-	    }
-	}
-	offset++;
-    }
-    if (newdoc) {
-	LOGLINE(DB, "MultiPostList::next() newdoc=" << newdoc <<
-		    " (olddoc=" << currdoc << ")");
-	currdoc = newdoc;
-    } else {
-	LOGLINE(DB, "MultiPostList::next() finished" <<
-		    " (olddoc=" << currdoc << ")");
-	finished = true;
-    }
-    RETURN(NULL);
-}
-
-PostList *
-MultiPostList::skip_to(Xapian::docid did, double w_min)
-{
-    LOGCALL(DB, PostList *, "MultiPostList::skip_to", did | w_min);
-    Assert(!at_end());
-    Xapian::docid newdoc = 0;
-    Xapian::docid offset = 0;
-    Xapian::docid realdid = (did - 1) / multiplier + 2;
-    Xapian::doccount dbnumber = (did - 1) % multiplier;
-    std::vector<LeafPostList *>::iterator i;
-    for (i = postlists.begin(); i != postlists.end(); ++i) {
-	if (offset == dbnumber) --realdid;
-	++offset;
-	Assert((realdid - 1) * multiplier + offset >= did);
-	Assert((realdid - 1) * multiplier + offset < did + multiplier);
-	if (!(*i)->at_end()) {
-	    (*i)->skip_to(realdid, w_min);
-	    if (!(*i)->at_end()) {
-		Xapian::docid id = ((*i)->get_docid() - 1) * multiplier + offset;
-		if (newdoc == 0 || id < newdoc) newdoc = id;
-	    }
-	}
-    }
-    if (newdoc) {
-	currdoc = newdoc;
-    } else {
-	finished = true;
-    }
-    RETURN(NULL);
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
+    Assert(false);
+    return 0;
 }
 
 bool
 MultiPostList::at_end() const
 {
-    return finished;
+    return docids_size == 0;
+}
+
+double
+MultiPostList::recalc_maxweight()
+{
+    // MultiPostList is only used by PostingIterator which should never call
+    // this method.
+    Assert(false);
+    return 0;
+}
+
+PositionList*
+MultiPostList::open_position_list() const
+{
+    return postlists[current]->open_position_list();
+}
+
+PostList*
+MultiPostList::next(double w_min)
+{
+    if (docids_size == 0) {
+       // Make a heap of the mapped docids so that the smallest is at the top
+       // of the heap.
+       size_t j = 0;
+       for (size_t i = 0; i != n_shards; ++i) {
+           PostList* pl = postlists[i];
+           if (!pl) continue;
+           pl->next(w_min);
+           if (pl->at_end()) {
+               delete pl;
+               postlists[i] = NULL;
+           } else {
+               docids[j++] = unshard(pl->get_docid(), i, n_shards);
+           }
+       }
+       docids_size = j;
+       make_heap(docids, docids + docids_size);
+    } else {
+       Xapian::docid old_did = docids[0];
+       pop_heap(docids, docids + docids_size);
+       size_t shard = shard_number(old_did, n_shards);
+       PostList* pl = postlists[shard];
+       pl->next(w_min);
+       if (pl->at_end()) {
+           delete pl;
+           postlists[shard] = NULL;
+           --docids_size;
+       } else {
+           docids[docids_size - 1] = pl->get_docid();
+           push_heap(docids, docids + docids_size);
+       }
+    }
+
+    return NULL;
+}
+
+PostList*
+MultiPostList::skip_to(Xapian::docid did, double w_min)
+{
+    size_t j = 0;
+    if (docids_size == 0) {
+       // Make a heap of the mapped docids so that the smallest is at the top
+       // of the heap.
+       for (size_t i = 0; i != n_shards; ++i) {
+           PostList* pl = postlists[i];
+           if (!pl) continue;
+           pl->skip_to(did, w_min);
+           if (pl->at_end()) {
+               delete pl;
+               postlists[i] = NULL;
+           } else {
+               docids[j++] = unshard(pl->get_docid(), i, n_shards);
+           }
+       }
+    } else {
+       if (did <= docids[0])
+           return NULL;
+       // For a skip by < n_shards docids, pop/push may be more efficient than
+       // rebuilding the heap.  For now, always just rebuild the heap unless
+       // we're just skipping the next docid, in which case do next() instead.
+       if (did == docids[0] + 1)
+           return MultiPostList::next(w_min);
+       for (size_t i = 0; i != docids_size; ++i) {
+           Xapian::docid old_did = docids[i];
+           if (old_did < did) {
+               size_t shard = shard_number(old_did, n_shards);
+               PostList* pl = postlists[shard];
+               pl->skip_to(did, w_min);
+               if (pl->at_end()) {
+                   delete pl;
+                   postlists[shard] = NULL;
+               } else {
+                   docids[j++] = pl->get_docid();
+               }
+           } else {
+               docids[j++] = old_did;
+           }
+       }
+    }
+    docids_size = j;
+    make_heap(docids, docids + docids_size);
+
+    return NULL;
 }
 
 std::string
 MultiPostList::get_description() const
 {
-    std::string desc;
-
-    std::vector<LeafPostList *>::const_iterator i;
-    for (i = postlists.begin(); i != postlists.end(); ++i) {
-	if (!desc.empty()) desc += ',';
-	desc += (*i)->get_description();
+    string desc = "MultiPostList(";
+    for (size_t i = 0; i != n_shards; ++i) {
+       if (postlists[i]) {
+           desc += postlists[i]->get_description();
+           desc += ',';
+       } else {
+           desc += "NULL,";
+       }
     }
-
+    desc.back() = ')';
     return desc;
 }
