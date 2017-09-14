87d1b57abf0b6d47a5d14960122d7c665573a86a
Olly Betts <olly@survex.com>
2017-09-14 17:38:22 +1200

    dbcheck.cc: Update copyright details
    The oldest code there's still any trace of in this file is from
    2007.

--- xapian-core/backends/dbcheck.cc
+++ xapian-core/backends/dbcheck.cc
@@ -1,8 +1,7 @@
 /** @file dbcheck.cc
  * @brief Check the consistency of a database or table.
  */
-/* Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017 Olly Betts
+/* Copyright 2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
