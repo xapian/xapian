2fe5ccfdf7d028f45eff508a65e14758963a35d9
Olly Betts <olly@survex.com>
2017-10-18 22:51:15 +1300

    Improve stub file handling of disabled backends
    Disabling a backend used to mean that lines for that backend would
    throw DatabaseError with message "Bad line" - now they they throw
    FeatureUnavailableError.

--- xapian-core/backends/dbfactory.cc
+++ xapian-core/backends/dbfactory.cc
@@ -1,7 +1,7 @@
 /** @file dbfactory.cc
  * @brief Database factories for non-remote databases.
  */
-/* Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2011,2012,2013,2014,2015,2016 Olly Betts
+/* Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2011,2012,2013,2014,2015,2016,2017 Olly Betts
  * Copyright 2008 Lemur Consulting Ltd
  *
  * This program is free software; you can redistribute it and/or
@@ -125,16 +125,18 @@ open_stub(Database &db, const string &file)
 	    continue;
 	}
 
-#ifdef XAPIAN_HAS_GLASS_BACKEND
 	if (type == "glass") {
+#ifdef XAPIAN_HAS_GLASS_BACKEND
 	    resolve_relative_path(line, file);
 	    db.add_database(Database(new GlassDatabase(line)));
 	    continue;
-	}
+#else
+	    throw FeatureUnavailableError("Glass backend disabled");
 #endif
+	}
 
-#ifdef XAPIAN_HAS_REMOTE_BACKEND
 	if (type == "remote" && !line.empty()) {
+#ifdef XAPIAN_HAS_REMOTE_BACKEND
 	    if (line[0] == ':') {
 		// prog
 		// FIXME: timeouts
@@ -168,15 +170,19 @@ open_stub(Database &db, const string &file)
 		    continue;
 		}
 	    }
-	}
+#else
+	    throw FeatureUnavailableError("Remote backend disabled");
 #endif
+	}
 
-#ifdef XAPIAN_HAS_INMEMORY_BACKEND
 	if (type == "inmemory" && line.empty()) {
+#ifdef XAPIAN_HAS_INMEMORY_BACKEND
 	    db.add_database(Database(string(), DB_BACKEND_INMEMORY));
 	    continue;
-	}
+#else
+	    throw FeatureUnavailableError("Inmemory backend disabled");
 #endif
+	}
 
 	if (type == "chert") {
 	    throw FeatureUnavailableError("Chert backend no longer supported");
@@ -240,16 +246,18 @@ open_stub(WritableDatabase &db, const string &file, int flags)
 	    continue;
 	}
 
-#ifdef XAPIAN_HAS_GLASS_BACKEND
 	if (type == "glass") {
+#ifdef XAPIAN_HAS_GLASS_BACKEND
 	    resolve_relative_path(line, file);
 	    db.add_database(WritableDatabase(line, flags|DB_BACKEND_GLASS));
 	    continue;
-	}
+#else
+	    throw FeatureUnavailableError("Glass backend disabled");
 #endif
+	}
 
+	if (type == "remote" && !line.empty()) {
 #ifdef XAPIAN_HAS_REMOTE_BACKEND
-	if (type == "remote") {
 	    if (line[0] == ':') {
 		// prog
 		// FIXME: timeouts
@@ -283,15 +291,19 @@ open_stub(WritableDatabase &db, const string &file, int flags)
 		    continue;
 		}
 	    }
-	}
+#else
+	    throw FeatureUnavailableError("Remote backend disabled");
 #endif
+	}
 
-#ifdef XAPIAN_HAS_INMEMORY_BACKEND
 	if (type == "inmemory" && line.empty()) {
+#ifdef XAPIAN_HAS_INMEMORY_BACKEND
 	    db.add_database(WritableDatabase(string(), DB_BACKEND_INMEMORY));
 	    continue;
-	}
+#else
+	    throw FeatureUnavailableError("Inmemory backend disabled");
 #endif
+	}
 
 	if (type == "chert") {
 	    throw FeatureUnavailableError("Chert backend no longer supported");
