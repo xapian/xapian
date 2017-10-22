e66984fbb67c9549eb683d7ed0da26fd770816a1
Olly Betts <olly@survex.com>
2017-10-22 13:58:34 +1300

    Add .exe to XAPIAN_TCPSRV and XAPIAN_PROGSRV on __WIN32__
    Without an extension they don't seem to be found.

--- xapian-core/tests/harness/backendmanager.h
+++ xapian-core/tests/harness/backendmanager.h
@@ -2,7 +2,7 @@
  * @brief Base class for backend handling in test harness
  */
 /* Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2017 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -39,11 +39,13 @@
 # else
 #  define XAPIAN_BIN_PATH "..\\bin\\" // mingw
 # endif
+# define EXE_SUFFIX ".exe"
 #else
 # define XAPIAN_BIN_PATH "../bin/"
+# define EXE_SUFFIX
 #endif
-#define XAPIAN_TCPSRV XAPIAN_BIN_PATH"xapian-tcpsrv"
-#define XAPIAN_PROGSRV XAPIAN_BIN_PATH"xapian-progsrv"
+#define XAPIAN_TCPSRV XAPIAN_BIN_PATH "xapian-tcpsrv" EXE_SUFFIX
+#define XAPIAN_PROGSRV XAPIAN_BIN_PATH "xapian-progsrv" EXE_SUFFIX
 
 class BackendManager {
     /// The current data directory
