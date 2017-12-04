0532bc3cda78e6b7241adae1b6a3ea67e159945b
Olly Betts <olly@survex.com>
2017-12-05 10:29:51 +1300

    Don't use WIN32 API to parse/unparse UUIDs
    So much glue code is needed that it's simpler to just do the parsing
    and unparsing ourselves, and we already have an implementation which
    is used when generating UUIDs using /proc on Linux.  We still use
    UuidCreate() to generate a new UUID.

--- xapian-core/common/win32_uuid.cc
+++ xapian-core/common/win32_uuid.cc
@@ -2,7 +2,7 @@
  * @brief Provide UUID functions compatible with libuuid from util-linux-ng.
  */
 /* Copyright (C) 2008 Lemur Consulting Ltd
- * Copyright (C) 2013,2015,2016 Olly Betts
+ * Copyright (C) 2013,2015,2016,2017 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,6 +26,7 @@
 #include "xapian/error.h"
 
 #include <cstring>
+#include "stringutils.h"
 
 #ifdef __WIN32__
 # include "safewinsock2.h" // For htonl() and htons().
@@ -38,9 +39,6 @@ using namespace std;
 
 /// The size of a UUID in bytes.
 const size_t UUID_SIZE = 16;
-
-/// The size of a UUID string in bytes (not including trailing '\0').
-const size_t UUID_STRING_SIZE = 36;
 
 void
 uuid_generate(uuid_t uu)
@@ -63,36 +61,23 @@ uuid_generate(uuid_t uu)
 int
 uuid_parse(const char * in, uuid_t uu)
 {
-    UUID uuid;
-    // UuidFromString() requires a non-const unsigned char * pointer, though it
-    // doesn't modify the passed string.
-    unsigned char * in_ =
-	reinterpret_cast<unsigned char *>(const_cast<char *>(in));
-    if (UuidFromString(in_, &uuid) != RPC_S_OK)
-	return -1;
-    uuid.Data1 = htonl(uuid.Data1);
-    uuid.Data2 = htons(uuid.Data2);
-    uuid.Data3 = htons(uuid.Data3);
-    memcpy(uu, &uuid, UUID_SIZE);
+    for (unsigned i = 0; i != UUID_SIZE; ++i) {
+	uu[i] = hex_digit(in[0]) << 4 | hex_digit(in[1]);
+	in += ((0x2a8 >> i) & 1) | 2;
+    }
     return 0;
 }
 
 void uuid_unparse_lower(const uuid_t uu, char * out)
 {
-    UUID uuid;
-    unsigned char *uuidstr;
-    memcpy(&uuid, uu, UUID_SIZE);
-    uuid.Data1 = htonl(uuid.Data1);
-    uuid.Data2 = htons(uuid.Data2);
-    uuid.Data3 = htons(uuid.Data3);
-    if (rare(UuidToString(&uuid, &uuidstr) != RPC_S_OK)) {
-	// The only documented (or really conceivable) error code is
-	// RPC_S_OUT_OF_MEMORY.
-	throw std::bad_alloc();
+    for (unsigned i = 0; i != UUID_SIZE; ++i) {
+	unsigned char ch = uu[i];
+	*out++ = "0123456789abcdef"[ch >> 4];
+	*out++ = "0123456789abcdef"[ch & 0x0f];
+	if ((0x2a8 >> i) & 1)
+	   *out++ = '-';
     }
-    memcpy(out, strlwr(reinterpret_cast<char*>(uuidstr)), UUID_STRING_SIZE);
-    out[UUID_STRING_SIZE] = '\0';
-    RpcStringFree(&uuidstr);
+    *out = '\0';
 }
 
 void uuid_clear(uuid_t uu)
