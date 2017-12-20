534d52ba9086d1d6da07373f060125f9d1ddb19c
Olly Betts <olly@survex.com>
2017-12-21 11:24:45 +1300

    Add missing parameter name in internal header
    (cherry picked from commit b70acbef56e864f159332dec51b4fd3867084a73)

--- xapian-core/api/postlist.h
+++ xapian-core/api/postlist.h
@@ -163,7 +163,7 @@ class Xapian::PostingIterator::Internal : public Xapian::Internal::intrusive_bas
      *		delete us.  This "pruning" can only happen for a non-leaf
      *		subclass of this class.
      */
-    virtual Internal * skip_to(Xapian::docid, double w_min) = 0;
+    virtual Internal * skip_to(Xapian::docid did, double w_min) = 0;
 
     /** Check if the specified docid occurs in this postlist.
      *
