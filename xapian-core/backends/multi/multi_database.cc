26cef069426b4e5a9a068a5a4069a1662c3da27e
Olly Betts <olly@survex.com>
2017-12-27 14:16:24 +1300

    Check for last_docid wrapping more
    Check for last_docid wrapping in MultiDatabase::replace_document(term, doc).

--- xapian-core/backends/multi/multi_database.cc
+++ xapian-core/backends/multi/multi_database.cc
@@ -628,7 +628,14 @@ MultiDatabase::replace_document(const string& term, const Xapian::Document& doc)
     // If no unique_term in the database, this is just an add_document().
     if (pl->at_end()) {
 	// Which database will the next never used docid be in?
-	auto shard = shards[shard_number(get_lastdocid() + 1, n_shards)];
+	Xapian::docid did = get_lastdocid() + 1;
+	if (rare(did == 0)) {
+	    throw Xapian::DatabaseError("Run out of docids - you'll have to "
+					"use copydatabase to eliminate any "
+					"gaps before you can add more "
+					"documents");
+	}
+	auto shard = shards[shard_number(did, n_shards)];
 	return shard->add_document(doc);
     }
 
