bae71009137ab291f57ec167385f7793b6f812de
Olly Betts <olly@survex.com>
2018-01-29 17:49:03 +1300

    xapian-inspect: Add "first" and "last" commands
    These jump to the first/last entry in the current table respectively.

--- xapian-core/bin/xapian-inspect.cc
+++ xapian-core/bin/xapian-inspect.cc
@@ -84,6 +84,8 @@ show_help()
     cout << "Commands:\n"
 	    "next   : Next entry (alias 'n' or '')\n"
 	    "prev   : Previous entry (alias 'p')\n"
+	    "first  : First entry (alias 'f')\n"
+	    "last   : Last entry (alias 'l')\n"
 	    "goto K : Goto entry with key K (alias 'g')\n"
 	    "until K: Display entries until key K (alias 'u')\n"
 	    "open T : Open table T instead (alias 'o') - e.g. open postlist\n"
@@ -319,6 +321,16 @@ wait_for_input:
 	    } else if (input == "u" || input == "until") {
 		do_until(cursor, string());
 		goto wait_for_input;
+	    } else if (input == "f" || input == "first") {
+		cursor.find_entry(string());
+		cursor.next();
+		continue;
+	    } else if (input == "l" || input == "last") {
+		// To position on the last key we just search for a key with
+		// the longest possible length consisting entirely of the
+		// highest sorting byte value.
+		cursor.find_entry(string(GLASS_BTREE_MAX_KEY_LEN, '\xff'));
+		continue;
 	    } else if (startswith(input, "g ")) {
 		if (!cursor.find_entry(input.substr(2))) {
 		    cout << "No exact match, going to entry before." << endl;
