c5abe09cf0e640c0a61ae9f5fed434fe0b11b2c4
Robert Stepanek <rsto@fastmail.com>
2018-02-16 12:36:41 +0100

    Add cjk_flags to Mset.snippet after removal of omenquire.

--- xapian-core/api/mset.cc
+++ xapian-core/api/mset.cc
@@ -187,11 +187,12 @@ MSet::snippet(const std::string& text,
 	      unsigned flags,
 	      const std::string& hi_start,
 	      const std::string& hi_end,
-	      const std::string& omit) const
+	      const std::string& omit,
+	      unsigned cjk_flags) const
 {
     // The actual implementation is in queryparser/termgenerator_internal.cc.
     return internal->snippet(text, length, stemmer, flags,
-			     hi_start, hi_end, omit);
+			     hi_start, hi_end, omit, cjk_flags);
 }
 
 std::string
