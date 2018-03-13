f297dc776c3991f342c7b12c1d2d00798fb567f7
Addy007-icy <kumaradityagr@gmail.com>
2018-03-13 09:28:50 +0530

    fixup! Handling negative indices when maxlen <= ind.size() Setting output to an empty string to avoid negative index reference

--- xapian-applications/omega/sample.cc
+++ xapian-applications/omega/sample.cc
@@ -50,12 +50,12 @@ generate_sample(const string & input, size_t maxlen,
 	if (output.size() >= maxlen) {
 	    // Need to truncate output.
 	    if (last_word_end <= maxlen / 2) {
-	    	if (maxlen - ind.size() <= 0){
-	    		output.resize(0);
-	    	} else {
+	    	if (maxlen - ind.size() <= 0) {
+	   		output.resize(0);
+			} else {
 			// Monster word!  We'll have to just split it.
 			output.replace(maxlen - ind.size(), string::npos, ind);
-	    	}
+			}
 	    } else {
 		output.replace(last_word_end, string::npos, ind2);
 	    }
