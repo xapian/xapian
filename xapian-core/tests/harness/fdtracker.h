589242638d47dd283d51bf9aeb6d580484739eb8
Olly Betts <olly@survex.com>
2018-04-21 17:41:36 +1200

    Make testsuite fd leak check work on more platforms
    Now looks in /dev/fd except on Linux and Cygwin.
    
    (cherry picked from commit cc1f2dbbc021355105393cb8d4be11d34e924997)

--- xapian-core/tests/harness/fdtracker.h
+++ xapian-core/tests/harness/fdtracker.h
@@ -1,7 +1,7 @@
 /** @file fdtracker.h
  * @brief Track leaked file descriptors.
  */
-/* Copyright (C) 2010 Olly Betts
+/* Copyright (C) 2010,2018 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -24,15 +24,25 @@
 #include <set>
 #include <string>
 
+// Disable fd tracking where it can't possibly work.
+#ifndef __WIN32__
+# define XAPIAN_TESTSUITE_TRACK_FDS
+#endif
+
 class FDTracker {
+#ifdef XAPIAN_TESTSUITE_TRACK_FDS
     std::set<int> fds;
 
+    /** The DIR* from opendir("/proc/self/fd") (or equivalent) cast to void*.
+     *
+     *  We store this cast to void* here to minimise the header we have to
+     *  include here.
+     */
     void * dir_void;
 
     std::string message;
 
   public:
-
     FDTracker() : dir_void(NULL) { }
 
     ~FDTracker();
@@ -42,6 +52,20 @@ class FDTracker {
     bool check();
 
     const std::string & get_message() const { return message; }
+#else
+  public:
+    FDTracker() { }
+
+    void init() { }
+
+    bool check() { return true; }
+
+    std::string get_message() const { return std::string(); }
+#endif
+
+    FDTracker(const FDTracker&) = delete;
+
+    FDTracker& operator=(const FDTracker&) = delete;
 };
 
 #endif // XAPIAN_INCLUDED_FDTRACKER_H
