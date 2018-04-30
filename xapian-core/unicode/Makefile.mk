eb7da20520c7ee65fefa10d4fca70088f41ca1d1
Olly Betts <olly@survex.com>
2018-05-01 08:10:04 +1200

    Ensure we create tests/soaktest and unicode directories
    In a VPATH build configured with --enable-maintainer-mode
    --disable-dependency-tracking the build fails because these directories
    don't exist.
    
    Patch from Gaurav Arora.
    
    (cherry picked from commit b350aaacea94d4e4923987904a6f57f9cb2fe544)

--- xapian-core/unicode/Makefile.mk
+++ xapian-core/unicode/Makefile.mk
@@ -11,9 +11,15 @@ EXTRA_DIST +=\
 
 if MAINTAINER_MODE
 unicode/unicode-data.cc: unicode/uniParse.tcl unicode/UnicodeData.txt
+	## Need to create the directory here in a VPATH build configured with:
+	## --enable-maintainer-mode --disable-dependency-tracking
+	$(MKDIR_P) unicode
 	tclsh $(srcdir)/unicode/uniParse.tcl $(srcdir)/unicode/UnicodeData.txt 9.0.0 unicode/unicode-data.cc
 
 unicode/c_istab.h: unicode/gen_c_istab
+	## Need to create the directory here in a VPATH build configured with:
+	## --enable-maintainer-mode --disable-dependency-tracking
+	$(MKDIR_P) unicode
 	$(PERL) $(srcdir)/unicode/gen_c_istab unicode/c_istab.h
 
 BUILT_SOURCES += unicode/unicode-data.cc unicode/c_istab.h
