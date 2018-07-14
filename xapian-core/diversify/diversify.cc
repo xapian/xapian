ba5bd7517463bcefd4c3f98c5a18562ef2041a33
Uppinder Chugh <uppinderchugh@gmail.com>
2018-07-14 16:29:35 +0530

    diversify: Use Xapian::LC instead of KMeans
    Use LC clustering instead of KMeans in diversification module.

--- xapian-core/diversify/diversify.cc
+++ xapian-core/diversify/diversify.cc
@@ -176,8 +176,8 @@ Diversify::Internal::get_dmset(const MSet& mset)
     initialise_points(mset);
 
     // Cluster the given mset into k clusters
-    KMeans km(k);
-    Xapian::ClusterSet cset = km.cluster(mset);
+    Xapian::LC lc(k);
+    Xapian::ClusterSet cset = lc.cluster(mset);
     compute_similarities(cset);
 
     // topC contains union of top-r relevant documents of each cluster
