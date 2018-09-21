2949622a1abc8313b48ad9b0e47e5e1619a95bb4
Olly Betts <olly@survex.com>
2018-09-21 13:51:48 +1200

    Handle initially empty DB same as overwrite==true
    This probably has no user-visible consequences, but it's cleaner
    for the handling to be exactly the same.

--- xapian-applications/omega/index_file.cc
+++ xapian-applications/omega/index_file.cc
@@ -252,19 +252,23 @@ index_init(const string & dbpath, const Xapian::Stem & stemmer,
     if (!overwrite) {
 	db = Xapian::WritableDatabase(dbpath, Xapian::DB_CREATE_OR_OPEN);
 	old_docs_not_seen = db.get_doccount();
-	old_lastdocid = db.get_lastdocid();
-	if (delete_removed_documents) {
-	    // + 1 so that old_lastdocid is a valid subscript.
-	    updated.resize(old_lastdocid + 1);
-	}
-	try {
-	    Xapian::valueno slot = use_ctime ? VALUE_CTIME : VALUE_LASTMOD;
-	    string ubound = db.get_value_upper_bound(slot);
-	    if (!ubound.empty())
-		last_altered_max = binary_string_to_int(ubound);
-	} catch (const Xapian::UnimplementedError &) {
-	    numeric_limits<time_t> n;
-	    last_altered_max = n.max();
+	// Handle an initially empty database exactly the same way as when
+	// overwrite is true.
+	if (old_docs_not_seen != 0) {
+	    old_lastdocid = db.get_lastdocid();
+	    if (delete_removed_documents) {
+		// + 1 so that old_lastdocid is a valid subscript.
+		updated.resize(old_lastdocid + 1);
+	    }
+	    try {
+		Xapian::valueno slot = use_ctime ? VALUE_CTIME : VALUE_LASTMOD;
+		string ubound = db.get_value_upper_bound(slot);
+		if (!ubound.empty())
+		    last_altered_max = binary_string_to_int(ubound);
+	    } catch (const Xapian::UnimplementedError &) {
+		numeric_limits<time_t> n;
+		last_altered_max = n.max();
+	    }
 	}
     } else {
 	db = Xapian::WritableDatabase(dbpath, Xapian::DB_CREATE_OR_OVERWRITE);
