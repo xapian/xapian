581386e48b8c8c4950fda46c70cd41e3b04b1b05
Olly Betts <olly@survex.com>
2018-11-30 10:43:00 +1300

    Stop watching fds for exceptional conditions
    None of the "exceptional conditions" which select() monitors for are
    relevant to our usage.
    
    (cherry picked from commit 82987dd80505037210a6f6d8d542520a52f6facb)

--- xapian-core/net/remoteconnection.cc
+++ xapian-core/net/remoteconnection.cc
@@ -177,15 +177,13 @@ RemoteConnection::read_at_least(size_t min_len, double end_time)
 	    }
 
 	    // Use select to wait until there is data or the timeout is reached.
-	    fd_set rfdset, efdset;
-	    FD_ZERO(&rfdset);
-	    FD_SET(fdin, &rfdset);
-	    FD_ZERO(&efdset);
-	    FD_SET(fdin, &efdset);
+	    fd_set fdset;
+	    FD_ZERO(&fdset);
+	    FD_SET(fdin, &fdset);
 
 	    struct timeval tv;
 	    RealTime::to_timeval(time_diff, &tv);
-	    int select_result = select(fdin + 1, &rfdset, 0, &efdset, &tv);
+	    int select_result = select(fdin + 1, &fdset, 0, 0, &tv);
 	    if (select_result > 0) break;
 
 	    if (select_result == 0)
@@ -213,16 +211,14 @@ RemoteConnection::ready_to_read() const
     if (!buffer.empty()) RETURN(true);
 
     // Use select to see if there's data available to be read.
-    fd_set rfdset, efdset;
-    FD_ZERO(&rfdset);
-    FD_SET(fdin, &rfdset);
-    FD_ZERO(&efdset);
-    FD_SET(fdin, &efdset);
+    fd_set fdset;
+    FD_ZERO(&fdset);
+    FD_SET(fdin, &fdset);
 
     struct timeval tv;
     tv.tv_sec = 0;
     tv.tv_usec = 0;
-    RETURN(select(fdin + 1, &rfdset, 0, &efdset, &tv) > 0);
+    RETURN(select(fdin + 1, &fdset, 0, 0, &tv) > 0);
 }
 
 void
@@ -282,7 +278,7 @@ RemoteConnection::send_message(char type, const string &message,
 
     const string * str = &header;
 
-    fd_set wfdset, efdset;
+    fd_set fdset;
     size_t count = 0;
     while (true) {
 	// We've set write to non-blocking, so just try writing as there
@@ -306,10 +302,8 @@ RemoteConnection::send_message(char type, const string &message,
 	    throw Xapian::NetworkError("write failed", context, errno);
 
 	// Use select to wait until there is space or the timeout is reached.
-	FD_ZERO(&wfdset);
-	FD_SET(fdout, &wfdset);
-	FD_ZERO(&efdset);
-	FD_SET(fdout, &efdset);
+	FD_ZERO(&fdset);
+	FD_SET(fdout, &fdset);
 
 	double time_diff = end_time - RealTime::now();
 	if (time_diff < 0) {
@@ -319,7 +313,7 @@ RemoteConnection::send_message(char type, const string &message,
 
 	struct timeval tv;
 	RealTime::to_timeval(time_diff, &tv);
-	int select_result = select(fdout + 1, 0, &wfdset, &efdset, &tv);
+	int select_result = select(fdout + 1, 0, &fdset, 0, &tv);
 
 	if (select_result < 0) {
 	    if (errno == EINTR) {
@@ -409,7 +403,7 @@ RemoteConnection::send_file(char type, int fd, double end_time)
 				   context, errno);
     }
 
-    fd_set wfdset, efdset;
+    fd_set fdset;
     size_t count = 0;
     while (true) {
 	// We've set write to non-blocking, so just try writing as there
@@ -441,10 +435,8 @@ RemoteConnection::send_file(char type, int fd, double end_time)
 	    throw Xapian::NetworkError("write failed", context, errno);
 
 	// Use select to wait until there is space or the timeout is reached.
-	FD_ZERO(&wfdset);
-	FD_SET(fdout, &wfdset);
-	FD_ZERO(&efdset);
-	FD_SET(fdout, &efdset);
+	FD_ZERO(&fdset);
+	FD_SET(fdout, &fdset);
 
 	double time_diff = end_time - RealTime::now();
 	if (time_diff < 0) {
@@ -454,7 +446,7 @@ RemoteConnection::send_file(char type, int fd, double end_time)
 
 	struct timeval tv;
 	RealTime::to_timeval(time_diff, &tv);
-	int select_result = select(fdout + 1, 0, &wfdset, &efdset, &tv);
+	int select_result = select(fdout + 1, 0, &fdset, 0, &tv);
 
 	if (select_result < 0) {
 	    if (errno == EINTR) {
@@ -668,14 +660,12 @@ RemoteConnection::do_close(bool wait)
 #else
 		// Wait for the connection to be closed - when this happens
 		// select() will report that a read won't block.
-		fd_set rfdset, efdset;
-		FD_ZERO(&rfdset);
-		FD_SET(fdin, &rfdset);
-		FD_ZERO(&efdset);
-		FD_SET(fdin, &efdset);
+		fd_set fdset;
+		FD_ZERO(&fdset);
+		FD_SET(fdin, &fdset);
 		int res;
 		do {
-		    res = select(fdin + 1, &rfdset, 0, &efdset, NULL);
+		    res = select(fdin + 1, &fdset, 0, 0, NULL);
 		} while (res < 0 && errno == EINTR);
 #endif
 	    } catch (...) {
