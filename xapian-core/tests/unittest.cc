3ec6f8d8b7c930ce07900c88da12b946531d3ba3
Vaibhav Kansagara <vaibhavkansagara249@gmail.com>
2019-04-14 19:49:11 +0530

    Fix tostring method when a lvalue of a particular type is passed

--- xapian-core/tests/unittest.cc
+++ xapian-core/tests/unittest.cc
@@ -536,10 +536,22 @@ static bool test_tostring1()
     TEST_EQUAL(str(-1), "-1");
     TEST_EQUAL(str(-9), "-9");
     TEST_EQUAL(str(-10), "-10");
+    TEST_EQUAL(str(0x7f), "127");
+    TEST_EQUAL(str(-0x80), "-128");
+    TEST_EQUAL(str(0x7fff), "32767");
+    // test for lvalue with minimum value for short type.
+    short temp = -32768;
+    TEST_EQUAL(str(temp), "-32768");
+
     TEST_EQUAL(str(0xffffffff), "4294967295");
     TEST_EQUAL(str(0x7fffffff), "2147483647");
     TEST_EQUAL(str(0x7fffffffu), "2147483647");
-    TEST_EQUAL(str(-0x7fffffff), "-2147483647");
+
+    // test for lvalue with minimum value for int type.
+    int temp1 = -2147483648;
+    TEST_EQUAL(str(temp1), "-2147483648");
+
+    TEST_EQUAL(str(0x7fffffffffffffff), "9223372036854775807");
 
 #ifdef __WIN32__
     /* Test the 64 bit integer conversion to string.
