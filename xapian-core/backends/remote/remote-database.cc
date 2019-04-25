84ee1eb5deb1e7835c72a292d3c9aab9f8b6050b
Germán Méndez Bravo <german.mb@gmail.com>
2019-04-25 00:09:28 -0500

    Close RemoteDatabase connection on exceptions
    If an exception was being thrown in RemoteDatabase constructor, the connection is never closed. This fixes ticket #781

--- xapian-core/backends/remote/remote-database.cc
+++ xapian-core/backends/remote/remote-database.cc
@@ -83,23 +83,30 @@ RemoteDatabase::RemoteDatabase(int fd, double timeout_,
       mru_slot(Xapian::BAD_VALUENO),
       timeout(timeout_)
 {
+    try {
 #ifndef __WIN32__
-    // It's simplest to just ignore SIGPIPE.  We'll still know if the
-    // connection dies because we'll get EPIPE back from write().
-    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
-	throw Xapian::NetworkError("Couldn't set SIGPIPE to SIG_IGN", errno);
-    }
+	// It's simplest to just ignore SIGPIPE.  We'll still know if the
+	// connection dies because we'll get EPIPE back from write().
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+	    throw Xapian::NetworkError("Couldn't set SIGPIPE to SIG_IGN", errno);
+	}
 #endif
 
-    update_stats(MSG_MAX);
+	update_stats(MSG_MAX);
 
-    if (writable) {
-	if (flags & Xapian::DB_RETRY_LOCK) {
-	    const string & body = encode_length(flags & Xapian::DB_RETRY_LOCK);
-	    update_stats(MSG_WRITEACCESS, body);
-	} else {
-	    update_stats(MSG_WRITEACCESS);
+	if (writable) {
+	    if (flags & Xapian::DB_RETRY_LOCK) {
+		const string & body = encode_length(flags & Xapian::DB_RETRY_LOCK);
+		update_stats(MSG_WRITEACCESS, body);
+	    } else {
+		update_stats(MSG_WRITEACCESS);
+	    }
 	}
+    } catch (...) {
+	// Make sure we close the connection to avoid leaking
+	// resources.
+	do_close();
+	throw;
     }
 }
 
