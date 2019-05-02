0a72deb87fbe91a91e310347df329f1b129d8a46
Olly Betts <olly@survex.com>
2019-05-02 14:29:21 +1200

    Don't close remote DB socket on EOF
    The levels above won't know we've closed it and may try to perform
    operations on it, which would be problematic if that fd gets reused in
    the meantime.  Leaving it open means any further operations will also
    get EOF.
    
    (cherry picked from commit 9ca3a2554f708a673b9d024f0a642c786761b474)

--- xapian-core/net/remoteconnection.cc
+++ xapian-core/net/remoteconnection.cc
@@ -140,7 +140,6 @@ RemoteConnection::read_at_least(size_t min_len, double end_time)
 	}
 
 	if (received == 0) {
-	    do_close(false);
 	    return false;
 	}
 
@@ -167,7 +166,6 @@ RemoteConnection::read_at_least(size_t min_len, double end_time)
 	}
 
 	if (received == 0) {
-	    do_close(false);
 	    return false;
 	}
 
