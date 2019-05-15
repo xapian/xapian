d8dd6646f12ca4d72bf8bae43b88202264d10ac4
Olly Betts <olly@survex.com>
2019-05-15 14:20:03 +1200

    scriptindex: Set TZ=UTC to fix parsedate bug
    Without this, strptime() seems to treat formats without a timezone as
    being local time, including %s.
    
    (cherry picked from commit 39937a236faf0c64097c640faa72ee5741e81508)

--- xapian-applications/omega/scriptindex.cc
+++ xapian-applications/omega/scriptindex.cc
@@ -45,6 +45,7 @@
 #include "hashterm.h"
 #include "loadfile.h"
 #include "myhtmlparse.h"
+#include "setenv.h"
 #include "str.h"
 #include "stringutils.h"
 #include "timegm.h"
@@ -1161,6 +1162,10 @@ try {
     int database_mode = Xapian::DB_CREATE_OR_OPEN;
     verbose = false;
     Xapian::Stem stemmer("english");
+
+    // Without this, strptime() seems to treat formats without a timezone as
+    // being local time, including %s.
+    setenv("TZ", "UTC", 1);
 
     constexpr auto NO_ARG = no_argument;
     constexpr auto REQ_ARG = required_argument;
