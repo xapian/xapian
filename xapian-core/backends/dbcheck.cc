xapian/xapian

commit 0cb7e0f17bbd0d2962ede26c1e78562ed82b8c63

Support stub databases in Xapian::Database::check()
Attempting to check databases for backends that don't support
checking will throw Xapian::UnimplementError.

(cherry picked from commit b77fa1e0fa826049a16bb41f3befaf3248883db5)

xapian-core/backends/dbcheck.cc
@@ -2,7 +2,7 @@
  * @brief Check the consistency of a database or table.
  */
 /* Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2019 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -39,9 +39,10 @@
 #include "chert/chert_version.h"
 #endif
 
+#include "backends.h"
+#include "databasehelpers.h"
 #include "filetests.h"
 #include "omassert.h"
-#include "posixy_wrapper.h"
 #include "stringutils.h"
 
 #include <ostream>
@@ -61,40 +62,6 @@ static const struct { char name[9]; } glass_tables[] = {
     { "synonym" }
 };
 #endif
-
-static bool
-check_if_single_file_db(const struct stat & sb, const string & path,
-			int * fd_ptr = NULL)
-{
-#ifdef XAPIAN_HAS_GLASS_BACKEND
-    if (!S_ISREG(sb.st_mode)) return false;
-    // Look at the size as a clue - if it's 0 or not a multiple of 2048,
-    // then it's not a single-file glass database.  If it is, peek at the start
-    // of the file to determine which it is.
-    if (sb.st_size == 0 || sb.st_size % 2048 != 0) return false;
-    int fd = posixy_open(path.c_str(), O_RDONLY|O_BINARY);
-    if (fd != -1) {
-	char magic_buf[14];
-	// FIXME: Don't duplicate magic check here...
-	if (io_read(fd, magic_buf, 14) == 14 &&
-	    (!fd_ptr || lseek(fd, 0, SEEK_SET) == 0) &&
-	    memcmp(magic_buf, "\x0f\x0dXapian Glass", 14) == 0) {
-	    if (fd_ptr) {
-		*fd_ptr = fd;
-	    } else {
-		::close(fd);
-	    }
-	    return true;
-	}
-	::close(fd);
-    }
-#else
-    (void)sb;
-    (void)path;
-    (void)fd_ptr;
-#endif
-    return false;
-}
 
 // FIXME: We don't currently cross-check wdf between postlist and termlist.
 // It's hard to see how to efficiently.  We do cross-check doclens, but that
@@ -293,18 +260,15 @@ check_db_dir(const string & path, int opts, std::ostream *out)
 	    "Directory does not contain a Xapian database");
 }
 
-typedef enum { UNKNOWN, CHERT, GLASS } backend_type;
-
 /** Check a database table.
  *
  *  @param filename	The filename of the table (only used to get the directory and
  *  @param opts		Xapian::check() options
  *  @param out		std::ostream to write messages to (or NULL for no messages)
- *  @param backend	Backend type
+ *  @param backend	Backend type (a BACKEND_XXX constant)
  */
 static size_t
-check_db_table_(const string & filename, int opts, std::ostream *out,
-		backend_type backend)
+check_db_table(const string& filename, int opts, std::ostream* out, int backend)
 {
     size_t p = filename.find_last_of(DIR_SEPS);
     // If we found a directory separator, advance p to the next character.  If
@@ -328,15 +292,25 @@ check_db_table_(const string & filename, int opts, std::ostream *out,
     (void)out;
 #endif
 
-    if (backend == GLASS) {
+    switch (backend) {
+      case BACKEND_GLASS: {
 #ifndef XAPIAN_HAS_GLASS_BACKEND
-	throw Xapian::FeatureUnavailableError("Glass database support isn't enabled");
+	auto msg = "Glass database support isn't enabled";
+	throw Xapian::FeatureUnavailableError(msg);
 #else
 	GlassVersion version_file(dir);
 	version_file.read();
 	return check_glass_table(tablename.c_str(), dir, version_file, opts,
 				 doclens, out);
 #endif
+      }
+
+      case BACKEND_OLD:
+	break;
+
+      default:
+	Assert(false);
+	break;
     }
 
     Assert(backend == CHERT);
@@ -361,65 +335,90 @@ check_db_table_(const string & filename, int opts, std::ostream *out,
 #endif
 }
 
-static size_t
-check_db_table(const string & filename, int opts, std::ostream *out)
-{
-    backend_type backend = UNKNOWN;
-    // Just check a single Btree, given the correct filename.  Look at the
-    // extension to determine the type.
-    if (endswith(filename, ".DB")) {
-	// It could also be flint or brass, but we check for those below.
-	backend = CHERT;
-    } else if (endswith(filename, ".glass")) {
-	backend = GLASS;
-    } else {
-	throw Xapian::DatabaseOpeningError(
-		"File is not a Xapian database or database table");
-    }
-
-    return check_db_table_(filename, opts, out, backend);
-}
-
 /** Check a single file DB from an fd.
  *
- *  Closes the fd (via GlassVersion doing so in its destructor).
+ *  Closes the fd.
  */
 static size_t
-check_db_fd(int fd, int opts, std::ostream *out)
+check_db_fd(int fd, int opts, std::ostream* out, int backend)
 {
-#ifndef XAPIAN_HAS_GLASS_BACKEND
-    (void)opts;
-    (void)out;
-    ::close(fd);
-    throw Xapian::FeatureUnavailableError("Glass database support isn't enabled");
-#else
-    // Check a single-file glass database.
-    GlassVersion version_file(fd);
-    version_file.read();
+    if (backend == BACKEND_UNKNOWN) {
+	// FIXME: Actually probe.
+	backend = BACKEND_GLASS;
+    }
 
     size_t errors = 0;
-    Xapian::docid doccount = version_file.get_doccount();
-    Xapian::docid db_last_docid = version_file.get_last_docid();
-    if (db_last_docid < doccount) {
-	if (out)
-	    *out << "last_docid = " << db_last_docid << " < doccount = "
-		 << doccount << endl;
-	++errors;
-    }
-    vector<Xapian::termcount> doclens;
-    reserve_doclens(doclens, db_last_docid, out);
+    switch (backend) {
+      case BACKEND_GLASS: {
+	// Check a single-file glass database.
+#ifdef XAPIAN_HAS_GLASS_BACKEND
+	// GlassVersion's destructor will close fd.
+	GlassVersion version_file(fd);
+	version_file.read();
 
-    // Check all the tables.
-    for (auto t : glass_tables) {
-	errors += check_glass_table(t.name, fd, version_file.get_offset(),
-				    version_file, opts, doclens,
-				    out);
+	Xapian::docid doccount = version_file.get_doccount();
+	Xapian::docid db_last_docid = version_file.get_last_docid();
+	if (db_last_docid < doccount) {
+	    if (out)
+		*out << "last_docid = " << db_last_docid << " < doccount = "
+		     << doccount << endl;
+	    ++errors;
+	}
+	vector<Xapian::termcount> doclens;
+	reserve_doclens(doclens, db_last_docid, out);
+
+	// Check all the tables.
+	for (auto t : glass_tables) {
+	    errors += check_glass_table(t.name, fd, version_file.get_offset(),
+					version_file, opts, doclens,
+					out);
+	}
+	break;
+#else
+	(void)opts;
+	(void)out;
+	::close(fd);
+	throw Xapian::FeatureUnavailableError("Glass database support isn't enabled");
+#endif
+      }
+      default:
+	Assert(false);
     }
     return errors;
-#endif
 }
 
 namespace Xapian {
+
+static size_t
+check_stub(const string& stub_path, int opts, std::ostream* out)
+{
+    size_t errors = 0;
+    read_stub_file(stub_path,
+		   [&errors, opts, out](const string& path) {
+		       errors += Database::check(path, opts, out);
+		   },
+		   [&errors, opts, out](const string& path) {
+		       // FIXME: Doesn't check the database type is chert.
+		       errors += Database::check(path, opts, out);
+		   },
+		   [&errors, opts, out](const string& path) {
+		       // FIXME: Doesn't check the database type is glass.
+		       errors += Database::check(path, opts, out);
+		   },
+		   [](const string&, const string&) {
+		       auto msg = "Remote database checking not implemented";
+		       throw Xapian::UnimplementedError(msg);
+		   },
+		   [](const string&, unsigned) {
+		       auto msg = "Remote database checking not implemented";
+		       throw Xapian::UnimplementedError(msg);
+		   },
+		   []() {
+		       auto msg = "InMemory database checking not implemented";
+		       throw Xapian::UnimplementedError(msg);
+		   });
+    return errors;
+}
 
 size_t
 Database::check_(const string * path_ptr, int fd, int opts, std::ostream *out)
@@ -431,7 +430,7 @@ Database::check_(const string * path_ptr, int fd, int opts, std::ostream *out)
     }
 
     if (path_ptr == NULL) {
-	return check_db_fd(fd, opts, out);
+	return check_db_fd(fd, opts, out, BACKEND_UNKNOWN);
     }
 
     const string & path = *path_ptr;
@@ -442,10 +441,22 @@ Database::check_(const string * path_ptr, int fd, int opts, std::ostream *out)
 	}
 
 	if (S_ISREG(sb.st_mode)) {
-	    if (check_if_single_file_db(sb, path, &fd)) {
-		return check_db_fd(fd, opts, out);
+	    int backend = test_if_single_file_db(sb, path, &fd);
+	    if (backend != BACKEND_UNKNOWN) {
+		return check_db_fd(fd, opts, out, backend);
 	    }
-	    return check_db_table(path, opts, out);
+	    // Could be a single table or a stub database file.  Look at the
+	    // extension to determine the type.
+	    if (endswith(path, ".DB")) {
+		// It could also be flint or brass, but we check for those below.
+		backend = BACKEND_CHERT;
+	    } else if (endswith(path, "." GLASS_TABLE_EXTENSION)) {
+		backend = BACKEND_GLASS;
+	    } else {
+		return check_stub(path, opts, out);
+	    }
+
+	    return check_db_table(path, opts, out, backend);
 	}
 
 	throw Xapian::DatabaseOpeningError("Not a regular file or directory");
@@ -460,18 +471,18 @@ Database::check_(const string * path_ptr, int fd, int opts, std::ostream *out)
 	filename.resize(filename.size() - 1);
     }
 
-    backend_type backend = UNKNOWN;
+    int backend = BACKEND_UNKNOWN;
     if (stat((filename + ".DB").c_str(), &sb) == 0) {
 	// It could also be flint or brass, but we check for those below.
-	backend = CHERT;
-    } else if (stat((filename + ".glass").c_str(), &sb) == 0) {
-	backend = GLASS;
+	backend = BACKEND_CHERT;
+    } else if (stat((filename + "." GLASS_TABLE_EXTENSION).c_str(), &sb) == 0) {
+	backend = BACKEND_GLASS;
     } else {
-	throw Xapian::DatabaseOpeningError(
-		"Couldn't find Xapian database or table to check", ENOENT);
+	auto msg = "Couldn't find Xapian database or table to check";
+	throw Xapian::DatabaseOpeningError(msg, ENOENT);
     }
 
-    return check_db_table_(path, opts, out, backend);
+    return check_db_table(path, opts, out, backend);
 }
 
 }

xapian-core/backends/Makefile.mk
@@ -4,6 +4,7 @@ noinst_HEADERS +=\
 	backends/byte_length_strings.h\
 	backends/contiguousalldocspostlist.h\
 	backends/database.h\
+	backends/databasehelpers.h\
 	backends/databasereplicator.h\
 	backends/document.h\
 	backends/flint_lock.h\
@@ -22,6 +23,7 @@ lib_src +=\
 	backends/alltermslist.cc\
 	backends/dbcheck.cc\
 	backends/database.cc\
+	backends/databasehelpers.cc\
 	backends/databasereplicator.cc\
 	backends/dbfactory.cc\
 	backends/slowvaluelist.cc\

xapian-core/backends/backends.h
@@ -22,6 +22,7 @@
 #define XAPIAN_INCLUDED_BACKENDS_H
 
 enum {
+    BACKEND_OLD = -2,
     BACKEND_UNKNOWN = -1,
     BACKEND_REMOTE = 0,
     BACKEND_INMEMORY = 1,

xapian-core/backends/databasehelpers.cc
@@ -1,0 +1,74 @@
+/** @file databasehelpers.cc
+ * @brief Helper functions for database handling
+ */
+/* Copyright 2002-2019 Olly Betts
+ * Copyright 2008 Lemur Consulting Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#include <config.h>
+
+#include "databasehelpers.h"
+
+#include "backends.h"
+
+#include "glass/glass_defs.h"
+
+#include "io_utils.h"
+#include "omassert.h"
+#include "posixy_wrapper.h"
+
+#include <cstring>
+#include <string>
+#include <sys/types.h>
+#include "safesysstat.h"
+#include "safeunistd.h"
+
+using namespace std;
+
+int
+test_if_single_file_db(const struct stat& sb,
+		       const string& path,
+		       int* fd_ptr)
+{
+    Assert(fd_ptr);
+#if defined XAPIAN_HAS_GLASS_BACKEND
+    if (!S_ISREG(sb.st_mode)) return BACKEND_UNKNOWN;
+    // Look at the size as a clue - if it's less than GLASS_MIN_BLOCKSIZE then
+    // it's not a single-file glass database.  For a larger file, we peek at
+    // the start of the file to determine what it is.
+    if (sb.st_size < GLASS_MIN_BLOCKSIZE)
+	return BACKEND_UNKNOWN;
+    int fd = posixy_open(path.c_str(), O_RDONLY|O_BINARY);
+    if (fd != -1) {
+	char magic_buf[14];
+	// FIXME: Don't duplicate magic check here...
+	if (io_read(fd, magic_buf, 14) == 14 &&
+	    lseek(fd, 0, SEEK_SET) == 0 &&
+	    memcmp(magic_buf, "\x0f\x0dXapian Glass", 14) == 0) {
+	    *fd_ptr = fd;
+	    return BACKEND_GLASS;
+	}
+	::close(fd);
+    }
+#else
+    (void)sb;
+    (void)path;
+    (void)fd_ptr;
+#endif
+    return BACKEND_UNKNOWN;
+}

xapian-core/backends/databasehelpers.h
@@ -1,0 +1,188 @@
+/** @file databasehelpers.h
+ * @brief Helper functions for database handling
+ */
+/* Copyright 2002-2019 Olly Betts
+ * Copyright 2008 Lemur Consulting Ltd
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
+
+#ifndef XAPIAN_INCLUDED_DATABASEHELPERS_H
+#define XAPIAN_INCLUDED_DATABASEHELPERS_H
+
+#include <fstream>
+#include <string>
+
+#include "fileutils.h"
+#include "safesysstat.h"
+#include "safeunistd.h"
+#include "str.h"
+#include "xapian/error.h"
+
+/** Probe if a path is a single-file database.
+ *
+ *  @param sb      struct statbuf from calling stat() on path
+ *  @param path    path to probe
+ *  @param fd_ptr  used to return fd open of path (unless BACKEND_UNKNOWN is
+ *                 returned)
+ *
+ *  @return  A BACKEND_* constant from backends.h:
+ *           * BACKEND_UNKNOWN : unknown (could be a stub file)
+ *           * BACKEND_GLASS : glass single file
+ */
+int
+test_if_single_file_db(const struct stat& sb,
+		       const std::string& path,
+		       int* fd_ptr);
+
+/** Open, read and process a stub database file.
+ *
+ *  Implemented as a template with separate actions for each database type.
+ */
+template<typename A1,
+	 typename A2,
+	 typename A3,
+	 typename A4,
+	 typename A5,
+	 typename A6>
+void
+read_stub_file(const std::string& file,
+	       A1 action_auto,
+	       A2 action_chert,
+	       A3 action_glass,
+	       A4 action_remote_prog,
+	       A5 action_remote_tcp,
+	       A6 action_inmemory)
+{
+    // A stub database is a text file with one or more lines of this format:
+    // <dbtype> <serialised db object>
+    //
+    // Lines which start with a "#" character are ignored.
+    //
+    // Any paths specified in stub database files which are relative will be
+    // considered to be relative to the directory containing the stub database.
+    std::ifstream stub(file.c_str());
+    if (!stub) {
+	std::string msg = "Couldn't open stub database file: ";
+	msg += file;
+	throw Xapian::DatabaseNotFoundError(msg, errno);
+    }
+    std::string line;
+    unsigned int line_no = 0;
+    while (std::getline(stub, line)) {
+	++line_no;
+	if (line.empty() || line[0] == '#')
+	    continue;
+	std::string::size_type space = line.find(' ');
+	if (space == std::string::npos) space = line.size();
+
+	std::string type(line, 0, space);
+	line.erase(0, space + 1);
+
+	if (type == "auto") {
+	    resolve_relative_path(line, file);
+	    action_auto(line);
+	    continue;
+	}
+
+	if (type == "chert") {
+#ifdef XAPIAN_HAS_CHERT_BACKEND
+	    resolve_relative_path(line, file);
+	    action_chert(line);
+	    continue;
+#else
+	    throw Xapian::FeatureUnavailableError("Chert backend disabled");
+#endif
+	}
+
+	if (type == "glass") {
+#ifdef XAPIAN_HAS_GLASS_BACKEND
+	    resolve_relative_path(line, file);
+	    action_glass(line);
+	    continue;
+#else
+	    throw Xapian::FeatureUnavailableError("Glass backend disabled");
+#endif
+	}
+
+	if (type == "remote" && !line.empty()) {
+#ifdef XAPIAN_HAS_REMOTE_BACKEND
+	    if (line[0] == ':') {
+		// prog
+		// FIXME: timeouts
+		// Is it a security risk?
+		space = line.find(' ');
+		std::string args;
+		if (space != std::string::npos) {
+		    args.assign(line, space + 1, std::string::npos);
+		    line.assign(line, 1, space - 1);
+		} else {
+		    line.erase(0, 1);
+		}
+		action_remote_prog(line, args);
+		continue;
+	    }
+	    std::string::size_type colon = line.rfind(':');
+	    if (colon != std::string::npos) {
+		// tcp
+		// FIXME: timeouts
+		// Avoid misparsing an IPv6 address without a port number.  The
+		// port number is required, so just leave that case to the
+		// error handling further below.
+		if (!(line[0] == '[' && line.back() == ']')) {
+		    unsigned int port = atoi(line.c_str() + colon + 1);
+		    line.erase(colon);
+		    if (line[0] == '[' && line.back() == ']') {
+			line.erase(line.size() - 1, 1);
+			line.erase(0, 1);
+		    }
+		    action_remote_tcp(line, port);
+		    continue;
+		}
+	    }
+#else
+	    throw Xapian::FeatureUnavailableError("Remote backend disabled");
+#endif
+	}
+
+	if (type == "inmemory" && line.empty()) {
+#ifdef XAPIAN_HAS_INMEMORY_BACKEND
+	    action_inmemory();
+	    continue;
+#else
+	    throw Xapian::FeatureUnavailableError("Inmemory backend disabled");
+#endif
+	}
+
+	if (type == "flint") {
+	    auto msg = "Flint backend no longer supported";
+	    throw Xapian::FeatureUnavailableError(msg);
+	}
+
+	// Don't include the line itself - that might help an attacker
+	// by revealing part of a sensitive file's contents if they can
+	// arrange for it to be read as a stub database via infelicities in
+	// an application which uses Xapian.  The line number is enough
+	// information to identify the problem line.
+	std::string msg = file;
+	msg += ':';
+	msg += str(line_no);
+	msg += ": Bad line";
+	throw Xapian::DatabaseOpeningError(msg);
+    }
+}
+
+#endif

xapian-core/backends/dbfactory.cc
@@ -29,6 +29,8 @@
 #include "xapian/error.h"
 #include "xapian/version.h" // For XAPIAN_HAS_XXX_BACKEND.
 
+#include "backends.h"
+#include "databasehelpers.h"
 #include "debuglog.h"
 #include "filetests.h"
 #include "fileutils.h"
@@ -56,169 +58,7 @@
 
 using namespace std;
 
-static bool
-check_if_single_file_db(const struct stat & sb, const string & path,
-			int * fd_ptr = NULL)
-{
-#ifdef XAPIAN_HAS_GLASS_BACKEND
-    if (!S_ISREG(sb.st_mode)) return false;
-    // Look at the size as a clue - if it's 0 or not a multiple of 2048,
-    // then it's not a single-file glass database.  If it is, peek at the start
-    // of the file to determine which it is.
-    if (sb.st_size == 0 || sb.st_size % 2048 != 0) return false;
-    int fd = posixy_open(path.c_str(), O_RDONLY|O_BINARY);
-    if (fd != -1) {
-	char magic_buf[14];
-	// FIXME: Don't duplicate magic check here...
-	if (io_read(fd, magic_buf, 14) == 14 &&
-	    lseek(fd, 0, SEEK_SET) == 0 &&
-	    memcmp(magic_buf, "\x0f\x0dXapian Glass", 14) == 0) {
-	    if (fd_ptr) {
-		*fd_ptr = fd;
-	    } else {
-		::close(fd);
-	    }
-	    return true;
-	}
-	::close(fd);
-    }
-#else
-    (void)sb;
-    (void)path;
-    (void)fd_ptr;
-#endif
-    return false;
-}
-
 namespace Xapian {
-
-template<typename A1,
-	 typename A2,
-	 typename A3,
-	 typename A4,
-	 typename A5,
-	 typename A6>
-static void
-read_stub_file(const string& file,
-	       A1 action_auto,
-	       A2 action_chert,
-	       A3 action_glass,
-	       A4 action_remote_prog,
-	       A5 action_remote_tcp,
-	       A6 action_inmemory)
-{
-    // A stub database is a text file with one or more lines of this format:
-    // <dbtype> <serialised db object>
-    //
-    // Lines which start with a "#" character are ignored.
-    //
-    // Any paths specified in stub database files which are relative will be
-    // considered to be relative to the directory containing the stub database.
-    ifstream stub(file.c_str());
-    if (!stub) {
-	string msg = "Couldn't open stub database file: ";
-	msg += file;
-	throw Xapian::DatabaseNotFoundError(msg, errno);
-    }
-    string line;
-    unsigned int line_no = 0;
-    while (getline(stub, line)) {
-	++line_no;
-	if (line.empty() || line[0] == '#')
-	    continue;
-	string::size_type space = line.find(' ');
-	if (space == string::npos) space = line.size();
-
-	string type(line, 0, space);
-	line.erase(0, space + 1);
-
-	if (type == "auto") {
-	    resolve_relative_path(line, file);
-	    action_auto(line);
-	    continue;
-	}
-
-	if (type == "chert") {
-#ifdef XAPIAN_HAS_CHERT_BACKEND
-	    resolve_relative_path(line, file);
-	    action_chert(line);
-	    continue;
-#else
-	    throw FeatureUnavailableError("Chert backend disabled");
-#endif
-	}
-
-	if (type == "glass") {
-#ifdef XAPIAN_HAS_GLASS_BACKEND
-	    resolve_relative_path(line, file);
-	    action_glass(line);
-	    continue;
-#else
-	    throw FeatureUnavailableError("Glass backend disabled");
-#endif
-	}
-
-	if (type == "remote" && !line.empty()) {
-#ifdef XAPIAN_HAS_REMOTE_BACKEND
-	    if (line[0] == ':') {
-		// prog
-		// FIXME: timeouts
-		// Is it a security risk?
-		space = line.find(' ');
-		string args;
-		if (space != string::npos) {
-		    args.assign(line, space + 1, string::npos);
-		    line.assign(line, 1, space - 1);
-		} else {
-		    line.erase(0, 1);
-		}
-		action_remote_prog(line, args);
-		continue;
-	    }
-	    string::size_type colon = line.rfind(':');
-	    if (colon != string::npos) {
-		// tcp
-		// FIXME: timeouts
-		// Avoid misparsing an IPv6 address without a port number.  The
-		// port number is required, so just leave that case to the
-		// error handling further below.
-		if (!(line[0] == '[' && line.back() == ']')) {
-		    unsigned int port = atoi(line.c_str() + colon + 1);
-		    line.erase(colon);
-		    if (line[0] == '[' && line.back() == ']') {
-			line.erase(line.size() - 1, 1);
-			line.erase(0, 1);
-		    }
-		    action_remote_tcp(line, port);
-		    continue;
-		}
-	    }
-#else
-	    throw FeatureUnavailableError("Remote backend disabled");
-#endif
-	}
-
-	if (type == "inmemory" && line.empty()) {
-#ifdef XAPIAN_HAS_INMEMORY_BACKEND
-	    action_inmemory();
-	    continue;
-#else
-	    throw FeatureUnavailableError("Inmemory backend disabled");
-#endif
-	}
-
-	if (type == "flint") {
-	    throw FeatureUnavailableError("Flint backend no longer supported");
-	}
-
-	// Don't include the line itself - that might help an attacker
-	// by revealing part of a sensitive file's contents if they can
-	// arrange for it to be read as a stub database via infelicities in
-	// an application which uses Xapian.  The line number is enough
-	// information to identify the problem line.
-	throw DatabaseOpeningError(file + ':' + str(line_no) + ": Bad line");
-    }
-}
 
 static void
 open_stub(Database& db, const string& file)
@@ -327,7 +167,8 @@ Database::Database(const string &path, int flags)
 
 	// Initialise to avoid bogus warning from GCC 4.9.2 with -Os.
 	int fd = -1;
-	if (check_if_single_file_db(statbuf, path, &fd)) {
+	switch (test_if_single_file_db(statbuf, path, &fd)) {
+	  case BACKEND_GLASS:
 #ifdef XAPIAN_HAS_GLASS_BACKEND
 	    // Single file glass format.
 	    internal.push_back(new GlassDatabase(fd));

xapian-core/backends/glass/glass_defs.h
@@ -1,7 +1,7 @@
 /** @file glass_defs.h
  * @brief Definitions, types, etc for use inside glass.
  */
-/* Copyright (C) 2010,2014,2015 Olly Betts
+/* Copyright (C) 2010,2014,2015,2017 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,6 +28,12 @@
 
 /// Default B-tree block size.
 #define GLASS_DEFAULT_BLOCKSIZE 8192
+
+/// Minimum B-tree block size.
+#define GLASS_MIN_BLOCKSIZE 2048
+
+/// Maximum B-tree block size.
+#define GLASS_MAX_BLOCKSIZE 65536
 
 /** The largest docid value supported by glass.
  *

xapian-core/tests/api_db.cc
@@ -2,7 +2,7 @@
  *
  * Copyright 1999,2000,2001 BrightStation PLC
  * Copyright 2002 Ananova Ltd
- * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2011,2012,2013,2015,2016,2017 Olly Betts
+ * Copyright 2002,2003,2004,2005,2006,2007,2008,2009,2011,2012,2013,2015,2016,2017,2019 Olly Betts
  * Copyright 2006,2007,2008,2009 Lemur Consulting Ltd
  *
  * This program is free software; you can redistribute it and/or
@@ -90,6 +90,8 @@ DEFINE_TESTCASE(stubdb1, path) {
 	enquire.set_query(Xapian::Query("word"));
 	enquire.get_mset(0, 10);
     }
+
+    TEST_EQUAL(Xapian::Database::check(dbpath), 0);
 
     return true;
 }
@@ -340,10 +342,31 @@ DEFINE_TESTCASE(stubdb8, inmemory) {
     try {
 	Xapian::Database::check(dbpath);
 	FAIL_TEST("Managed to check inmemory stub");
-    } catch (const Xapian::DatabaseOpeningError & e) {
+    } catch (const Xapian::UnimplementedError& e) {
 	// Check the message is appropriate.
 	TEST_STRINGS_EQUAL(e.get_msg(),
-			   "File is not a Xapian database or database table");
+			   "InMemory database checking not implemented");
+    }
+    return true;
+}
+
+/// Test error running Database::check() on a remote stub database.
+DEFINE_TESTCASE(stubdb9, path) {
+    mkdir(".stub", 0755);
+    const char * dbpath = ".stub/stubdb9";
+    ofstream out(dbpath);
+    TEST(out.is_open());
+    out << "remote :" << BackendManager::get_xapian_progsrv_command()
+	<< ' ' << get_database_path("apitest_simpledata") << endl;
+    out.close();
+
+    try {
+	Xapian::Database::check(dbpath);
+	FAIL_TEST("Managed to check remote stub");
+    } catch (const Xapian::UnimplementedError& e) {
+	// Check the message is appropriate.
+	TEST_STRINGS_EQUAL(e.get_msg(),
+			   "Remote database checking not implemented");
     }
     return true;
 }

commit 60e89e2f2d3a073cbaa1c4b0af62ac014ce6ed76

Fix build with --enable-assertions
Broken by 0cb7e0f17bbd0d2962ede26c1e78562ed82b8c63.

xapian-core/backends/dbcheck.cc
@@ -313,7 +313,7 @@ check_db_table(const string& filename, int opts, std::ostream* out, int backend)
 	break;
     }
 
-    Assert(backend == CHERT);
+    Assert(backend == BACKEND_CHERT);
     // Flint and brass also used the extension ".DB", so check that we
     // haven't been passed a single table in a flint or brass database.
     struct stat sb;