7d4ba3aa857685ae5fbcdc21b3c0e0b3e6bedd33
Bruno Baruffaldi <baruffaldibruno@gmail.com>
2019-07-09 19:31:29 -0300

    fixup! fixup! fixup! fixup! fixup! Add e-book support to Omega

--- xapian-applications/omega/handler_libebook.cc
+++ xapian-applications/omega/handler_libebook.cc
@@ -161,7 +161,7 @@ extract(const string& filename,
 	RVNGTextTextGenerator content(content_dump, false);
 
 	if (EBOOKDocument::RESULT_OK !=
-	    EBOOKDocument::parse(input.get(), &content, type)){
+	    EBOOKDocument::parse(input.get(), &content, type)) {
 	    return false;
 	}
 	clear_text(dump, content_dump.cstr());
