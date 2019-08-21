01ac0308d534f8f982e52a248d73690cdfbec8bf
Bruno Baruffaldi <baruffaldibruno@gmail.com>
2019-08-21 14:40:17 -0300

    fixup! fixup! Add omindextest to Omega

--- xapian-applications/omega/omindexcheck.cc
+++ xapian-applications/omega/omindexcheck.cc
@@ -1,5 +1,5 @@
 /** @file omindexcheck.cc
- * @brief test omindex
+ * @brief Auxiliar program of omindextest
  */
 /* Copyright (C) 2019 Bruno Baruffaldi
  *
@@ -33,157 +33,34 @@
 
 using namespace std;
 
-typedef struct {
-    string title;
-    string author;
-    string dump;
-}testcase;
+typedef vector<string> testcase;
 
 unordered_map<string, testcase> tests;
 
 static void
 index_test(void)
 {
-    tests.insert({"test.txt", {"", "",
-    "Joey doesn't share food "
-    "Джои не делится едой 喬伊不分享食物 "}});
-    tests.insert({"test-csv.csv", {"", "",
-    "We were on a break ! 1 2 3 4 5 6 0.1 0.2 0.3 0.01 40.2 $9,00"}});
-    tests.insert({"test-html.html", {"Hello World", "Jeroen Ooms",
-    "helloworldjeroenooms3/20/2018chapter1subsection1todayisabeautifulday!1"}});
+    tests.insert({"test.txt", {"Zjoey", "Zfood", "Zедой", "Z喬伊不分享食物"}});
 #if defined HAVE_POPPLER
-    tests.insert({"pdf/hello.pdf", {"Hello World", "Jeroen Ooms",
-    "helloworldjeroenooms3/20/2018chapter1subsection1todayisabeautifulday!1"}});
-    tests.insert({"pdf/poppler.pdf", {"PDFTITLE", "Someone",
-    "Abstracthereisyourabstract1topicsometexttotestomindex.1"}});
+    tests.insert({"pdf/hello.pdf", {"Shello", "Sworld", "Ajeroen", "Aooms",
+		  "Zhello", "Zworld", "Zsubsect"}});
+    tests.insert({"pdf/poppler.pdf", {"Zabstract", "Zkeyword", "Ztopic",
+		  "Ztext", "Spdftitle"}});
 #endif
-#if defined HAVE_LIBEBOOK
-    tests.insert({"fb2/hello.fb2", {"", "Jeroen Ooms",
-    "helloworldjeroenooms3/20/2018chapter1subsection1todayisabeautifulday!1"
-    "documentoutlinechapter1subsection1"}});
-    tests.insert({"fb2/lang-name.fb2",
-    {"", "David Tardon", "myhovercraftisfullofeels."}});
-    tests.insert({"fb2/lang2.fb2", {"", "David Tardon",
-    "myhovercraftisfullofeels.mojevznášedlojeplnéúhořů."
-    "mójpoduszkowiecjestpełenwęgorzy."}});
-    tests.insert({"lrf/hello.lrf",
-    {"", "", "helloworld."}});
-    tests.insert({"pdb/test.pdb",
-    {"demodemo", "", "wherearewe"}});
-    tests.insert({"pdb/PalmDOC-hello.pdb",
-    {"helloworld", "", "helloworld."}});
-    tests.insert({"pdb/PeanutPress-hello.pdb",
-    {"", "", "helloworld."}});
-#endif
-#if defined HAVE_LIBETONYEK
-    tests.insert({"keynotes/test-keynote.key", {"", "",
-    "titleslidesubtitlecapitalizedtitletextinboldanditalic,"
-    "continuinginitaliconly,untilnowcoloredtextmorecoloredtexthere"}});
-    tests.insert({"pages/test-pages.pages", {"", "",
-    "myhovercraftisfullofeels.mévznášedlojeplnéúhořů."
-    "mójpoduszkowiecjestpełenwęgorzy."}});
-#endif
-#if defined HAVE_TESSERACT
-    tests.insert({"img/poster.jpg", {"", "",
-    "SKATEBOARDINGBICYCLERIDINGROLLERBLADINGSCOOTERRIDING®"}});
-    tests.insert({"img/poster-2.jpg", {"", "",
-    "THISPROPERTYISPROTECTEDBYVIDEOSURVEILLANCE"}});
-    tests.insert({"img/scan-page.png", {"", "",
-    "of9.5%annuallywhiletheFed-eratedjunkfundreturned"
-    "11.9%fearoffinancialcollapse,"}});
-    tests.insert({"img/Test1.gif", {"", "",
-    "noisy image to test tesseract ocr"}});
-    tests.insert({"img/Test2.pgm", {"", "",
-    "noisy image to test tesseract ocr"}});
-    tests.insert({"img/Test3.ppm", {"", "",
-    "noisy image to test tesseract ocr"}});
-    tests.insert({"img/Test4.tiff", {"", "",
-    "noisy image to test tesseract ocr"}});
-    tests.insert({"img/Test5.webp", {"", "",
-    "noisy image to test tesseract ocr"}});
-#endif
-#if defined HAVE_GMIME
-    tests.insert({"eml/test-correo1.eml", {"asunto", "Me<me@mail.com>",
-    "prueba para descargar correos--atte.me"}});
-    tests.insert({"eml/test-correo2.eml",
-    {"полностью переписана статья", "Me<me@mail.com>",
-    "Венгерский алгоритм решения задачи"
-    "о назначениях--Atte.BrunoBaruffaldi"}});
-#endif
-#if defined HAVE_CATDOC
-    tests.insert({"xls/test-xls.xls", {"", "",
-    "I am testing catdoc with calc 1 2 3 0.4 5.01 1.33%"}});
-#endif
-}
-
-static testcase
-get_from_data(const string& data)
-{
-    testcase* t = new testcase;
-    size_t len = data.length();
-    const char* p = data.c_str();
-    const char* end = p + len;
-
-    while (p != end) {
-	const char* start = p;
-	size_t start_l = start - data.c_str();
-	p = static_cast<const char*>(memchr(p, '\n', end - start));
-	const char* eol;
-	if (p)
-	    eol = p++;
-	else
-	    p = eol = end;
-	if ((end - start) > 8 && memcmp(start, "caption=", 8) == 0) {
-	    start_l += 8;
-	    t->title.assign(data, start_l, data.find('\n', start_l) - start_l);
-	} else if ((end - start) > 7 && memcmp(start, "author=", 7) == 0) {
-	    start_l += 7;
-	    t->author.assign(data, start_l, data.find('\n', start_l) - start_l);
-	} else if ((end - start) > 7 && memcmp(start, "sample=", 7) == 0) {
-	    start_l += 7;
-	    t->dump.assign(data, start_l, data.find('\n', start_l) - start_l);
-	}
-    }
-    return *t;
 }
 
 static bool
-compare_text(const char* s, const char* t)
+compare_test(testcase& test, const Xapian::Document& doc, const string& file)
 {
-    int s_it, t_it;
-    for (s_it = t_it = 0; s[s_it] != '\0' && t[t_it] != '\0'; ++s_it, ++t_it) {
-	while (isspace(s[s_it]) && s[s_it] != '\0') s_it++;
-	while (isspace(t[t_it]) && t[t_it] != '\0') t_it++;
-	if (s[s_it] == '\0' || t[t_it] == '\0')
-	    break;
-	if (tolower(s[s_it]) != tolower(t[t_it]))
+    sort(test.begin(), test.end());
+    Xapian::TermIterator term_iterator = doc.termlist_begin();
+    for(auto& t: test){
+	term_iterator.skip_to(t);
+	if(term_iterator == doc.termlist_end() || *term_iterator != t){
+	    cerr << "Error in " << file << ": Term " << t <<
+		 " does not belong to this file" << endl;
 	    return false;
-    }
-    while (isspace(s[s_it]) && s[s_it] != '\0') s_it++;
-    while (isspace(t[t_it]) && t[t_it] != '\0') t_it++;
-    return s[s_it] == '\0' && t[t_it] == '\0';
-}
-
-static bool
-compare_test(const string& file, const testcase& x, const testcase& y)
-{
-    if (!compare_text(x.author.c_str(), y.author.c_str())) {
-	cerr << "Error " << file << ": The author is wrong" << endl;
-	cerr << "\tExpected: " << x.author << endl;
-	cerr << "\tGot: " << y.author << endl;
-	return false;
-    }
-    if (!compare_text(x.title.c_str(), y.title.c_str())) {
-	cerr << "Error " << file << ": The title is wrong" << endl;
-	cerr << "\tExpected: " << x.title << endl;
-	cerr << "\tGot: " << y.title << endl;
-	return false;
-    }
-    if (!compare_text(x.dump.c_str(), y.dump.c_str())) {
-	cerr << "Error " << file << ": The dump is wrong" << endl;
-	cerr << "\tExpected: " << x.dump << endl;
-	cerr << "\tGot: " << y.dump << endl;
-	return false;
+	}
     }
     return true;
 }
@@ -220,10 +97,9 @@ main(int argc, char** argv)
 	url.assign(data, start, data.find('\n', start) - start);
 	start = url.find(current_dir) + current_dir.length();
 	url = url.substr(start, url.length());
-	if (tests.find(url) != tests.end()) {
-	    testcase dc = get_from_data(data);
-	    succeed &= compare_test(url, tests[url], dc);
-	}
+	auto it = tests.find(url);
+	if (it != tests.end())
+	    succeed &= compare_test(it->second, doc, url);
     }
     return succeed ? 0 : 1;
 }
