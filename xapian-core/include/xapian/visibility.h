797c79bd5bc4e0217ca54215d5722afb6868feb0
Olly Betts <olly@survex.com>
2019-10-16 16:36:24 +1300

    Don't check __GNUC__ in visibility.h
    The configure check gating defining XAPIAN_ENABLE_VISIBILITY checks
    that the visibility attributes work so there's no need to disallow
    this for a hypothetical compiler which supports them but doesn't define
    __GNUC__.

--- xapian-core/include/xapian/visibility.h
+++ xapian-core/include/xapian/visibility.h
@@ -1,7 +1,7 @@
 /** @file visibility.h
- * @brief Define XAPIAN_VISIBILITY macro.
+ * @brief Define XAPIAN_VISIBILITY_* macros.
  */
-// Copyright (C) 2007,2010,2014,2017 Olly Betts
+// Copyright (C) 2007,2010,2014,2017,2019 Olly Betts
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
 // symbol visibility support.
 
 #include "xapian/version.h"
-#if defined XAPIAN_ENABLE_VISIBILITY && defined __GNUC__
+#ifdef XAPIAN_ENABLE_VISIBILITY
 # define XAPIAN_VISIBILITY_DEFAULT __attribute__((visibility("default")))
 # define XAPIAN_VISIBILITY_INTERNAL __attribute__((visibility("internal")))
 #else
