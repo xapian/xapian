a98c65f46bddc284a1d2c711eb4e9f6210e6101f
barufa <baruffaldibruno@gmail.com>
2019-12-27 16:54:20 -0300

    fixup! fixup! fixup! fixup! fixup! fixup! Add omindextest to Omega

--- xapian-applications/omega/omindexcheck.cc
+++ xapian-applications/omega/omindexcheck.cc
@@ -42,11 +42,47 @@ index_test()
 {
     tests.insert({"test.txt", {"Zjoey", "Zfood",
 		  "Zедой", "Z喬伊不分享食物"}});
+    tests.insert({"test-csv.csv", {"ZFcsv", "Zbreak", "Zwere"}});
+    tests.insert({"test-html.html", {"Ajeroen", "ZAoom", "ZSworld", "Shello",
+		  "Zchapter"}});
 #if defined HAVE_POPPLER
-    tests.insert({"pdf/hello.pdf", {"Shello", "Sworld", "Ajeroen", "Aooms",
-		  "Zhello", "Zworld", "Zsubsect"}});
-    tests.insert({"pdf/poppler.pdf", {"Zabstract", "Zkeyword", "Ztopic",
-		  "Ztext", "Spdftitle"}});
+    tests.insert({"pdf/poppler.pdf", {"ZFpoppler", "Zsub", "Ztext", "Ztitl",
+		  "Zpie"}});
+#endif
+#if defined HAVE_LIBEBOOK
+    tests.insert({"fb2/hello.fb2", {"Ajeroen", "Aooms", "Zbeauti", "Zchapter",
+		  "Zdocument", "Zooms3", "Zsubsect", "Ztoday", "ZFhello",
+		  "Zoutlin"}});
+    tests.insert({"fb2/lang-name.fb2", {"Adavid", "Atardon", "ZAdavid",
+		  "ZFlang", "Zeel", "Zhovercraft", "Zlanguag"}});
+    tests.insert({"fb2/lang2.fb2", {"Adavid", "Atardon", "ZAtardon",
+		  "Zúhořů", "Zhovercraft", "Zlanguag", "Zmój", "Zof",
+		  "Zpełen", "Zpoduszkowiec", "Zvznášedlo", "Zwęgorzi"}});
+    tests.insert({"lrf/hello.lrf", {"ZFhello", "Zhello", "Zworld"}});
+    tests.insert({"pdb/PalmDOC-hello.pdb", {"Shello", "Sworld", "ZFpalmdoc",
+		  "Zworld"}});
+    tests.insert({"pdb/PeanutPress-hello.pdb", {"Fpeanutpress", "ZFhello",
+		  "Zhello"}});
+    tests.insert({"pdb/test.pdb", {"Sdemodemo", "ZFtest", "Zwherearew"}});
+#endif
+#if defined HAVE_LIBETONYEK
+    tests.insert({"keynotes/test-keynote.key", {"ZFkeynot", "Zbold", "Znow",
+		  "Zsubtitl"}});
+    tests.insert({"pages/test-pages.pages", {"ZFpage", "Zfull", "Zhovercraft",
+		  "Zwęgorzi"}});
+#endif
+#if defined HAVE_TESSERACT
+    tests.insert({"img/Test1.gif", {"Znoisyimag", "Zocr", "Ztesseract"}});
+    tests.insert({"img/Test2.pgm", {"ZFtest2", "Znoisyimag", "Ztesseract"}});
+    tests.insert({"img/Test3.ppm", {"ZFtest3", "Zocr", "Ztest"}});
+    tests.insert({"img/Test4.tiff", {"Znoisyimag", "Ztesseract", "Zto"}});
+    tests.insert({"img/Test5.webp", {"Znoisyimag", "Ztesseract", "Ztest"}});
+    tests.insert({"img/poster-2.jpg", {"ZFposter", "Zby", "Zproperti",
+		  "Zsurveil", "Zvideo"}});
+    tests.insert({"img/poster.jpg", {"Zbicycl", "Zride", "Zroller",
+		  "Zskateboard"}});
+    tests.insert({"img/scan-page.png", {"Zannual", "Zfed", "Zreturn",
+		  "Zwhile"}});
 #endif
 }
 
