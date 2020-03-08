d0fb966e8a005415b503835e6b38c55d44345223
boda sadalla <boda998@yahoo.com>
2020-03-08 11:14:42 +0200

    a

--- xapian-core/api/query.cc
+++ xapian-core/api/query.cc
@@ -75,8 +75,7 @@ Query::Query(op op_, const Xapian::Query & subquery, double factor)
 	case OP_VALUE_LE:
 	case OP_VALUE_GT:
 	case OP_VALUE_LT:
-	case OP_VALUE_EQ:
-	
+	case OP_VALUE_EQ:	
 	    // These operators always return weight 0, so OP_SCALE_WEIGHT has
 	    // no effect on them.
 	    internal = subquery.internal;
@@ -95,13 +94,13 @@ Query::Query(op op_, Xapian::valueno slot, const std::string & limit)
 	if (limit.empty())
 	    internal = new Xapian::Internal::QueryTerm();
 	else
-	    internal = new Xapian::Internal::QueryValueGE(slot, limit);
-    }else if (usual(op_ == OP_VALUE_GT)){
+	    internal = new Xapian::Internal::QueryValueGE(slot,limit);
+    } else if (usual(op_ == OP_VALUE_GT)) {
 	internal = new Xapian::Internal::QueryValueGT(slot,limit);
-	}else if (usual(op_ == OP_VALUE_LE)) {
-	internal = new Xapian::Internal::QueryValueLE(slot, limit);
+	} else if (usual(op_ == OP_VALUE_LE)) {
+	internal = new Xapian::Internal::QueryValueLE(slot,limit);
 	} else if (usual(op_ == OP_VALUE_LT)) {
-	internal = new Xapian::Internal::QueryValueLT(slot, limit);
+	internal = new Xapian::Internal::QueryValueLT(slot,limit);
     } else {
 	throw Xapian::InvalidArgumentError("op must be OP_VALUE_LE or OP_VALUE_LT or OP_VALUE_GE or OP_VALUE_GT");
     }
