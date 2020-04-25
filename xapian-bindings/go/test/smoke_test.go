50896e98a8ecfab832b1b7727eb3f3ab003f0dcd
Srinivas Yadav <srinivasyadav@Srinivass-MacBook-Pro.local>
2020-04-25 08:09:50 +0530

    checking x86

--- xapian-bindings/go/test/smoke_test.go
+++ xapian-bindings/go/test/smoke_test.go
@@ -2,23 +2,29 @@ package main
 
 import ("xapian")
 import ("testing")
-
+import ("fmt")
 func TestMajorVersion(t *testing.T){
 	actual := xapian.Major_version()
+	fmt.Print("Running test: MajorVersion... ")
 	expected := 1
 	if actual!=expected {
 		t.Error("fail")
+	}else{
+		fmt.Println("ok")
 	}
 }
 
 func TestDocument(t *testing.T){
 	doc := xapian.NewDocument()
         input_str := "hello go xapian"
-        doc.Set_data(input_str)
+        fmt.Print("Running test: Document... ")
+	doc.Set_data(input_str)
         output_str := doc.Get_data()
 
         if input_str != output_str{
 		t.Error("Build Failed")
+	}else{
+		fmt.Println("ok")
 	}
 }
 
