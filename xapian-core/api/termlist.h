xapian/xapian

commit 145503bbe4a5bf702cd13cb2e592111e8d7ca89a

Reimplement Database and WritableDatabase
WritableDatabase::remove_spelling() now works more sensibly with shards
- it tries to remove the spelling from each shard until all of freqdec
has been dealt with, and now returns any "unused" freqdec.  To support
this for the remote backend, the remote protocol major version has been
bumped.

Database and WritableDatabase are now reference counted handles like
most other API classes - previously they worked like a typedef to
vector<shard> which makes copying and assignment O(#shards).  The
main observable difference is that if you add a shard to a copy,
it's now added to the original (which is consistent with other API
classes, e.g. adding a document to an RSet).

WritableDatabase has supported sharding since 1.3.2, but this is now
better documented.

Adding a read-only shard to a WritableDatabase is now statically
caught at compile time in simple cases, and if the static check is
bypassed then InvalidOperationError is thrown when you attempt a
WritableDatabase operation on that shard.  Previously such a shard acted
as a black hole which quietly accepted and ignored changes.

Operations on a database with a single shard should have a bit less
overhead.

Spelling correction with multiple shards should be faster.

xapian-core/backends/inmemory/inmemory_database.cc
@@ -399,12 +399,11 @@ InMemoryAllDocsPostList::get_description() const
 // Actual database class //
 ///////////////////////////
 
+// Updates are applied immediately so we can't support transactions.
 InMemoryDatabase::InMemoryDatabase()
-	: totdocs(0), totlen(0), positions_present(false), closed(false)
+    : Xapian::Database::Internal(TRANSACTION_UNIMPLEMENTED),
+      totdocs(0), totlen(0), positions_present(false), closed(false)
 {
-    // Updates are applied immediately so we can't support transactions.
-    transaction_state = TRANSACTION_UNIMPLEMENTED;
-
     // We keep an empty entry in postlists for convenience of implementing
     // allterms iteration and returning a PostList for an absent term.
     postlists.insert(make_pair(string(), InMemoryTerm()));
@@ -436,22 +435,28 @@ InMemoryDatabase::close()
     closed = true;
 }
 
-LeafPostList *
-InMemoryDatabase::open_post_list(const string & tname) const
+PostList*
+InMemoryDatabase::open_post_list(const string& term) const
+{
+    return InMemoryDatabase::open_leaf_post_list(term);
+}
+
+LeafPostList*
+InMemoryDatabase::open_leaf_post_list(const string& term) const
 {
     if (closed) InMemoryDatabase::throw_database_closed();
-    if (tname.empty()) {
+    if (term.empty()) {
 	intrusive_ptr<const InMemoryDatabase> ptrtothis(this);
 	return new InMemoryAllDocsPostList(ptrtothis);
     }
-    map<string, InMemoryTerm>::const_iterator i = postlists.find(tname);
+    map<string, InMemoryTerm>::const_iterator i = postlists.find(term);
     if (i == postlists.end() || i->second.term_freq == 0) {
 	i = postlists.begin();
 	// Check that our dummy entry for string() is present.
 	Assert(i->first.empty());
     }
     intrusive_ptr<const InMemoryDatabase> ptrtothis(this);
-    return new InMemoryPostList(ptrtothis, i->second, tname);
+    return new InMemoryPostList(ptrtothis, i->second, term);
 }
 
 bool
@@ -565,6 +570,12 @@ InMemoryDatabase::open_term_list(Xapian::docid did) const
     }
     return new InMemoryTermList(intrusive_ptr<const InMemoryDatabase>(this), did,
 				termlists[did - 1], doclengths[did - 1]);
+}
+
+TermList *
+InMemoryDatabase::open_term_list_direct(Xapian::docid did) const
+{
+    return InMemoryDatabase::open_term_list(did);
 }
 
 Xapian::Document::Internal *
@@ -919,7 +930,9 @@ bool
 InMemoryDatabase::term_exists(const string & tname) const
 {
     if (closed) InMemoryDatabase::throw_database_closed();
-    Assert(!tname.empty());
+    if (tname.empty()) {
+	return totdocs != 0;
+    }
     map<string, InMemoryTerm>::const_iterator i = postlists.find(tname);
     if (i == postlists.end()) return false;
     return (i->second.term_freq != 0);
@@ -945,4 +958,10 @@ void
 InMemoryDatabase::throw_database_closed()
 {
     throw Xapian::DatabaseError("Database has been closed");
+}
+
+string
+InMemoryDatabase::get_description() const
+{
+    return "InMemory";
 }

commit 7ee01561fc687f77fce621ef07fabbc064970169

Fix query expansion on sharded database
The mechanism for passing in which shard a TermList is from wasn't
hooked up and as a result we always think it's from the first shard.
This means our suggested terms may not be as good as they should be
in this situation.

Probably broken by 145503bbe4a5bf702cd13cb2e592111e8d7ca89a but I
haven't tested revisions before/after that to confirm this.

xapian-core/api/termlist.h
@@ -1,7 +1,7 @@
 /** @file termlist.h
  * @brief Abstract base class for termlists.
  */
-/* Copyright (C) 2007,2010,2013 Olly Betts
+/* Copyright (C) 2007,2010,2013,2020 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -108,6 +108,12 @@ class Xapian::TermIterator::Internal : public Xapian::Internal::intrusive_base {
 
     /// Return PositionList for the current position.
     virtual PositionList* positionlist_begin() const = 0;
+
+    /** Which shard of a multidatabase this is from.
+     *
+     *  Used by Enquire::get_eset().
+     */
+    size_t shard_index = 0;
 };
 
 // In the external API headers, this class is Xapian::TermIterator::Internal,

xapian-core/backends/glass/glass_termlist.cc
@@ -113,7 +113,8 @@ GlassTermList::accumulate_stats(Xapian::Internal::ExpandStats & stats) const
 {
     LOGCALL_VOID(DB, "GlassTermList::accumulate_stats", stats);
     Assert(!at_end());
-    stats.accumulate(current_wdf, doclen, get_termfreq(), db->get_doccount());
+    stats.accumulate(shard_index,
+		     current_wdf, doclen, get_termfreq(), db->get_doccount());
 }
 
 string

xapian-core/backends/honey/honey_termlist.cc
@@ -100,7 +100,8 @@ void
 HoneyTermList::accumulate_stats(Xapian::Internal::ExpandStats& stats) const
 {
     Assert(!at_end());
-    stats.accumulate(current_wdf,
+    stats.accumulate(shard_index,
+		     current_wdf,
 		     doclen,
 		     get_termfreq(),
 		     db->get_doccount());

xapian-core/backends/inmemory/inmemory_database.cc
@@ -225,7 +225,8 @@ InMemoryTermList::accumulate_stats(Xapian::Internal::ExpandStats & stats) const
     if (db->is_closed()) InMemoryDatabase::throw_database_closed();
     Assert(started);
     Assert(!at_end());
-    stats.accumulate(InMemoryTermList::get_wdf(), document_length,
+    stats.accumulate(shard_index,
+		     InMemoryTermList::get_wdf(), document_length,
 		     InMemoryTermList::get_termfreq(),
 		     db->get_doccount());
 }

xapian-core/backends/multi/multi_database.cc
@@ -1,7 +1,7 @@
 /** @file multi_database.cc
  * @brief Sharded database backend
  */
-/* Copyright (C) 2017,2019 Olly Betts
+/* Copyright (C) 2017,2019,2020 Olly Betts
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -96,9 +96,12 @@ TermList*
 MultiDatabase::open_term_list_direct(Xapian::docid did) const
 {
     Xapian::doccount n_shards = shards.size();
-    auto shard = shards[shard_number(did, n_shards)];
+    auto shard_index = shard_number(did, n_shards);
+    auto shard = shards[shard_index];
     Xapian::docid shard_did = shard_docid(did, n_shards);
-    return shard->open_term_list(shard_did);
+    TermList* res = shard->open_term_list(shard_did);
+    res->shard_index = shard_index;
+    return res;
 }
 
 TermList*

xapian-core/backends/remote/remote_termlist.cc
@@ -43,7 +43,8 @@ RemoteTermList::accumulate_stats(Xapian::Internal::ExpandStats& stats) const
     // Used for query expansion with remote databases.  FIXME: Rework that and
     // drop this?
     Assert(!at_end());
-    stats.accumulate(current_wdf, doclen, current_termfreq, db_size);
+    stats.accumulate(shard_index,
+		     current_wdf, doclen, current_termfreq, db_size);
 }
 
 string

xapian-core/expand/expandweight.h
@@ -60,9 +60,6 @@ class ExpandStats {
     /// The multiplier to be used in TradWeight query expansion.
     double multiplier;
 
-    /// Keeps track of the index of the sub-database we're accumulating for.
-    size_t db_index;
-
     /** Constructor.
      *
      *  @param avlen_	    Average document length
@@ -71,7 +68,8 @@ class ExpandStats {
     ExpandStats(Xapian::doclength avlen_, double expand_k_ = 0.0)
 	: avlen(avlen_), expand_k(expand_k_) { }
 
-    void accumulate(Xapian::termcount wdf, Xapian::termcount doclen,
+    void accumulate(size_t shard_index,
+		    Xapian::termcount wdf, Xapian::termcount doclen,
 		    Xapian::doccount subtf, Xapian::doccount subdbsize)
     {
 	// Boolean terms may have wdf == 0, but treat that as 1 so such terms
@@ -84,9 +82,11 @@ class ExpandStats {
 
 	// If we've not seen this sub-database before, then update dbsize and
 	// termfreq and note that we have seen it.
-	if (db_index >= dbs_seen.size() || !dbs_seen[db_index]) {
-	    if (db_index >= dbs_seen.size()) dbs_seen.resize(db_index + 1);
-	    dbs_seen[db_index] = true;
+	if (shard_index >= dbs_seen.size() || !dbs_seen[shard_index]) {
+	    if (shard_index >= dbs_seen.size()) {
+		dbs_seen.resize(shard_index + 1);
+	    }
+	    dbs_seen[shard_index] = true;
 	    dbsize += subdbsize;
 	    termfreq += subtf;
 	}
@@ -103,7 +103,6 @@ class ExpandStats {
 	rcollection_freq = 0;
 	rtermfreq = 0;
 	multiplier = 0;
-	db_index = 0;
     }
 };
 

xapian-core/tests/api_anydb.cc
@@ -313,19 +313,13 @@ DEFINE_TESTCASE(expandweights2, backend) {
     Xapian::ESet eset = enquire.get_eset(3, myrset);
     TEST_EQUAL(eset.size(), 3);
     TEST_REL(eset.get_ebound(), >=, eset.size());
-    if (!startswith(get_dbtype(), "multi")) {
-	// For a single database, the weights should be the same with or
-	// without USE_EXACT_TERMFREQ.
-	TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    } else {
-	// For multiple databases, we expect that using USE_EXACT_TERMFREQ
-	// will result in different weights in some cases.
-	TEST_NOT_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_NOT_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    }
+    // With a multi backend, the top three terms all happen to occur in both
+    // shard so their termfreq is exactly known even without
+    // USE_EXACT_TERMFREQ and so the weights should be the same for all
+    // test harness backends.
+    TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
+    TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
+    TEST_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
 }
 
 DEFINE_TESTCASE(expandweights3, backend) {
@@ -339,24 +333,16 @@ DEFINE_TESTCASE(expandweights3, backend) {
     myrset.add_document(*i);
     myrset.add_document(*(++i));
 
-    // Set min_wt to 0.0
-    Xapian::ESet eset = enquire.get_eset(50, myrset, 0, 0, 0.0);
-    TEST_EQUAL(eset.size(), 50);
+    // Set min_wt to 6.0
+    Xapian::ESet eset = enquire.get_eset(50, myrset, 0, 0, 6.0);
+    TEST_EQUAL(eset.size(), 2);
     TEST_REL(eset.get_ebound(), >=, eset.size());
-    if (!startswith(get_dbtype(), "multi")) {
-	// For a single database, the weights should be the same with or
-	// without USE_EXACT_TERMFREQ.
-	TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    } else {
-	// For multiple databases, we expect that using USE_EXACT_TERMFREQ
-	// will result in different weights in some cases.
-	TEST_NOT_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_NOT_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    }
-    TEST_REL(eset.back().get_weight(),>=,0);
+    // With a multi backend, the top two terms all happen to occur in both
+    // shard so their termfreq is exactly known even without
+    // USE_EXACT_TERMFREQ and so the weights should be the same for all
+    // test harness backends.
+    TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
+    TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
 }
 
 // tests that negative weights are returned
@@ -446,19 +432,13 @@ DEFINE_TESTCASE(expandweights8, backend) {
     // Set expand_k to 1.0 and min_wt to 0
     enquire.set_expansion_scheme("trad", 1.0);
     Xapian::ESet eset = enquire.get_eset(50, myrset, 0, 0, 0);
-    if (!startswith(get_dbtype(), "multi")) {
-	// For a single database, the weights should be the same with or
-	// without USE_EXACT_TERMFREQ.
-	TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    } else {
-	// For multiple databases, we expect that using USE_EXACT_TERMFREQ
-	// will result in different weights in some cases.
-	TEST_NOT_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
-	TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
-	TEST_NOT_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
-    }
+    // With a multi backend, the top three terms all happen to occur in both
+    // shard so their termfreq is exactly known even without
+    // USE_EXACT_TERMFREQ and so the weights should be the same for all
+    // test harness backends.
+    TEST_EQUAL_DOUBLE(eset[0].get_weight(), 6.08904001099445);
+    TEST_EQUAL_DOUBLE(eset[1].get_weight(), 6.08904001099445);
+    TEST_EQUAL_DOUBLE(eset[2].get_weight(), 4.73383620844021);
     TEST_REL(eset.back().get_weight(),>=,0);
 }
 