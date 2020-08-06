7660d8b5c26ecb758a05cc5c8c1a572a3754a0f6
Exter-dg <prkandmap23@rocketmail.com>
2020-08-06 08:56:50 +0530

    fixup! Support Abiword formats using libabw

--- xapian-applications/omega/omindexcheck.cc
+++ xapian-applications/omega/omindexcheck.cc
@@ -134,8 +134,6 @@ index_test()
 #if defined HAVE_LIBABW
     // Title term is not being tested here because some older versions of Libabw
     // lack a bug fix for the title to be handled properly. (< libabw-0.1.2)
-    // This was fixed in:
-    // https://github.com/LibreOffice/libabw/commit/254d9f55ca60e070c11f2c3f7ca196b0a6c5aceb
     tests.insert({"abw/test.abw",
 		  {"ZAparth", "Zabiword", "Zsampl", "Zdocument"}});
     tests.insert({"abw/test1.abw",
