1c64356c7d790e9f864c7a3a986cea3322c29ceb
Olly Betts <olly@survex.com>
2020-09-01 16:27:59 +1200

    Add DB_NO_TERMLIST to WritableDatabase ctor docs
    The other DB_* constants are listed here, but this one was missing.
    
    (cherry picked from commit 25e188ec5f6e6824700b7d63fe95c82a722359e1)

--- xapian-core/include/xapian/database.h
+++ xapian-core/include/xapian/database.h
@@ -825,7 +825,9 @@ class XAPIAN_VISIBILITY_DEFAULT WritableDatabase : public Database {
 	 *  using bitwise-or (| in C++):
 	 *
 	 *   - Xapian::DB_NO_SYNC don't call fsync() or similar
+	 *   - Xapian::DB_FULL_SYNC try harder to ensure data is safe
 	 *   - Xapian::DB_DANGEROUS don't be crash-safe, no concurrent readers
+	 *   - Xapian::DB_NO_TERMLIST don't use a termlist table
 	 *   - Xapian::DB_RETRY_LOCK to wait to get a write lock
 	 *
 	 *  @param block_size If a new database is created, this specifies
