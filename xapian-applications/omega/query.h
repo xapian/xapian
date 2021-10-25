88703f416e71ef8f8a54eb3f88f9fce706745a6b
Olly Betts <olly@survex.com>
2021-10-26 12:26:24 +1300

    Fix GCC -Wshadow warning
    (cherry picked from commit 23ec83fc7fcf7ea9811c6d02df033e9f77ff0cac)

--- xapian-applications/omega/query.h
+++ xapian-applications/omega/query.h
@@ -75,7 +75,7 @@ class OmegaExpandDecider : public Xapian::ExpandDecider {
     Xapian::Database db;
     set<string> exclude_stems;
   public:
-    OmegaExpandDecider(const Xapian::Database & db,
+    OmegaExpandDecider(const Xapian::Database & db_,
 		       set<string> * querytermset = NULL);
     bool operator()(const string & term) const;
 };
