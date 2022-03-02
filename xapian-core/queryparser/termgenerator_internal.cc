5398d0156518bb6d91e77663f4776049c5e2947b
Olly Betts <olly@survex.com>
2022-03-02 16:47:31 +1300

    Better C++20 fix
    (cherry picked from commit dd83b3d91a0fdd1d5db528722c6576829f58dd5b)

--- xapian-core/queryparser/termgenerator_internal.cc
+++ xapian-core/queryparser/termgenerator_internal.cc
@@ -260,7 +260,13 @@ TermGenerator::Internal::index_text(Utf8Iterator itor, termcount wdf_inc,
     }
 
     parse_terms(itor, cjk_ngram, with_positions,
-	[=, this](const string & term, bool positional, const Utf8Iterator &) {
+	[=
+#if __cplusplus >= 201907L
+// C++20 no longer supports implicit `this` in lambdas but older C++ versions
+// don't allow `this` here.
+	, this
+#endif
+	](const string & term, bool positional, const Utf8Iterator &) {
 	    if (term.size() > max_word_length) return true;
 
 	    if (current_stop_mode == TermGenerator::STOP_ALL &&
@@ -278,7 +284,9 @@ TermGenerator::Internal::index_text(Utf8Iterator itor, termcount wdf_inc,
 		}
 	    }
 
-	    if ((flags & FLAG_SPELLING) && prefix.empty())
+	    // MSVC seems to need "this->" on member variables in this
+	    // situation.
+	    if ((this->flags & FLAG_SPELLING) && prefix.empty())
 		db.add_spelling(term);
 
 	    if (strategy == TermGenerator::STEM_NONE || stemmer.is_none())
