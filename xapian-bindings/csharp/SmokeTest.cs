a4f1e0c2133ca8938dcd517c5c7634d10f6fc959
Olly Betts <olly@survex.com>
2023-03-23 15:52:36 +1300

    Fix code and output which should say GetDescription

--- xapian-bindings/csharp/SmokeTest.cs
+++ xapian-bindings/csharp/SmokeTest.cs
@@ -104,7 +104,7 @@ class SmokeTest {
 	    // Check exception handling for Xapian::DocNotFoundError.
 	    try {
 		Xapian.Document doc2 = db.GetDocument(2);
-		System.Console.WriteLine("Retrieved non-existent document: " + doc2.ToString());
+		System.Console.WriteLine("Retrieved non-existent document: " + doc2.GetDescription());
 		System.Environment.Exit(1);
 	    } catch (System.Exception e) {
 		// We expect DocNotFoundError
@@ -144,12 +144,12 @@ class SmokeTest {
 	    qp.AddRangeprocessor(rpdate, "foo");
 
             if (Xapian.Query.MatchAll.GetDescription() != "Query(<alldocuments>)") {
-		System.Console.WriteLine("Unexpected Query.MatchAll.toString()");
+		System.Console.WriteLine("Unexpected Query.MatchAll.GetDescription()");
 		System.Environment.Exit(1);
             }
 
             if (Xapian.Query.MatchNothing.GetDescription() != "Query()") {
-		System.Console.WriteLine("Unexpected Query.MatchNothing.toString()");
+		System.Console.WriteLine("Unexpected Query.MatchNothing.GetDescription()");
 		System.Environment.Exit(1);
             }
 
