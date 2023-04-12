0692b3cde8c7823cd19f37ed6c852f5f12189d14
Olly Betts <olly@survex.com>
2023-04-12 17:18:27 +1200

    Overhaul TcpClient

--- xapian-core/net/tcpclient.cc
+++ xapian-core/net/tcpclient.cc
@@ -1,9 +1,7 @@
 /** @file
- * @brief Open a TCP connection to a server.
+ *  @brief Open a TCP connection to a server.
  */
-/* Copyright 1999,2000,2001 BrightStation PLC
- * Copyright 2002 Ananova Ltd
- * Copyright 2004,2005,2006,2007,2008,2010,2012,2013,2015,2017 Olly Betts
+/* Copyright 2007-2023 Olly Betts
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -17,23 +15,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
- * USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include <config.h>
 
 #include "tcpclient.h"
 
-#include "resolver.h"
-#include "str.h"
 #include <xapian/error.h>
 
 #include "realtime.h"
-#include "safefcntl.h"
-#include "safenetdb.h"
-#include "safesyssocket.h"
+#include "resolver.h"
 #include "socket_utils.h"
+
+#include "safefcntl.h"
+#include "safesyssocket.h"
 
 #ifdef HAVE_POLL_H
 # include <poll.h>
@@ -42,7 +38,6 @@
 #endif
 
 #include <cerrno>
-#include <cstring>
 #ifndef __WIN32__
 # include <netinet/in.h>
 # include <netinet/tcp.h>
@@ -60,10 +55,14 @@ TcpClient::open_socket(const string& hostname, int port,
     for (auto&& r : Resolver(hostname, port)) {
 	int socktype = r.ai_socktype | SOCK_CLOEXEC;
 #ifdef SOCK_NONBLOCK
+	// Set the socket as non-blocking so we can implement a timeout on
+	// the connection attempt by using select() or poll().  If
+	// SOCK_NONBLOCK is available we can get socket() to do this, but
+	// if not we call fcntl()/ioctlsocket() below to set it.
 	socktype |= SOCK_NONBLOCK;
 #endif
 	int fd = socket(r.ai_family, socktype, r.ai_protocol);
-	if (fd == -1)
+	if (fd < 0)
 	    continue;
 
 #if !defined __WIN32__ && defined F_SETFD && defined FD_CLOEXEC
@@ -76,8 +75,8 @@ TcpClient::open_socket(const string& hostname, int port,
 
 #ifndef SOCK_NONBLOCK
 #ifdef __WIN32__
-	ULONG enabled = 1;
-	int rc = ioctlsocket(fd, FIONBIO, &enabled);
+	ULONG on = 1;
+	int rc = ioctlsocket(fd, FIONBIO, &on);
 #define FLAG_NAME "FIONBIO"
 #elif defined O_NONBLOCK
 	int rc = fcntl(fd, F_SETFL, O_NONBLOCK);
@@ -87,7 +86,7 @@ TcpClient::open_socket(const string& hostname, int port,
 #define FLAG_NAME "O_NDELAY"
 #endif
 	if (rc < 0) {
-	    int saved_errno = socket_errno(); // note down in case close hits an error
+	    int saved_errno = socket_errno();
 	    CLOSESOCKET(fd);
 	    throw Xapian::NetworkError("Couldn't set " FLAG_NAME,
 				       context,
@@ -102,13 +101,13 @@ TcpClient::open_socket(const string& hostname, int port,
 	    // since C++ allows implicit conversion to void* but not from
 	    // void*.
 	    if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY,
-			   reinterpret_cast<char *>(&optval),
+			   reinterpret_cast<char*>(&optval),
 			   sizeof(optval)) < 0) {
-		int saved_errno = socket_errno(); // note down in case close hits an error
+		int setsockopt_errno = socket_errno();
 		CLOSESOCKET(fd);
 		throw Xapian::NetworkError("Couldn't set TCP_NODELAY",
 					   context,
-					   saved_errno);
+					   setsockopt_errno);
 	    }
 	}
 
@@ -119,15 +118,9 @@ TcpClient::open_socket(const string& hostname, int port,
 	}
 
 	int err = socket_errno();
-	if (
-#ifdef __WIN32__
-	    WSAGetLastError() == WSAEWOULDBLOCK
-#else
-	    err == EINPROGRESS
-#endif
-	    ) {
+	if (err == EINPROGRESS) {
 	    // Wait for the socket to be writable or give an error, with a
-	    // timeout.
+	    // timeout.  FIXME: Reduce the timeout if we retry.
 #ifdef HAVE_POLL
 	    struct pollfd fds;
 	    fds.fd = fd;
@@ -135,26 +128,29 @@ TcpClient::open_socket(const string& hostname, int port,
 	    do {
 		retval = poll(&fds, 1, int(timeout_connect * 1000));
 	    } while (retval < 0 && (errno == EINTR || errno == EAGAIN));
+# define FUNC_NAME "poll()"
 #else
 	    fd_set fdset;
 	    FD_ZERO(&fdset);
 	    do {
 		FD_SET(fd, &fdset);
-		// FIXME: Reduce the timeout if we retry on EINTR.
 		struct timeval tv;
 		RealTime::to_timeval(timeout_connect, &tv);
 		retval = select(fd + 1, 0, &fdset, 0, &tv);
 	    } while (retval < 0 && (errno == EINTR || errno == EAGAIN));
+# define FUNC_NAME "select()"
 #endif
 
 	    if (retval <= 0) {
 		int saved_errno = errno;
 		CLOSESOCKET(fd);
-		if (retval < 0)
-		    throw Xapian::NetworkError("Couldn't connect (poll() or "
-					       "select() on socket failed)",
+		if (retval < 0) {
+		    throw Xapian::NetworkError("Couldn't connect ("
+					       FUNC_NAME " on socket failed)",
 					       context,
 					       saved_errno);
+#undef FUNC_NAME
+		}
 		throw Xapian::NetworkTimeoutError("Timed out waiting to "
 						  "connect",
 						  context,
@@ -167,14 +163,14 @@ TcpClient::open_socket(const string& hostname, int port,
 	    // 4th argument might need to be void* or char* - cast it to char*
 	    // since C++ allows implicit conversion to void* but not from void*.
 	    retval = getsockopt(fd, SOL_SOCKET, SO_ERROR,
-				reinterpret_cast<char *>(&err), &len);
+				reinterpret_cast<char*>(&err), &len);
 
 	    if (retval < 0) {
-		int saved_errno = socket_errno(); // note down in case close hits an error
+		int getsockopt_errno = socket_errno();
 		CLOSESOCKET(fd);
-		throw Xapian::NetworkError("Couldn't get socket options",
+		throw Xapian::NetworkError("getsockopt failed",
 					   context,
-					   saved_errno);
+					   getsockopt_errno);
 	    }
 	    if (err == 0) {
 		// Connected successfully.
@@ -193,21 +189,19 @@ TcpClient::open_socket(const string& hostname, int port,
 	CLOSESOCKET(fd);
     }
 
-    if (socketfd == -1) {
-	throw Xapian::NetworkError("Couldn't connect",
+    if (socketfd < 0) {
+	throw Xapian::NetworkError("connect failed",
 				   context,
 				   connect_errno);
     }
 
-#ifdef __WIN32__
-    ULONG enabled = 0;
-    ioctlsocket(socketfd, FIONBIO, &enabled);
-#else
+    // Set the socket to be blocking.
+#ifndef __WIN32__
     fcntl(socketfd, F_SETFL, 0);
+#else
+    ULONG off = 0;
+    ioctlsocket(socketfd, FIONBIO, &off);
 #endif
+
     return socketfd;
 }
-
-#ifdef DISABLE_GPL_LIBXAPIAN
-# error GPL source we cannot relicense included in libxapian
-#endif
