99b7f1d529b462a538879f30277b97699b87c77f
Olly Betts <olly@survex.com>
2023-04-30 11:09:30 +1200

    Enable testcase databasemodified1 for remote DBs
    Apparently "The remote backend doesn't work as expected here, I think
    due to test harness issues", but actually the testcase passes if enabled
    so those issues seem to have been resolved since 2010 when that comment
    was written.
    
    (cherry picked from commit c351652a1e0aca0a0339f9011e76b4aea49c14e6)

--- xapian-core/tests/api_backend.cc
+++ xapian-core/tests/api_backend.cc
@@ -509,11 +509,8 @@ DEFINE_TESTCASE(replacedoc8, writable) {
 }
 
 /// Test coverage for DatabaseModifiedError.
-DEFINE_TESTCASE(databasemodified1, writable && !inmemory && !remote && !multi) {
+DEFINE_TESTCASE(databasemodified1, writable && !inmemory && !multi) {
     // The inmemory backend doesn't support revisions.
-    //
-    // The remote backend doesn't work as expected here, I think due to
-    // test harness issues.
     //
     // With multi, DatabaseModifiedError doesn't trigger as easily.
     Xapian::WritableDatabase db(get_writable_database());
