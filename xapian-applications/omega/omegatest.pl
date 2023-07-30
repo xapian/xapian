841d8ab576d02722925e0c6a8815d27aa0d6f460
Olly Betts <olly@survex.com>
2023-07-31 11:20:18 +1200

    omegatest.pl: Tighten up stripping CR from output
    Replace CR+LF by CR so that literal CR by itself isn't quietly ignored.

--- xapian-applications/omega/omegatest.pl
+++ xapian-applications/omega/omegatest.pl
@@ -120,8 +120,8 @@ sub testcase {
     $output = <$out>;
     close $out;
   }
+  $output =~ s/\r\n/\n/g;
   chomp($output);
-  $output =~ s/\r//g;
 
   if ($output ne $expected) {
     print "$omega @_:\n";
@@ -156,8 +156,8 @@ $input
 __END__`;
   }
   my $rc = $? >> 8;
+  $out =~ s/\r\n/\n/g;
   chomp($out);
-  $out =~ s/\r//g;
   if ($rc == 0) {
     print "scriptindex didn't exit with non-zero return code for $what\n";
     print "Output: $out\n";
@@ -192,8 +192,8 @@ $input
 __END__`;
   }
   my $rc = $? >> 8;
+  $out =~ s/\r\n/\n/g;
   chomp($out);
-  $out =~ s/\r//g;
   if ($rc == 0) {
     if ($out !~ $summary_re) {
       print "scriptindex gave unexpected output for $what\n";
@@ -231,8 +231,8 @@ $input
 __END__`;
   }
   my $rc = $? >> 8;
+  $out =~ s/\r\n/\n/g;
   chomp($out);
-  $out =~ s/\r//g;
   if ($out !~ s/$summary_re//) {
     print "scriptindex output lacked summary line for $what\n";
     print "Output: $out\n";
@@ -271,8 +271,8 @@ $input
 __END__`;
   }
   my $rc = $? >> 8;
+  $out =~ s/\r\n/\n/g;
   chomp($out);
-  $out =~ s/\r//g;
   if ($rc != 0) {
     print "scriptindex gave an error for $what\n";
     print "Output: $out\n";
