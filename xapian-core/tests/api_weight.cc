6d979f45c22a3e6d4af63a2b6572717a30d56ce9
Olly Betts <olly@survex.com>
2024-06-24 10:56:35 +1200

    Remove now-redundant part of tfidfweight1
    (cherry picked from commit 8cb7af1444779a28d5edd1ed99355161ed87da7e)

--- xapian-core/tests/api_weight.cc
+++ xapian-core/tests/api_weight.cc
@@ -601,11 +601,6 @@ DEFINE_TESTCASE(tfidfweight1, !backend) {
 
     TEST_EXCEPTION(Xapian::InvalidArgumentError,
 	Xapian::TfIdfWeight b("LOL"));
-
-    /* Normalization string should be set to "ntn" by constructor if none is
-      given. */
-    Xapian::TfIdfWeight weight2;
-    TEST_EQUAL(weight2.serialise(), Xapian::TfIdfWeight("ntn").serialise());
 }
 
 // Feature tests for various normalization functions.
