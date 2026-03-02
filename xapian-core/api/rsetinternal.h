b418bbabfaa3169feba75bc0ee9a30af555e9413
Olly Betts <olly@survex.com>
2026-03-02 14:40:40 +1300

    Eliminate old FSF address
    Update GPL/LGPL licence files and boilerplate to direct people who
    didn't receive the licence text to the FSF website rather than
    giving a postal address (which is what the FSF licence texts now
    do).

--- xapian-core/api/rsetinternal.h
+++ xapian-core/api/rsetinternal.h
@@ -12,8 +12,8 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
  */
 
 #ifndef XAPIAN_INCLUDED_RSETINTERNAL_H
