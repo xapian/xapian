dee33d8a64e8df36c2ec67c76a9c1bdd118b4975
Olly Betts <olly@survex.com>
2026-03-20 13:48:42 +1300

    mkdtemp.cc: Revert to tab indentation
    This files comes from NetBSD so keep the original indentation style.

--- xapian-applications/omega/mkdtemp.cc
+++ xapian-applications/omega/mkdtemp.cc
@@ -46,96 +46,96 @@
 static int
 do_mkdtemp(char *path)
 {
-        char *start, *trv;
-        struct stat sbuf;
-        pid_t pid;
+	char *start, *trv;
+	struct stat sbuf;
+	pid_t pid;
 
-        /* To guarantee multiple calls generate unique names even if
-           the file is not created. 676 different possibilities with 7
-           or more X's, 26 with 6 or less. */
-        static char xtra[2] = { 'a', 'a' };
-        int xcnt = 0;
+	/* To guarantee multiple calls generate unique names even if
+	   the file is not created. 676 different possibilities with 7
+	   or more X's, 26 with 6 or less. */
+	static char xtra[2] = { 'a', 'a' };
+	int xcnt = 0;
 
-        pid = getpid();
+	pid = getpid();
 
-        /* Move to end of path and count trailing X's. */
-        for (trv = path; *trv; ++trv)
-                if (*trv == 'X')
-                        xcnt++;
-                else
-                        xcnt = 0;
+	/* Move to end of path and count trailing X's. */
+	for (trv = path; *trv; ++trv)
+		if (*trv == 'X')
+			xcnt++;
+		else
+			xcnt = 0;
 
-        /* Use at least one from xtra.  Use 2 if more than 6 X's. */
-        if (*(trv-1) == 'X')
-                *--trv = xtra[0];
-        if (xcnt > 6 && *(trv-1) == 'X')
-                *--trv = xtra[1];
+	/* Use at least one from xtra.  Use 2 if more than 6 X's. */
+	if (*(trv-1) == 'X')
+		*--trv = xtra[0];
+	if (xcnt > 6 && *(trv-1) == 'X')
+		*--trv = xtra[1];
 
-        /* Set remaining X's to pid digits with 0's to the left. */
-        while (*--trv == 'X') {
-                *trv = (pid % 10) + '0';
-                pid /= 10;
-        }
+	/* Set remaining X's to pid digits with 0's to the left. */
+	while (*--trv == 'X') {
+		*trv = (pid % 10) + '0';
+		pid /= 10;
+	}
 
-        /* update xtra for next call. */
-        if (xtra[0] != 'z')
-                xtra[0]++;
-        else {
-                xtra[0] = 'a';
-                if (xtra[1] != 'z')
-                        xtra[1]++;
-                else
-                        xtra[1] = 'a';
-        }
+	/* update xtra for next call. */
+	if (xtra[0] != 'z')
+		xtra[0]++;
+	else {
+		xtra[0] = 'a';
+		if (xtra[1] != 'z')
+			xtra[1]++;
+		else
+			xtra[1] = 'a';
+	}
 
-        /*
-         * check the target directory; if you have six X's and it
-         * doesn't exist this runs for a *very* long time.
-         */
-        for (start = trv + 1;; --trv) {
-                if (trv <= path)
-                        break;
-                if (*trv == '/') {
-                        *trv = '\0';
-                        if (stat(path, &sbuf))
-                                return (0);
-                        if (!S_ISDIR(sbuf.st_mode)) {
-                                errno = ENOTDIR;
-                                return (0);
-                        }
-                        *trv = '/';
-                        break;
-                }
-        }
+	/*
+	 * check the target directory; if you have six X's and it
+	 * doesn't exist this runs for a *very* long time.
+	 */
+	for (start = trv + 1;; --trv) {
+		if (trv <= path)
+			break;
+		if (*trv == '/') {
+			*trv = '\0';
+			if (stat(path, &sbuf))
+				return (0);
+			if (!S_ISDIR(sbuf.st_mode)) {
+				errno = ENOTDIR;
+				return (0);
+			}
+			*trv = '/';
+			break;
+		}
+	}
 
-        for (;;) {
-                if (mkdir(path, 0700) >= 0)
-                        return (1);
-                if (errno != EEXIST)
-                        return (0);
+	for (;;) {
+		if (mkdir(path, 0700) >= 0)
+			return (1);
+		if (errno != EEXIST)
+			return (0);
 
-                /* tricky little algorithm for backward compatibility */
-                for (trv = start;;) {
-                        if (!*trv)
-                                return (0);
-                        if (*trv == 'z')
-                                *trv++ = 'a';
-                        else {
-                                if (*trv >= '0' && *trv <= '9')
-                                        *trv = 'a';
-                                else
-                                        ++*trv;
-                                break;
-                        }
-                }
-        }
-        /*NOTREACHED*/
+		/* tricky little algorithm for backward compatibility */
+		for (trv = start;;) {
+			if (!*trv)
+				return (0);
+			if (*trv == 'z')
+				*trv++ = 'a';
+			else {
+				if (*trv >= '0' && *trv <= '9')
+					*trv = 'a';
+				else
+					++*trv;
+				break;
+			}
+		}
+	}
+	/*NOTREACHED*/
 }
 
 char *
 fallback_mkdtemp(char *path)
 {
-        return (do_mkdtemp(path) ? path : (char *)0);
+	return (do_mkdtemp(path) ? path : (char *)0);
 }
 
 #endif
