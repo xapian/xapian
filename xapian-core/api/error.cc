a08a54a8e91ad50fbc08c431f48b84a3c321f177
Jean-Francois Dockes <jf@dockes.org>
2026-08-20 09:07:19 +0200

    Windows: use explicit A-suffixed Win32 APIs (CreateProcessA(), CreateFileA()...)
    Rationale: this is mostly a cosmetic change. Using the explicit name makes the
    code a bit clearer, and prevents a compilation error if UNICODE is defined in
    the context.

--- xapian-core/api/error.cc
+++ xapian-core/api/error.cc
@@ -56,7 +56,7 @@ Xapian::Error::get_error_string() const
             int e = abs(my_errno);
             DWORD len;
             char * error;
-            len = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM|
+            len = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|
                                 FORMAT_MESSAGE_ALLOCATE_BUFFER,
                                 0, e, 0, (CHAR*)&error, 0, 0);
             if (error) {
